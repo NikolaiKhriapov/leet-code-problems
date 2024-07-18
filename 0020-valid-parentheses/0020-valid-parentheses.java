f6544922ae519def96ba7aebae32d0ebf38959b2
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-19 01:57:27 +0800

    Time: 2 ms (53.84%), Space: 41.4 MB (40.65%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        String opening = "({[";
+        String closing = ")}]";
+        Stack<Character> stack = new Stack();
+
+        for (char c : s.toCharArray()) {
+            if (opening.indexOf(c) != -1) stack.push(c);
+            else if (stack.empty()) return false;
+            else {
+                int index = opening.indexOf(stack.pop());
+                if (closing.charAt(index) != c) return false;
+            }
+        }
+
+        return stack.empty();
+    }
+}
