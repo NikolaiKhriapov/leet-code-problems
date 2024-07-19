6ba18bbc83da2d1720903d76689e4cc9215f375d
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-20 04:30:26 +0800

    Time: 4 ms (54.83%), Space: 44.7 MB (31.87%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        char[] sChars = s.toCharArray();
+        List<Character> list = new ArrayList<>();
+
+        for (char sChar : sChars) {
+            if (Character.isLetterOrDigit(sChar)) {
+                list.add(Character.toLowerCase(sChar));
+            }
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) != list.get(list.size() - 1 - i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
