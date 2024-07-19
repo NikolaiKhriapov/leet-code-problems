9681a8bb5f161a1475a02477739f7a6f4aac4c4e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-20 04:36:54 +0800

    Time: 10 ms (68.06%), Space: 42.3 MB (61.73%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+                
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Character> map = new HashMap<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char sc = s.charAt(i);
+            char tc = t.charAt(i);
+
+            if (!map.containsKey(sc)) {
+                if (map.containsValue(tc)) return false;
+                map.put(sc, tc);
+            } else {
+                if (tc != map.get(sc)) return false;
+            }
+        }
+
+        return true;
+    }
+}
