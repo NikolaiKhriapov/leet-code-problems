2383f9d5bcd521ed3a1f5d920b0c08eb95ada9ab
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-20 20:24:04 +0800

    Time: 7 ms (30.56%), Space: 40.7 MB (58.58%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int arrangeCoins(int n) {
+
+        int rows = 0;
+
+        while (n >= rows + 1) {
+            n -= (rows + 1);
+            rows++;
+        }
+        
+        return rows;
+    }
+}
