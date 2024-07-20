1f5b4a0b2803e5438ce3ecc17641b8b3c658601a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-21 01:14:22 +0800

    Time: 1 ms (99.03%), Space: 43.7 MB (40.16%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
-
-        Stack<Integer[]> stack = new Stack<>();
-
-        for (int i = 0; i < prices.length; i++) {
-            int p = prices[i];
-            while (!stack.isEmpty() && stack.peek()[1] >= p) {
-                Integer[] v = stack.pop();
-                prices[v[0]] = v[1] - p;
-            }
-            stack.push(new Integer[] { i, p });
-        }
         
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[j] <= prices[i]) {
+                    prices[i] = prices[i] - prices[j];
+                    break;
+                }
+            }
+        }
+
         return prices;
     }
 }
