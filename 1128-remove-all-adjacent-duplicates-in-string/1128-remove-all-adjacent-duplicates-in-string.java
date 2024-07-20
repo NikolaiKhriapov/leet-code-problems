c31d5dd33997dce3756ac7b19e5aca45c3a8e572
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-21 00:42:36 +0800

    Time: 4 ms (99.31%), Space: 45.6 MB (32.76%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
@@ -1,16 +1,16 @@
 class Solution {
     public String removeDuplicates(String s) {
-        char[] sChars = s.toCharArray();
 
-        int left = -1;
-        
-        for(char ch : sChars) {
-            if (left >= 0 && ch == sChars[left]) {
-                left--;
-            } else {
-                sChars[++left] = ch;
+        char[] arr = s.toCharArray();
+
+        int p = 0;
+        for (char c : s.toCharArray()) {
+            if (p != 0 && arr[p - 1] == c) p--;
+            else {
+                arr[p++] = c;
             }
         }
-        return new String(sChars, 0, left +1);
+        
+        return new String(arr, 0, p);
     }
 }
