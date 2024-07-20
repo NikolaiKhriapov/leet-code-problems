2b75f34f4224df20d8c448534108be710aa93c4d
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-20 20:13:42 +0800

    Time: 0 ms (100%), Space: 40.6 MB (14.63%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        return search(num, 1, num) != -1;
+    }
+
+    private int search(int num, int l, int r) {
+        if (r < l) return -1;
+
+        int m = l + ((r - l) / 2);
+        long ms = (long) m * m;
+        
+        if (ms == num) return m;
+        if (ms > num) return search(num, l, m - 1);
+        else return search(num, m + 1, r);
+    }
+}
