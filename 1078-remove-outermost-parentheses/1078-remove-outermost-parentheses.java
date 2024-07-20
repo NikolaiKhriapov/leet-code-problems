b9a401f7dcd85e8782fd682e211ea80bd23758de
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-21 00:18:43 +0800

    Time: 12 ms (18.85%), Space: 45.4 MB (8.27%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
@@ -1,19 +1,19 @@
 class Solution {
     public String removeOuterParentheses(String s) {
 
-        StringBuilder sb = new StringBuilder();
+        String sb = new String();
         int i = 0;
 
         for (char c : s.toCharArray()) {
             if (c == '(') {
-                if (i != 0) sb.append(c);
+                if (i != 0) sb += c;
                 i++;
             } else {
                 i--;
-                if (i != 0) sb.append(c);
+                if (i != 0) sb += c;
             }
         }
 
-        return sb.toString();
+        return sb;
     }
 }
