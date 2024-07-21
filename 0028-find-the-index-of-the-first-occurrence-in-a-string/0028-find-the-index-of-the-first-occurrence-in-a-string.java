2b254b80e9f5862ced890458ab42f2f5cc475308
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-21 22:36:41 +0800

    Time: 1 ms (30.22%), Space: 41.4 MB (36.7%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,6 +1,20 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        
-        return haystack.indexOf(needle);
+
+        int h = 0;
+        int n = 0;
+
+        while (h < haystack.length()) {
+            if (haystack.charAt(h) == needle.charAt(n)) {
+                h++;
+                n++;
+                if (n == needle.length()) return h - n;
+            } else {
+                h = h - n + 1;
+                n = 0;
+            }
+        }
+
+        return -1;
     }
 }
