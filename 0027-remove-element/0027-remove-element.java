e1893fb6f7ba0a4f0ba4cb53e1e3f6b328a76ad8
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-21 22:11:34 +0800

    Time: 0 ms (100%), Space: 41.7 MB (56.5%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,18 +1,19 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        
-        int lastIndex = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[lastIndex] = nums[i];
-                lastIndex++;
+
+        int p = 0;
+        int idx = 0;
+
+        while (idx < nums.length) {
+            if (nums[idx] == val) {
+                idx++;
+            } else {
+                nums[p] = nums[idx];
+                p++;
+                idx++;
             }
         }
-
-        for (int i = lastIndex; i < nums.length; i++) {
-            nums[i] = 0;
-        }
-
-        return lastIndex;
+        
+        return p;
     }
 }
