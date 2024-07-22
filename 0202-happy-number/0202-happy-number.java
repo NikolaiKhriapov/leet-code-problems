1fe9d280d4948bc8990bb0a8084e345f2a757230
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-22 12:59:13 +0800

    Time: 0 ms (100%), Space: 40.4 MB (78.88%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,40 +1,26 @@
 class Solution {
     public boolean isHappy(int n) {
 
-        int slow = n;
-        int fast = n;
+        int p1 = n;
+        int p2 = n;
 
         do {
-            slow = calcSquared(slow);
-            fast = calcSquared(calcSquared(fast));
-        } while (slow != fast);
+            p1 = calc(p1);
+            p2 = calc(calc(p2));
+        } while (p1 != p2);
 
-        return slow == 1;
+        return p1 == 1;
     }
 
-    public int calcSquared(int n) {
-        int result = 0;
-        while (n > 0) {
-            int tmp = n % 10;
-            result += tmp * tmp;
+    private int calc(int n) {
+        int sum = 0;
+        while (n > 9) {
+            int a = n % 10;
+            sum += a * a;
             n /= 10;
         }
-        return result;
+        sum += n * n;
+
+        return sum;
     }
 }
-
-        // Set<Integer> set = new HashSet<>();
-        
-        // while (n != 1) {
-        //     int answer = 0;
-        //     while (n > 0) {
-        //         int remainder = n % 10;
-        //         answer += remainder * remainder;
-        //         n /= 10;
-        //     }
-        //     boolean isAdded = set.add(answer);
-        //     if (!isAdded) return false;
-        //     n = answer;
-        // }
-
-        // return true;
