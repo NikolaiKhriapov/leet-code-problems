1d59315ce45d61d0acc6bdf7275468d06d5b5a9a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-22 12:16:07 +0800

    Time: 0 ms (100%), Space: 44.6 MB (22.63%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -18,7 +18,7 @@ public class Solution {
         while (p2 != null && p2.next != null) {
             p1 = p1.next;
             p2 = p2.next.next;
-            if (Objects.equals(p1, p2)) return true;
+            if (p1 == p2) return true;
         }
         
         return false;
