6488a9e948965b8fbb5940a065f4e6d422379f9e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-24 23:55:06 +0800

    Time: 5 ms (33.71%), Space: 44.6 MB (18.61%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            set.add(n);
+        }
+
+        
+        Integer[] array = set.toArray(new Integer[0]);
+
+        Arrays.sort(array);
+
+        if (array.length > 2) {
+            return array[array.length - 3];
+        } else {
+            return array[array.length - 1];
+        }
+    }
+}
