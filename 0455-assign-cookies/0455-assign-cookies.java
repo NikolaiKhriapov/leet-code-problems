4bdfade2d4307af49b9ed7d9cb36decc321c6cb6
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-24 23:56:38 +0800

    Time: 8 ms (98.32%), Space: 45.7 MB (6.55%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int pg = 0;
+        int ps = 0;
+
+        while (pg < g.length && ps < s.length) {
+            if (g[pg] <= s[ps]) pg++;
+            ps++;
+        }
+
+        return pg;
+    }
+}
