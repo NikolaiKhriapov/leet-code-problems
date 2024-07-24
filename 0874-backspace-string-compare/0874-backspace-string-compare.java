61dc9381e8f295c3eebbd0397cba67cb8ce301c6
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-25 00:05:45 +0800

    Time: 3 ms (23.87%), Space: 41.5 MB (47.91%) - LeetHub

---
+++ 0874-backspace-string-compare/0874-backspace-string-compare.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        
+        Stack<Character> stack1 = new Stack<>();
+        Stack<Character> stack2 = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (Objects.equals(c, '#')) {
+                if (!stack1.empty()) stack1.pop();
+            } else stack1.push(c);
+        }
+
+        for (char c : t.toCharArray()) {
+            if (Objects.equals(c, '#')) {
+                if (!stack2.empty()) stack2.pop();
+            } else stack2.push(c);
+        }
+
+        return Objects.equals(stack1, stack2);
+    }
+}
