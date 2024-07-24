8c787d7da1115fec727a56f27d5ec4bac65e5b72
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-25 00:05:25 +0800

    Time: 0 ms (100%), Space: 41.4 MB (46.87%) - LeetHub

---
+++ 0748-largest-number-at-least-twice-of-others/0748-largest-number-at-least-twice-of-others.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int dominantIndex(int[] nums) {
+
+        int largest = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[largest]) largest = i;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i != largest && nums[i] * 2 > nums[largest]) return -1;
+        }
+        
+        return largest;
+    }
+}
