ef738c9b3ee74b7a2fd7de402bd854b2ea2d4640
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-25 12:48:31 +0800

    Time: 5 ms (40.05%), Space: 44.9 MB (47.8%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -8,16 +8,16 @@ class Solution {
         int p2 = s.length() - 1;
 
         while (p1 < p2) {
-            while (!set.contains(arr[p1]) && p1 < arr.length - 1) p1++;
-            while (!set.contains(arr[p2]) && p2 > 0) p2--;
+            while (!set.contains(arr[p1]) && p1 < p2) p1++;
+            while (!set.contains(arr[p2]) && p1 < p2) p2--;
 
-            if (p1 > p2) break;
-
-            char temp = arr[p1];
-            arr[p1] = arr[p2];
-            arr[p2] = temp;
-            p1++;
-            p2--;
+            if (p1 < p2) {     
+                char temp = arr[p1];
+                arr[p1] = arr[p2];
+                arr[p2] = temp;
+                p1++;
+                p2--;
+            }
         }
         
         return new String(arr);
