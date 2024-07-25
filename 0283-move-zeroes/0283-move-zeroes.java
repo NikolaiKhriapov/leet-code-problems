206ee9fbd420b7e418cd35844613d0d58f58406e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-25 11:45:09 +0800

    Time: 3 ms (25.88%), Space: 46.4 MB (9.56%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -5,10 +5,10 @@ class Solution {
         int p2 = 0;
 
         while (p2 < nums.length) {
-            while ((nums[p2] == 0 || p2 <= p1) && p2 < nums.length - 1) p2++;
+            while ((p2 <= p1 || nums[p2] == 0) && p2 < nums.length - 1) p2++;
             while (nums[p1] != 0 && p1 < nums.length - 1) p1++;
 
-            if (p2 > p1 && nums[p1] == 0 && nums[p2] != 0) {
+            if (p2 > p1 && nums[p1] == 0) {
                 nums[p1] = nums[p2];
                 nums[p2] = 0;
             }
