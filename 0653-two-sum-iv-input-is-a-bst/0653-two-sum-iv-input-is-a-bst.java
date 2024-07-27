04b10cf689a47e0105f1cc739c3335b81c290e0b
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-27 16:26:18 +0800

    Time: 6 ms (14.4%), Space: 45.6 MB (7.92%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -20,8 +20,8 @@ class Solution {
 
         handleNode(root, list);
 
-        for (int i = 0; i < list.size() - 1; i++) {
-            for (int j = i + 1; j < list.size(); j++) {
+        for (int i = 0, size = list.size(); i < size - 1; i++) {
+            for (int j = i + 1; j < size; j++) {
                 if (i != j && list.get(i) + list.get(j) == k) {
                     return true;
                 }
