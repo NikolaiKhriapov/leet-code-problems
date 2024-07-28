2af0f1c73e1a2329613bb0edff26b31c43b245d1
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-28 14:40:07 +0800

    Time: 2 ms (61.09%), Space: 42.4 MB (17.71%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.java
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -1,23 +1,24 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
+        int length = code.length;
 
-        int[] arr = new int[code.length];
+        int[] arr = new int[length];
 
         if (k > 0) {
-            for (int i = 0; i < code.length; i++) {
+            for (int i = 0; i < length; i++) {
                 for (int j = 1; j <= k; j++) {
                     int tmp = i + j;
-                    if (tmp >= code.length) tmp -= code.length;
+                    if (tmp >= length) tmp -= length;
                     arr[i] += code[tmp];
                 }
             }
         }
 
         if (k < 0) {
-            for (int i = 0; i < code.length; i++) {
+            for (int i = 0; i < length; i++) {
                 for (int j = 1, kk = k * -1; j <= kk; j++) {
                     int tmp = i - j;
-                    if (tmp < 0) tmp += code.length;
+                    if (tmp < 0) tmp += length;
                     arr[i] += code[tmp];
                 }
             }
