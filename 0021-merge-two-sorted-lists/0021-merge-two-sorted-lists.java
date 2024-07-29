1099551a857e8e39822d2109c3b424caac9641c2
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-07-29 14:28:54 +0800

    Time: 0 ms (100%), Space: 42.8 MB (7.18%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,28 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode head = list1;
+        if (list1 == null && list2 == null) return null;
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        ListNode head = list1.val <= list2.val ? list1 : list2;
+        
         ListNode prev = null;
-
-        while (list2 != null) {
-            ListNode curr = list2;
-            
-            while (list1 != null && curr.val > list1.val) {
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
                 prev = list1;
-                if (list1.next != null) list1 = list1.next;
-                else {
-                    list1.next = list2;
-                    return head;
+                list1 = list1.next;
+            } else {
+                ListNode temp = list2.next;
+
+                list2.next = list1;
+                if (prev != null) {
+                    prev.next = list2;
+                } else {
+                    head = list2;
                 }
+
+                prev = list2;
+                list2 = temp;
             }
-
-            if (prev == null) head = list2;
-            if (list2.next != null) list2 = curr.next;
-            else list2 = null;
-
-            curr.next = list1;
-            if (prev != null) prev.next = curr;
-            prev = curr;
-            list1 = curr.next;
+        }
+        if (list2 != null) {
+            prev.next = list2;
         }
 
         return head;
