942e4ee01b93671d0fa46e18abc6544da9d330b2
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-08-07 19:36:18 +0800

    Time: 1 ms (58.31%), Space: 41.7 MB (43.28%) - LeetHub

--- 1764-maximum-repeating-substring/1764-maximum-repeating-substring.java
+++ 1764-maximum-repeating-substring/1764-maximum-repeating-substring.java
@@ -2,12 +2,12 @@ class Solution {
     public int maxRepeating(String sequence, String word) {
 
         int count = 0;
-        String repeatedWord = word;
+        StringBuilder repeatedWord = new StringBuilder(word);
 
         for (int i = 0; i < sequence.length(); i++) {
             if (sequence.contains(repeatedWord)) {
                 count++;
-                repeatedWord += word;
+                repeatedWord.append(word);
             } else break;
         }
 
