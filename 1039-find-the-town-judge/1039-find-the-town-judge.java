e3aa87c44496b579bc10e114e0724bc7d503734e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-08-07 20:18:49 +0800

    Time: 2 ms (97.44%), Space: 53.5 MB (64.56%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.java
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -4,11 +4,8 @@ class Solution {
         int[] count = new int[n + 1];
 
         for (int i = 0; i < trust.length; i++) {
-            int personA = trust[i][0];
-            int personB = trust[i][1];
-
-            count[personA]--;
-            count[personB]++;
+            count[trust[i][0]]--;
+            count[trust[i][1]]++;
         }
 
         for (int i = 1; i <= n; i++) {
