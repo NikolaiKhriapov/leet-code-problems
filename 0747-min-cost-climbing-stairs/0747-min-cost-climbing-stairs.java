9b7e8e15518523435b836995e578178d6ccce70a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-08-07 13:39:28 +0800

    Time: 0 ms (100%), Space: 42.7 MB (85.24%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -1,19 +1,19 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        Map<Integer, Integer> memo = new HashMap<>();
+        int[] memo = new int[cost.length + 1];
         return Math.min(calcCost(cost, 0, memo), calcCost(cost, 1, memo));
     }
 
-    private int calcCost(int[] cost, int n, Map<Integer, Integer> memo) {
+    private int calcCost(int[] cost, int n, int[] memo) {
         if (n >= cost.length) return 0;
 
-        if (memo.containsKey(n)) return memo.get(n);
+        if (memo[n] != 0) return memo[n];
 
-        int a = calcCost(cost, n + 1, memo);
-        int b = calcCost(cost, n + 2, memo);
+        int constOneStep = calcCost(cost, n + 1, memo);
+        int constTwoSteps = calcCost(cost, n + 2, memo);
 
-        int subTotal = cost[n] + Math.min(a, b);
-        memo.put(n, subTotal);
+        int subTotal = cost[n] + Math.min(constOneStep, constTwoSteps);
+        memo[n] = subTotal;
 
         return subTotal;
     }
