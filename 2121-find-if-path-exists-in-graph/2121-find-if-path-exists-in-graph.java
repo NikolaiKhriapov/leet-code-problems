a3581ffc8720ceb1b53d95a9f13552878418760b
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-08-09 12:51:22 +0800

    Time: 72 ms (75.93%), Space: 145.1 MB (70.52%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
@@ -2,14 +2,16 @@ class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         if (source == destination) return true;
 
-        Map<Integer, List<Integer>> connections = new HashMap<>();
+        List<Integer>[] connections = new ArrayList[n];
         for (int[] edge : edges) {
-            List<Integer> curr1 = connections.getOrDefault(edge[0], new ArrayList<>());
+            List<Integer> curr1 = connections[edge[0]];
+            if (curr1 == null) curr1 = new ArrayList<>();
             curr1.add(edge[1]);
-            connections.put(edge[0], curr1);
-            List<Integer> curr2 = connections.getOrDefault(edge[1], new ArrayList<>());
+            connections[edge[0]] = curr1;
+            List<Integer> curr2 = connections[edge[1]];
+            if (curr2 == null) curr2 = new ArrayList<>();
             curr2.add(edge[0]);
-            connections.put(edge[1], curr2);
+            connections[edge[1]] = curr2;
         }
         
         Queue<Integer> queue = new LinkedList<>();
@@ -23,7 +25,7 @@ class Solution {
 
             if (curr == destination) return true;
 
-            List<Integer> neighbors = connections.get(curr);
+            List<Integer> neighbors = connections[curr];
             for (int neighbor : neighbors) {
                 if (!visited.contains(neighbor)) {
                     queue.add(neighbor);
