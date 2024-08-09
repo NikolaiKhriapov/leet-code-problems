2191c7f7fefccb37d3de5aa26cf1b58c9eaa0e8d
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-08-09 13:11:35 +0800

    Time: 10 ms (11.52%), Space: 41.8 MB (49.96%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -10,18 +10,20 @@ class Solution {
         }
         
         int result = 0;
-        boolean oneAdded = false;
         for (var entry : map.entrySet()) {
             while (entry.getValue() > 1) {
                 result += 2;
                 entry.setValue(entry.getValue() - 2);
             }
-            if (!oneAdded && entry.getValue() == 1) {
-                result++;
-                oneAdded = true;
-            }
         }
 
+        for (var entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                result++;
+                return result;
+            }
+        }
+   
         return result;
     }
 }
