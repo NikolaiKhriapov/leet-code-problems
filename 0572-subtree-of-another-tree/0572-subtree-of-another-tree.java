092fa7dced63dd6ae212ca01de24f33b025e36f3
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-08-10 15:28:53 +0800

    Time: 2 ms (96.89%), Space: 44.3 MB (36.52%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -14,15 +14,19 @@
  * }
  */
 class Solution {
-    public boolean isSubtree(TreeNode root, TreeNode subRoot) {        
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+
         if (root == null) return false;
         if (isSameTree(root, subRoot)) return true;
+
         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
 
-    public boolean isSameTree(TreeNode root, TreeNode subRoot) {
+    private boolean isSameTree(TreeNode root, TreeNode subRoot) {
         if (root == null && subRoot == null) return true;
         if (root == null || subRoot == null) return false;
-        return root.val == subRoot.val && isSameTree(root.left, subRoot.left) && isSameTree(root.right, subRoot.right);
+        if (root.val != subRoot.val) return false;
+        
+        return isSameTree(root.left, subRoot.left) && isSameTree(root.right, subRoot.right);
     }
 }
