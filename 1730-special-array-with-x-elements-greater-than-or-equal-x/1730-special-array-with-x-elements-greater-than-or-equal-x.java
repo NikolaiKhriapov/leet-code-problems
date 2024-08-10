45794f493598883fb3e6bdec702c2ac9d21b33f7
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-08-10 16:45:28 +0800

    Time: 3 ms (14.16%), Space: 41.1 MB (63.49%) - LeetHub

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -3,19 +3,13 @@ class Solution {
         Arrays.sort(nums);
 
         for (int i = 0; i <= nums.length; i++) {
-            int v = nums.length - binarySearch(nums, i, 0, nums.length - 1);
-            if (v == i) return i;
+            int count = 0;
+            for (int num : nums) {
+                if (num >= i) count++;
+            }
+            if (count == i) return i;
         }
 
         return -1;
-    }
-
-    private int binarySearch(int[] nums, int target, int l, int r) {
-        if (l > r) return l;
-
-        int m = l + (r - l) / 2;
-
-        if (nums[m] >= target) return binarySearch(nums, target, l, m - 1);
-        else return binarySearch(nums, target, m + 1, r);
     }
 }
