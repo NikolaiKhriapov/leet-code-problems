ac3daf4b7b0fd9dcfa3c8addaa8c23d83ac2391a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-08-11 21:41:26 +0800

    Time: 0 ms (100%), Space: 40.5 MB (58.53%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,26 +1,22 @@
 class Solution {
     public boolean isHappy(int n) {
-
-        int p1 = n;
-        int p2 = n;
+        int slow = n;
+        int fast = n;
 
         do {
-            p1 = calc(p1);
-            p2 = calc(calc(p2));
-        } while (p1 != p2);
+            slow = calcSquared(slow);
+            fast = calcSquared(calcSquared(fast));
+        } while (slow != fast);
 
-        return p1 == 1;
+        return slow == 1;
     }
 
-    private int calc(int n) {
-        int sum = 0;
-        while (n > 9) {
-            int a = n % 10;
-            sum += a * a;
+    private int calcSquared(int n) {
+        int a = 0;
+        while (n > 0) {
+            a += Math.pow(n % 10, 2);
             n /= 10;
         }
-        sum += n * n;
-
-        return sum;
+        return a;
     }
 }
