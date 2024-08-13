4bce54e2ba0c8404339f951ce9ad515d364bc61f
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-08-13 13:04:53 +0800

    Time: 8 ms (10.43%), Space: 45.3 MB (43.6%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,19 +1,27 @@
 class Solution {
     public boolean validPalindrome(String s) {
-
-        return isPalindrome(s, 0, s.length() - 1, false);
+        return validPalindrome(s, false);
     }
 
-    private boolean isPalindrome(String s, int l, int r, boolean deleted) {
+    private boolean validPalindrome(String s, boolean isDeleted) {
+        char[] arr = s.toCharArray();
+
+        int l = 0;
+        int r = arr.length - 1;
+
         while (l < r) {
-            if (s.charAt(l) != s.charAt(r)) {
-                if (deleted) return false;
-                return isPalindrome(s, l + 1, r, true) || isPalindrome(s, l, r - 1, true);
+            if (arr[l] != arr[r]) {
+                if (isDeleted) {
+                    return false;
+                } else {
+                    return validPalindrome(s.substring(l, r), true) || validPalindrome(s.substring(l + 1, r + 1), true);
+                }
             }
             l++;
             r--;
         }
-
+        
         return true;
+
     }
 }
