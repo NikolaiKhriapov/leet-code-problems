521a4a2a7ae65c98996ef08f3d4e1d08b49a25cf
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-08-13 13:49:26 +0800

    Time: 14 ms (97.44%), Space: 56.3 MB (68.64%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,19 +1,18 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-
         Set<Integer> set = new HashSet<>();
 
         for (int i = 0; i < nums.length; i++) {
             if (i > k) {
                 set.remove(nums[i - k - 1]);
             }
-
+            
             boolean isAdded = set.add(nums[i]);
             if (!isAdded) {
                 return true;
             }
         }
-
+        
         return false;
     }
 }
