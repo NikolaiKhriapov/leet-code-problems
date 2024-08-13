a8bbf3cd1fc08b000d66c4ccb41e363ea9c2685b
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-08-13 13:20:30 +0800

    Time: 1 ms (99.38%), Space: 44.7 MB (55.41%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
@@ -1,30 +1,30 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-
-        Queue<int[]> queue = new PriorityQueue<>((a, b) -> {
-            if (a[1] == b[1]) {
-                return a[0] - b[0];
-            }
-            return a[1] - b[1];
+        
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> {
+            if (a[0] == b[0]) return a[1] - b[1];
+            else return a[0] - b[0];
         });
 
         for (int i = 0; i < mat.length; i++) {
-            queue.offer(new int[]{ i, calcSoldiers(mat[i], 0, mat[i].length - 1) });
-        }
-        
-        int[] arr = new int[k];
-        for (int i = 0; i < k; i++) {
-            arr[i] = queue.poll()[0];
+            int soldiers = calcSoldiers(mat[i], 0, mat[i].length);
+            queue.add(new int[] { soldiers, i });
         }
 
-        return arr;
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = queue.poll()[1];
+        }
+
+        return result;
     }
 
     private int calcSoldiers(int[] row, int l, int r) {
-        if (r < l) return r;
-
-        int m = l + (r - l) / 2;
-        if (row[m] == 0) return calcSoldiers(row, l, m - 1);
-        else return calcSoldiers(row, m + 1, r);
+        int counter = 0;
+        for (int n : row) {
+            if (n == 1) counter++;
+            else break;
+        }
+        return counter;
     }
 }
