c8d89bfd5e2330dd3e74064b704504276091dbc1
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-08-13 12:54:53 +0800

    Time: 6 ms (13.98%), Space: 45 MB (52.92%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -15,14 +15,34 @@
  */
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        return traverse(root, new HashSet<>(), k);
+
+        List<Integer> list = new ArrayList<>();
+
+        traverse(root, list);
+        
+        int size = list.size();
+
+        if (size < 2) return false;
+
+        Collections.sort(list);
+        
+        int l = 0;
+        int r = size - 1;
+
+        while (l < r) {
+            if (list.get(l) + list.get(r) > k) r--;
+            else if (list.get(l) + list.get(r) < k) l++;
+            else return true;
+        }
+
+        return false;
     }
 
-    private boolean traverse(TreeNode root, Set<Integer> set, int k) {
-        if (root == null) return false;
-        if (set.contains(k - root.val)) return true;
-
-        set.add(root.val);
-        return traverse(root.left, set, k) || traverse(root.right, set, k);
+    private void traverse(TreeNode root, List<Integer> list) {
+        if (root != null) {
+            list.add(root.val);
+            traverse(root.left, list);
+            traverse(root.right, list);
+        }
     }
 }
