a998a4902abf784ab795bff787d56af4de07e80c
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-08-14 14:02:20 +0800

    Time: 0 ms (100%), Space: 42.4 MB (48.2%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -11,15 +11,14 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
 
-        if (head == null || head.next == null) {
-            return head;
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
         }
-
-        ListNode newHead = reverseList(head.next);
-
-        head.next.next = head;
-        head.next = null;
         
-        return newHead;
+        return prev;
     }
 }
