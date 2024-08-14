32d5c2005f46aae427f50d01b8526eed6452cb47
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-08-14 14:16:41 +0800

    Time: 0 ms (100%), Space: 40.3 MB (47.01%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,20 @@
 class Solution {
     public int climbStairs(int n) {
-        return climbStairs(n, new HashMap<>());
+        Map<Integer, Integer> memo = new HashMap<>();
+        return climbStairs(n, memo);
     }
 
-    public int climbStairs(int n, HashMap<Integer, Integer> memo) {
+    public int climbStairs(int n, Map<Integer, Integer> memo) {
         if (n == 0 || n == 1) return 1;
 
-        if (memo.containsKey(n)) return memo.get(n);
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
 
         int result = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
+
         memo.put(n, result);
+
         return result;
     }
 }
