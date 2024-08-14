f31feac20380b41f7632a45b30d3371e29205bad
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-08-14 15:27:38 +0800

    Time: 141 ms (52.09%), Space: 143.4 MB (72.34%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
@@ -2,35 +2,37 @@ class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         if (source == destination) return true;
 
-        List<Integer>[] connections = new ArrayList[n];
-        for (int i = 0; i < connections.length; i++) {
-            connections[i] = new ArrayList<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int[] edge : edges) {
+            List<Integer> v1 = map.getOrDefault(edge[0], new ArrayList<>());
+            v1.add(edge[1]);
+            map.put(edge[0], v1);
+
+            List<Integer> v2 = map.getOrDefault(edge[1], new ArrayList<>());
+            v2.add(edge[0]);
+            map.put(edge[1], v2);
         }
 
-        for (int[] edge : edges) {
-            connections[edge[0]].add(edge[1]);
-            connections[edge[1]].add(edge[0]);
-        }
-        
         Queue<Integer> queue = new LinkedList<>();
-        boolean[] visited = new boolean[n];
+        boolean[] visited = new boolean[n + 1];
 
         queue.add(source);
-        visited[source] = true;
-
         while (!queue.isEmpty()) {
             int curr = queue.poll();
+            visited[curr] = true;
 
-            if (curr == destination) return true;
-
-            for (int neighbor : connections[curr]) {
-                if (!visited[neighbor]) {
-                    queue.add(neighbor);
-                    visited[neighbor] = true;
+            for (int option : map.get(curr)) {
+                if (option == destination) {
+                    return true;
+                } else {
+                    if (!visited[option]) {
+                        queue.add(option);
+                        visited[option] = true;
+                    }
                 }
             }
         }
-
+        
         return false;
     }
 }
