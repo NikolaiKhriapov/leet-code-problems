95017716c34d15aa8b550ae1ea79f5dec664f730
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-09-04 10:00:35 +0800

    Time: 0 ms (100%), Space: 46.1 MB (19.41%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,23 +1,12 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        
-        int l = binarySearch(nums, target, 0, nums.length - 1);
-        int r = l;
-
-        if (l != -1) {
-            for (int i = l; i < nums.length; i++) {
-                if (nums[i] == nums[l]) {
-                    r = i;
-                } else {
-                    break;
-                }
-            }
-        }
+        int l = binarySearchLeft(nums, target, 0, nums.length - 1);
+        int r = binarySearchRight(nums, target, l, nums.length - 1);
 
         return new int[] { l, r };
     }
 
-    public int binarySearch(int[] nums, int target, int l, int r) {
+    public int binarySearchLeft(int[] nums, int target, int l, int r) {
         if (l > r) return -1;
 
         int m = l + (r - l) / 2;
@@ -26,11 +15,28 @@ class Solution {
             if (m == 0 || nums[m - 1] < target) {
                 return m;
             } else {
-                return binarySearch(nums, target, l, m - 1);
+                return binarySearchLeft(nums, target, l, m - 1);
             }
         }
 
-        if (nums[m] > target) return binarySearch(nums, target, l, m - 1);
-        else return binarySearch(nums, target, m + 1, r);
+        if (nums[m] > target) return binarySearchLeft(nums, target, l, m - 1);
+        else return binarySearchLeft(nums, target, m + 1, r);
+    }
+
+    public int binarySearchRight(int[] nums, int target, int l, int r) {
+        if (l > r || l == -1) return -1;
+
+        int m = l + (r - l) / 2;
+
+        if (nums[m] == target) {
+            if (m == nums.length - 1 || nums[m + 1] > target) {
+                return m;
+            } else {
+                return binarySearchRight(nums, target, m + 1, r);
+            }
+        }
+
+        if (nums[m] > target) return binarySearchRight(nums, target, l, m - 1);
+        else return binarySearchRight(nums, target, m + 1, r);
     }
 }
