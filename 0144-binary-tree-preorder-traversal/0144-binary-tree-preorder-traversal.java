7916b00422fb7756bb4c0e7bb88fc776d6f7f3cc
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-09-14 15:09:23 +0800

    Time: 0 ms (100%), Space: 41.4 MB (68.53%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -15,19 +15,20 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-
         List<Integer> list = new ArrayList<>();
-
         handleNode(root, list);
-
         return list;
     }
 
     private void handleNode(TreeNode node, List<Integer> list) {
         if (node != null){ 
             list.add(node.val);
-            if (node.left != null) handleNode(node.left, list);
-            if (node.right != null) handleNode(node.right, list);
+            if (node.left != null) {
+                handleNode(node.left, list);
+            }
+            if (node.right != null) {
+                handleNode(node.right, list);
+            }
         }
     }
 }
