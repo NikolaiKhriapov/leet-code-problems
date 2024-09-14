8e91a2e5f4b1b96ec0f2cab4a8ae01eb32339183
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-09-14 15:36:32 +0800

    Time: 1 ms (99.73%), Space: 44.8 MB (31.61%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
@@ -1,9 +1,11 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-        
         PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> {
-            if (a[0] == b[0]) return a[1] - b[1];
-            else return a[0] - b[0];
+            if (a[0] == b[0]) {
+                return a[1] - b[1];
+            } else {
+                return a[0] - b[0];
+            }
         });
 
         for (int i = 0; i < mat.length; i++) {
@@ -20,12 +22,18 @@ class Solution {
     }
 
     private int calcSoldiers(int[] row, int l, int r) {
-        if (l > r) return r;
+        if (l > r) {
+            return r;
+        }
         
         int m = l + (r - l) / 2;
 
-        if ((row[m] == 1 && m == row.length - 1) || (row[m] == 1 && row[m + 1] == 0)) return m + 1;
-        else if (row[m] == 1) return calcSoldiers(row, m + 1, r);
-        else return calcSoldiers(row, l, m - 1);
+        if ((row[m] == 1 && m == row.length - 1) || (row[m] == 1 && row[m + 1] == 0)) {
+            return m + 1;
+        } else if (row[m] == 1) {
+            return calcSoldiers(row, m + 1, r);
+        } else {
+            return calcSoldiers(row, l, m - 1);
+        }
     }
 }
