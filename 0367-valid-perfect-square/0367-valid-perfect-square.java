5f0075684604eeee2889c496778310d903893f0b
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-09-14 15:19:41 +0800

    Time: 0 ms (100%), Space: 40.2 MB (70.44%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -4,13 +4,20 @@ class Solution {
     }
 
     private int search(int num, int l, int r) {
-        if (r < l) return -1;
+        if (r < l) {
+            return -1;
+        }
 
         int m = l + ((r - l) / 2);
         long ms = (long) m * m;
         
-        if (ms == num) return m;
-        if (ms > num) return search(num, l, m - 1);
-        else return search(num, m + 1, r);
+        if (ms == num) {
+            return m;
+        }
+        if (ms > num) {
+            return search(num, l, m - 1);
+        } else {
+            return search(num, m + 1, r);
+        }
     }
 }
