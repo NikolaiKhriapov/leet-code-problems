157af78c16ca5db22f242ccc208e0f88a1a709e7
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-09-14 15:11:55 +0800

    Time: 8 ms (92.19%), Space: 60.6 MB (8.8%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        
         Set<Integer> set = new HashSet<>();
 
         for (int n : nums) {
             boolean isAdded = set.add(n);
-            if (!isAdded) return true;
+            if (!isAdded) {
+                return true;
+            }
         }
 
         return false;
