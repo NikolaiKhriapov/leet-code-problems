12b44effd120e4fbd2dca1a07d54bdce8c9fc2ba
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-09-14 15:31:27 +0800

    Time: 0 ms (100%), Space: 42.7 MB (95.58%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -5,9 +5,13 @@ class Solution {
     }
 
     private int calcCost(int[] cost, int n, int[] memo) {
-        if (n >= cost.length) return 0;
+        if (n >= cost.length) {
+            return 0;
+        }
 
-        if (memo[n] != 0) return memo[n];
+        if (memo[n] != 0) {
+            return memo[n];
+        }
 
         int constOneStep = calcCost(cost, n + 1, memo);
         int constTwoSteps = calcCost(cost, n + 2, memo);
