f6ec297922bc903742845c070ab74179593d02e2
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-09-14 15:06:59 +0800

    Time: 0 ms (100%), Space: 43.9 MB (94.27%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -19,14 +19,22 @@ class Solution {
     }
 
     public int getHeight(TreeNode node) {
-        if (node == null) return 0;
+        if (node == null) {
+            return 0;
+        }
 
         int left = getHeight(node.left);
-        if (left == -1) return -1;
+        if (left == -1) {
+            return -1;
+        }
         int right = getHeight(node.right);
-        if (right == -1) return -1;
+        if (right == -1) {
+            return -1;
+        }
 
-        if (Math.abs(right - left) > 1) return -1;
+        if (Math.abs(right - left) > 1) {
+            return -1;
+        }
 
         return Math.max(left, right) + 1;
     }
