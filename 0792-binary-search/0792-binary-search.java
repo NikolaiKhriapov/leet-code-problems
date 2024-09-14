f6a6cda00afa284084d5298126e1b28f16305fb8
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-09-14 15:30:28 +0800

    Time: 0 ms (100%), Space: 45.5 MB (78.84%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -4,12 +4,19 @@ class Solution {
     }
 
     public int search(int[] nums, int target, int l, int r) {
-        if (r < l) return -1;
+        if (r < l) {
+            return -1;
+        }
 
         int m = l + ((r - l) / 2);
 
-        if (nums[m] == target) return m;
-        if (nums[m] > target) return search(nums, target, 0, m - 1);
-        else return search(nums, target, m + 1, r);
+        if (nums[m] == target) {
+            return m;
+        }
+        if (nums[m] > target) {
+            return search(nums, target, 0, m - 1);
+        } else {
+            return search(nums, target, m + 1, r);
+        }
     }
 }
