74f9f4307d0fdab24007ed927d3b455ec7e7874e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-09-14 15:18:09 +0800

    Time: 8 ms (88.69%), Space: 43.4 MB (98.63%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,7 +1,11 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if (n == 1) return true;
-        if (n < 1 || n % 3 != 0) return false;
+        if (n == 1) {
+            return true;
+        }
+        if (n < 1 || n % 3 != 0) {
+            return false;
+        }
 
         return isPowerOfThree(n / 3);
     }
