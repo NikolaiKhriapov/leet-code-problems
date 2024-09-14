c2b1d3cf9c5f375e51699e07e49f0a93c1090a70
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-09-14 15:04:11 +0800

    Time: 1 ms (85.85%), Space: 41.2 MB (8.08%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,18 +1,26 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0) return 0;
+        if (x == 0) {
+            return 0;
+        }
         return search(x, 1, x);
     }
 
     private int search(int x, int l, int r) {
-        if (l > r) return r;
+        if (l > r) {
+            return r;
+        }
 
         int m = l + ((r - l) / 2);
 
         long ms = (long) m * m;
 
-        if (ms <= x && (long)(m + 1) * (m + 1) > x) return m;
-        if (ms > x) return search(x, l, m - 1);
-        else return search(x, m + 1, r);
+        if (ms <= x && (long)(m + 1) * (m + 1) > x) {
+            return m;
+        } else if (ms > x) {
+            return search(x, l, m - 1);
+        } else {
+            return search(x, m + 1, r);
+        }
     }
 }
