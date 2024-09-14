e7f1a9057c30e97526e95a25ac7290c85425de26
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-09-14 15:22:13 +0800

    Time: 0 ms (100%), Space: 41.6 MB (18.34%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -15,13 +15,21 @@
  */
 class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
-        if (root == null) return 0;
+        if (root == null) {
+            return 0;
+        }
         int sum = 0;
 
-        if (root.left != null && root.left.left == null && root.left.right == null) sum += root.left.val;
-        
-        if (root.left != null) sum += sumOfLeftLeaves(root.left);
-        if (root.right != null) sum += sumOfLeftLeaves(root.right);
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            sum += root.left.val;
+        }
+
+        if (root.left != null) {
+            sum += sumOfLeftLeaves(root.left);
+        }
+        if (root.right != null) {
+            sum += sumOfLeftLeaves(root.right);
+        }
 
         return sum;
     }
