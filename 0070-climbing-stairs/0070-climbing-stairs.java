9c7dc7a0ecedcaea19aa6481522c1c81ae67fd56
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-09-14 15:04:40 +0800

    Time: 0 ms (100%), Space: 40.2 MB (53.04%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -5,7 +5,9 @@ class Solution {
     }
 
     public int climbStairs(int n, Map<Integer, Integer> memo) {
-        if (n == 0 || n == 1) return 1;
+        if (n == 0 || n == 1) {
+            return 1;
+        }
 
         if (memo.containsKey(n)) {
             return memo.get(n);
