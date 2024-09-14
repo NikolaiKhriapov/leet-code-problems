0dc105c8ebfd089dddbfe6af1bc61c8a28f15065
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-09-14 15:23:36 +0800

    Time: 1 ms (98.24%), Space: 41 MB (39.64%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -4,7 +4,9 @@ class Solution {
     }
 
     public int search(int n, int l, int r) {
-        if (l > r) return r;
+        if (l > r) {
+            return r;
+        }
 
         int m = l + ((r - l) / 2);
         long ml = (long) m;
@@ -12,8 +14,13 @@ class Solution {
         long coinsForRowM = ml * (ml + 1) / 2;
         long coinsForRowMM = (ml + 1) * (ml + 2) / 2;
 
-        if (n >= coinsForRowM && n < coinsForRowMM) return m;
-        if (n > coinsForRowM) return search(n, m + 1, r);
-        else return search(n, l, m - 1);
+        if (n >= coinsForRowM && n < coinsForRowMM) {
+            return m;
+        }
+        if (n > coinsForRowM) {
+            return search(n, m + 1, r);
+        } else {
+            return search(n, l, m - 1);
+        }
     }
 }
