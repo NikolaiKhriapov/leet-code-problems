8533db5fa2af33285ee11c60c9e291094de0e6f0
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-01-27 12:04:42 +0800

    Time: 0 ms (100%), Space: 41.8 MB (65.41%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,5 +1,24 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        
+
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+
+        if (digits[0] == 0) {
+            int[] newDigits = new int[digits.length + 1];
+            newDigits[0] = 1;
+            for (int i = 0; i < digits.length; i++) {
+                newDigits[i + 1] = digits[i];
+            }
+            return newDigits;
+        }
+
+        return digits;
     }
 }
