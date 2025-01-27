8b01c3514a70c18a09c9c760ce984ba09c4b49ec
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-01-27 11:39:49 +0800

    Time: 2 ms (98.9%), Space: 45.1 MB (29.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,31 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
-        int[] arraySorted = Arrays.copyOf(nums, nums.length);
-        Arrays.sort(arraySorted);
+        Map<Integer, Integer> map = new HashMap<>();
 
-        int l = 0;
-        int r = arraySorted.length - 1;
-
-        while (l < r) {
-            if (arraySorted[l] + arraySorted[r] == target) {
-                int first = -1;
-                int second = -1;
-                for (int i = 0; i < nums.length; i++) {
-                    if (first == -1 && nums[i] == arraySorted[l]) {
-                        first = i;
-                        nums[i] = -1;
-                    }
-                    if (second == -1 && nums[i] == arraySorted[r]) {
-                        second = i;
-                        nums[i] = -1;
-                    }
-                }
-                return new int[] {first, second};
-            } else if (arraySorted[l] + arraySorted[r] < target) {
-                l += 1;
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
             } else {
-                r -= 1;
+                map.put(nums[i], i);
             }
         }
 
