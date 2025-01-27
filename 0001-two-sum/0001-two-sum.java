742292b05bb2531e2d1130f4d6e513229687a0cf
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-01-27 11:40:38 +0800

    Time: 2 ms (98.9%), Space: 44.9 MB (76.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
@@ -12,6 +11,6 @@ class Solution {
             }
         }
 
-        return new int[] {-1, -1};
+        return new int[] {};
     }
 }
