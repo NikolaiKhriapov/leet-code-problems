42e0841fa9dc46c535a0fee64f461a2a3925bcbf
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-01-27 11:10:30 +0800

    Time: 5 ms (55.98%), Space: 45.4 MB (9.59%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,38 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            if (!map.keySet().contains(target - nums[i])) {
-                map.put(nums[i], i);
+        int[] numsS = new int[nums.length];
+        for (int i = 0; i < numsS.length; i++) {
+            numsS[i] = nums[i];
+        }
+
+        Arrays.sort(numsS);
+
+        int l = 0;
+        int r = numsS.length - 1;
+
+        while (l < r) {
+            if (numsS[l] + numsS[r] == target) {
+                int first = -1;
+                int second = -1;
+                for (int i = 0; i < nums.length; i++) {
+                    if (first == -1 && nums[i] == numsS[l]) {
+                        first = i;
+                        nums[i] = -1;
+                    }
+                    if (second == -1 && nums[i] == numsS[r]) {
+                        second = i;
+                        nums[i] = -1;
+                    }
+                }
+                return new int[] {first, second};
+            } else if (numsS[l] + numsS[r] < target) {
+                l += 1;
             } else {
-                return new int[] { i, map.get(target - nums[i]) };
+                r -= 1;
             }
-        }        
+        }
 
-        return new int[2];
+        return new int[] {-1, -1};
     }
 }
