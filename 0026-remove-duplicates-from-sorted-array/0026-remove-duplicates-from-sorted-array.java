17a22fc317a72a1e1b0594a25cc2fb3f68812172
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-01-27 11:51:11 +0800

    Time: 1 ms (65.37%), Space: 45.1 MB (23.04%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,17 +1,27 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        if (nums.length == 1) {
+            return 1;
+        }
 
         int l = 0;
-        int r = 0;
+        int r = 1;
 
         while (r < nums.length) {
-            if (nums[r] > nums[l]) {
+            if (nums[r] == nums[l]) {
+                r++;
+            } else {
                 l++;
                 nums[l] = nums[r];
             }
-            r++;
         }
-        
-        return l + 1;
+
+        int res = l + 1;
+
+        for (int i = l + 1; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+
+        return res;
     }
 }
