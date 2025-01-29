5bd40ac43fbdfcfaebad31bfa25b86bd4bd1fd18
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-01-29 12:13:30 +0800

    Time: 2 ms (84.8%), Space: 45.7 MB (95.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,25 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        if (nums.length < 2) {
+            return;
+        }
 
-        int last = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[last++] = nums[i];
+        int l = 0;
+        int r = 1;
+
+        while (r < nums.length) {
+            if (nums[l] != 0) {
+                l++;
             }
+            if (nums[r] != 0 && l != r) {
+                nums[l] = nums[r];
+                nums[r] = 0;
+            }
+            r++;
         }
 
-        for (int i = last; i < nums.length; i++) {
+        for (int i = l + 1; i < nums.length; i++) {
             nums[i] = 0;
-        }
+        }        
     }
 }
