98cfe8584f08832942c1deaf75c7f9eb51ac375b
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-01-30 14:07:10 +0800

    Time: 6 ms (27.48%), Space: 41.5 MB (95.29%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -2,6 +2,10 @@ class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> list = new ArrayList<>();
 
+        if (nums.length == 0) {
+            return list;
+        }
+
         int l = 0;
         int r = 1;
         
@@ -16,12 +20,11 @@ class Solution {
             }
             r++;
         }
-        if (r == nums.length) {
-            if (l == r - 1) {
-                list.add(String.valueOf(nums[l]));
-            } else {
-                list.add(nums[l] + "->" + nums[r - 1]);
-            }
+
+        if (l == r - 1) {
+            list.add(String.valueOf(nums[l]));
+        } else {
+            list.add(nums[l] + "->" + nums[r - 1]);
         }
         
         return list;
