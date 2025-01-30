aa08a40425609b7583561717e1fd4d5e12673352
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-01-30 11:39:13 +0800

    Time: 0 ms (100%), Space: 42.3 MB (9.05%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -3,20 +3,18 @@ class Solution {
 
         int p1 = 0;
         int p2 = 0;
-        int counter = 0;
         
         while (p2 < nums.length) {
             if (nums[p2] != val) {
                 int temp = nums[p2];
                 nums[p2] = nums[p1];
                 nums[p1] = temp;
-                counter++;
                 p1++;
             }
 
             p2++;
         }
         
-        return counter;
+        return nums.length - (p2 - p1);
     }
 }
