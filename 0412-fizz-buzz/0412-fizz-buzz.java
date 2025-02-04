fa33f164f57be22d47e1f338bdc770991c7096a1
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-05 00:53:15 +0800

    Time: 9 ms (3.01%), Space: 46 MB (5.5%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,12 +1,20 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> result = new ArrayList<>();
-
+        
         for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) result.add("FizzBuzz");
-            else if (i % 3 == 0) result.add("Fizz");
-            else if (i % 5 == 0) result.add("Buzz");
-            else result.add(String.valueOf(i));
+            String s = "";
+            if (i % 3 == 0 || i % 5 == 0) {
+                if (i % 3 == 0) {
+                    s += "Fizz";
+                }
+                if (i % 5 == 0) {
+                    s += "Buzz";
+                }
+            } else {
+                s += i;
+            }
+            result.add(s);
         }
 
         return result;
