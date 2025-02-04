8968a7ae2093aec443c5d91901cc0e0089cb091b
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-04 22:04:51 +0800

    Time: 1 ms (24.41%), Space: 42 MB (13.64%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,6 +1,24 @@
 class Solution {
     public int strStr(String haystack, String needle) {
+        int pN = 0;
+        int pH = 0;
+        int p = 0;
         
-        return haystack.indexOf(needle);
+        while (p < haystack.length()) {
+            if (haystack.charAt(p) != needle.charAt(pN)) {
+                pH++;
+                p = pH;
+                pN = 0;
+            } else {
+                p++;
+                pN++;
+            }
+
+            if (p - pH == needle.length()) {
+                return pH;
+            }
+        }
+
+        return -1;
     }
 }
