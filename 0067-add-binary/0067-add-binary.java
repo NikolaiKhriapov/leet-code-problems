fe4e4522d4588b8df28d03a8f4f9c5fef26dc361
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-05 18:55:20 +0800

    Time: 1 ms (99.95%), Space: 42.1 MB (90.78%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -4,20 +4,29 @@ class Solution {
 
         int pa = a.length() - 1;
         int pb = b.length() - 1;
-        int n = 0;
+        int carry = 0;
 
-        while (pa >= 0 || pb >= 0 || n > 0) {
+        while (pa >= 0 || pb >= 0 || carry > 0) {
             int na = (pa >= 0) ? a.charAt(pa) - '0' : 0;
             int nb = (pb >= 0) ? b.charAt(pb) - '0' : 0;
-            int sum = na + nb + n;
+            int sum = na + nb + carry;
 
             sb.append(sum % 2);
-            n = sum / 2;
+            carry = sum / 2;
 
             pa--;
             pb--;
         }
 
-        return sb.reverse().toString();
+        String result = sb.toString();
+
+        char[] resultArr = result.toCharArray();
+        for (int i = 0; i < resultArr.length / 2; i++) {
+            char temp = resultArr[i];
+            resultArr[i] = resultArr[resultArr.length - i - 1];
+            resultArr[resultArr.length - i - 1] = temp;
+        }
+
+        return new String(resultArr);
     }
 }
