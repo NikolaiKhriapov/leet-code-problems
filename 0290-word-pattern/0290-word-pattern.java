ea5d87c58e8a11691cc1097e4e42a20bbdd77743
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-05 20:35:32 +0800

    Time: 1 ms (82.4%), Space: 41.5 MB (65.68%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,22 +1,26 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        String[] arr = s.split(" ");
-        if (arr.length != pattern.length()) {
+        String[] words = s.split(" ");
+
+        if (pattern.length() != words.length) {
             return false;
         }
-
+        
         Map<Character, String> map = new HashMap<>();
-
         for (int i = 0; i < pattern.length(); i++) {
-            char currP = pattern.charAt(i);
-            String currS = arr[i];
-            if (map.containsKey(currP) && !Objects.equals(map.get(currP), currS)) {
-                return false;
+            char c = pattern.charAt(i);
+            String w = words[i];
+
+            if (map.containsKey(c)) {
+                if (!Objects.equals(map.get(c), w)) {
+                    return false;
+                }
+            } else {
+                if (map.containsValue(w)) {
+                    return false;
+                }
+                map.put(c, w);
             }
-            if (!map.containsKey(currP) && map.containsValue(currS)) {
-                return false;
-            }
-            map.put(currP, currS);
         }
 
         return true;
