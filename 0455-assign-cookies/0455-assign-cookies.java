3b8c630bfdd6af113c48994ae4546e0bf7731566
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-06 20:56:43 +0800

    Time: 10 ms (81.4%), Space: 48.7 MB (44.29%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -3,16 +3,23 @@ class Solution {
         Arrays.sort(g);
         Arrays.sort(s);
 
+        int result = 0;
         int p1 = 0;
         int p2 = 0;
-
-        while (p1 < g.length && p2 < s.length) {
-            if (g[p1] <= s[p2]) {
-                p1++;
-            }
-            p2++;
-        }
         
-        return p1;
+        while (p1 < g.length && p2 < s.length) {
+            while (p2 < s.length - 1 && g[p1] > s[p2]) {
+                p2++;
+            }
+            if (g[p1] <= s[p2]) {
+                result++;
+                p1++;
+                p2++;
+            } else {
+                break;
+            }
+        }
+    
+        return result;
     }
 }
