4058032981a871c1200ac432f9f02dc82548a233
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-08 16:15:52 +0800

    Time: 2 ms (95.94%), Space: 46 MB (16.78%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int[] array = new int[10001];
+        int[] array = new int[nums.length];
 
         for (int n : nums) {
-            array[n]++;
+            array[n - 1]++;
         }
 
         int dupl = -1;
         int miss = -1;
 
-        for (int i = 1; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             if (array[i] != 1) {
                 if (array[i] == 0) {
-                    miss = i;
+                    miss = i + 1;
                 } else if (array[i] == 2) {
-                    dupl = i;
+                    dupl = i + 1;
                 }
                 if (miss != -1 && dupl != -1) {
                     return new int[] {dupl, miss};
