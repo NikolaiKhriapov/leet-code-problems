cdb7bb59062eff4b77b910b51f84b25cb110e307
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-11 11:24:54 +0800

    Time: 14 ms (44%), Space: 46.2 MB (73.33%) - LeetHub

--- 1094-matrix-cells-in-distance-order/1094-matrix-cells-in-distance-order.java
+++ 1094-matrix-cells-in-distance-order/1094-matrix-cells-in-distance-order.java
@@ -1,51 +1,20 @@
 class Solution {
     public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
-        int[][] arr = new int[rows*cols][2];
+        int[][] result = new int[rows * cols][2];
 
-        int idx = 0;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                arr[idx] = new int[] { i, j };
-                idx++;
+        int i = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                result[i++] = new int[] {r, c};
             }
         }
 
-        Arrays.sort(arr, (a, b) -> {
-            int d1 = Math.abs(a[0] - rCenter) + Math.abs(a[1] - cCenter);
-            int d2 = Math.abs(b[0] - rCenter) + Math.abs(b[1] - cCenter);
-            return d1 - d2;
-        });
+        Arrays.sort(result, (a, b) -> 
+            (Math.abs(a[0] - rCenter) + Math.abs(a[1] - cCenter)) - 
+            (Math.abs(b[0] - rCenter) + Math.abs(b[1] - cCenter))
+        );
 
-        // sort(arr, rCenter, cCenter);
-
-        return arr;
+        return result;
     }
-
-    // public void sort(int[][] arr, int rCenter, int cCenter) {
-    //     int[] distance = new int[arr.length];
-
-    //     int counter = 0;
-    //     for (int[] cell : arr) {
-    //         distance[counter] = Math.abs(cell[0] - rCenter) + Math.abs(cell[1] - cCenter);
-    //         counter++;
-    //     }
-
-    //     for (int i = 0; i < distance.length - 1; i++) {
-    //         for (int j = 0; j < distance.length - 1; j++) {
-    //             if (distance[j] > distance[j + 1]) {
-    //                 int tmp = distance[j];
-    //                 distance[j] = distance[j + 1];
-    //                 distance[j + 1] = tmp;
-
-    //                 tmp = arr[j][0];
-    //                 arr[j][0] = arr[j+1][0];
-    //                 arr[j+1][0] = tmp;
-
-    //                 tmp = arr[j][1];
-    //                 arr[j][1] = arr[j+1][1];
-    //                 arr[j+1][1] = tmp;
-    //             }
-    //         }
-    //     }
-    // }
 }
