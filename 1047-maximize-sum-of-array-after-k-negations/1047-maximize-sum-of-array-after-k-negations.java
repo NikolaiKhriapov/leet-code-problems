1ecc404e1ada3e69e2c979411200212452dc9dfb
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-11 11:04:07 +0800

    Time: 2 ms (96.41%), Space: 42.4 MB (69.13%) - LeetHub

--- 1047-maximize-sum-of-array-after-k-negations/1047-maximize-sum-of-array-after-k-negations.java
+++ 1047-maximize-sum-of-array-after-k-negations/1047-maximize-sum-of-array-after-k-negations.java
@@ -1,26 +1,52 @@
-import java.util.Arrays;
-
 class Solution {
     public int largestSumAfterKNegations(int[] nums, int k) {
         Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length && k > 0; i++) {
+        int counter = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (counter >= k) {
+                break;
+            }
+
             if (nums[i] < 0) {
                 nums[i] *= -1;
-                k--;
+                counter++;
+            } else if (nums[i] == 0) {
+                break;
+            } else if (nums[i] > 0) {
+                if (i == 0) {
+                    while (counter < k) {
+                        nums[i] *= -1;
+                        counter++;
+                    }
+                } else {
+                    if (Math.abs(nums[i]) >= Math.abs(nums[i - 1])) {
+                        while (counter < k) {
+                            nums[i - 1] *= -1;
+                            counter++;
+                        }
+                    } else {
+                        while (counter < k) {
+                            nums[i] *= -1;
+                            counter++;
+                        }
+                    }
+                }
+            }
+
+            if (i == nums.length - 1 && counter < k) {
+                i--;
             }
         }
 
-        if (k % 2 == 1) {
-            Arrays.sort(nums);
-            nums[0] *= -1;
-        }
+        return calculateSum(nums);
+    }
 
+    private int calculateSum(int[] nums) {
         int result = 0;
         for (int n : nums) {
             result += n;
         }
-
         return result;
     }
 }
