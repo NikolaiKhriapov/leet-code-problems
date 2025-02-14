5095349060ed90678afb17c0c89f2f0168a2a204
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-14 13:36:04 +0800

    Time: 2 ms (95.85%), Space: 41.8 MB (55.06%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -3,47 +3,25 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
 
         for (String op : operations) {
-            if (Objects.equals(op, "+")) {
-                Integer tmp = stack.pop();
-                Integer score = stack.peek() + tmp;
-                stack.push(tmp);
-                stack.push(score);
-            } else if (Objects.equals(op, "D")) {
-                stack.push(2 * stack.peek());
-            } else if (Objects.equals(op, "C")) {
+            if (Objects.equals(op, "C")) {
                 stack.pop();
+            } else if (Objects.equals(op, "D")) {
+                stack.add(2 * stack.peek());
+            } else if (Objects.equals(op, "+")) {
+                Integer temp = stack.pop();
+                Integer next = temp + stack.peek();
+                stack.add(temp);
+                stack.add(next);
             } else {
-                stack.push(Integer.valueOf(op));
+                stack.add(Integer.valueOf(op));
             }
         }
-
-        int sum = 0;
-        while (!stack.empty()) {
-            sum += stack.pop();
+        
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
         }
 
-        return sum;
+        return result;
     }
 }
-
-        // List<Integer> list = new ArrayList<>();
-
-        // for (String op : operations) {
-        //     if (Objects.equals(op, "+")) {
-        //         Integer score = list.get(list.size() - 2) + list.get(list.size() - 1);
-        //         list.add(score);
-        //     } else if (Objects.equals(op, "D")) {
-        //         list.add(2 * list.get(list.size() - 1));
-        //     } else if (Objects.equals(op, "C")) {
-        //         list.remove(list.size() - 1);
-        //     } else {
-        //         list.add(Integer.valueOf(op));
-        //     }
-        // }
-
-        // int sum = 0;
-        // for (Integer n : list) {
-        //     sum += n;
-        // }
-
-        // return sum;
