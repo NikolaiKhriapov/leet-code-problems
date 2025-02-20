6d2708b481cef492b93d2d2bb650311427567784
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-20 19:36:03 +0800

    Time: 1 ms (99.13%), Space: 44.9 MB (63.49%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,20 +1,15 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        if (ransomNote.length() > magazine.length()) {
-            return false;
-        }
-
         int[] array = new int[26];
 
         for (char c : magazine.toCharArray()) {
             array[c - 'a']++;
         }
-        
+
         for (char c : ransomNote.toCharArray()) {
-            if (array[c - 'a'] == 0) {
+            if (--array[c - 'a'] < 0) {
                 return false;
             }
-            array[c - 'a']--;
         }
 
         return true;
