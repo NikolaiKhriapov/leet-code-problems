c2e51add6fbebeb061afcd975edfd78870e68734
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-20 19:39:26 +0800

    Time: 1 ms (99.75%), Space: 41.9 MB (62.14%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,17 +1,17 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int[] arr = new int[26];
+        int[] array = new int[26];
 
         for (char c : s.toCharArray()) {
-            arr[c - 'a']++;
+            array[c - 'a']++;
         }
 
         for (char c : t.toCharArray()) {
-            if (--arr[c - 'a'] < 0) {
+            if (--array[c - 'a'] < 0) {
                 return c;
             }
         }
-
+        
         return '0';
     }
 }
