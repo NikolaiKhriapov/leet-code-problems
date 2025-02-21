b9c5e1356cff35918da398d667b80e354b0543c3
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-21 13:47:20 +0800

    Time: 0 ms (100%), Space: 44.5 MB (68.83%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -1,47 +1,21 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+
+    private TreeNode prev = null;
+    private int diffMin = Integer.MAX_VALUE;
+
     public int getMinimumDifference(TreeNode root) {
-        Set<Integer> set = new HashSet<>();
-
-        handleNode(root, set);
-
-        List<Integer> list = new ArrayList<>(set);
-        Collections.sort(list);
-
-        int diff = list.get(list.size() - 1);
-        for (int i = 0; i < list.size() - 1; i++) {
-            int tmp = list.get(i + 1) - list.get(i);
-            if (tmp < diff) {
-                diff = tmp;
-            }
-        }
-
-        return diff;
+        helper(root);
+        return diffMin;
     }
 
-    private void handleNode(TreeNode node, Set<Integer> set) {
-        if (node != null) {
-            set.add(node.val);
-            if (node.left != null) {
-                handleNode(node.left, set);
-            }
-            if (node.right != null) {
-                handleNode(node.right, set);
-            }
-        }
+    private void helper(TreeNode node) {
+        if (node == null) return;
+
+        if (node.left != null) helper(node.left);
+
+        if (prev != null) diffMin = Math.min(diffMin, node.val - prev.val);
+        prev = node;
+
+        if (node.right != null) helper(node.right);
     }
 }
