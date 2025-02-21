b07bbe9bc494974a1c4bbd93b7f397b63cf4dc58
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-21 22:53:57 +0800

    Time: 2 ms (22.16%), Space: 44.4 MB (16.95%) - LeetHub

--- 0774-maximum-depth-of-n-ary-tree/0774-maximum-depth-of-n-ary-tree.java
+++ 0774-maximum-depth-of-n-ary-tree/0774-maximum-depth-of-n-ary-tree.java
@@ -19,11 +19,13 @@ class Node {
 
 class Solution {
     public int maxDepth(Node root) {
-        if (root == null) return 0;
-        
-        int depth = 0;
+        if (root == null) {
+            return 0;
+        }
+
         Queue<Node> q = new LinkedList<>();
         q.add(root);
+        int depth = 0;
 
         while (!q.isEmpty()) {
             int size = q.size();
