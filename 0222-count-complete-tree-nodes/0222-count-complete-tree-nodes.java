7a70c343fdf4d423bfd16fa74b2580d192f7c50b
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-21 11:14:05 +0800

    Time: 0 ms (100%), Space: 48.2 MB (5.51%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -3,7 +3,32 @@ class Solution {
         if (root == null) {
             return 0;
         }
+
+        int leftHeight = getLeftHeight(root);
+        int rightHeight = getRightHeight(root);
+
+        if (leftHeight == rightHeight) {
+            return (int) Math.pow(2, leftHeight) - 1;
+        }
         
         return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
+    private int getLeftHeight(TreeNode node) {
+        int height = 0;
+        while (node != null) {
+            node = node.left;
+            height++;
+        }
+        return height;
+    }
+
+    private int getRightHeight(TreeNode node) {
+        int height = 0;
+        while (node != null) {
+            node = node.right;
+            height++;
+        }
+        return height;
     }
 }
