948b0f4e73377896daf65a6e185131f7382e0fbf
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-21 13:51:17 +0800

    Time: 0 ms (100%), Space: 44.8 MB (32.76%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -1,5 +1,4 @@
 class Solution {
-
     private TreeNode prev = null;
     private int diffMin = Integer.MAX_VALUE;
 
@@ -9,13 +8,17 @@ class Solution {
     }
 
     private void helper(TreeNode node) {
-        if (node == null) return;
+        if (node == null) {
+            return;
+        }
 
-        if (node.left != null) helper(node.left);
+        helper(node.left);
 
-        if (prev != null) diffMin = Math.min(diffMin, node.val - prev.val);
+        if (prev != null) {
+            diffMin = Math.min(diffMin, node.val - prev.val);
+        }
         prev = node;
 
-        if (node.right != null) helper(node.right);
+        helper(node.right);
     }
 }
