91aabaa3a8bdc5f7298537a8187e714aa675b2f3
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-22 16:25:14 +0800

    Time: 20 ms (99.46%), Space: 40.4 MB (63.67%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -12,7 +12,7 @@ public class Solution extends VersionControl {
         int m = l + (r - l) / 2;
 
         if (isBadVersion(m)) {
-            return (m == 0 || !isBadVersion(m - 1)) ? m : helper(l, m - 1);
+            return (m == 0) ? m : helper(l, m - 1);
         } else {
             return helper(m + 1, r);
         }
