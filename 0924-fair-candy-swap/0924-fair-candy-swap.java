76d0bcbf85b0a2aceed608e9fcf01a55383bcf3b
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-22 19:46:36 +0800

    Time: 3 ms (99.92%), Space: 46.6 MB (5.56%) - LeetHub

--- 0924-fair-candy-swap/0924-fair-candy-swap.java
+++ 0924-fair-candy-swap/0924-fair-candy-swap.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
-        Set<Integer> boxesAlice = new HashSet<>();
+        boolean[] boxesAlice = new boolean[100001];
         int totalAlice = 0;
         int totalBob = 0;
 
         for (int n : aliceSizes) {
             totalAlice += n;
-            boxesAlice.add(n);
+            boxesAlice[n] = true;
         }
         for (int n : bobSizes) {
             totalBob += n;
@@ -14,7 +14,7 @@ class Solution {
 
         for (int n : bobSizes) {
             int complement = (totalAlice - totalBob) / 2 + n;
-            if (boxesAlice.contains(complement)) {
+            if (complement >= 0 && complement < boxesAlice.length && boxesAlice[complement]) {
                 return new int[] {complement, n};
             }
         }
