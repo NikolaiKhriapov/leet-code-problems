6e0ad7777cb8e9d6cec7c0b991d9be047afe309e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-22 20:52:09 +0800

    Time: 2 ms (94.3%), Space: 43.2 MB (31.86%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
@@ -3,15 +3,10 @@ class Solution {
         Set<Integer> set = new HashSet<>();
         int zeroes = 0;
         for (int n : arr) {
-            if (n == 0) zeroes++;
-            set.add(n);
-        }
-
-        if (zeroes > 1) return true;
-        for (int n : arr) {
-            if (n != 0 && set.contains(2 * n)) {
+            if ( (n != 0 && (set.contains(2 * n) || (n % 2 == 0 && set.contains(n / 2)))) || (n == 0 && ++zeroes > 1) ) {
                 return true;
             }
+            set.add(n);
         }
 
         return false;
