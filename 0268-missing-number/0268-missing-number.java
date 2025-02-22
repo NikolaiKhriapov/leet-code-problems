94163c1ab8da5a556d85fa1c843a86b0cc2a7b03
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-22 15:18:20 +0800

    Time: 0 ms (100%), Space: 45.7 MB (13.02%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,14 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int length = nums.length;
-
-        int sumExpected = length * (length + 1) / 2;
-
+        int expectedSum = nums.length * (nums.length + 1) / 2;
+        
         int sum = 0;
         for (int n : nums) {
             sum += n;
         }
-
-        return sumExpected - sum;
+        
+        return expectedSum - sum;
     }
 }
