9ab9148b6d4fd16949894313398849d1d5c7c8e1
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-22 15:11:50 +0800

    Time: 1 ms (86.84%), Space: 40.9 MB (67.49%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,17 +1,16 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int) helper(x, 0, x);
+        return helper(x, 0, x);
     }
 
-    private long helper(long x, long l, long r) {
+    private int helper(int x, int l, int r) {
         if (l > r) return r;
 
-        long m = l + (r - l) / 2;
+        int m = l + (r - l) / 2;
 
-        if (m * m == x) return m;
-        if (m * m > x) return helper(x, l, m - 1);
-        if (m * m < x) return helper(x, m + 1, r);
-
-        return -1;
+        long n = (long) m * m;
+        if (n == x) return m;
+        if (n > x) return helper(x, l, m - 1);
+        else return helper(x, m + 1, r);
     }
 }
