27357a1efba8d9b50742d379332ded2eec8c9b99
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-22 15:28:09 +0800

    Time: 0 ms (100%), Space: 43.2 MB (27.5%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,22 +1,15 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        return binarySearch(nums, 0, nums.length - 1, target);
+        return helper(nums, target, 0, nums.length - 1);
     }
 
-    private int binarySearch(int[] nums, int l, int r, int target) {
-        if (l > r) {
-            return l;
-        }
+    private int helper(int[] nums, int target, int l, int r) {
+        if (l > r) return l;
 
         int m = l + (r - l) / 2;
 
-        if (nums[m] == target) {
-            return m;
-        }
-        if (nums[m] > target) {
-            return binarySearch(nums, l, m - 1, target);
-        } else {
-            return binarySearch(nums, m + 1, r, target);
-        }
+        if (nums[m] == target) return m;
+        else if (nums[m] > target) return helper(nums, target, l, m - 1);
+        else return helper(nums, target, m + 1, r);
     }
 }
