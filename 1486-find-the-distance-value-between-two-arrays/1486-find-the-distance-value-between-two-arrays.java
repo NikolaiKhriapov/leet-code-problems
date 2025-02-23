ac6fcfce36d513c504acce20e42c7bddd1f18177
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-23 19:24:57 +0800

    Time: 4 ms (59.96%), Space: 43.8 MB (96.78%) - LeetHub

--- 1486-find-the-distance-value-between-two-arrays/1486-find-the-distance-value-between-two-arrays.java
+++ 1486-find-the-distance-value-between-two-arrays/1486-find-the-distance-value-between-two-arrays.java
@@ -1,31 +1,31 @@
 class Solution {
     public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
-        int count = 0;
+        int counter = 0;
 
         Arrays.sort(arr2);
+
         for (int n : arr1) {
-            boolean isAllHigher = search(arr2, n, d, 0, arr2.length - 1);
-            if (isAllHigher) {
-                count++;
+            if (helper(arr2, n, d, 0, arr2.length - 1)) {
+                counter++;
             }
         }
 
-        return count;
+        return counter;        
     }
 
-    private boolean search(int[] arr2, int n, int d, int l, int r) {
-        if (l > r) {
-            return true;
-        }
+    private boolean helper(int[] arr, int n, int d, int l, int r) {
+        if (l > r) return false;
 
         int m = l + (r - l) / 2;
-        
-        if (Math.abs(n - arr2[m]) <= d) {
-            return false;
-        } else if (arr2[m] < n) {
-            return search(arr2, n, d, m + 1, r);
-        } else {
-            return search(arr2, n, d, l, m - 1);
+
+        if (arr[m] == n) return false;
+        if (arr[m] < n) {
+            if (m == arr.length - 1) return Math.abs(n - arr[m]) > d;
+            if (arr[m + 1] > n) return Math.abs(n - arr[m]) > d && Math.abs(n - arr[m + 1]) > d;
+            return helper(arr, n, d, m + 1, r);
         }
+        if (m == 0) return Math.abs(n - arr[m]) > d;
+        if (arr[m - 1] < n) return Math.abs(n - arr[m]) > d && Math.abs(n - arr[m - 1]) > d;
+        return helper(arr, n, d, l, m - 1);
     }
 }
