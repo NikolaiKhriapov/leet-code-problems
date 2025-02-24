833a8cb1bd06d67516cd5a05783009df34c4010e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-24 13:17:49 +0800

    Time: 3 ms (75%), Space: 42.5 MB (99.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isPalindrome(String s) {
         int pl = 0;
         int pr = s.length() - 1;
-
+        
         while (pl < pr) {
             while (pl < pr && !Character.isLetterOrDigit(s.charAt(pl))) {
                 pl++;
@@ -11,10 +11,13 @@ class Solution {
                 pr--;
             }
 
-            if (pl < pr && Character.toLowerCase(s.charAt(pl)) != Character.toLowerCase(s.charAt(pr))) {
+            if (pl >= pr) {
+                return true;
+            }
+            if (Character.toLowerCase(s.charAt(pl)) != Character.toLowerCase(s.charAt(pr))) {
                 return false;
             }
-
+            
             pl++;
             pr--;
         }
