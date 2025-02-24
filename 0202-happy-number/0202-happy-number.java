67d3f142880f7b70da814029e1192b8ad3289147
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-24 13:46:01 +0800

    Time: 1 ms (64.55%), Space: 41.2 MB (14.67%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,17 +1,18 @@
 class Solution {
     public boolean isHappy(int n) {
-        int slow = n;
-        int fast = getProduct(n);
+        Set<Integer> set = new HashSet<>();
 
-        while (slow != fast && fast != 1) {
-            slow = getProduct(slow);
-            fast = getProduct(getProduct(fast));
+        while (n != 1) {
+            n = helper(n);
+            if (!set.add(n)) {
+                break;
+            }
         }
-
-        return fast == 1;
+        
+        return n == 1;
     }
 
-    private int getProduct(int n) {
+    private int helper(int n) {
         int result = 0;
         while (n > 0) {
             result += (n % 10) * (n % 10);
