49683925b664ef03ada5e6fe11cf1c3560a7fa98
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-25 12:21:59 +0800

    Time: 3 ms (22.76%), Space: 45.9 MB (79.63%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -7,8 +7,7 @@ class Solution {
             while (pl < nums.length && nums[pl] != 0) {
                 pl++;
             }
-            pr = pl + 1;
-            while (pr < nums.length && nums[pr] == 0 ) {
+            while (pr < pl || (pr < nums.length && nums[pr] == 0)) {
                 pr++;
             }
 
