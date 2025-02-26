381dd0d22b55c017ada0b6d2f4fa59820e7d7c71
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-26 10:43:05 +0800

    Time: 1 ms (99.38%), Space: 45 MB (40.03%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
@@ -1,34 +1,29 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-        List<Integer>[] map = new List[101];
-        for (int i = 0; i < map.length; i++) {
-            map[i] = new ArrayList<>();
+        Queue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            int d = a[1] - b[1];
+            if (d > 0) return 1;
+            else if (d < 0) return -1;
+            else return a[0] - b[0];
+        });
+        for (int i = 0; i < mat.length; i++) {
+            pq.add(new int[] {i, helper(mat[i], 0, mat[i].length - 1)});
         }
 
-        for (int i = 0; i < mat.length; i++) {
-            int n = helper(mat[i]);
-            map[n].add(i);
-        }
-        
         int[] result = new int[k];
-        for (int i = 0; i <= mat[0].length; i++) {
-            if (map[i].size() > 0) {
-                for (int n : map[i]) {
-                    if (k > 0) {
-                        result[result.length - k--] = n;
-                    }
-                }
-            }
+        for (int i = 0; i < result.length; i++) {
+            result[i] = pq.poll()[0];
         }
 
         return result;
     }
 
-    private int helper(int[] row) {
-        int sum = 0;
-        for (int n : row) {
-            sum += n;
-        }
-        return sum;
+    private int helper(int[] row, int l, int r) {
+        if (l > r) return l;
+
+        int m = l + (r - l) / 2;
+
+        if (row[m] == 1) return helper(row, m + 1, r);
+        else return helper(row, l, m - 1);
     }
 }
