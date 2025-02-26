007968ca4ca59eb987e49b92777c4003a05e3152
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-26 11:04:07 +0800

    Time: 16 ms (96.22%), Space: 57.5 MB (63.12%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -3,15 +3,14 @@ class Solution {
         Set<Integer> set = new HashSet<>();
 
         for (int i = 0; i < nums.length; i++) {
-            if (i > k) {
+            if (set.size() > k) {
                 set.remove(nums[i - k - 1]);
-            }            
-
+            }
             if (!set.add(nums[i])) {
                 return true;
             }
         }
-
+        
         return false;
     }
 }
