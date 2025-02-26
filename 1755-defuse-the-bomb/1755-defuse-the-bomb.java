a7c7ccf010fed48c3e7368fd283afe7bb400d22a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-26 13:20:22 +0800

    Time: 0 ms (100%), Space: 42.6 MB (24.12%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.java
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -2,7 +2,9 @@ class Solution {
     public int[] decrypt(int[] code, int k) {
         int[] arr = new int[code.length];
 
-        if (k == 0) return arr;
+        if (k == 0) {
+            return arr;
+        }
 
         int sum = 0;
         
@@ -10,34 +12,22 @@ class Solution {
             for (int i = 1; i <= k; i++) {
                 sum += code[i];
             }
-            arr[0] = sum;
-
-            for (int i = 1; i < arr.length; i++) {
-                int l = i;
-                int r = i + k;
-                if (l >= code.length) l -= code.length;
-                if (r >= code.length) r -= code.length;
-                
-                sum += code[r] - code[l];
-                arr[i] = sum;
+        } else {
+            for (int i = -1; i >= k; i--) {
+                sum += code[(code.length + i) % code.length]; 
             }
         }
-        
-        if (k < 0) {
-            for (int i = code.length - 1; i >= code.length + k; i--) {
-                sum += code[i];
-            }
-            arr[0] = sum;
+        arr[0] = sum;
 
-            for (int i = arr.length - 1; i >= 0; i--) {
-                int l = i;
-                int r = i + k;
-                if (l < 0) l += code.length;
-                if (r < 0) r += code.length;
-                
-                sum += code[r] - code[l];
-                arr[i] = sum;
+        for (int i = 1; i < code.length; i++) {
+            if (k > 0) {
+                sum -= code[i % code.length];
+                sum += code[(i + k) % code.length];
+            } else {
+                sum -= code[(code.length + i + k - 1) % code.length];
+                sum += code[(i - 1) % code.length];
             }
+            arr[i] = sum;
         }
 
         return arr;
