5d06576d65060ed938861a2d877362a88f0281b4
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-26 13:10:17 +0800

    Time: 0 ms (100%), Space: 42.7 MB (9.72%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.java
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -1,35 +1,45 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
         int[] arr = new int[code.length];
-        for (int i = 0; i < code.length; i++) {
-            arr[i] = helper(code, k, i);
-        }
-        return arr;
-    }
 
-    private int helper(int[] code, int k, int idx) {
+        if (k == 0) return arr;
+
         int sum = 0;
         
-        while (k > 0) {
-            if (++idx == code.length) {
-                idx = 0;
+        if (k > 0) {
+            for (int i = 1; i <= k; i++) {
+                sum += code[i];
             }
-            sum += code[idx];
-            if (--k == 0) {
-                break;
+            arr[0] = sum;
+
+            for (int i = 1; i < arr.length; i++) {
+                int l = i;
+                int r = i + k;
+                if (l >= code.length) l -= code.length;
+                if (r >= code.length) r -= code.length;
+                
+                sum += code[r] - code[l];
+                arr[i] = sum;
             }
         }
         
-        while (k < 0) {
-            if (--idx == -1) {
-                idx = code.length - 1;
+        if (k < 0) {
+            for (int i = code.length - 1; i >= code.length + k; i--) {
+                sum += code[i];
             }
-            sum += code[idx];
-            if (++k == 0) {
-                break;
+            arr[0] = sum;
+
+            for (int i = arr.length - 1; i >= 0; i--) {
+                int l = i;
+                int r = i + k;
+                if (l < 0) l += code.length;
+                if (r < 0) r += code.length;
+                
+                sum += code[r] - code[l];
+                arr[i] = sum;
             }
         }
 
-        return sum;
+        return arr;
     }
 }
