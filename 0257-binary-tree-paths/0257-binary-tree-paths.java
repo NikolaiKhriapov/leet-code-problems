4a014f196708279196ea4ffe9bd33c0a48536fd6
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-27 12:32:20 +0800

    Time: 1 ms (98.8%), Space: 42.4 MB (85.59%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -15,27 +15,26 @@
  */
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
-        List<String> result = new ArrayList<>();
-        helper(root, result, new StringBuilder(""));
-        return result;
+        List<String> list = new ArrayList<>();
+        
+        StringBuilder sb = new StringBuilder();
+
+        helper(root, list, sb);
+        return list;
     }
 
-    private void helper(TreeNode node, List<String> result, StringBuilder sb) {
-        if (sb.length() != 0) {
-            sb.append("->");
-        }
+    private void helper(TreeNode node, List<String> list, StringBuilder sb) {
+        if (sb.length() != 0) sb.append("->");
         sb.append(node.val);
-        
-        if (node.left == null && node.right == null) {
-            result.add(sb.toString());
-        }
 
-        StringBuilder sbB = new StringBuilder(sb.toString());
-        if (node.left != null) {
-            helper(node.left, result, sb);
+        int sbl = sb.length();
+
+        if (node.left == null && node.right == null) {
+            list.add(sb.toString());
         }
-        if (node.right != null) {
-            helper(node.right, result, sbB);
-        }
+        
+        if (node.left != null) helper(node.left, list, sb);
+        sb = sb.delete(sbl, sb.length());
+        if (node.right != null) helper(node.right, list, sb);
     }
 }
