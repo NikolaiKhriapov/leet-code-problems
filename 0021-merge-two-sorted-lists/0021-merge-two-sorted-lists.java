45c6186268f476c2046b49d886e9df0c47e51584
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-27 10:47:04 +0800

    Time: 0 ms (100%), Space: 42.9 MB (23.58%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -24,17 +24,7 @@ class Solution {
             curr = curr.next;
         }
 
-        while (list1 != null) {
-            curr.next = list1;
-            curr = curr.next;
-            list1 = list1.next;
-        }
-
-        while (list2 != null) {
-            curr.next = list2;
-            curr = curr.next;
-            list2 = list2.next;
-        }
+        curr.next = list1 == null ? list2 : list1;
 
         return dummy.next;
     }
