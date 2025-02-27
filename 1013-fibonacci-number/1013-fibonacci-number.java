5d816c4e6b07df8daa023de543fb290c62a86c38
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-27 12:49:00 +0800

    Time: 9 ms (25.15%), Space: 40.4 MB (62.56%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,19 +1,10 @@
 class Solution {
     public int fib(int n) {
-        return fib(n, new HashMap<>());
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        return fib(n - 1) + fib(n - 2);
     }
 
-    public int fib(int n, HashMap<Integer, Integer> memo) {
-        if (n == 0 || n == 1) {
-            return n;
-        }
-
-        if (memo.containsKey(n)) {
-            return memo.get(n);
-        }
-
-        int result = fib(n - 1, memo) + fib(n - 2, memo);
-        memo.put(n, result);
-        return result;
-    }
+    
 }
