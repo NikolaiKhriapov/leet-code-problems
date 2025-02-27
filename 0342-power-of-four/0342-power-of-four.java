27a1930992a12b2c48fd2a17a96aa8e5c26342d6
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-27 12:41:27 +0800

    Time: 0 ms (100%), Space: 40.8 MB (59.92%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,8 +1,11 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if (n == 1) return true;
-        if (n < 1 || n % 4 != 0) return false;
-
-        return isPowerOfFour(n / 4);
+        while (n > 1) {
+            if (n % 4 != 0) {
+                return false;
+            }
+            n /= 4;
+        }
+        return n == 1;
     }
 }
