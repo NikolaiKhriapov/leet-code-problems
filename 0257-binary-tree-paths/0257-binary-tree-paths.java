5a88af68c1d9648ed715160f109f758927692bb9
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-27 12:40:18 +0800

    Time: 1 ms (98.8%), Space: 42.9 MB (37.26%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -1,38 +1,27 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> list = new ArrayList<>();
+
         if (root != null) {
-            helper(root, list, new String());
+            helper(root, list, new StringBuilder());
         }
+
         return list;
     }
 
-    private void helper(TreeNode node, List<String> list, String s) {
-        if (s.length() != 0) {
-            s += "->";
+    private void helper(TreeNode node, List<String> list, StringBuilder sb) {
+        if (sb.length() != 0) {
+            sb.append("->");
         }
-        s += node.val;
+        sb.append(node.val);
 
         if (node.left == null && node.right == null) {
-            list.add(s);
+            list.add(sb.toString());
         }
+        
+        StringBuilder copy = new StringBuilder(sb.toString());
 
-        if (node.left != null) helper(node.left, list, s);
-        if (node.right != null) helper(node.right, list, s);
+        if (node.left != null) helper(node.left, list, sb);
+        if (node.right != null) helper(node.right, list, copy);
     }
 }
