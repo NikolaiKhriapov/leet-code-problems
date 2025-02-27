8acf8e1efdd3f461cd11cd02393c31ead2cf5787
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-27 12:39:34 +0800

    Time: 8 ms (41.96%), Space: 43.1 MB (14.04%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -16,28 +16,23 @@
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> list = new ArrayList<>();
-
-        if (root == null) {
-            return list;
+        if (root != null) {
+            helper(root, list, new String());
         }
-
-        helper(root, list, new StringBuilder());
         return list;
     }
 
-    private void helper(TreeNode node, List<String> list, StringBuilder sb) {
-        if (sb.length() != 0) {
-            sb.append("->");
+    private void helper(TreeNode node, List<String> list, String s) {
+        if (s.length() != 0) {
+            s += "->";
         }
-        sb.append(node.val);
+        s += node.val;
 
         if (node.left == null && node.right == null) {
-            list.add(sb.toString());
+            list.add(s);
         }
-        
-        StringBuilder copy = new StringBuilder(sb.toString());
 
-        if (node.left != null) helper(node.left, list, sb);
-        if (node.right != null) helper(node.right, list, copy);
+        if (node.left != null) helper(node.left, list, s);
+        if (node.right != null) helper(node.right, list, s);
     }
 }
