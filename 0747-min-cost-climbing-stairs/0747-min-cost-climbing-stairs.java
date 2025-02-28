f7b7b3625666ba248d7c9b653a6ecaec90ecc270
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-28 12:31:30 +0800

    Time: 1 ms (38.97%), Space: 43.2 MB (61.68%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -1,16 +1,19 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] map = new int[cost.length + 1];
+        for (int i = 0; i < map.length; i++) {
+            map[i] = -1;
+        }
         return Math.min(helper(cost, 0, map), helper(cost, 1, map));
     }
 
-    private int helper(int[] cost, int step, Map<Integer, Integer> map) {
+    private int helper(int[] cost, int step, int[] map) {
         if (step >= cost.length) return 0;
 
-        if (map.containsKey(step)) return map.get(step);
+        if (map[step] != -1) return map[step];
 
         int result = cost[step] + Math.min(helper(cost, step + 1, map), helper(cost, step + 2, map));
-        map.put(step, result);
+        map[step] = result;
         return result;
     }
 }
