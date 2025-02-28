4f9cf5bfa733661a37412511b3f9f6415854c0dc
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-28 23:50:36 +0800

    Time: 2 ms (94.76%), Space: 46.6 MB (66.14%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -2,17 +2,8 @@ class Solution {
     public int[] countBits(int n) {
         int[] result = new int[n + 1];
         for (int i = 0; i < result.length; i++) {
-            result[i] = helper(i);
+            result[i] = result[i / 2] + (i % 2);
         }
         return result;
-    }
-
-    private int helper(int n) {
-        int count = 0;
-        for (int i = 0; i < 32; i++) {
-            if ((n & 1) == 1) count++;
-            n >>= 1;
-        }
-        return count;
     }
 }
