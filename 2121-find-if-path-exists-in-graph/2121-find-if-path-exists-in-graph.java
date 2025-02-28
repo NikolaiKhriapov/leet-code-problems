0be6490bc3336f6467942291f14aaf544a695191
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-02-28 21:14:01 +0800

    Time: 52 ms (88.67%), Space: 143.5 MB (79.1%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
@@ -2,29 +2,30 @@ class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         if (source == destination) return true;
 
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        List<Integer>[] map = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            map[i] = new ArrayList<>();
+        }
         for (int[] edge : edges) {
-            if (map.containsKey(edge[0])) map.get(edge[0]).add(edge[1]);
-            else map.put(edge[0], new ArrayList<>(List.of(edge[1])));
-            
-            if (map.containsKey(edge[1])) map.get(edge[1]).add(edge[0]);
-            else map.put(edge[1], new ArrayList<>(List.of(edge[0])));
+            map[edge[0]].add(edge[1]);
+            map[edge[1]].add(edge[0]);
         }
 
         boolean[] visited = new boolean[n];
         Queue<Integer> q = new LinkedList<>();
+        
         visited[source] = true;
         q.add(source);
 
         while (!q.isEmpty()) {
-            int node = q.poll();
+            int curr = q.poll();
 
-            if (node == destination) return true;
+            if (curr == destination) return true;
 
-            for (int neighbor : map.get(node)) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    q.add(neighbor);
+            for (int option : map[curr]) {
+                if (!visited[option]) {
+                    visited[option] = true;
+                    q.add(option);
                 }
             }
         }
