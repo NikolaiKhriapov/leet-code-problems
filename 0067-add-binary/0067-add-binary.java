55dbd59c3e121dbb0ee87f2f4a6b22946f1c295c
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-01 18:57:06 +0800

    Time: 3 ms (25.65%), Space: 43.2 MB (13.83%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,32 +1,20 @@
 class Solution {
     public String addBinary(String a, String b) {
-        StringBuilder sb = new StringBuilder();
+        while (a.length() != b.length()) {
+            if (a.length() < b.length()) a = "0" + a;
+            else b = "0" + b;
+        }
 
-        int pa = a.length() - 1;
-        int pb = b.length() - 1;
+        StringBuilder result = new StringBuilder();
         int carry = 0;
-
-        while (pa >= 0 || pb >= 0 || carry > 0) {
-            int na = (pa >= 0) ? a.charAt(pa) - '0' : 0;
-            int nb = (pb >= 0) ? b.charAt(pb) - '0' : 0;
-            int sum = na + nb + carry;
-
-            sb.append(sum % 2);
-            carry = sum / 2;
-
-            pa--;
-            pb--;
+        for (int i = a.length() - 1; i >= 0; i--) {
+            int n = (a.charAt(i) - '0') + (b.charAt(i) - '0') + carry;
+            result.append(n % 2);
+            carry = 0;
+            if (n > 1) carry = 1;
         }
 
-        String result = sb.toString();
-
-        char[] resultArr = result.toCharArray();
-        for (int i = 0; i < resultArr.length / 2; i++) {
-            char temp = resultArr[i];
-            resultArr[i] = resultArr[resultArr.length - i - 1];
-            resultArr[resultArr.length - i - 1] = temp;
-        }
-
-        return new String(resultArr);
+        if (carry == 1) result.append(carry);
+        return result.reverse().toString();
     }
 }
