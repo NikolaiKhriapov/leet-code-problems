482e1d202f5f07ebcadd91688bb33efe683ed76e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-03 13:52:47 +0800

    Time: 3 ms (81.91%), Space: 44.6 MB (82.56%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,30 +1,30 @@
 class Solution {
     public int romanToInt(String s) {
-        int result = 0;
-        int prev = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            int value = getRomanValue(s.charAt(i));
-            if (value >= prev) {
-                result += value;
-            } else {
-                result -= value;
-            }
-            prev = value;
-        }
+        char[] sArr = s.toCharArray();
 
+        int result = 0;
+        for (int i = sArr.length - 1; i >= 0; i--) {
+            int curr = helper(sArr[i]);
+            if (i != sArr.length - 1 && curr < helper(sArr[i + 1])) {
+                result -= curr;
+            } else {
+                result += curr;
+            }
+        }
+        
         return result;
     }
 
-        private int getRomanValue(char c) {
-        switch (c) {
-            case 'I': return 1;
-            case 'V': return 5;
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
-            case 'D': return 500;
-            case 'M': return 1000;
-            default: return 0;
-        }
+    private int helper(char c) {
+        return switch(c) {
+            case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X' -> 10;
+            case 'L' -> 50;
+            case 'C' -> 100;
+            case 'D' -> 500;
+            case 'M' -> 1000;
+            default -> -1;
+        };
     }
 }
