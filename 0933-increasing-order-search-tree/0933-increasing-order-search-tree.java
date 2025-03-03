8e52797e10899e18a5ff9e6d375eff16944daebe
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-03 13:20:47 +0800

    Time: 0 ms (100%), Space: 41.5 MB (23.88%) - LeetHub

--- 0933-increasing-order-search-tree/0933-increasing-order-search-tree.java
+++ 0933-increasing-order-search-tree/0933-increasing-order-search-tree.java
@@ -1,55 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public TreeNode increasingBST(TreeNode root) {
-        List<TreeNode> list = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
+        return helper(root, null);        
+    }
 
-        stack.push(root);
-        while (!stack.empty() || root != null) {
-            while (root != null) {
-                stack.push(root);
-                root = root.left;
-            }
-
-            root = stack.pop();
-            list.add(root);
-            root = root.right;
+    private TreeNode helper(TreeNode node, TreeNode prev) {
+        if (node == null) {
+            return prev;
         }
 
-        for (int i = 0; i < list.size() - 1; i++) {
-            TreeNode node = list.get(i);
-            node.left = null;
-            node.right = list.get(i + 1);
-        }
-        TreeNode last = list.get(list.size() - 1);
-        last.left = null;
-        last.right = null;
-
-        return list.get(0);
+        TreeNode newRoot = helper(node.left, node);
+        
+        node.left = null;
+        node.right = helper(node.right, prev);
+        
+        return newRoot;
     }
 }
-
-// class Solution {
-//     public TreeNode increasingBST(TreeNode root) {
-        
-//         List<TreeNode> list = new ArrayList<>();
-
-//         handleNode(root, list);
-
-//         for (int i = 0; i < list.size() - 1; i++) {
-//             TreeNode node = list.get(i);
-//             node.left = null;
-//             node.right = list.get(i + 1);
-//         }
-//         TreeNode last = list.get(list.size() - 1);
-//         last.left = null;
-//         last.right = null;
-
-//         return list.get(0);
-//     }
-
-//     public void handleNode(TreeNode root, List<TreeNode> list) {
-//         if (root.left != null) handleNode(root.left, list);
-//         list.add(root);
-//         if (root.right != null) handleNode(root.right, list);
-//     }
-// }
