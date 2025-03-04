d7c0bcc20b7987820c607961bbe051b63231cec6
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-04 12:29:51 +0800

    Time: 1 ms (26.67%), Space: 44.5 MB (43.3%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,24 +15,14 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        return helper(root) != -1;
+        if (root == null) return true;
+        if (!isBalanced(root.left) || !isBalanced(root.right)) return false;
+
+        return Math.abs(helper(root.left) - helper(root.right)) <= 1;
     }
 
     private int helper(TreeNode node) {
-        if (node == null) {
-            return 0;
-        }
-
-        int left = helper(node.left);
-        int right = helper(node.right);
-
-        if (left == -1 || right == -1) {
-            return -1;
-        }
-        if (Math.abs(left - right) > 1) {
-            return -1;
-        }
-
-        return 1 + Math.max(left, right);
+        if (node == null) return 0;
+        return 1 + Math.max(helper(node.left), helper(node.right));
     }
 }
