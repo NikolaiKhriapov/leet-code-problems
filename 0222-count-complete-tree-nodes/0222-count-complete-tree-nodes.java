479649f22472023daab87c54b195de7b57a366f1
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-05 10:58:23 +0800

    Time: 0 ms (100%), Space: 47.5 MB (85.02%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -1,34 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public int countNodes(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-
-        int leftHeight = getLeftHeight(root);
-        int rightHeight = getRightHeight(root);
-
-        if (leftHeight == rightHeight) {
-            return (int) Math.pow(2, leftHeight) - 1;
+        int hl = getHeightLeft(root);
+        int hr = getHeightRight(root);
+        if (hl == hr) {
+            return (int) Math.pow(2, hr) - 1;
         }
         
         return 1 + countNodes(root.left) + countNodes(root.right);
     }
 
-    private int getLeftHeight(TreeNode node) {
-        int height = 0;
+    private int getHeightLeft(TreeNode node) {
+        int h = 0;
         while (node != null) {
+            h++;
             node = node.left;
-            height++;
         }
-        return height;
+        return h;
     }
 
-    private int getRightHeight(TreeNode node) {
-        int height = 0;
+    private int getHeightRight(TreeNode node) {
+        int h = 0;
         while (node != null) {
+            h++;
             node = node.right;
-            height++;
         }
-        return height;
+        return h;
     }
 }
