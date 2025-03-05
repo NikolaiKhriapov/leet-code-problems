141f2e7f696c4ff7cc9fbbe229b0a8dfb92b8445
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-05 11:24:05 +0800

    Time: 3 ms (98.11%), Space: 44.3 MB (55.14%) - LeetHub

--- 1486-find-the-distance-value-between-two-arrays/1486-find-the-distance-value-between-two-arrays.java
+++ 1486-find-the-distance-value-between-two-arrays/1486-find-the-distance-value-between-two-arrays.java
@@ -2,13 +2,14 @@ class Solution {
     public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
         Arrays.sort(arr2);
 
-        int counter = 0;
+        int result = 0;
         for (int n : arr1) {
             if (helper(arr2, n, d, 0, arr2.length - 1)) {
-                counter++;
+                result++;
             }
         }
-        return counter;        
+
+        return result;
     }
 
     private boolean helper(int[] arr, int n, int d, int l, int r) {
@@ -16,8 +17,9 @@ class Solution {
 
         int m = l + (r - l) / 2;
 
-        if (Math.abs(n - arr[m]) <= d) return false;
-        if (arr[m] < n) return helper(arr, n, d, m + 1, r);
-        return helper(arr, n, d, l, m - 1);
+        if (arr[m] > n + d) return helper(arr, n, d, l, m - 1);
+        if (arr[m] < n - d) return helper(arr, n, d, m + 1, r);
+
+        return false;
     }
 }
