e8631b7f0fcedc13cd62452f584c6ff3fe346a94
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-06 09:54:51 +0800

    Time: 1 ms (99.53%), Space: 44.9 MB (39.96%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
         Queue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            int d = a[1] - b[1];
-            if (d > 0) return 1;
-            else if (d < 0) return -1;
-            else return a[0] - b[0];
+            int x = a[1] - b[1];
+            if (x == 0) return a[0] - b[0];
+            return x;
         });
+
         for (int i = 0; i < mat.length; i++) {
             pq.add(new int[] {i, helper(mat[i], 0, mat[i].length - 1)});
         }
-
+        
         int[] result = new int[k];
-        for (int i = 0; i < result.length; i++) {
+        for (int i = 0; i < k; i++) {
             result[i] = pq.poll()[0];
         }
 
@@ -24,6 +24,7 @@ class Solution {
         int m = l + (r - l) / 2;
 
         if (row[m] == 1) return helper(row, m + 1, r);
-        else return helper(row, l, m - 1);
+        if (row[m] == 0 && (m == 0 || row[m - 1] == 1)) return m;
+        return helper(row, l, m - 1);
     }
 }
