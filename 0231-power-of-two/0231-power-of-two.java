913b16a53a82f2fc9f076ead2f274c52c5ffc60d
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-07 13:20:36 +0800

    Time: 1 ms (41.43%), Space: 40.9 MB (62.42%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,7 +1,9 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        while (n > 1) {
-            if (n % 2 != 0) return false;
+        while (n > 0 && n != 1) {
+            if (n % 2 != 0) {
+                return false;
+            }
             n /= 2;
         }
         return n == 1;
