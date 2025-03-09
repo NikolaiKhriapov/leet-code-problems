ee54b614f17cd42279a7dc1ef7325cfe8a5bd351
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-09 14:03:59 +0800

    Time: 0 ms (100%), Space: 40.8 MB (20.42%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.java
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.java
@@ -4,14 +4,10 @@ class Solution {
         int prev = -1;
         for (int i = 0; i < 32; i++) {
             int last = (n & 1);
-            if (!isTrailing) {
-                if (last != prev) {
-                    prev = last;
-                } else {
-                    if (last != 0) return false;
-                    isTrailing = true;
-                }
+            if (!isTrailing && last != prev) {
+                prev = last;
             } else {
+                isTrailing = true;
                 if (last != 0) return false;
             }
             n >>= 1;
