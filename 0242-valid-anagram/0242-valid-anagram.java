18fd741131a4fda8b5fbfc8220ad1feb741e92ac
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-09 16:37:52 +0800

    Time: 1 ms (99.88%), Space: 43.4 MB (69.59%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
         int[] count = new int[26];
 
         for (char c : s.toCharArray()) {
