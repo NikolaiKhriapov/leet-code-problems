6e44ceaab57341f0908f3f8b1a866ef8ea31c561
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-09 15:29:26 +0800

    Time: 14 ms (42.47%), Space: 46.4 MB (46.58%) - LeetHub

--- 1094-matrix-cells-in-distance-order/1094-matrix-cells-in-distance-order.java
+++ 1094-matrix-cells-in-distance-order/1094-matrix-cells-in-distance-order.java
@@ -1,20 +1,18 @@
 class Solution {
     public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
-        Queue<int[]> pq = new PriorityQueue<>((a, b) -> 
+        int[][] result = new int[rows * cols][2];
+
+        int p = 0;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                result[p++] = new int[] {r, c};
+            }
+        }
+        
+        Arrays.sort(result, (a, b) -> 
             (Math.abs(a[0] - rCenter) + Math.abs(a[1] - cCenter)) - 
             (Math.abs(b[0] - rCenter) + Math.abs(b[1] - cCenter))
         );
-
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                pq.add(new int[] {r, c});
-            }
-        }
-
-        int[][] result = new int[rows * cols][2];
-        for (int i = 0; i < rows * cols; i++) {
-            result[i] = pq.poll();
-        }
 
         return result;
     }
