7f3dfc47dc9b9b833bb6bef7d4a6a617a77a3bac
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-09 16:37:01 +0800

    Time: 1 ms (99.88%), Space: 43.4 MB (69.59%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-
         int[] count = new int[26];
 
         for (char c : s.toCharArray()) {
             count[c - 'a']++;
         }
-        
+
         for (char c : t.toCharArray()) {
-            if (count[c - 'a']-- <= 0) {
+            if (count[c - 'a']-- == 0) {
                 return false;
             }
         }
-
+        
         for (int n : count) {
             if (n != 0) {
                 return false;
