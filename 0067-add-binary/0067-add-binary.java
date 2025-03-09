571c6b7849fb4e9d5068a4d89cd6578d229ae846
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-09 14:22:19 +0800

    Time: 2 ms (45.56%), Space: 43.4 MB (12.84%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -7,9 +7,12 @@ class Solution {
         while (a.length() < l) a = "0" + a;
         while (b.length() < l) b = "0" + b;
 
+        char[] aArr = a.toCharArray();
+        char[] bArr = b.toCharArray();
+
         int carry = 0;
         for (int i = l - 1; i >= 0; i--) {
-            int bit = (a.charAt(i) - '0') + (b.charAt(i) - '0') + carry;
+            int bit = (aArr[i] - '0') + (bArr[i] - '0') + carry;
             sb.append(bit % 2);
             carry = bit / 2;
         }
