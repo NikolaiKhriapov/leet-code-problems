7d985b046c88e8f3d073804b7fd0a2744498784e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-13 01:38:46 +0800

    Time: 0 ms (100%), Space: 41.5 MB (35.12%) - LeetHub

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,21 +1,16 @@
 class Solution {
     public int specialArray(int[] nums) {
-        int[] arr = new int[nums.length + 1];
-        
+        int[] count = new int[nums.length + 1];
+
         for (int n : nums) {
-            if (n >= arr.length - 1) {
-                arr[arr.length - 1]++;
-            } else {
-                arr[n]++;
-            }
+            if (n >= nums.length) count[nums.length]++;
+            else count[n]++;
         }
         
         int sum = 0;
-        for (int i = arr.length - 1; i >= 0; i --) {
-            sum += arr[i];
-            if (sum == i) {
-                return i;
-            }
+        for (int i = nums.length; i >= 0; i--) {
+            sum += count[i];
+            if (sum == i) return i;
         }
 
         return -1;
