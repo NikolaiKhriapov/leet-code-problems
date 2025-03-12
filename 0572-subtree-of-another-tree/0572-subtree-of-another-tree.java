cc457cd6d141b2355ee632110c19648471e5ff48
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-13 00:36:52 +0800

    Time: 2 ms (97.34%), Space: 44.1 MB (92.23%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -1,25 +1,13 @@
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        
-        while (!q.isEmpty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode curr = q.poll();
-                if (isSubroot(curr, subRoot)) return true;
-                if (curr.left != null) q.add(curr.left);
-                if (curr.right != null) q.add(curr.right);
-            }
-        }
-
-        return false;
+        if (root == null) return false;
+        if (isSubroot(root, subRoot)) return true;
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
 
     private boolean isSubroot(TreeNode node, TreeNode subRoot) {
         if (node == null && subRoot == null) return true;
         if (node == null || subRoot == null) return false;
-
         return node.val == subRoot.val && isSubroot(node.left, subRoot.left) && isSubroot(node.right, subRoot.right);
     }
 }
