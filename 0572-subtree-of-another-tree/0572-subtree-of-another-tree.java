603067fa8b0b2b9d34ee14f8c610fb91af276147
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-13 00:35:07 +0800

    Time: 4 ms (11.85%), Space: 44.8 MB (21.83%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -1,32 +1,25 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (root == null && subRoot == null) return true;
-        if (root == null || subRoot == null) return false;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = q.poll();
+                if (isSubroot(curr, subRoot)) return true;
+                if (curr.left != null) q.add(curr.left);
+                if (curr.right != null) q.add(curr.right);
+            }
+        }
 
-        if (isSameTree(root, subRoot)) return true;
-
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);        
+        return false;
     }
 
-    private boolean isSameTree(TreeNode node1, TreeNode node2) {
-        if (node1 == null && node2 == null) return true;
-        if (node1 == null || node2 == null) return false;
+    private boolean isSubroot(TreeNode node, TreeNode subRoot) {
+        if (node == null && subRoot == null) return true;
+        if (node == null || subRoot == null) return false;
 
-        return node1.val == node2.val && isSameTree(node1.left, node2.left) && isSameTree(node1.right, node2.right);
+        return node.val == subRoot.val && isSubroot(node.left, subRoot.left) && isSubroot(node.right, subRoot.right);
     }
 }
