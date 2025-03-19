466cc091177f9a8e69a307306ab3e42652057740
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-19 13:34:48 +0800

    Time: 2 ms (76.18%), Space: 44.6 MB (86.08%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
@@ -3,15 +3,11 @@ class Solution {
         int[][] arr = new int[mat.length][1];
 
         for (int i = 0; i < mat.length; i++) {
-            int countSoldiers = countSoldiers(mat[i]);
+            int countSoldiers = countSoldiers(mat[i], 0, mat[i].length - 1);
             arr[i] = new int[] {i, countSoldiers};
         }
         
-        Arrays.sort(arr, (a, b) -> {
-            int diff = a[1] - b[1];
-            if (diff != 0) return diff;
-            return a[0] - b[0];
-        });
+        Arrays.sort(arr, (a, b) -> (a[1] - b[1] != 0) ? (a[1] - b[1]) : (a[0] - b[0]));
 
         int[] result = new int[k];
         for (int i = 0; i < result.length; i++) {
@@ -21,12 +17,12 @@ class Solution {
         return result;
     }
 
-    private int countSoldiers(int[] arr) {
-        int count = 0;
-        for (int n : arr) {
-            if (n == 1) count++;
-            else break;
-        }
-        return count;
+    private int countSoldiers(int[] arr, int l, int r) {
+        if (l > r) return l;
+
+        int m = l + (r - l) / 2;
+
+        if (arr[m] == 1) return countSoldiers(arr, m + 1, r);
+        return countSoldiers(arr, l, m - 1);
     }
 }
