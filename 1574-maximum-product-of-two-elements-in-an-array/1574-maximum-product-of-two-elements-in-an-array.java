859d2931d6abaf7a75798887cf4288c6bdc4654e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-19 13:39:43 +0800

    Time: 0 ms (100%), Space: 42.5 MB (89.95%) - LeetHub

--- 1574-maximum-product-of-two-elements-in-an-array/1574-maximum-product-of-two-elements-in-an-array.java
+++ 1574-maximum-product-of-two-elements-in-an-array/1574-maximum-product-of-two-elements-in-an-array.java
@@ -1,8 +1,8 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int max1 = 0;
-        int max2 = 0;
-
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        
         for (int n : nums) {
             if (n > max1) {
                 max2 = max1;
@@ -13,6 +13,5 @@ class Solution {
         }
 
         return (max1 - 1) * (max2 - 1);
-        
     }
 }
