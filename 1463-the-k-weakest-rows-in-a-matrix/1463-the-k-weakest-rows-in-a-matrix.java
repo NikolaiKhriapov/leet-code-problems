6ae423101d318a93ebc21abf1407234079b4368c
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-19 13:28:47 +0800

    Time: 2 ms (76.18%), Space: 45 MB (29.39%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
@@ -1,30 +1,32 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-        Queue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            int x = a[1] - b[1];
-            if (x == 0) return a[0] - b[0];
-            return x;
-        });
+        int[][] arr = new int[mat.length][1];
 
         for (int i = 0; i < mat.length; i++) {
-            pq.add(new int[] {i, helper(mat[i], 0, mat[i].length - 1)});
+            int countSoldiers = countSoldiers(mat[i]);
+            arr[i] = new int[] {i, countSoldiers};
         }
         
+        Arrays.sort(arr, (a, b) -> {
+            int diff = a[1] - b[1];
+            if (diff != 0) return diff;
+            return a[0] - b[0];
+        });
+
         int[] result = new int[k];
-        for (int i = 0; i < k; i++) {
-            result[i] = pq.poll()[0];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = arr[i][0];
         }
 
         return result;
     }
 
-    private int helper(int[] row, int l, int r) {
-        if (l > r) return l;
-
-        int m = l + (r - l) / 2;
-
-        if (row[m] == 1) return helper(row, m + 1, r);
-        if (row[m] == 0 && (m == 0 || row[m - 1] == 1)) return m;
-        return helper(row, l, m - 1);
+    private int countSoldiers(int[] arr) {
+        int count = 0;
+        for (int n : arr) {
+            if (n == 1) count++;
+            else break;
+        }
+        return count;
     }
 }
