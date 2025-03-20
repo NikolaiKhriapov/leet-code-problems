11ec04068af5c89ac045cf2e21f77684bdcab3bf
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-20 13:53:02 +0800

    Time: 1 ms (99.65%), Space: 42.5 MB (31.34%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,21 +1,25 @@
 class Solution {
     public String addBinary(String a, String b) {
+        int la = a.length();
+        int lb = b.length();
+
+        int l = la >= lb ? la : lb;
+        
         StringBuilder sb = new StringBuilder();
 
-        int pa = a.length() - 1;
-        int pb = b.length() - 1;
-
         int carry = 0;
-        while (pa >= 0 || pb >= 0) {
-            int ba = (pa >= 0) ? (a.charAt(pa) - '0') : 0;
-            int bb = (pb >= 0) ? (b.charAt(pb) - '0') : 0;
-            int bit = ba + bb + carry;
-            sb.append(bit % 2);
-            carry = bit / 2;
-            pa--;
-            pb--;
+        for (int i = 0; i < l; i++) {
+            int ca = (la > i) ? (a.charAt(la - 1 - i) - '0') : 0;
+            int cb = (lb > i) ? (b.charAt(lb - 1 - i) - '0') : 0;
+
+            int sum = ca + cb + carry;
+            sb.append(sum % 2);
+            carry = sum / 2;
         }
-        if (carry == 1) sb.append(carry);
+
+        if (carry == 1) {
+            sb.append(carry);
+        }
 
         return sb.reverse().toString();
     }
