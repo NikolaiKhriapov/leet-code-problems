ba2196d25b6a8859acf711c64b6f3b44c9e7b973
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-20 10:46:13 +0800

    Time: 2 ms (80.86%), Space: 43.2 MB (32.91%) - LeetHub

--- 1873-longest-nice-substring/1873-longest-nice-substring.java
+++ 1873-longest-nice-substring/1873-longest-nice-substring.java
@@ -1,20 +1,32 @@
 class Solution {
     public String longestNiceSubstring(String s) {
-        Set<Character> set = new HashSet<>();
-        for (char c : s.toCharArray()) {
-            set.add(c);
-        }
-        
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (!set.contains(Character.toLowerCase(c)) || !set.contains(Character.toUpperCase(c))) {
-                String left = longestNiceSubstring(s.substring(0, i));
-                String right = longestNiceSubstring(s.substring(i + 1, s.length()));
+        int sl = s.length();
 
-                s = (left.length() >= right.length()) ? left : right;
-            }
+        int p = isNice(s);
+        if (p != -1) {
+            String l = longestNiceSubstring(s.substring(0, p));
+            String r = longestNiceSubstring(s.substring(p + 1, sl));
+            s = l.length() >= r.length() ? l : r;
         }
 
         return s;
+    }
+
+    private int isNice(String s) {
+        Set<Character> set = new HashSet<>();
+        
+        for (char c : s.toCharArray()) {
+            set.add(c);
+        }
+
+        int sl = s.length();
+        for (int i = 0; i < sl; i++) {
+            char c = s.charAt(i);
+            if (!set.contains(Character.toLowerCase(c)) || !set.contains(Character.toUpperCase(c))) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
