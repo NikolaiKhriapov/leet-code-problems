dc730515d6f8022f5b3bc258c1106b795663d406
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-20 13:29:46 +0800

    Time: 0 ms (100%), Space: 40.9 MB (21.6%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,19 +1,10 @@
 class Solution {
     public int findComplement(int num) {
-        int mask = 1;
-
-        int temp = num;
-        int h = -1;
-        for (int i = 0; i < 32; i++) {
-            if (temp % 2 == 1) h = i;
-            temp >>= 1;
-        }
-
-        for (int i = 0; i < h; i++) {
+        long mask = 1;
+        while (mask <= num) {
             mask <<= 1;
-            mask += 1;
         }
-
-        return mask - num;
+        mask -= 1;
+        return (int) mask - num;
     }
 }
