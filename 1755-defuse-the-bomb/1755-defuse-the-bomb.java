b6db8fcd059f60fd28a79e79a303fdae81e9ff88
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-21 13:39:05 +0800

    Time: 0 ms (100%), Space: 42.1 MB (85.41%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.java
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -1,30 +1,33 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
-        int cl = code.length;
-        int[] result = new int[cl];
-
+        int[] result = new int[code.length];
+        
         if (k == 0) return result;
 
-        int sum = 0;
         int start;
         int end;
-
         if (k > 0) {
             start = 1;
             end = k;
         } else {
-            start = cl + k;
-            end = cl - 1;
+            start = code.length + k;
+            end = code.length - 1;
         }
 
+        int sum = 0;
         for (int i = start; i <= end; i++) {
-            sum += code[i % cl];
+            sum += code[i];
         }
 
-        for (int i = 0; i < cl; i++) {
+        for (int i = 0; i < code.length; i++) {
             result[i] = sum;
-            sum -= code[start++ % cl];
-            sum += code[++end % cl];
+            if (k > 0) {
+                sum -= code[(i + 1) % code.length];
+                sum += code[(i + 1 + k) % code.length];
+            } else {
+                sum -= code[(i + code.length + k) % code.length];
+                sum += code[i];
+            }
         }
 
         return result;
