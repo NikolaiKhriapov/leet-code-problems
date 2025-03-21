22589d1c650a64ef1c1ebf00bec0cff13f28d0ef
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-21 12:04:39 +0800

    Time: 2 ms (97.28%), Space: 44.3 MB (81.76%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -1,13 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (root == null) return false;
-        if (isSubroot(root, subRoot)) return true;
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        if (root == null && subRoot == null) return true;
+        if (root == null || subRoot == null) return false;
+        return isSameTree(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
 
-    private boolean isSubroot(TreeNode node, TreeNode subRoot) {
-        if (node == null && subRoot == null) return true;
-        if (node == null || subRoot == null) return false;
-        return node.val == subRoot.val && isSubroot(node.left, subRoot.left) && isSubroot(node.right, subRoot.right);
+    private boolean isSameTree(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null) return false;
+        return root1.val == root2.val && isSameTree(root1.left, root2.left) && isSameTree(root1.right, root2.right);
     }
 }
