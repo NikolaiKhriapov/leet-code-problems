3c9bbff371784b159fe5139c1da065d3166adf22
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-03-23 19:34:54 +0800

    Time: 8 ms (79.57%), Space: 47 MB (5.46%) - LeetHub

--- 1094-matrix-cells-in-distance-order/1094-matrix-cells-in-distance-order.java
+++ 1094-matrix-cells-in-distance-order/1094-matrix-cells-in-distance-order.java
@@ -1,26 +1,25 @@
 class Solution {
     public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
-        List<int[]>[] listArr = new List[rows + cols];
-
-        for (int i = 0; i < listArr.length; i++) {
-            listArr[i] = new ArrayList<>();
+        List<int[]>[] arrList = new List[rows + cols];
+        for (int i = 0; i < arrList.length; i++) {
+            arrList[i] = new ArrayList<>();
         }
 
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
                 int dist = Math.abs(r - rCenter) + Math.abs(c - cCenter);
-                listArr[dist].add(new int[] {r, c});
+                arrList[dist].add(new int[] {r, c});
             }
         }
 
         int[][] result = new int[rows * cols][2];
         int idx = 0;
-        for (List<int[]> list : listArr) {
+        for (List<int[]> list : arrList) {
             for (int[] el : list) {
                 result[idx++] = el;
             }
         }
-        
+
         return result;
     }
 }
