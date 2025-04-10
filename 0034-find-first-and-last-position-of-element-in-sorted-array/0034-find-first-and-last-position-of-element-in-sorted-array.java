d2a87e1d4773ae9c5d10bee769146ccf444ce99f
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-10 21:06:25 +0800

    Time: 1 ms (20.26%), Space: 45.9 MB (84.37%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,50 +1,22 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int l = binarySearchLeft(nums, target, 0, nums.length - 1);
-        int r = binarySearchRight(nums, target, l, nums.length - 1);
 
-        return new int[] { l, r };
-    }
+        int l = -1;
+        int r = -1;
 
-    public int binarySearchLeft(int[] nums, int target, int l, int r) {
-        if (l > r) {
-            return -1;
-        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > target) break;
 
-        int m = l + (r - l) / 2;
-
-        if (nums[m] == target) {
-            if (m == 0 || nums[m - 1] < target) {
-                return m;
-            } else {
-                return binarySearchLeft(nums, target, l, m - 1);
+            if (nums[i] == target) {
+                if (l == -1) {
+                    l = i;
+                    r = i;
+                } else {
+                    r = i;
+                }
             }
         }
-
-        if (nums[m] > target) {
-            return binarySearchLeft(nums, target, l, m - 1);
-        } else return binarySearchLeft(nums, target, m + 1, r);
-    }
-
-    public int binarySearchRight(int[] nums, int target, int l, int r) {
-        if (l > r || l == -1) {
-            return -1;
-        }
-
-        int m = l + (r - l) / 2;
-
-        if (nums[m] == target) {
-            if (m == nums.length - 1 || nums[m + 1] > target) {
-                return m;
-            } else {
-                return binarySearchRight(nums, target, m + 1, r);
-            }
-        }
-
-        if (nums[m] > target) {
-            return binarySearchRight(nums, target, l, m - 1);
-        } else {
-            return binarySearchRight(nums, target, m + 1, r);
-        }
+        
+        return new int[] {l, r};
     }
 }
