7e05218716bfa7d14885e95eccb07ffd12c86c60
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-10 21:31:09 +0800

    Time: 0 ms (100%), Space: 46.3 MB (8.94%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,11 +1,7 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int l = binarySearchLeft(nums, target, 0, nums.length - 1);
-        
-        int r = -1;
-        if (l != -1) {
-            r = binarySearchRight(nums, target, l, nums.length - 1);
-        }
+        int r = binarySearchRight(nums, target, l, nums.length - 1);
 
         return new int[] {l, r};
     }
@@ -16,8 +12,11 @@ class Solution {
         int m = l + (r - l) / 2;
         
         if (nums[m] == target) {
-            if (m == 0 || nums[m - 1] < nums[m]) return m;
-            else return binarySearchLeft(nums, target, l, m - 1);
+            if (m == 0 || nums[m - 1] < nums[m]) {
+                return m;
+            } else {
+                return binarySearchLeft(nums, target, l, m - 1);
+            }
         }
         
         if (nums[m] > target) {
@@ -28,11 +27,16 @@ class Solution {
     }
 
     private int binarySearchRight(int[] nums, int target, int l, int r) {
+        if (l == -1) return -1;
+
         int m = l + (r - l) / 2;
         
         if (nums[m] == target) {
-            if (m == nums.length - 1 || nums[m + 1] > nums[m]) return m;
-            else return binarySearchRight(nums, target, m + 1, r);
+            if (m == nums.length - 1 || nums[m + 1] > nums[m]) {
+                return m;
+            } else {
+                return binarySearchRight(nums, target, m + 1, r);
+            }
         }
         
         return binarySearchRight(nums, target, l, m - 1);
