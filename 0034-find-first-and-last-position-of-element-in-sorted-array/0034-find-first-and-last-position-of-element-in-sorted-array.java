29fa15041040576cd2949f0fabf02286417f87ac
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-10 21:24:10 +0800

    Time: 0 ms (100%), Space: 45.9 MB (68.24%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,22 +1,43 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-
         int l = -1;
         int r = -1;
+        
+        l = binarySearchLeft(nums, target, 0, nums.length - 1);
+        if (l != -1) {
+            r = binarySearchRight(nums, target, l, nums.length - 1);
+        }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > target) break;
+        return new int[] {l, r};
+    }
 
-            if (nums[i] == target) {
-                if (l == -1) {
-                    l = i;
-                    r = i;
-                } else {
-                    r = i;
-                }
-            }
+    private int binarySearchLeft(int[] nums, int target, int l, int r) {
+        if (l > r) return -1;
+
+        int m = l + (r - l) / 2;
+        
+        if (nums[m] == target) {
+            if (m == 0 || nums[m - 1] < nums[m]) return m;
+            else return binarySearchLeft(nums, target, l, m - 1);
         }
         
-        return new int[] {l, r};
+        if (nums[m] > target) {
+            return binarySearchLeft(nums, target, l, m - 1);
+        } else {
+            return binarySearchLeft(nums, target, m + 1, r);
+        }
+    }
+
+    private int binarySearchRight(int[] nums, int target, int l, int r) {
+        if (l > r) return -1;
+
+        int m = l + (r - l) / 2;
+        
+        if (nums[m] == target) {
+            if (m == nums.length - 1 || nums[m + 1] > nums[m]) return m;
+            else return binarySearchRight(nums, target, m + 1, r);
+        }
+        
+        return binarySearchRight(nums, target, l, m - 1);
     }
 }
