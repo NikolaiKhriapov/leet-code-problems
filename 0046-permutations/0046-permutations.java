df09f63dc524b6de8636c551c8d23ab0bebc1cc1
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-11 20:29:39 +0800

    Time: 2 ms (39.33%), Space: 44.5 MB (64.67%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,23 +1,33 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), nums);
+        permute(nums, result, new ArrayList<>());
         return result;
     }
 
-    private void backtrack(List<List<Integer>> result, List<Integer> curr, int[] nums) {
-        if (curr.size() == nums.length) {
-            result.add(new ArrayList<>(curr));
-        } else {
-            for (int n : nums) {
-                if (curr.contains(n)) {
-                    continue;
-                }
+    private void permute(int[] nums, List<List<Integer>> result, List<Integer> curr) {
+        for (int i = 0; i < nums.length; i++) {
+            if (curr.contains(nums[i])) continue;
 
-                curr.add(n);
-                backtrack(result, curr, nums);
-                curr.remove(curr.size() - 1);
+            List<Integer> temp = copyList(curr);
+
+            curr.add(nums[i]);
+
+            if (curr.size() == nums.length) {
+                result.add(curr);
+            } else {
+                permute(nums, result, curr);
             }
+
+            curr = temp;
         }
+    }
+
+    private List<Integer> copyList(List<Integer> original) {
+        List<Integer> copy = new ArrayList<>();
+        for (int n : original) {
+            copy.add(n);
+        }
+        return copy;
     }
 }
