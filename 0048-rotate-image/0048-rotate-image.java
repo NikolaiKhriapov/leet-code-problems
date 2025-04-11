62f75acfbf820af678f23efbf2494b9af8046f9e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-11 21:05:19 +0800

    Time: 0 ms (100%), Space: 42.5 MB (15.03%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,26 +1,32 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        transposeMatrix(matrix);
-        reverseRows(matrix);
+        if (matrix == null || matrix.length == 0) return;
+
+        transpose(matrix);
+        mirror(matrix);
     }
 
-    private void transposeMatrix(int[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = i; j < matrix[i].length; j++) {
-                int tmp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = tmp;
+    private void transpose(int[][] matrix) {
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = r; c < matrix[0].length; c++) {
+                int temp = matrix[r][c];
+                matrix[r][c] = matrix[c][r];
+                matrix[c][r] = temp;
             }
         }
     }
 
-    private void reverseRows(int[][] matrix) {
+    private void mirror(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length / 2; j++) {
-                int tmp = matrix[i][j];
-                matrix[i][j] = matrix[i][matrix[i].length - 1 - j];
-                matrix[i][matrix[i].length - 1 - j] = tmp;
-            }
+            reverseRow(matrix[i]);
+        }
+    }
+
+    private void reverseRow(int[] row) {
+        for (int i = 0; i < row.length / 2; i++) {
+            int temp = row[i];
+            row[i] = row[row.length - 1 - i];
+            row[row.length - 1 - i] = temp;
         }
     }
 }
