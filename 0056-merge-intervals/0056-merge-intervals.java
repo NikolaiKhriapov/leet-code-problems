d118156ec872b6a0fb75b793697f6b8c9afa134c
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-14 21:53:57 +0800

    Time: 8 ms (86.68%), Space: 46.6 MB (71.36%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,26 +1,43 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return new int[0][0];
+        }
+
+        List<int[]> list = new ArrayList<>();
+        boolean[] used = new boolean[intervals.length];
+
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
-        List<int[]> resultList = new ArrayList<>();
-        resultList.add(intervals[0]);
-        
-        for (int i = 1; i < intervals.length; i++) {
-            int[] lastInterval = resultList.get(resultList.size() - 1);
-            int[] currInterval = intervals[i];
-
-            if (currInterval[0] <= lastInterval[1]) {
-                lastInterval[1] = Math.max(lastInterval[1], currInterval[1]);
-            } else {
-                resultList.add(currInterval);
+        for (int i = 0; i < intervals.length; i++) {
+            int[] curr = intervals[i];
+            
+            if (used[i]) {
+                continue;
             }
+            used[i] = true;
+
+            for (int j = i + 1; j < intervals.length; j++) {
+                int[] next = intervals[j];
+                if (curr[1] < next[0]) {
+                    break;
+                } else {
+                    curr[0] = Math.min(curr[0], next[0]);
+                    curr[1] = Math.max(curr[1], next[1]);
+                    used[j] = true;
+                }
+            }
+            list.add(curr);
         }
         
-        int[][] result = new int[resultList.size()][2];
-        for (int i = 0; i < resultList.size(); i++) {
-            result[i] = resultList.get(i);
-        }
+        return listToArray(list);
+    }
 
+    private int[][] listToArray(List<int[]> list) {
+        int[][] result = new int[list.size()][2];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
         return result;
     }
 }
