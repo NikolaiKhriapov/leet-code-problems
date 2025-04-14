8fb8abdeb49ee3bb5d67baa013eb65ff078d1e50
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-14 21:20:22 +0800

    Time: 356 ms (6.42%), Space: 45.6 MB (84.64%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,19 +1,28 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int farthest = 0;
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (i > farthest) {
-                return false;
-            }
+        return canJump(nums, 0, new boolean[nums.length]);
+    }
 
-            farthest = Math.max(farthest, i + nums[i]);
+    private boolean canJump(int[] nums, int index, boolean[] visited) {
+        if (index >= nums.length - 1) {
+            return true;
+        }
 
-            if (farthest >= nums.length - 1) {
+        if (visited[index]) {
+            return false;
+        }
+
+        for (int i = 1; i <= nums[index]; i++) {
+            if (canJump(nums, index + i, visited)) {
                 return true;
             }
         }
-
+        
+        visited[index] = true;
         return false;
     }
 }
