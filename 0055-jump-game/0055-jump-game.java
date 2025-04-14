b6407cfd8226cadc80c63726d05800790d235a24
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-14 21:31:13 +0800

    Time: 3 ms (43.27%), Space: 45.7 MB (71.06%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -4,25 +4,13 @@ class Solution {
             return false;
         }
 
-        return canJump(nums, 0, new boolean[nums.length]);
-    }
-
-    private boolean canJump(int[] nums, int index, boolean[] visited) {
-        if (index >= nums.length - 1) {
-            return true;
-        }
-
-        if (visited[index]) {
-            return false;
-        }
-
-        for (int i = 1; i <= nums[index]; i++) {
-            if (canJump(nums, index + i, visited)) {
-                return true;
+        int reachable = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (reachable < i) {
+                return false;
             }
+            reachable = Math.max(reachable, i + nums[i]);
         }
-        
-        visited[index] = true;
-        return false;
+        return true;
     }
 }
