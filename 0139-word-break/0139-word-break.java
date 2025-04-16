f015a192cf26b6e74c50ec136737dd7e9277170a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-16 12:26:35 +0800

    Time: 28 ms (5.8%), Space: 44.5 MB (46.57%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return wordBreak(s, wordDict, new HashMap<>());
+    }
+
+    private boolean wordBreak(String s, List<String> wordDict, Map<String, Boolean> memo) {
+        if (memo.containsKey(s)) {
+            return memo.get(s);
+        }
+
+        for (String word : wordDict) {
+            if (s.contains(word)) {
+                int index = s.indexOf(word);
+                boolean leftWordBreaks = wordBreak(s.substring(0, index), wordDict, memo);
+                boolean rightWordBreaks = wordBreak(s.substring(index + word.length(), s.length()), wordDict, memo);
+                if (leftWordBreaks && rightWordBreaks) {
+                    return true;
+                }
+            }
+        }
+
+        boolean result = s.length() == 0;
+        memo.put(s, result);
+        return result;
+    }
+}
