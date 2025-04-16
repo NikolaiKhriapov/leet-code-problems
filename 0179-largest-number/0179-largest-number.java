c747b455f3f3e0ab2b42c4e956fd9166651ab913
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-16 22:00:32 +0800

    Time: 6 ms (53.46%), Space: 43.2 MB (60.63%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        String[] strs = new String[nums.length];
+
+        boolean isMaxZero = true;
+        for (int i = 0; i < nums.length; i++) {
+            if (isMaxZero && nums[i] > 0) isMaxZero = false;
+            strs[i] = String.valueOf(nums[i]);
+        }
+        if (isMaxZero) return "0";
+
+
+        Arrays.sort(strs, (a, b) -> (b + a).compareTo(a + b));
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < strs.length; i++) {
+            result.append(strs[i]);
+        }
+
+        return result.toString();
+    }
+}
