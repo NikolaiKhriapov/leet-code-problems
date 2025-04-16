812f92e39e5d21aeaf07b568ac7ea918086c54c5
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-16 11:10:34 +0800

    Time: 2 ms (84.99%), Space: 45.4 MB (60.73%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,35 +1,38 @@
 class Solution {
     public void solve(char[][] board) {
+        if (board == null || board.length == 0 || board[0].length == 0) return;
+
         for (int r = 0; r < board.length; r++) {
-            checkAndMarkIfNecessary(board, r, 0);
-            checkAndMarkIfNecessary(board, r, board[0].length - 1);
+            checkCellAndMarkIfNecessary(board, r, 0);
+            checkCellAndMarkIfNecessary(board, r, board[0].length - 1);
         }
         for (int c = 0; c < board[0].length; c++) {
-            checkAndMarkIfNecessary(board, 0, c);
-            checkAndMarkIfNecessary(board, board.length - 1, c);
+            checkCellAndMarkIfNecessary(board, 0, c);
+            checkCellAndMarkIfNecessary(board, board.length - 1, c);
         }
 
-        unmarkSafeCells(board);
+        markCapturedCellsAndUnmarkSafeCells(board);
     }
 
     /**
      * check whether 'O' is safe and mark it as '.' to change back in the future
      */
-    private void checkAndMarkIfNecessary(char[][] board, int r, int c) {
+    private void checkCellAndMarkIfNecessary(char[][] board, int r, int c) {
         if (board[r][c] == 'X' || board[r][c] == '.') return;
         
         board[r][c] = '.';
 
-        if (r > 0) checkAndMarkIfNecessary(board, r - 1, c);
-        if (r < board.length - 1) checkAndMarkIfNecessary(board, r + 1, c);
-        if (c > 0) checkAndMarkIfNecessary(board, r, c - 1);
-        if (c < board[0].length - 1) checkAndMarkIfNecessary(board, r, c + 1);
+        if (r > 0) checkCellAndMarkIfNecessary(board, r - 1, c);
+        if (r < board.length - 1) checkCellAndMarkIfNecessary(board, r + 1, c);
+        if (c > 0) checkCellAndMarkIfNecessary(board, r, c - 1);
+        if (c < board[0].length - 1) checkCellAndMarkIfNecessary(board, r, c + 1);
     }
 
     /**
+     * 'capture' unsafe 'X' cells by marking them as 'O', and
      * unmark safe cells from '.' back to 'O'
      */
-    private void unmarkSafeCells(char[][] board) {
+    private void markCapturedCellsAndUnmarkSafeCells(char[][] board) {
         for (int r = 0; r < board.length; r++) {
             for (int c = 0; c < board[0].length; c++) {
                 if (board[r][c] == 'O') board[r][c] = 'X';
