2ac86cd4d79822aa1668e1ceb077d10721d2a927
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-18 01:14:31 +0800

    Time: 5 ms (99.58%), Space: 46.5 MB (6.49%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,22 +1,33 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        return visitCell(matrix, target, 0, 0, new HashSet<>());
-    }
 
-    private boolean visitCell(int[][] matrix, int target, int r, int c, Set<String> visited) {
-        if (
-            r >= matrix.length ||
-            c >= matrix[0].length ||
-            matrix[r][c] > target ||
-            visited.contains(String.valueOf(r) + String.valueOf(c))
-           ) {
-            return false;
+        int c = 0;
+        int col = 0;
+        while (c < matrix[0].length && matrix[0][c] <= target) {
+            col = c++;
         }
-        if (matrix[r][c] == target) {
-            return true;
-        }
-        visited.add(String.valueOf(r) + String.valueOf(c));
 
-        return visitCell(matrix, target, r + 1, c, visited) || visitCell(matrix, target, r, c + 1, visited);
+        int r = 0;
+        int row = 0;
+        while (r < matrix.length && matrix[r][0] <= target) {
+            row = r++;
+        }
+
+        for (int i = 0; i <= row; i++) {
+            if (matrix[i][col] == target) return true;
+            if (matrix[i][col] > target && col > 0) {
+                col--;
+                i--;
+            }
+        }
+        for (int i = 0; i <= col; i++) {
+            if (matrix[row][i] == target) return true;
+            if (matrix[row][i] > target && row > 0) {
+                row--;
+                i--;
+            }
+        }
+
+        return false;
     }
 }
