852b8fa39c9e9e889d7a8279c469b8b5c0294f72
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-17 11:58:48 +0800

    Time: 1 ms (3.72%), Space: 40.9 MB (77.13%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+
+        Map<Integer, Integer> memo = new HashMap<>();
+        return Math.max(rob(nums, 0, memo), rob(nums, 1, memo));
+    }
+
+    private int rob(int[] nums, int i, Map<Integer, Integer> memo) {
+        if (i > nums.length - 1) {
+            return 0;
+        }
+
+        if (memo.containsKey(i)) {
+            return memo.get(i);
+        }
+
+        int result = nums[i] + Math.max(rob(nums, i + 2, memo), rob(nums, i + 3, memo));
+        memo.put(i, result);
+        return result;
+    }
+}
