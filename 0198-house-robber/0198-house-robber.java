dfd8f021d72c031203eacb8105082d01a5d87457
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-17 12:08:05 +0800

    Time: 0 ms (100%), Space: 40.9 MB (77.13%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,21 +3,20 @@ class Solution {
         if (nums == null || nums.length == 0) return 0;
         if (nums.length == 1) return nums[0];
 
-        Map<Integer, Integer> memo = new HashMap<>();
+        int[] memo = new int[nums.length];
+        for (int i = 0; i < memo.length; i++) {
+            memo[i] = -1;
+        }
         return Math.max(rob(nums, 0, memo), rob(nums, 1, memo));
     }
 
-    private int rob(int[] nums, int i, Map<Integer, Integer> memo) {
-        if (i > nums.length - 1) {
-            return 0;
-        }
+    private int rob(int[] nums, int i, int[] memo) {
+        if (i > nums.length - 1) return 0;
 
-        if (memo.containsKey(i)) {
-            return memo.get(i);
-        }
+        if (memo[i] != -1) return memo[i];
 
         int result = nums[i] + Math.max(rob(nums, i + 2, memo), rob(nums, i + 3, memo));
-        memo.put(i, result);
+        memo[i] = result;
         return result;
     }
 }
