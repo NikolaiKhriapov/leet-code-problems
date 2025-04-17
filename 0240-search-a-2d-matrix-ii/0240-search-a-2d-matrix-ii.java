c16c10857b2ad8389ef5a01d0bfcebd8973efa9b
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-18 01:29:58 +0800

    Time: 6 ms (49.55%), Space: 45.7 MB (93.9%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,5 +1,11 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null) {
+            throw new RuntimeException("Matrix cannot be null");
+        }
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
 
         int c = 0;
         int col = 0;
@@ -9,8 +15,7 @@ class Solution {
         for (int row = 0; row < matrix.length && col >= 0; row++) {
             if (matrix[row][col] == target) {
                 return true;
-            } else if (matrix[row][col] < target) {
-            } else {
+            } else if (matrix[row][col] > target) {
                 col--;
                 row--;
             }
