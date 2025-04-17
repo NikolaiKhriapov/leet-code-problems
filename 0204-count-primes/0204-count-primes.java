e8f685c8aac9505ed9fd14f105dfe3d8014e7bf8
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-17 14:16:45 +0800

    Time: 92 ms (88.62%), Space: 49.1 MB (84.33%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -3,21 +3,22 @@ class Solution {
         int count = 0;
 
         boolean[] primes = new boolean[n];
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i < primes.length; i++) {
             primes[i] = true;
         }
 
+        int limit = (int) Math.sqrt(n);
         for (int i = 2; i < n; i++) {
             if (primes[i]) {
                 count++;
-                if (i <= (int) Math.sqrt(n)) {
+                if (i <= limit) {
                     for (int j = i * i; j < n; j += i) {
                         primes[j] = false;
                     }
                 }
             }
         }
-
+        
         return count;
     }
 }
