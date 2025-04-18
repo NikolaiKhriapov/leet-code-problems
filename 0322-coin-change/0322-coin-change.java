349b630029f2e56e28de14a7e7f216105bf46d2a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-18 16:03:25 +0800

    Time: 16 ms (58.93%), Space: 44.4 MB (64.2%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,23 +1,21 @@
 class Solution {
-    private static final int INF = Integer.MAX_VALUE - 1;
-
     public int coinChange(int[] coins, int amount) {
         if (coins == null || coins.length == 0 || amount < 0) return -1;
         if (amount == 0) return 0;
 
         int[] dp = new int[amount + 1];
         for (int i = 1; i <= amount; i++) {
-            dp[i] = INF;
+            dp[i] = amount + 1;
         }
 
         for (int i = 1; i <= amount; i++) {
             for (int coin : coins) {
-                if (coin > 0 && i >= coin) {
+                if (i >= coin) {
                     dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
         
-        return dp[amount] == INF ? -1 : dp[amount];
+        return dp[amount] == (amount + 1) ? -1 : dp[amount];
     }
 }
