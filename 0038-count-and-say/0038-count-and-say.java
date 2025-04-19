3282fe6ad5ace540c5523d5437ef3a36fa799b5f
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-20 01:32:00 +0800

    Time: 2 ms (92.41%), Space: 41.9 MB (36.87%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,26 +1,32 @@
 class Solution {
     public String countAndSay(int n) {
-        if (n < 1) return "";
-        if (n == 1) return "1";
+        if (n < 1) {
+            return "";
+        }
+        if (n == 1) {
+            return "1";
+        }
 
         String prevString = countAndSay(n - 1);
+        return generateCurrString(prevString);
+    }
 
+    private static String generateCurrString(String str) {
         StringBuilder sb = new StringBuilder();
-
         int count = 0;
-        for (int i = 0; i < prevString.length(); i++) {
+        for (int i = 0; i < str.length(); i++) {
             count++;
-            if (i < prevString.length() - 1 && prevString.charAt(i) == prevString.charAt(i + 1)) {
+            if (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
                 continue;
             } else {
-                sb.append(count).append(prevString.charAt(i));
+                sb.append(count).append(str.charAt(i));
                 count = 0;
             }
         }
-        if (count > 0) {
-            sb.append(count).append(prevString.charAt(prevString.length() - 1));
-        }
-
+        // if (count > 0) {
+        //     sb.append(count).append(str.charAt(str.length() - 1));
+        // }
         return sb.toString();
+
     }
 }
