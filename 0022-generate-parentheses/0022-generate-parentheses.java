a88df6546b062793eb3fd21a137748d614bb8df5
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-20 01:11:22 +0800

    Time: 0 ms (100%), Space: 43.3 MB (59.05%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,15 +1,16 @@
 class Solution {
-    private static final String OPENED = "(";
-    private static final String CLOSED = ")";
-
     public List<String> generateParenthesis(int n) {
-
         List<String> result = new ArrayList<>();
-        addParenthesis(n, n, n, new StringBuilder(), result);
+
+        if (n <= 0) {
+            return result;
+        }
+
+        addParenthesis(n, n, new StringBuilder(), result);
         return result;
     }
 
-    private static void addParenthesis(int n, int openedLeft, int closedLeft, StringBuilder curr, List<String> result) {
+    private static void addParenthesis(int openedLeft, int closedLeft, StringBuilder curr, List<String> result) {
         if (openedLeft <= 0 && closedLeft <= 0) {
             result.add(curr.toString());
             return;
@@ -17,13 +18,13 @@ class Solution {
 
         int index = curr.length();
         if (openedLeft > 0) {
-            curr.append(OPENED);
-            addParenthesis(n, openedLeft - 1, closedLeft, curr, result);
+            curr.append("(");
+            addParenthesis(openedLeft - 1, closedLeft, curr, result);
             curr.delete(index, curr.length());
         }
         if (closedLeft > 0 && closedLeft > openedLeft) {
-            curr.append(CLOSED);
-            addParenthesis(n, openedLeft, closedLeft - 1, curr, result);
+            curr.append(")");
+            addParenthesis(openedLeft, closedLeft - 1, curr, result);
             curr.delete(index, curr.length());
         }
     }
