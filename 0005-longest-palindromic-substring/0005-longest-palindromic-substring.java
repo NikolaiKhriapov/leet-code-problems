5adbef238472c83455d10616b5bf6d9557b9f7da
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-19 23:06:24 +0800

    Time: 19 ms (57.44%), Space: 45.2 MB (41.87%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,27 +1,30 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s == null) return "";
-        if (s.length() < 2) return s;
-        
+        if (s == null) {
+            return "";
+        }
+        if (s.length() <= 1) {
+            return s;
+        }
+
         String longest = "";
 
         for (int i = 0; i < s.length(); i++) {
-            String currLongest = getLongestSubstring(s, i, i);
-            longest = currLongest.length() > longest.length() ? currLongest : longest;
+            String currentLongest = getLongestPalindrome(s, i, i);
+            longest = currentLongest.length() > longest.length() ? currentLongest : longest;
 
-            currLongest = getLongestSubstring(s, i, i + 1);
-            longest = currLongest.length() > longest.length() ? currLongest : longest;
+            currentLongest = getLongestPalindrome(s, i, i + 1);
+            longest = currentLongest.length() > longest.length() ? currentLongest : longest;
         }
         
         return longest;
     }
 
-    private static String getLongestSubstring(String s, int left, int right) {
+    private static String getLongestPalindrome(String s, int left, int right) {
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
         }
-
         return s.substring(left + 1, right);
     }
 }
