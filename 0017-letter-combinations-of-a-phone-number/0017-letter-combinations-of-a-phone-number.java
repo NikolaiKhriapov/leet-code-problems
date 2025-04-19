7ba48e2799d3f7a70a98167de89aefa9191b064f
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-20 00:51:10 +0800

    Time: 0 ms (100%), Space: 42.5 MB (17.38%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,12 +1,5 @@
 class Solution {
-    private static final String[] LETTERS_2 = new String[] {"a", "b", "c"};
-    private static final String[] LETTERS_3 = new String[] {"d", "e", "f"};
-    private static final String[] LETTERS_4 = new String[] {"g", "h", "i"};
-    private static final String[] LETTERS_5 = new String[] {"j", "k", "l"};
-    private static final String[] LETTERS_6 = new String[] {"m", "n", "o"};
-    private static final String[] LETTERS_7 = new String[] {"p", "q", "r", "s"};
-    private static final String[] LETTERS_8 = new String[] {"t", "u", "v"};
-    private static final String[] LETTERS_9 = new String[] {"w", "x", "y", "z"};
+    private static final String[] LETTERS = new String[] {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
     public List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList<>();
@@ -25,27 +18,11 @@ class Solution {
             return;
         }
 
-        String[] letters = getLettersForDigit(digits.charAt(i));
-
         int index = curr.length();
-        for (String letter : letters) {
+        for (char letter : LETTERS[digits.charAt(i) - '0'].toCharArray()) {
             curr.append(letter);
             addLetterCombinations(digits, i + 1, curr, result);
             curr.delete(index, curr.length());
         }
-    }
-
-    private static String[] getLettersForDigit(char c) {
-        return switch (c) {
-            case '2' -> LETTERS_2;
-            case '3' -> LETTERS_3;
-            case '4' -> LETTERS_4;
-            case '5' -> LETTERS_5;
-            case '6' -> LETTERS_6;
-            case '7' -> LETTERS_7;
-            case '8' -> LETTERS_8;
-            case '9' -> LETTERS_9;
-            default -> throw new RuntimeException("Invalid number: " + c);
-        };
     }
 }
