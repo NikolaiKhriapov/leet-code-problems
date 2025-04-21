c72c4f6ba98d64726a24323b6aa7c01ebd152204
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-21 11:17:25 +0800

    Time: 2 ms (17.92%), Space: 42.1 MB (26.3%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int numDecodings(String s) {
+        if (s == null || s.startsWith("0")) return 0;
+        if (s.length() <= 1) return s.length();
+
+        return numDecodings(s, 0, new HashMap<>());
+    }
+
+    private static int numDecodings(String s, int idx, Map<Integer, Integer> memo) {
+        if (idx >= s.length()) {
+            return 1;
+        }
+
+        if (memo.containsKey(idx)) {
+            return memo.get(idx);
+        }
+
+        if (s.charAt(idx) - '0' == 0) {
+            return 0;
+        }
+
+        int count = 0;
+
+        int one = numDecodings(s, idx + 1, memo);
+        count += one;
+
+        if (idx + 2 <= s.length() && Integer.parseInt(s.substring(idx, idx + 2)) <= 26) {
+            int two = numDecodings(s, idx + 2, memo);
+            count += two;
+        }
+
+        memo.put(idx, count);
+
+        return count;
+    }
+}
