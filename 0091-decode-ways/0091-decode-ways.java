844c1a33696d5250db1182b3510b25b503276993
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-21 11:33:47 +0800

    Time: 1 ms (83.79%), Space: 41.7 MB (77.92%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,41 +1,21 @@
 class Solution {
     public int numDecodings(String s) {
-        if (s == null || s.startsWith("0")) {
-            return 0;
-        }
-        if (s.length() <= 1) {
-            return s.length();
-        }
+        if (s == null || s.isEmpty() || s.charAt(0) == '0') return 0;
+        
+        int[] dp = new int[s.length() + 1];
+        dp[s.length()] = 1;
 
-        return numDecodings(s, 0, new HashMap<>());
-    }
-
-    private static int numDecodings(String s, int idx, Map<Integer, Integer> memo) {
-        if (idx >= s.length()) {
-            return 1;
-        }
-
-        if (memo.containsKey(idx)) {
-            return memo.get(idx);
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                dp[i] = 0;
+            } else {
+                dp[i] = dp[i + 1];
+                if (i + 1 < s.length() && Integer.parseInt(s.substring(i, i + 2)) <= 26) {
+                    dp[i] += dp[i + 2];
+                }
+            }
         }
 
-        int count = 0;
-
-        if (s.charAt(idx) - '0' == 0) {
-            memo.put(idx, count);
-            return count;
-        }
-
-        int one = numDecodings(s, idx + 1, memo);
-        count += one;
-
-        if (idx + 2 <= s.length() && Integer.parseInt(s.substring(idx, idx + 2)) <= 26) {
-            int two = numDecodings(s, idx + 2, memo);
-            count += two;
-        }
-
-        memo.put(idx, count);
-
-        return count;
+        return dp[0];
     }
 }
