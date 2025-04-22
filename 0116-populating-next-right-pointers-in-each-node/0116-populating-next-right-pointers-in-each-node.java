cf815ed08c91e1866ab507ac080f9a28858dbbd1
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-22 21:41:54 +0800

    Time: 0 ms (100%), Space: 44.3 MB (70.91%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -27,19 +27,16 @@ class Solution {
             return null;
         }
 
-        connect(root.left, root.right);
-        return root;
-    }
-
-    private static void connect(Node left, Node right) {
-        if (left == null || right == null) {
-            return;
+        if (root.left != null) {
+            root.left.next = root.right;
+            if (root.next != null) {
+                root.right.next = root.next.left;
+            }
         }
 
-        left.next = right;
-
-        connect(left.left, left.right);
-        connect(left.right, right.left);
-        connect(right.left, right.right);
+        connect(root.left);
+        connect(root.right);
+        
+        return root;
     }
 }
