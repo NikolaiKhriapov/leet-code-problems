eef6e97bbbfe77093a75c2a1b69663a711e32ca4
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-23 01:00:54 +0800

    Time: 9 ms (95.26%), Space: 57 MB (46.74%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        
+        ListNode mid = getMid(head);
+
+        ListNode right = sortList(mid.next);
+        mid.next = null;
+        ListNode left = sortList(head);
+
+        return merge(left, right);
+    }
+
+    private static ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        ListNode prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return prev;
+    }
+
+    private static ListNode merge(ListNode first, ListNode second) {
+        ListNode dummy = new ListNode(-1);
+
+        ListNode curr = dummy;
+        while (first != null && second != null) {
+            if (first.val <= second.val) {
+                curr.next = first;
+                first = first.next;
+            } else {
+                curr.next = second;
+                second = second.next;
+            }
+            curr = curr.next;
+        }
+        curr.next = (first != null) ? first : second;
+
+        return dummy.next;
+    }
+}
