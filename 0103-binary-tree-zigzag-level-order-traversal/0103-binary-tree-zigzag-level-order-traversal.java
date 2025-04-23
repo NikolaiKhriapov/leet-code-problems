ec695e01755c146384729c67ddd41bf89a3893c5
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-23 13:28:27 +0800

    Time: 0 ms (100%), Space: 42.2 MB (59.11%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -17,7 +17,9 @@ class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
 
-        if (root == null) return result;
+        if (root == null) {
+            return result;
+        }
 
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
@@ -27,26 +29,20 @@ class Solution {
             int size = q.size();
             List<Integer> list = new ArrayList<>();
 
-            List<TreeNode> nodes = new ArrayList<>();
             for (int i = 0; i < size; i++) {
-                nodes.add(q.poll());
-            }
-
-            if (leftToRight) {
-                for (int i = 0; i < nodes.size(); i++) {
-                    TreeNode node = nodes.get(i);
+                TreeNode node = q.poll();
+                if (leftToRight) {
                     list.add(node.val);
+                } else {
+                    list.add(0, node.val);
                 }
-            } else {
-                for (int i = nodes.size() - 1; i >= 0; i--) {
-                    TreeNode node = nodes.get(i);
-                    list.add(node.val);
-                }
-            }
 
-            for (TreeNode node : nodes) {
-                if (node.left != null) q.add(node.left);
-                if (node.right != null) q.add(node.right);
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+                if (node.right != null) {
+                    q.add(node.right);
+                }
             }
 
             leftToRight = !leftToRight;
