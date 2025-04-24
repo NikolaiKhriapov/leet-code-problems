301dfb4d8f1aecb0928db2deb92da069cf7a6883
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-24 11:13:08 +0800

    Time: 35 ms (55.77%), Space: 42.6 MB (87.62%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -3,32 +3,17 @@ class Solution {
         if (n <= 0) {
             return 0;
         }
-        if (n == 1) {
-            return 1;
-        }
-
-        List<Integer> perfectSquares = getPerfectSquares(n);
 
         int[] dp = new int[n + 1];
         Arrays.fill(dp, n);
         dp[0] = 0;
         
         for (int i = 1; i <= n; i++) {
-            for (int square : perfectSquares) {
-                if (i >= square) {
-                    dp[i] = Math.min(dp[i], dp[i - square] + 1);
-                }
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
         }
 
         return dp[n];
-    }
-
-    private List<Integer> getPerfectSquares(int n) {
-        List<Integer> result = new ArrayList<>();
-        for (int i = 1; i * i <= n; i++) {
-            result.add(i * i);
-        }
-        return result;
     }
 }
