11980723fe8e7c118af081f72fc8e7d5568ead11
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-24 11:07:01 +0800

    Time: 192 ms (12.57%), Space: 44.9 MB (18.31%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,46 +1,34 @@
 class Solution {
     public int numSquares(int n) {
-        if (n <= 0) return 0;
-        if (n == 1) return 1;
-
-        Set<Integer> perfectSquareNumbers = getPerfectSquares(n);
-        return numSquares(n, perfectSquareNumbers, new int[n + 1]);
-    }
-
-    private int numSquares(int n, Set<Integer> perfectSquareNumbers, int[] memo) {
-        if (n < 0) return -1;
-        if (n == 0) return 0;
-
-        if (memo[n] != 0) return memo[n];
-
-        if (perfectSquareNumbers.contains(n)) {
-            memo[n] = 1;
+        if (n <= 0) {
+            return 0;
+        }
+        if (n == 1) {
             return 1;
         }
 
-        int min = Integer.MAX_VALUE;
-        for (int i : perfectSquareNumbers) {
-            if (n - i >= 0) {
-                int numSquares = numSquares(n - i, perfectSquareNumbers, memo);
-                if (numSquares != -1) {
-                    min = Math.min(min, numSquares);
+        List<Integer> perfectSquares = getPerfectSquares(n);
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, n);
+        dp[0] = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int square : perfectSquares) {
+                if (i >= square) {
+                    dp[i] = Math.min(dp[i], dp[i - square] + 1);
                 }
             }
         }
 
-        int result = 1 + min;
-        memo[n] = result;
-        return result;
+        return dp[n];
     }
 
-    private Set<Integer> getPerfectSquares(int n) {
-        Set<Integer> result = new HashSet<>();
-        int nSqrt = (int) Math.sqrt(n);
-        for (int i = 1; i <= nSqrt; i++) {
+    private List<Integer> getPerfectSquares(int n) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i * i <= n; i++) {
             result.add(i * i);
         }
         return result;
     }
-
-
 }
