f6964cd01e0f08d3effa2cfb6159fdd5cf9172d7
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-24 13:54:54 +0800

    Time: 5 ms (78.93%), Space: 45.8 MB (72.21%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        Map<Integer, List<Integer>> graph = new HashMap<>();
         for (int[] prerequisite : prerequisites) {
-            List<Integer> list = map.getOrDefault(prerequisite[1], new ArrayList<>());
+            List<Integer> list = graph.getOrDefault(prerequisite[1], new ArrayList<>());
             list.add(prerequisite[0]);
-            map.put(prerequisite[1], list);
+            graph.put(prerequisite[1], list);
         }
 
         boolean[] visited = new boolean[numCourses];
         boolean[] visiting = new boolean[numCourses];
         List<Integer> visitedList = new ArrayList<>();
         for (int course = 0; course < numCourses; course++) {
-            if (hasCycle(course, map, visited, visiting, visitedList)) {
+            if (hasCycle(course, graph, visited, visiting, visitedList)) {
                 return new int[] {};
             }
         }
@@ -25,13 +25,13 @@ class Solution {
         return result;
     }
 
-    private boolean hasCycle(int course, Map<Integer, List<Integer>> map, boolean[] visited, boolean[] visiting, List<Integer> visitedList) {
+    private boolean hasCycle(int course, Map<Integer, List<Integer>> graph, boolean[] visited, boolean[] visiting, List<Integer> visitedList) {
         if (visited[course]) return false;
         if (visiting[course]) return true;
         visiting[course] = true;
 
-        for (int el : map.getOrDefault(course, new ArrayList<>())) {
-            if (hasCycle(el, map, visited, visiting, visitedList)) {
+        for (int el : graph.getOrDefault(course, new ArrayList<>())) {
+            if (hasCycle(el, graph, visited, visiting, visitedList)) {
                 return true;
             }
         }
