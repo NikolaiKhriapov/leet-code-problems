cefed64c9021bc565e61fb0a9cb08e32e20b0d33
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-25 19:46:15 +0800

    Time: 1 ms (74.65%), Space: 41.2 MB (24.63%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,18 +1,20 @@
 class Solution {
     public int divide(int dividend, int divisor) {
+        // handle overflow
         if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE;
         }
 
         int result = 0;
 
+        // to avoid overflow
         long dividendPos = Math.abs((long) dividend);
         long divisorPos = Math.abs((long) divisor);
 
         while (dividendPos >= divisorPos) {
             long temp = divisorPos;
             int multiple = 1;
-            while (dividendPos >= temp << 1) {
+            while (dividendPos >= (temp << 1)) {
                 temp <<= 1;
                 multiple <<= 1;
             }
