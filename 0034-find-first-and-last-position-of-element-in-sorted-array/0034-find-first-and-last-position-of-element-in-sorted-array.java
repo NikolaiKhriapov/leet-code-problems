3e51a9f89279693390383a3754eb0d4eaacc6d3d
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-27 10:05:14 +0800

    Time: 0 ms (100%), Space: 46.2 MB (16.65%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,44 +1,52 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int l = binarySearchLeft(nums, target, 0, nums.length - 1);
-        int r = binarySearchRight(nums, target, l, nums.length - 1);
+        if (nums == null) {
+            throw new IllegalArgumentException("Invalid input"); // for simplicity
+        }
+        if (nums.length == 0) {
+            return new int[] {-1, -1};
+        }
 
+        int l = findLeft(nums, target, 0, nums.length - 1);
+        int r = -1;
+        if (l != -1) {
+            r = findRight(nums, target, l, nums.length - 1);
+        }
         return new int[] {l, r};
     }
 
-    private int binarySearchLeft(int[] nums, int target, int l, int r) {
-        if (l > r) return -1;
+    private int findLeft(int[] nums, int target, int l, int r) {
+        if (l > r) {
+            return -1;
+        }
 
         int m = l + (r - l) / 2;
-        
+
         if (nums[m] == target) {
             if (m == 0 || nums[m - 1] < nums[m]) {
                 return m;
-            } else {
-                return binarySearchLeft(nums, target, l, m - 1);
             }
+            return findLeft(nums, target, l, m - 1);
         }
-        
         if (nums[m] > target) {
-            return binarySearchLeft(nums, target, l, m - 1);
-        } else {
-            return binarySearchLeft(nums, target, m + 1, r);
+            return findLeft(nums, target, l, m - 1);
         }
+        return findLeft(nums, target, m + 1, r);
     }
 
-    private int binarySearchRight(int[] nums, int target, int l, int r) {
-        if (l == -1) return -1;
+    private int findRight(int[] nums, int target, int l, int r) {
+        if (l > r) {
+            return -1;
+        }
 
         int m = l + (r - l) / 2;
-        
+
         if (nums[m] == target) {
             if (m == nums.length - 1 || nums[m + 1] > nums[m]) {
                 return m;
-            } else {
-                return binarySearchRight(nums, target, m + 1, r);
             }
+            return findRight(nums, target, m + 1, r);
         }
-        
-        return binarySearchRight(nums, target, l, m - 1);
+        return findRight(nums, target, l, m - 1);
     }
 }
