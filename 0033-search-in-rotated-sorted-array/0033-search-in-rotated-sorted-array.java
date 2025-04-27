2331753a9a24438935e5344f9f5b83d98e685a16
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-27 09:42:56 +0800

    Time: 0 ms (100%), Space: 42.1 MB (52.79%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,21 +1,30 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return binarySearch(nums, target, 0, nums.length - 1);
+        return helper(nums, target, 0, nums.length - 1);
     }
 
-    private int binarySearch(int[] nums, int target, int l, int r) {
-        if (l > r) return -1;
-        
+    private int helper(int[] nums, int target, int l, int r) {
+        if (l > r) {
+            return -1;
+        }
+
         int m = l + (r - l) / 2;
 
         if (nums[m] == target) return m;
 
-        if (nums[l] <= nums[m]) {
-            if (nums[l] <= target && target < nums[m]) return binarySearch(nums, target, l, m - 1);
-            else return binarySearch(nums, target, m + 1, r);
+        boolean isSortedL = nums[l] <= nums[m];
+        if (isSortedL) {
+            if (nums[l] <= target && target < nums[m]) {
+                return helper(nums, target, l, m - 1);
+            } else {
+                return helper(nums, target, m + 1, r);
+            }
         } else {
-            if (nums[m] < target && target <= nums[r]) return binarySearch(nums, target, m + 1, r);
-            else return binarySearch(nums, target, l, m - 1);
+            if (nums[m] < target && target <= nums[r]) {
+                return helper(nums, target, m + 1, r);
+            } else {
+                return helper(nums, target, l, m - 1);
+            }
         }
     }
 }
