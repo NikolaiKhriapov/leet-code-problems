79468ef184383f4c15270ffc585ed320d095b58e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-28 20:01:38 +0800

    Time: 1 ms (95.03%), Space: 44.5 MB (52.13%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,26 +14,32 @@
  * }
  */
 class Solution {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
+    private int preorderIndex = 0;
 
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        Map<Integer, Integer> inorderMap = createInorderMap(inorder);
+        return helper(preorder, inorder, 0, inorder.length - 1, inorderMap);
+    }
+
+    private TreeNode helper(int[] preorder, int[] inorder, int inStart, int inEnd, Map<Integer, Integer> inorderMap) {
+        if (inStart > inEnd) {
+            return null;
+        }
+
+        int val = preorder[preorderIndex++];
+        int inorderIndex = inorderMap.get(val);
+        return new TreeNode(
+            val,
+            helper(preorder, inorder, inStart, inorderIndex - 1, inorderMap),
+            helper(preorder, inorder, inorderIndex + 1, inEnd, inorderMap)
+        );
+    }
+
+    private Map<Integer, Integer> createInorderMap(int[] inorder) {
         Map<Integer, Integer> inorderMap = new HashMap<>();
         for (int i = 0; i < inorder.length; i++) {
             inorderMap.put(inorder[i], i);
         }
-        
-        return helper(preorder, inorder, 0, 0, inorder.length - 1, inorderMap);
-    }
-
-    private TreeNode helper(int[] preorder, int[] inorder, int preStart, int inStart, int inEnd, Map<Integer, Integer> inorderMap) {
-        if (inStart > inEnd || preStart >= preorder.length) return null;
-
-        int val = preorder[preStart];
-        int inorderIndex = inorderMap.get(val);
-        
-        return new TreeNode(
-            val,
-            helper(preorder, inorder, preStart + 1, inStart, inorderIndex - 1, inorderMap),
-            helper(preorder, inorder, preStart + inorderIndex - inStart + 1, inorderIndex + 1, inEnd, inorderMap)
-        );
+        return inorderMap;
     }
 }
