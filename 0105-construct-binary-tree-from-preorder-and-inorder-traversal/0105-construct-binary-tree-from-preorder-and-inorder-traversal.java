8debb2acd5cdbf7deda49d45874a01cf99ae7968
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-28 20:07:57 +0800

    Time: 1 ms (95.03%), Space: 44.9 MB (19.65%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -17,6 +17,13 @@ class Solution {
     private int preorderIndex = 0;
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || inorder == null || preorder.length != inorder.length) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        if (preorder.length == 0) {
+            return null;
+        }
+
         Map<Integer, Integer> inorderMap = createInorderMap(inorder);
         return helper(preorder, inorder, 0, inorder.length - 1, inorderMap);
     }
