91cf081a092e8ee76f8639537c4b2953ac9b0fab
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-28 21:12:30 +0800

    Time: 3 ms (33.59%), Space: 45.4 MB (60.25%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,42 +1,51 @@
 class Solution {
     public void solve(char[][] board) {
-        if (board == null || board.length == 0 || board[0].length == 0) return;
 
+        markEdgeRegions(board);
+        captureSurroundedRegions(board);
+        unmarkSaveRegions(board);        
+    }
+
+    private void markEdgeRegions(char[][] board) {
         for (int r = 0; r < board.length; r++) {
-            checkCellAndMarkIfNecessary(board, r, 0);
-            checkCellAndMarkIfNecessary(board, r, board[0].length - 1);
+            markEdgeRegion(board, r, 0);
+            markEdgeRegion(board, r, board[0].length - 1);
         }
         for (int c = 0; c < board[0].length; c++) {
-            checkCellAndMarkIfNecessary(board, 0, c);
-            checkCellAndMarkIfNecessary(board, board.length - 1, c);
+            markEdgeRegion(board, 0, c);
+            markEdgeRegion(board, board.length - 1, c);
+        }
+    }
+
+    private void markEdgeRegion(char[][] board, int r, int c) {
+        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] != 'O') {
+            return;
         }
 
-        markCapturedCellsAndUnmarkSafeCells(board);
-    }
-
-    /**
-     * check whether 'O' is safe and mark it as '.' to change back in the future
-     */
-    private void checkCellAndMarkIfNecessary(char[][] board, int r, int c) {
-        if (board[r][c] == 'X' || board[r][c] == '.') return;
-        
         board[r][c] = '.';
 
-        if (r > 0) checkCellAndMarkIfNecessary(board, r - 1, c);
-        if (r < board.length - 1) checkCellAndMarkIfNecessary(board, r + 1, c);
-        if (c > 0) checkCellAndMarkIfNecessary(board, r, c - 1);
-        if (c < board[0].length - 1) checkCellAndMarkIfNecessary(board, r, c + 1);
+        markEdgeRegion(board, r + 1, c);
+        markEdgeRegion(board, r, c + 1);
+        markEdgeRegion(board, r - 1, c);
+        markEdgeRegion(board, r, c - 1);
     }
 
-    /**
-     * 'capture' unsafe 'X' cells by marking them as 'O', and
-     * unmark safe cells from '.' back to 'O'
-     */
-    private void markCapturedCellsAndUnmarkSafeCells(char[][] board) {
+    private void captureSurroundedRegions(char[][] board) {
         for (int r = 0; r < board.length; r++) {
             for (int c = 0; c < board[0].length; c++) {
-                if (board[r][c] == 'O') board[r][c] = 'X';
-                else if (board[r][c] == '.') board[r][c] = 'O';
+                if (board[r][c] == 'O') {
+                    board[r][c] = 'X';
+                }
+            }
+        }
+    }
+
+    private void unmarkSaveRegions(char[][] board) {
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                if (board[r][c] == '.') {
+                    board[r][c] = 'O';
+                }
             }
         }
     }
