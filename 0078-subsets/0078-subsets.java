f7651052066b7982d6f90edfde7cc4b232b2688a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-28 13:57:41 +0800

    Time: 1 ms (51.72%), Space: 42.7 MB (65.31%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,14 +1,15 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
+        if (nums == null) {
+            throw new IllegalArgumentException("Invalid input"); // for simplicity
+        }
+
         List<List<Integer>> result = new ArrayList<>();
-
         helper(nums, 0, new ArrayList<>(), result);
-
         return result;
     }
 
     private void helper(int[] nums, int idx, List<Integer> curr, List<List<Integer>> result) {
-
         result.add(new ArrayList<>(curr));
 
         for (int i = idx; i < nums.length; i++) {
