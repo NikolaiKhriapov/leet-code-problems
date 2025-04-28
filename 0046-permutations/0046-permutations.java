5368d5a6ba888eea3bc82241b73e7eeb2d957989
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-28 10:25:11 +0800

    Time: 2 ms (39.52%), Space: 44.4 MB (81.6%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,26 +1,27 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        permute(nums, result, new ArrayList<>(), new boolean[nums.length]);
+
+        for (int n : nums) {
+            helper(nums, n, new ArrayList<>(), result);
+        }
+
         return result;
     }
 
-    private void permute(int[] nums, List<List<Integer>> result, List<Integer> curr, boolean[] visited) {
+    private void helper(int[] nums, int n, List<Integer> curr, List<List<Integer>> result) {
+
+        curr.add(n);
         if (curr.size() == nums.length) {
             result.add(new ArrayList<>(curr));
             return;
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (visited[i]) continue;
-
-            curr.add(nums[i]);
-            visited[i] = true;
-            
-            permute(nums, result, curr, visited);
-            
-            curr.remove(curr.size() - 1);
-            visited[i] = false;
+        for (int next : nums) {
+            if (!curr.contains(next)) {
+                helper(nums, next, curr, result);
+                curr.remove(curr.size() - 1);
+            }
         }
     }
 }
