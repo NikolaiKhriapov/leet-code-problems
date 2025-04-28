35d3774aa125db246a241e12aae03d2b1503c2ab
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-28 13:25:45 +0800

    Time: 1 ms (73.99%), Space: 45.8 MB (29.34%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,38 +1,75 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        if (matrix == null || matrix.length == 0) {
+        if (matrix == null) {
+            throw new IllegalArgumentException("Invalid state"); // for simplicity
+        }
+        if (matrix.length == 0 || matrix[0].length == 0) {
             return;
         }
 
-        boolean isFirstRowZero = false;
-        boolean isFirstColZero = false;
+        boolean isFirstRowZeroed = isFirstRowZeroed(matrix);
+        boolean isFirstColZeroed = isFirstColZeroed(matrix);
 
+        markFirstRowAndColumn(matrix);
+        updateRowsAndColumnsExceptFirst(matrix);
+        updateFirstRowAndColumn(matrix, isFirstRowZeroed, isFirstColZeroed);
+    }
+
+    private boolean isFirstRowZeroed(int[][] matrix) {
+        for (int c = 0; c < matrix[0].length; c++) {
+            if (matrix[0][c] == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isFirstColZeroed(int[][] matrix) {
         for (int r = 0; r < matrix.length; r++) {
-            for (int c = 0; c < matrix[0].length; c++) {
+            if (matrix[r][0] == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void markFirstRowAndColumn(int[][] matrix) {
+        for (int r = 1; r < matrix.length; r++) {
+            for (int c = 1; c < matrix[0].length; c++) {
                 if (matrix[r][c] == 0) {
-                    if (r == 0) isFirstRowZero = true;
-                    if (c == 0) isFirstColZero = true;
-                    matrix[0][c] = 0;
                     matrix[r][0] = 0;
+                    matrix[0][c] = 0;
                 }
             }
         }
-        
+    }
+
+    private void updateRowsAndColumnsExceptFirst(int[][] matrix) {
         for (int r = 1; r < matrix.length; r++) {
-            for (int c = 1; c < matrix[0].length; c++) {
-                if (matrix[r][0] == 0 || matrix[0][c] == 0) {
+            if (matrix[r][0] == 0) {
+                for (int c = 1; c < matrix[0].length; c++) {
                     matrix[r][c] = 0;
                 }
             }
         }
 
-        if (isFirstRowZero) {
+        for (int c = 1; c < matrix[0].length; c++) {
+            if (matrix[0][c] == 0) {
+                for (int r = 1; r < matrix.length; r++) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+    }
+
+    private void updateFirstRowAndColumn(int[][] matrix, boolean isFirstRowZeroed, boolean isFirstColZeroed) {
+        if (isFirstRowZeroed) {
             for (int c = 0; c < matrix[0].length; c++) {
                 matrix[0][c] = 0;
             }
         }
 
-        if (isFirstColZero) {
+        if (isFirstColZeroed) {
             for (int r = 0; r < matrix.length; r++) {
                 matrix[r][0] = 0;
             }
