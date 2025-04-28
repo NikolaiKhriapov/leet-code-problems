0fc79145f9022639fca1d62cac391f09fa6e64da
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-28 09:39:23 +0800

    Time: 8 ms (30.9%), Space: 44.7 MB (50.48%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        return isValidRows(board) && isValidCols(board) && isValidNines(board);
+        return isRowsValid(board) && isColsValid(board) && isSubboxesValid(board);
     }
 
-    private boolean isValidRows(char[][] board) {
-        for (int r = 0; r < 9; r++) {
+    private boolean isRowsValid(char[][] board) {
+        for (int r = 0; r < board.length; r++) {
             Set<Character> set = new HashSet<>();
-            for (int c = 0; c < 9; c++) {
-                char ch = board[r][c];
-                if (ch != '.' && !set.add(ch)) {
+            for (int c = 0; c < board[0].length; c++) {
+                if (board[r][c] != '.' && !set.add(board[r][c])) {
                     return false;
                 }
             }
@@ -16,12 +15,11 @@ class Solution {
         return true;
     }
 
-    private boolean isValidCols(char[][] board) {
-        for (int r = 0; r < 9; r++) {
+    private boolean isColsValid(char[][] board) {
+        for (int c = 0; c < board[0].length; c++) {
             Set<Character> set = new HashSet<>();
-            for (int c = 0; c < 9; c++) {
-                char ch = board[c][r];
-                if (ch != '.' && !set.add(ch)) {
+            for (int r = 0; r < board.length; r++) {
+                if (board[r][c] != '.' && !set.add(board[r][c])) {
                     return false;
                 }
             }
@@ -29,20 +27,20 @@ class Solution {
         return true;
     }
 
-    private boolean isValidNines(char[][] board) {
-        for (int r = 0; r < 9; r += 3) {
-            for (int c = 0; c < 9; c += 3) {
-                Set<Character> set = new HashSet<>();
-                for (int sr = r; sr < r + 3; sr++) {
-                    for (int sc = c; sc < c + 3; sc++) {
-                        char ch = board[sr][sc];
-                        if (ch != '.' && !set.add(ch)) {
-                            return false;
-                        }
-                    }
+    private boolean isSubboxesValid(char[][] board) {
+        Map<String, Set<Character>> map = new HashMap<>();
+
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                String key = String.valueOf(r / 3) + String.valueOf(c / 3);
+                Set<Character> value = map.getOrDefault(key, new HashSet<>());
+                if (board[r][c] != '.' && !value.add(board[r][c])) {
+                    return false;
                 }
+                map.put(key, value);
             }
         }
         return true;
     }
+
 }
