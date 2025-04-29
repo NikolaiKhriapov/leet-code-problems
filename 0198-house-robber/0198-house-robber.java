848175fb6cfaa1c321f6cb2f1caa5e9b97af7eaa
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-29 13:41:21 +0800

    Time: 0 ms (100%), Space: 41.5 MB (15.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,17 +1,27 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
-        if (nums.length == 1) return nums[0];
-
+        
         int[] memo = new int[nums.length];
+        Arrays.fill(memo, -1);
+        return Math.max(helper(nums, 0, memo), helper(nums, 1, memo));
+    }
 
-        memo[0] = nums[0];
-        memo[1] = Math.max(nums[0], nums[1]);
-
-        for (int i = 2; i < nums.length; i++) {
-            memo[i] = Math.max(memo[i - 1], memo[i - 2] + nums[i]);
+    private int helper(int[] nums, int currentIndex, int[] memo) {
+        if (currentIndex >= nums.length) {
+            return -1;
         }
 
-        return memo[memo.length - 1];
+        if (memo[currentIndex] != -1) {
+            return memo[currentIndex];
+        }
+
+        int sum = nums[currentIndex];
+
+        if (currentIndex < nums.length - 2) {
+            sum += Math.max(helper(nums, currentIndex + 2, memo), helper(nums, currentIndex + 3, memo));
+        }
+
+        memo[currentIndex] = sum;
+        return sum;
     }
 }
