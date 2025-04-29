10b95fa4c64b8e1a9559f4e37743506d6583fef5
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-29 10:27:15 +0800

    Time: 1 ms (99.28%), Space: 41.8 MB (92.49%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -14,6 +14,7 @@ class Solution {
         for (String word : wordDict) {
             if (s.startsWith(word, currentIndex)) {
                 if (wordBreak(s, currentIndex + word.length(), wordDict, memo)) {
+                    memo[currentIndex] = true;
                     return true;
                 }
             }
