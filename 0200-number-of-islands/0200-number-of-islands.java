ee0b52bd8ca07b6fd8acc0dc5cc54761ba0762c2
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-29 14:04:57 +0800

    Time: 3 ms (84.45%), Space: 49.9 MB (39.22%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,25 +1,34 @@
 class Solution {
+    private static final char MARK_ISLAND = '1';
+    private static final char MARK_WATER = '0';
+    private static final char MARK_TEMP = '.';
+
     public int numIslands(char[][] grid) {
-        int islandCount = 0;
+
+        int count = 0;
+
         for (int r = 0; r < grid.length; r++) {
             for (int c = 0; c < grid[0].length; c++) {
-                if (grid[r][c] == '1') islandCount++;
-                markIslandIfNecessary(grid, r, c);
+                if (grid[r][c] == MARK_ISLAND) {
+                    count++;
+                    markIsland(grid, r, c);
+                }
             }
         }
-        return islandCount;
+        
+        return count;
     }
 
-    private void markIslandIfNecessary(char[][] grid, int r, int c) {
-        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == '0') {
+    private void markIsland(char[][] grid, int r, int c) {
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] != MARK_ISLAND) {
             return;
         }
+        
+        grid[r][c] = MARK_TEMP;
 
-        grid[r][c] = '0';
-
-        markIslandIfNecessary(grid, r - 1, c);
-        markIslandIfNecessary(grid, r + 1, c);
-        markIslandIfNecessary(grid, r, c - 1);
-        markIslandIfNecessary(grid, r, c + 1);
+        markIsland(grid, r + 1, c);
+        markIsland(grid, r, c + 1);
+        markIsland(grid, r - 1, c);
+        markIsland(grid, r, c - 1);
     }
 }
