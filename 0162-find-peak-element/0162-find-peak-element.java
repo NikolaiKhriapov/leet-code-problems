972c6b998c6915df562c68d172f73f5ec5c858ba
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-29 11:48:33 +0800

    Time: 0 ms (100%), Space: 42.6 MB (43.62%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,29 +1,16 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return -1;
-        }
-        if (nums.length == 1) {
-            return 0;
-        }
 
-        return binarySearch(nums, 0, nums.length - 1);
-        
+        return helper(nums, 0, nums.length - 1);
     }
 
-    private int binarySearch(int[] nums, int l, int r) {
-        if (l > r) {
-            return l;
-        }
+    private int helper(int[] nums, int l, int r) {
+        if (l > r) return -1;
 
         int m = l + (r - l) / 2;
 
-        if (m > 0 && nums[m] < nums[m - 1]) {
-            return binarySearch(nums, l, m - 1);
-        }
-        if (m < nums.length - 1 && nums[m] < nums[m + 1]) {
-            return binarySearch(nums, m + 1, r);
-        }
+        if (m != 0 && nums[m] < nums[m - 1]) return helper(nums, l, m - 1);
+        if (m != nums.length - 1 && nums[m] < nums[m + 1]) return helper(nums, m + 1, r);
 
         return m;
     }
