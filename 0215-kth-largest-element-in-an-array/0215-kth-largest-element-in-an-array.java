4484f2fd223aba84c6681947c5f18705da2272a2
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-29 14:26:31 +0800

    Time: 60 ms (56.26%), Space: 61.4 MB (35.98%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,10 +1,10 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        if (nums == null || nums.length == 0 || nums.length < k || k < 0) {
+        if (nums == null || nums.length == 0 || nums.length < k || k <= 0) {
             throw new IllegalArgumentException("Invalid input"); // for simplicity
         }
 
-        Queue<Integer> pq = new PriorityQueue<>();
+        Queue<Integer> pq = new PriorityQueue<>((a, b) -> a - b);
         for (int n : nums) {
             pq.add(n);
             if (pq.size() > k) {
