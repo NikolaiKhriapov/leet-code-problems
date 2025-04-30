836e640207c1f999af6d532457805445c481d318
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-30 20:05:11 +0800

    Time: 19 ms (56.65%), Space: 45.3 MB (41.48%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,30 +1,41 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s == null) {
+        if (s == null || s.isEmpty()) {
             return "";
         }
-        if (s.length() <= 1) {
-            return s;
+
+        String longestPalindrome = "";
+
+        for (int i = 0, sl = s.length(); i < sl; i++) {
+            int l = i;
+            int r = l;
+            while (l >= 0 && r < sl) {
+                if (s.charAt(l) != s.charAt(r)) {
+                    break;
+                }
+                l--;
+                r++;
+            }
+            String palindrome = s.substring(l + 1, r);
+            if (palindrome.length() > longestPalindrome.length()) {
+                longestPalindrome = palindrome;
+            }
+
+            l = i;
+            r = l + 1;
+            while (l >= 0 && r < sl) {
+                if (s.charAt(l) != s.charAt(r)) {
+                    break;
+                }
+                l--;
+                r++;
+            }
+            palindrome = s.substring(l + 1, r);
+            if (palindrome.length() > longestPalindrome.length()) {
+                longestPalindrome = palindrome;
+            }
         }
 
-        String longest = "";
-
-        for (int i = 0; i < s.length(); i++) {
-            String currentLongest = getLongestPalindrome(s, i, i);
-            longest = currentLongest.length() > longest.length() ? currentLongest : longest;
-
-            currentLongest = getLongestPalindrome(s, i, i + 1);
-            longest = currentLongest.length() > longest.length() ? currentLongest : longest;
-        }
-        
-        return longest;
-    }
-
-    private static String getLongestPalindrome(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
-        }
-        return s.substring(left + 1, right);
+        return longestPalindrome;
     }
 }
