97ac1f5a9aab4b4b8422331caa8dd5de2c64aacb
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-30 14:34:39 +0800

    Time: 33 ms (6.68%), Space: 134.2 MB (29.5%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -3,16 +3,25 @@ class Solution {
         if (nums == null || nums.length < 3) {
             return false;
         }
-        
-        int firstMin = Integer.MAX_VALUE;
-        int secondMin = Integer.MAX_VALUE;
 
-        for (int n : nums) {
-            if (n <= firstMin) {
-                firstMin = n;
-            } else if (n <= secondMin) {
-                secondMin = n;
+        List<Integer> list = new ArrayList<>();
+        list.add(nums[0]);
+
+        for (int i = 1; i < nums.length; i++) {
+            int n = nums[i];
+            int prev = list.get(list.size() - 1);
+
+            if (n > prev) {
+                list.add(n);
             } else {
+                int index = Collections.binarySearch(list, n);
+                if (index < 0) {
+                    index = -index - 1;
+                }
+                list.set(index, n);
+            }
+
+            if (list.size() >= 3) {
                 return true;
             }
         }
