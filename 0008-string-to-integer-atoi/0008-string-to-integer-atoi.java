c6603f882d896db91a2de290d11f050a49d03d05
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-30 21:20:36 +0800

    Time: 2 ms (27.83%), Space: 42.6 MB (49.22%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,36 +1,30 @@
 class Solution {
     public int myAtoi(String s) {
-        if (s == null || s.isEmpty()) {
+        s = s.trim();
+
+        if (s.isEmpty()) {
             return 0;
         }
 
-        // ignore leading whitespaces
-        int p = 0;
-        while (p < s.length() && s.charAt(p) == ' ') {
-            p++;
-        }
-
-        // save the sign
+        int index = 0;
         boolean isNegative = false;
-        if (p < s.length() && (s.charAt(p) == '-' || s.charAt(p) == '+')) {
-            if (s.charAt(p) == '-') {
+        // save the sign
+        if (s.charAt(index) == '-' || s.charAt(index) == '+') {
+            if (s.charAt(index) == '-') {
                 isNegative = true;
             }
-            p++;
+            index++;
         }
         
         // build number
         int number = 0;
-        int left = p;
-        int right = p;
-        while (right < s.length() && Character.isDigit(s.charAt(right))) {
-            if (right - left > 7 && Long.parseLong(s.substring(left, right + 1)) > Integer.MAX_VALUE) {
+        while (index < s.length() && Character.isDigit(s.charAt(index))) {
+            int digit = s.charAt(index) - '0';
+            if (number > (Integer.MAX_VALUE - digit) / 10) {
                 return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
             }
-            right++;
-        }
-        if (right - left > 0) {
-            number = Integer.parseInt(s.substring(left, right));
+            number = number * 10 + digit;
+            index++;
         }
         
         return isNegative ? -number : number;
