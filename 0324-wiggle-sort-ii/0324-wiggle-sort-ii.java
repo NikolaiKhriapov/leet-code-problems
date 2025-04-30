606d2a67baa0e34552878ecf7a18f7da914055ec
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-30 13:46:18 +0800

    Time: 6 ms (81.01%), Space: 47.8 MB (45.44%) - LeetHub

---
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void wiggleSort(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return;
+        }
+
+        int[] sorted = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(sorted);
+
+        int left = (nums.length + 1) / 2 - 1;
+        int right = nums.length - 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = (i % 2 == 0) ? sorted[left--] : sorted[right--];
+        }
+    }
+}
