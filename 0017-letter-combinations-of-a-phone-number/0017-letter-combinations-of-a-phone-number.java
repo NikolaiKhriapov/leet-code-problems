7ba694ad6c49debcfa63c33379202c340b3c6934
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-04-30 21:47:02 +0800

    Time: 5 ms (28.48%), Space: 42.4 MB (38.93%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -7,24 +7,27 @@ class Solution {
         }
         
         List<String> list = new ArrayList<>();
-        for (int i = 0; i < digits.length(); i++) {
+        
+        // add first letters
+        int firstDigit = digits.charAt(0) - '0';
+        for (char c : DIGIT_TO_LETTERS_MAP[firstDigit].toCharArray()) {
+            list.add(String.valueOf(c));
+        }
+
+        // add other letters
+        for (int i = 1; i < digits.length(); i++) {
             list = addDigitToList(list, digits.charAt(i) - '0');
         }
+
         return list;
     }
 
     private List<String> addDigitToList(List<String> list, int digit) {
         List<String> result = new ArrayList<>();
 
-        if (list.isEmpty()) {
+        for (String curr : list) {
             for (char c : DIGIT_TO_LETTERS_MAP[digit].toCharArray()) {
-                result.add(String.valueOf(c));
-            }
-        } else {
-            for (String curr : list) {
-                for (char c : DIGIT_TO_LETTERS_MAP[digit].toCharArray()) {
-                    result.add(curr + c);
-                }
+                result.add(curr + c);
             }
         }
         return result;
