b8084d8ae4ab6fbef6be12811737ae758137c46d
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-02 00:38:47 +0800

    Time: 1 ms (80.98%), Space: 41.6 MB (76.35%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
@@ -1,30 +1,39 @@
 class Solution {
     public int longestSubstring(String s, int k) {
-        if (s == null || s.length() < k || k < 1) {
+        if (s == null || s.length() < k) {
             return 0;
         }
-        return longestSubstring(s, 0, s.length(), k);
+        return longestSubstring(s, k, 0, s.length());
     }
 
-    private static int longestSubstring(String s, int left, int right, int k) {
-        if (right - left < k) {
-            return 0;
+    private int longestSubstring(String s, int k, int left, int right) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = left; i < right; i++) {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
-        int[] count = new int[26];
+        int currentLongest = 0;
+        int start = left;
+        boolean isCurrentValid = true;
         for (int i = left; i < right; i++) {
-            count[s.charAt(i) - 'a']++;
-        }
-        
-        for (int i = left; i < right; i++) {
-            if (count[s.charAt(i) - 'a'] < k) {
-                return Math.max(
-                    longestSubstring(s, left, i, k),
-                    longestSubstring(s, i + 1, right, k)
-                );
+            if (map.get(s.charAt(i)) < k) {
+                if (i > start) {
+                    currentLongest = Math.max(currentLongest, longestSubstring(s, k, start, i));
+                }
+                start = i + 1;
+                isCurrentValid = false;
             }
         }
 
-        return right - left;
+        if (isCurrentValid) {
+            return right - left;
+        }
+
+        if (start < right) {
+            currentLongest = Math.max(currentLongest, longestSubstring(s, k, start, right));
+        }
+
+        return currentLongest;
     }
 }
