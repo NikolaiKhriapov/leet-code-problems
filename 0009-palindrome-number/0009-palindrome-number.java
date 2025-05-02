6d8ddd8f3a6646edda8bd8ee466e3d442951e1fd
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-02 13:57:47 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,23 +1,25 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
-            return false;
-        }
-        if (x <= 9) {
-            return true;
+        if (x < 0 || x % 10 == 0) return false;
+        if (x <= 9) return true;
+
+        int temp = x;
+
+        int reversed = 0;
+        while (temp != 0) {
+            int digit = temp % 10;
+            temp /= 10;
+            reversed = reversed * 10 + digit;
         }
 
-        String xString = String.valueOf(x);
-
-        int left = 0;
-        int right = xString.length() - 1;     
-        while (left < right) {
-            if (xString.charAt(left) != xString.charAt(right)) {
+        while (x != 0 || reversed != 0) {
+            if (x % 10 != reversed % 10) {
                 return false;
             }
-            left++;
-            right--;
+            x /= 10;
+            reversed /= 10;
         }
-        return true;        
+
+        return x == 0 && reversed == 0;
     }
 }
