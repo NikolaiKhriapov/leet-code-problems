12b5f6237eaaa5351fd6f347551848dfd88e34e2
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-02 17:13:27 +0800

    Time: 12 ms (6.77%), Space: 45 MB (12.95%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,15 +1,20 @@
 class Solution {
     public String intToRoman(int num) {
 
-        Map<Integer, Character> map = Map.of(
-            1, 'I',
-            5, 'V',
-            10, 'X',
-            50, 'L',
-            100, 'C',
-            500, 'D',
-            1000, 'M'
-        );
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
         
         StringBuilder result = new StringBuilder();
 
@@ -18,19 +23,12 @@ class Solution {
             for (var entry : map.entrySet()) {
                 int curr = entry.getKey();
                 if (curr <= num && curr > currNum) currNum = curr;
+                // else break;
             }
             result.append(map.get(currNum));
             num -= currNum;
         }
 
-        return result.toString()
-                     .replaceAll("DCCCC", "CM")
-                     .replaceAll("CCCC", "CD")
-                     .replaceAll("LXXXX", "XC")
-                     .replaceAll("XXXX", "XL")
-                     .replaceAll("VIIII", "IX")
-                     .replaceAll("IIII", "IV");
+        return result.toString();
     }
 }
-
-// 3749
