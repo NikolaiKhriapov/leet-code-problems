372171b75ee1efe25ba9d2ca829b95e36bd6b90d
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-02 18:05:07 +0800

    Time: 31 ms (59%), Space: 51.8 MB (44.62%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,43 +1,35 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        if (nums == null || nums.length < 3) return new ArrayList<>();
 
         List<List<Integer>> result = new ArrayList<>();
 
-        // sort
         Arrays.sort(nums);
 
-        // 1 fixed pointer from the left, 1 pointer from the left, 1 pointer from the right
-        int p1 = 0;
-        int p2 = 1;
-        int p3 = nums.length - 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int left = i + 1;
+            int right = nums.length - 1;
 
-        // if sum is <0 move left pointers, if >0 shift right pointer, skipping values
-        while (p1 < nums.length - 2) {
-            while (p2 < p3) {
-                int sum = nums[p1] + nums[p2] + nums[p3];
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0) {
-                    result.add(List.of(nums[p1], nums[p2], nums[p3]));
-                    do {
-                        p2++;
-                    } while (p2 < p3 && nums[p2] == nums[p2 - 1]);
+                    result.add(List.of(nums[i], nums[left], nums[right]));
+                    left++;
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
                 } else if (sum < 0) {
-                    do {
-                        p2++;
-                    } while (p2 < p3 && nums[p2] == nums[p2 - 1]);
+                    left++;
                 } else {
-                    do {
-                        p3--;
-                    } while (p2 < p3 && nums[p3] == nums[p3 + 1]);
+                    right--;
                 }
             }
-            do {
-                p1++;
-            } while (p1 < nums.length - 2 && nums[p1] == nums[p1 - 1]);
-            p2 = p1 + 1;
-            p3 = nums.length - 1;
         }
 
         return result;
     }
 }
+
+// [-4,-1,-1,0,1,2]
+//   .     .     .
