756a5ac72d66ebcf84a189f3c34f605ac2568e30
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-02 17:19:00 +0800

    Time: 3 ms (98.58%), Space: 44.7 MB (39.95%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,34 +1,16 @@
 class Solution {
     public String intToRoman(int num) {
 
-        Map<Integer, String> map = new LinkedHashMap<>();
-        map.put(1000, "M");
-        map.put(900, "CM");
-        map.put(500, "D");
-        map.put(400, "CD");
-        map.put(100, "C");
-        map.put(90, "XC");
-        map.put(50, "L");
-        map.put(40, "XL");
-        map.put(10, "X");
-        map.put(9, "IX");
-        map.put(5, "V");
-        map.put(4, "IV");
-        map.put(1, "I");
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM","D", "CD","C","XC","L","XL","X","IX","V","IV","I"};
         
         StringBuilder result = new StringBuilder();
 
-        while (num != 0) {
-            int currNum = 0;
-            for (var entry : map.entrySet()) {
-                int curr = entry.getKey();
-                if (curr <= num) {
-                    currNum = curr;
-                    break;
-                }
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                result.append(symbols[i]);
+                num -= values[i];
             }
-            result.append(map.get(currNum));
-            num -= currNum;
         }
 
         return result.toString();
