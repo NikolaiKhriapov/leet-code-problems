675f781678e0fb14022ac80a84da7e2a1e86f1d9
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-03 14:13:46 +0800

    Time: 15 ms (86.84%), Space: 42.7 MB (61.03%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -6,9 +6,9 @@ class Solution {
         int start = 0;
         int end = 0;
         for (int i = 0; i < s.length(); i++) {
-            int longestA = calculateLongest(s, i, i);
-            int longestB = calculateLongest(s, i, i + 1);
-            int longest = Math.max(longestA, longestB);
+            int longestOdd = calculateLongest(s, i, i);
+            int longestEven = calculateLongest(s, i, i + 1);
+            int longest = Math.max(longestOdd, longestEven);
             if (longest > end - start) {
                 start = i - (longest - 1) / 2;
                 end = i + longest / 2;
