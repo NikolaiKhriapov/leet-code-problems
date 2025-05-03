c04d5c5139f14bfcbff4c7e5840853f2f72d4e63
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-03 20:24:07 +0800

    Time: 2 ms (39.83%), Space: 44.4 MB (92.39%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,29 +1,25 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return new ArrayList<>();
-        }
 
         List<List<Integer>> result = new ArrayList<>();
-        boolean[] used = new boolean[nums.length];
-        helper(nums, new ArrayList<>(), result, used);
+        helper(nums, new ArrayList<>(), result, new boolean[nums.length]);
         return result;
     }
 
-    private void helper(int[] nums, List<Integer> curr, List<List<Integer>> result, boolean[] used) {
+    private void helper(int[] nums, List<Integer> curr, List<List<Integer>> result, boolean[] visited) {
         if (curr.size() == nums.length) {
             result.add(new ArrayList<>(curr));
             return;
         }
-        
+
         for (int i = 0; i < nums.length; i++) {
-            if (!used[i]) {
-                curr.add(nums[i]);
-                used[i] = true;
-                helper(nums, curr, result, used);
-                curr.remove(curr.size() - 1);
-                used[i] = false;
-            }
+            if (visited[i]) continue;
+
+            curr.add(nums[i]);
+            visited[i] = true;
+            helper(nums, curr, result, visited);
+            curr.remove(curr.size() - 1);
+            visited[i] = false;
         }
     }
 }
