78107a39afe8903630a08bcbd3eef4283500da01
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-03 14:22:11 +0800

    Time: 5 ms (61.1%), Space: 45.1 MB (62.74%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,28 +1,27 @@
 class Solution {
     public String convert(String s, int numRows) {
-        if (s == null || s.isEmpty() || numRows == 0) {
-            return "";
+        if (numRows <= 0) {
+            return null;
         }
-        if (numRows == 1) {
+        if (s == null || s.isEmpty() || numRows == 1) {
             return s;
         }
 
-        // create buckets for each row
         StringBuilder[] buckets = new StringBuilder[numRows];
         for (int i = 0; i < buckets.length; i++) {
             buckets[i] = new StringBuilder();
         }
-        
-        // populate buckets
-        int index = 0;
+
         int bucketIndex = 0;
         boolean isForward = true;
-        while (index < s.length()) {
-            buckets[bucketIndex].append(s.charAt(index));
+        
+        for (int i = 0; i < s.length(); i++) {
+            buckets[bucketIndex].append(s.charAt(i));
 
             if (bucketIndex == 0) {
                 isForward = true;
-            } else if (bucketIndex == buckets.length - 1) {
+            }
+            if (bucketIndex == buckets.length - 1) {
                 isForward = false;
             }
 
@@ -31,16 +30,13 @@ class Solution {
             } else {
                 bucketIndex--;
             }
-
-            index++;
-        }
-
-        // merge all buckets
+        } 
+        
         StringBuilder result = new StringBuilder();
         for (StringBuilder bucket : buckets) {
             result.append(bucket);
         }
-
+        
         return result.toString();
     }
 }
