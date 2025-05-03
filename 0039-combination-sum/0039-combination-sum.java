aa861b40aaa2b708f67037f1c6aa803614f22302
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-03 19:26:28 +0800

    Time: 2 ms (83.13%), Space: 44.7 MB (64.38%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,13 +2,11 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
         List<List<Integer>> result = new ArrayList<>();
-        helper(candidates, target, 0, new ArrayList<>(), result);
+        helper(candidates, target, 0, new ArrayList<>(), 0, result);
         return result;
     }
 
-    private void helper(int[] candidates, int target, int start, List<Integer> curr, List<List<Integer>> result) {
-        int sum = getSum(curr);
-
+    private void helper(int[] candidates, int target, int start, List<Integer> curr, int sum, List<List<Integer>> result) {
         if (sum > target) {
             return;
         } else if (sum == target) {
@@ -18,7 +16,7 @@ class Solution {
 
         for (int i = start; i < candidates.length; i++) {
             curr.add(candidates[i]);
-            helper(candidates, target, i, curr, result);
+            helper(candidates, target, i, curr, sum + candidates[i], result);
             curr.remove(curr.size() - 1);
         }
     }
