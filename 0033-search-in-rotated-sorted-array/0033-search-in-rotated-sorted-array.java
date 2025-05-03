57dbee1ac8ec56bfe0dfb1936a7053d6c324694d
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-03 18:51:33 +0800

    Time: 0 ms (100%), Space: 42.1 MB (68.32%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -3,31 +3,32 @@ class Solution {
         if (nums == null || nums.length == 0) {
             return -1;
         }
+
+        int l = 0;
+        int r = nums.length - 1;
         
-        int left = 0;
-        int right = nums.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
+            if (nums[m] == target) {
+                return m;
             }
-            if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
+
+            if (nums[l] <= nums[m]) {
+                if (nums[l] <= target && target <= nums[m]) {
+                    r = m - 1;
                 } else {
-                    left = mid + 1;
+                    l = m + 1;
                 }
             } else {
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
+                if (nums[m] <= target && target <= nums[r]) {
+                    l = m + 1;
                 } else {
-                    right = mid - 1;
+                    r = m - 1;
                 }
             }
         }
-        
+
         return -1;
     }
 }
