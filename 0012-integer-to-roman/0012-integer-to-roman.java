29ef4e6c949b9db8de969e17d3cb0204d9491706
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-03 15:07:48 +0800

    Time: 3 ms (98.57%), Space: 44.4 MB (71.73%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,17 +1,17 @@
 class Solution {
-    private final static int[] INTEGERS = new int[] {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    private final static String[] LETTERS = new String[] {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    private static final int[] NUMBERS = new int[] {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] LETTERS = new String[] {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
     public String intToRoman(int num) {
-        if (num <= 0) {
-            return "";
-        }
-        
+
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < INTEGERS.length; i++) {
-            while (num >= INTEGERS[i]) {
-                result.append(LETTERS[i]);
-                num -= INTEGERS[i];
+        while (num > 0) {
+            for (int i = 0; i < NUMBERS.length; i++) {
+                if (NUMBERS[i] <= num) {
+                    result.append(LETTERS[i]);
+                    num -= NUMBERS[i];
+                    break;
+                }
             }
         }
         return result.toString();
