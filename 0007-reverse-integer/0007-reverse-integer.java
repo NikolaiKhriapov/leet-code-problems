224565e91c74453e03ac3214ee8e38fe699fda06
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-03 14:35:21 +0800

    Time: 1 ms (83.68%), Space: 40.5 MB (97.53%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,22 +1,23 @@
 class Solution {
     public int reverse(int x) {
-        if (x == Integer.MIN_VALUE) return 0;
-        if (x >= 0 && x <= 9) return x;
+        if (x == Integer.MIN_VALUE) {
+            return 0;
+        }
+        if (x >= 0 && x <= 9) {
+            return x;
+        }
 
         int sign = (x < 0) ? -1 : 1;
-
         int xPos = Math.abs(x);
 
         int xReversed = 0;
         while (xPos != 0) {
             int digit = xPos % 10;
-            xPos /= 10;
-
             if ((xReversed > Integer.MAX_VALUE / 10) || (xReversed == Integer.MAX_VALUE / 10 && (digit > 7))) {
                 return 0;
             }
-
             xReversed = xReversed * 10 + digit;
+            xPos /= 10;
         }
         
         return sign * xReversed;
