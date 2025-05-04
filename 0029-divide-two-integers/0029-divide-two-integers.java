7c7c102ba6f06f8cfb21da654fa2449a7e47700c
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-04 10:19:40 +0800

    Time: 1 ms (74.91%), Space: 40.9 MB (59.83%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,19 +1,19 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        if (dividend == Integer.MIN_VALUE && divisor == -1) {
-            return Integer.MAX_VALUE;
-        }
         if (dividend == 0 || divisor == 0) {
             return 0;
+        }
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
         }
 
         int sign = 1;
         if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) {
             sign = -1;
         }
-        
-        int dividendNeg = dividend <= 0 ? dividend : -dividend;
-        int divisorNeg = divisor <= 0 ? divisor : -divisor;
+
+        int dividendNeg = dividend > 0 ? -dividend : dividend;
+        int divisorNeg = divisor > 0 ? -divisor : divisor;
 
         int result = 0;
         while (dividendNeg <= divisorNeg) {
@@ -26,7 +26,7 @@ class Solution {
             dividendNeg -= temp;
             result += multiple;
         }
-
+        
         return sign * result;
     }
 }
