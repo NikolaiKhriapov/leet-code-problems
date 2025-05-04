3efae15d558c5d4bc4dbc4f5ea90b12fcd4bd9b0
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-04 10:34:22 +0800

    Time: 1 ms (99.54%), Space: 45.3 MB (51.03%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,19 +1,22 @@
 class Solution {
     public int jump(int[] nums) {
-        if (nums == null || nums.length <= 1) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        if (nums.length == 1) {
             return 0;
         }
 
-        int result = 0;
-        int currReachable = 0;
-        int maxReachable = 0;
+        int count = 0;
+        int currFarthest = 0;
+        int maxFarthest = 0;
         for (int i = 0; i < nums.length - 1; i++) {
-            maxReachable = Math.max(maxReachable, i + nums[i]);
-            if (i == currReachable) {
-                result++;
-                currReachable = maxReachable;
+            maxFarthest = Math.max(maxFarthest, i + nums[i]);
+            if (i == currFarthest) {
+                count++;
+                currFarthest = maxFarthest;
             }
         }
-        return result;
+        return count;
     }
 }
