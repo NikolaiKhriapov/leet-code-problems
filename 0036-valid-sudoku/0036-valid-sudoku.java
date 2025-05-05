adca3da6726e20b33904e2ed24abc2e3f1a8aaa3
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-05 11:23:34 +0800

    Time: 2 ms (78.81%), Space: 44 MB (99.07%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        if (board == null || board.length == 0 || board[0].length == 0) {
+        if (board == null || board.length != 9 || board[0].length != 9) {
             throw new IllegalArgumentException("Invalid input"); // for simplicity
         }
         return isValidRows(board) && isValidCols(board) && isValidSubboxes(board);
@@ -8,10 +8,14 @@ class Solution {
 
     private boolean isValidRows(char[][] board) {
         for (int r = 0; r < board.length; r++) {
-            Set<Character> set = new HashSet<>();
+            boolean[] set = new boolean[board.length];
             for (int c = 0; c < board[0].length; c++) {
-                if (board[r][c] != '.' && !set.add(board[r][c])) {
-                    return false;
+                if (board[r][c] != '.') {
+                    int idx = board[r][c] - '1';
+                    if (set[idx]) {
+                        return false;
+                    }
+                    set[idx] = true;
                 }
             }
         }
@@ -20,10 +24,14 @@ class Solution {
 
     private boolean isValidCols(char[][] board) {
         for (int c = 0; c < board[0].length; c++) {
-            Set<Character> set = new HashSet<>();
+            boolean[] set = new boolean[board[0].length];
             for (int r = 0; r < board.length; r++) {
-                if (board[r][c] != '.' && !set.add(board[r][c])) {
-                    return false;
+                if (board[r][c] != '.') {
+                    int idx = board[r][c] - '1';                    
+                    if (set[idx]) {
+                        return false;
+                    }
+                    set[idx] = true;
                 }
             }
         }
@@ -33,11 +41,15 @@ class Solution {
     private boolean isValidSubboxes(char[][] board) {
         for (int r = 0; r < board.length; r += 3) {    
             for (int c = 0; c < board[0].length; c += 3) {
-                Set<Character> set = new HashSet<>();
+                boolean[] set = new boolean[board.length];
                 for (int i = r; i < r + 3; i++) {
                     for (int j = c; j < c + 3; j++) {
-                        if (board[i][j] != '.' && !set.add(board[i][j])) {
-                            return false;
+                        if (board[i][j] != '.') {
+                            int idx = board[i][j] - '1';                    
+                            if (set[idx]) {
+                                return false;
+                            }
+                            set[idx] = true;
                         }
                     }
                 }
