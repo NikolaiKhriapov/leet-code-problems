82612eceeb600bdfd923f5e410117ba8b8a3f32c
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-05 10:27:22 +0800

    Time: 2 ms (27.65%), Space: 42.2 MB (91.29%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,34 +1,31 @@
 class Solution {
     public int myAtoi(String s) {
-        if (s == null) {
-            return 0;
-        }
+        if (s == null) return 0;
 
         s = s.trim();
 
-        if (s.isEmpty()) {
-            return 0;
-        }
+        if (s.isEmpty()) return 0;
 
-        boolean isNegative = false;
         int index = 0;
+        
+        int sign = 1;
         if (s.charAt(index) == '-' || s.charAt(index) == '+') {
             if (s.charAt(index) == '-') {
-                isNegative = true;
+                sign = -1;
             }
             index++;
         }
 
-        long number = 0;
+        int result = 0;
         while (index < s.length() && Character.isDigit(s.charAt(index))) {
             int digit = s.charAt(index) - '0';
-            if ((number > Integer.MAX_VALUE / 10) || (number == Integer.MAX_VALUE / 10 && digit > (isNegative ? 8 : 7))) {
-                return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
-            number = number * 10 + digit;
+            result = result * 10 + digit;
             index++;
         }
         
-        return isNegative ? (int) -number : (int) number;
+        return sign * result;
     }
 }
