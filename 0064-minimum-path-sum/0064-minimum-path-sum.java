1828c906df615ab2e460412a3209b4d0feaf0089
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-05 15:49:40 +0800

    Time: 3 ms (72.69%), Space: 47.4 MB (89.7%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,16 +1,22 @@
 class Solution {
     public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            throw new IllegalArgumentException("Invalid input"); // for simplicity
+        }
 
         int rows = grid.length;
         int cols = grid[0].length;
 
         int[] dp = new int[cols];
-        dp[0] = grid[0][0];
+        Arrays.fill(dp, Integer.MAX_VALUE);
 
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
-                if (r == 0 && c == 0) continue;
-                int top = r > 0 ? dp[c] : Integer.MAX_VALUE;
+                if (r == 0 && c == 0) {
+                    dp[c] = grid[r][c];
+                    continue;
+                }
+                int top = dp[c];
                 int left = c > 0 ? dp[c - 1] : Integer.MAX_VALUE;
                 dp[c] = grid[r][c] + Math.min(top, left);
             }
