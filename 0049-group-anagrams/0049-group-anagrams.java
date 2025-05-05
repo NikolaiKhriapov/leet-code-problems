ab2afd6ef621a5ec552e7061d57519e37ff0d1bc
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-05 12:48:21 +0800

    Time: 6 ms (98.01%), Space: 48.3 MB (29.67%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         if (strs == null || strs.length == 0) {
-            return new ArrayList<>();
+            throw new IllegalArgumentException("Invalid input"); //for simplicity
         }
-        
+
         Map<String, List<String>> map = new HashMap<>();
         for (String str : strs) {
-            char[] charArr = str.toCharArray();
-            Arrays.sort(charArr);
-            String key = new String(charArr);
+            char[] strChars = str.toCharArray();
+            Arrays.sort(strChars);
+            String key = new String(strChars);
             List<String> value = map.getOrDefault(key, new ArrayList<>());
             value.add(str);
             map.put(key, value);
         }
-
+        
         List<List<String>> result = new ArrayList<>();
         for (var entry : map.entrySet()) {
             result.add(entry.getValue());
