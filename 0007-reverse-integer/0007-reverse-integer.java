56f861edc1041163179b6cf162581325e28dd921
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-05 09:58:48 +0800

    Time: 1 ms (83.82%), Space: 41.5 MB (6.22%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,21 +1,18 @@
 class Solution {
     public int reverse(int x) {
-
         int sign = x < 0 ? -1 : 1;
-        long xLong = Math.abs((long) x);
+        int xPos = Math.abs(x);
 
-        long result = 0;
-        while (xLong > 0) {
-            long digit = xLong % 10;
-            
+        int result = 0;
+        while (xPos > 0) {
+            int digit = xPos % 10;
             if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && digit > (sign == 1 ? 7 : 8))) {
                 return 0;
             }
-
             result = result * 10 + digit;
-            xLong /= 10;
+            xPos /= 10;
         }
         
-        return (int) (sign * result);
+        return sign * result;
     }
 }
