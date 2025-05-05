d95a76843406edf10442f7f8dbc8af53ef01c981
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-05 13:02:11 +0800

    Time: 1 ms (99.61%), Space: 56.7 MB (94.44%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,15 +1,20 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         if (nums == null || nums.length == 0) {
-            throw new IllegalArgumentException("Invalid input"); //for simplicity
+            throw new IllegalArgumentException("Invalid input"); // for simplicity
+        }
+        if (nums.length == 1) {
+            return nums[0];
         }
 
-        int maxSum = Integer.MIN_VALUE;
-        int sum = 0;
-        for (int n : nums) {
-            sum = Math.max(sum + n, n);
+        int maxSum = nums[0];
+        int sum = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(sum + nums[i], nums[i]);
             maxSum = Math.max(maxSum, sum);
         }
+
         return maxSum;
     }
 }
