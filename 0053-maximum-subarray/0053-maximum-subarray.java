486cb2bdfe012596311c1ddf219a0f8f75a86766
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-05 13:02:24 +0800

    Time: 1 ms (99.61%), Space: 56.8 MB (81.48%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,9 +3,6 @@ class Solution {
         if (nums == null || nums.length == 0) {
             throw new IllegalArgumentException("Invalid input"); // for simplicity
         }
-        if (nums.length == 1) {
-            return nums[0];
-        }
 
         int maxSum = nums[0];
         int sum = nums[0];
