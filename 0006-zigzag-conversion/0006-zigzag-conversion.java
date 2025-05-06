079a98020f4b613d476f60a23a4e37004cb4ddc6
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-06 21:22:03 +0800

    Time: 5 ms (61.03%), Space: 45 MB (69.75%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,30 +1,30 @@
 class Solution {
     public String convert(String s, int numRows) {
-        if (s == null || numRows < 0) {
-            throw new IllegalArgumentException("Invalid input"); // for simplicity
-        }
-        if (numRows == 0) {
-            return "";
-        }
-        if (s.length() <= 2 || numRows == 1) {
+        if (s == null || s.isEmpty() || numRows == 1) {
             return s;
         }
-
+        if (numRows <= 0) {
+            return "";
+        }
+        
         StringBuilder[] buckets = new StringBuilder[numRows];
         for (int i = 0; i < buckets.length; i++) {
             buckets[i] = new StringBuilder();
         }
+        populateBuckets(s, buckets);
+        return mergeBuckets(buckets);
+    }
 
+    private void populateBuckets(String s, StringBuilder[] buckets) {
         int bucketIndex = 0;
         boolean isForward = true;
-        
         for (int i = 0; i < s.length(); i++) {
             buckets[bucketIndex].append(s.charAt(i));
 
             if (bucketIndex == 0) {
                 isForward = true;
             }
-            if (bucketIndex == numRows - 1) {
+            if (bucketIndex == buckets.length - 1) {
                 isForward = false;
             }
 
@@ -34,12 +34,13 @@ class Solution {
                 bucketIndex--;
             }
         }
+    }
 
+    private String mergeBuckets(StringBuilder[] buckets) {
         StringBuilder result = new StringBuilder();
         for (StringBuilder bucket : buckets) {
             result.append(bucket);
         }
-        
         return result.toString();
     }
 }
