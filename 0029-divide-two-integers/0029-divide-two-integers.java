b7e92cdad8f7b52afcf0c29c3cb0b23519826ecf
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-06 21:53:49 +0800

    Time: 1 ms (74.98%), Space: 40.8 MB (76.46%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,5 +1,11 @@
 class Solution {
     public int divide(int dividend, int divisor) {
+        if (divisor == 0) {
+            throw new IllegalArgumentException("Invalid input"); // for simplicity
+        }
+        if (dividend == 0) {
+            return 0;
+        }
         if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE;
         }
@@ -14,13 +20,13 @@ class Solution {
         int result = 0;
         while (dividendNeg <= divisorNeg) {
             int temp = divisorNeg;
-            int multiply = 1;
-            while (temp >= Integer.MIN_VALUE / 2 && dividendNeg < temp + temp) {
+            int multiple = 1;
+            while (temp >= Integer.MIN_VALUE / 2 && dividendNeg <= temp + temp) {
                 temp += temp;
-                multiply += multiply;
+                multiple += multiple;
             }
             dividendNeg -= temp;
-            result += multiply;
+            result += multiple;
         }
         
         return sign * result;
