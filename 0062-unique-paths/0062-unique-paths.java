d3babd2cdd22d41a7799af530cdb85bd9369191a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-07 18:25:32 +0800

    Time: 0 ms (100%), Space: 40.7 MB (27.13%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -6,11 +6,11 @@ class Solution {
 
         int[] dp = new int[m];
         Arrays.fill(dp, 1);
-        for (int r = 1; r < n; r++) {
-            for (int c = 0; c < m; c++) {
-                int top = dp[c];
-                int left = c > 0 ? dp[c - 1] : 0;
-                dp[c] = top + left;
+        for (int row = 1; row < n; row++) {
+            for (int col = 0; col < m; col++) {
+                int top = dp[col];
+                int left = col > 0 ? dp[col - 1] : 0;
+                dp[col] = top + left;
             }
         }
         return dp[m - 1];
