26125f76054dfacce80d883a19916cce46ddc412
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-07 19:28:10 +0800

    Time: 3 ms (98.83%), Space: 43.3 MB (73.16%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,14 +1,9 @@
 class Solution {
     public String simplifyPath(String path) {
 
-        // init stack
         Stack<String> stack = new Stack<>();
-
-        // split path by `/`
         String[] parts = path.split("/");
 
-        // for-each 
-            // if (..) pop() else if (.) continue else add
         for (String part : parts) {
             if (part.isEmpty() || Objects.equals(part, ".")) {
                 continue;
@@ -21,12 +16,10 @@ class Solution {
                 stack.add(part);
             }
         }
-        Collections.reverse(stack);
 
-        // for each append to stringbuilder
         StringBuilder result = new StringBuilder();
-        while (!stack.isEmpty()) {
-            result.append("/").append(stack.pop());
+        for (String el : stack) {
+            result.append("/").append(el);
         }
         if (result.isEmpty()) {
             result.append("/");
