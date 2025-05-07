fd748c0c10a96e019a560db8caa16f63e693f04b
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-07 18:57:51 +0800

    Time: 4 ms (61.97%), Space: 47.3 MB (89.73%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,24 +1,22 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        if (grid == null || grid.length == 0 || grid[0].length == 0) {
-            throw new IllegalArgumentException("Invalid input"); // for simplicity
-        }
 
         int rows = grid.length;
         int cols = grid[0].length;
 
-        for (int c = 1; c < cols; c++) {
-            grid[0][c] += grid[0][c - 1];
-        }
-        for (int r = 1; r < rows; r++) {
-            grid[r][0] += grid[r - 1][0];
-        }
-        for (int r = 1; r < rows; r++) {
-            for (int c = 1; c < cols; c++) {
-                grid[r][c] += Math.min(grid[r - 1][c], grid[r][c - 1]);
+        int[] dp = new int[cols];
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (row == 0 && col == 0) {
+                    dp[col] = grid[row][col];
+                    continue;
+                }
+                int top = row > 0 ? dp[col] : Integer.MAX_VALUE;
+                int left = col > 0 ? dp[col - 1] : Integer.MAX_VALUE;
+                dp[col] = grid[row][col] + Math.min(top, left);
             }
         }
-
-        return grid[rows - 1][cols - 1];
+        
+        return dp[cols - 1];
     }
 }
