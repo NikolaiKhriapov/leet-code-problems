cecb38e136d7d6b438f3624f82c9cca46f626222
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-07 18:46:24 +0800

    Time: 0 ms (100%), Space: 41.4 MB (98.64%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,12 +1,13 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length <= 0 || obstacleGrid[0].length <= 0) {
+            return 0;
+        }
 
         int rows = obstacleGrid.length;
         int cols = obstacleGrid[0].length;
 
         int[] dp = new int[cols];
-        dp[0] = 1;
-
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
                 if (obstacleGrid[row][col] == 1) {
@@ -14,6 +15,7 @@ class Solution {
                     continue;
                 } else {
                     if (row == 0 && col == 0) {
+                        dp[col] = 1;
                         continue;
                     }
                     int top = dp[col];
@@ -22,7 +24,6 @@ class Solution {
                 }
             }
         }
-        
         return dp[cols - 1];
     }
 }
