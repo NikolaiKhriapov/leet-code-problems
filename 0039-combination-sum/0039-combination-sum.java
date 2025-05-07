45593c22fd5980c41659a89789cd14f4ce6580c1
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-07 14:36:13 +0800

    Time: 2 ms (83.2%), Space: 44.5 MB (91.95%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        if (candidates == null || target < 0) {
-            throw new IllegalArgumentException("Invalid input"); // for simplicity
-        }
-        if (candidates.length == 0) {
+        if (candidates == null || candidates.length == 0) {
             return new ArrayList<>();
         }
 
         Arrays.sort(candidates);
         List<List<Integer>> result = new ArrayList<>();
-        helper(candidates, 0, target, new ArrayList<>(), result);
+        addCombinationsToList(candidates, target, 0, new ArrayList<>(), result);
         return result;
     }
 
-    private void helper(int[] candidates, int start, int target, List<Integer> curr, List<List<Integer>> result) {
+    private void addCombinationsToList(int[] candidates, int target, int start, List<Integer> curr, List<List<Integer>> result) {
+        if (target < 0) {
+            return;
+        }
         if (target == 0) {
             result.add(new ArrayList<>(curr));
             return;
@@ -24,7 +24,7 @@ class Solution {
                 break;
             }
             curr.add(candidates[i]);
-            helper(candidates, i, target - candidates[i], curr, result);
+            addCombinationsToList(candidates, target - candidates[i], i, curr, result);
             curr.remove(curr.size() - 1);
         }
     }
