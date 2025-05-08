623b881872577061d95b89add56157e051447476
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-08 18:27:33 +0800

    Time: 0 ms (100%), Space: 42.2 MB (65.9%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,30 +1,45 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
 
-        int row = binarySearch1(matrix, target, 0, matrix.length - 1);
+        int row = binarySearchRows(matrix, target, 0, matrix.length - 1);
         if (row < 0 || row >= matrix.length) {
             return false;
         }
-        return binarySearch2(matrix, target, row, 0, matrix[0].length - 1);
+        return binarySearchCol(matrix, target, row, 0, matrix[0].length - 1);
     }
 
-    private int binarySearch1(int[][] matrix, int target, int l, int r) {
-        if (l > r) return r;
+    private int binarySearchRows(int[][] matrix, int target, int left, int right) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-        int m = l + (r - l) / 2;
-
-        if (matrix[m][0] == target) return m;
-        if (matrix[m][0] > target) return binarySearch1(matrix, target, l, m - 1);
-        else return binarySearch1(matrix, target, m + 1, r);
+            if (matrix[mid][0] == target) {
+                return mid;
+            }
+            if (matrix[mid][0] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
     }
 
-    private boolean binarySearch2(int[][] matrix, int target, int row, int l, int r) {
-        if (l > r) return false;
+    private boolean binarySearchCol(int[][] matrix, int target, int row, int left, int right) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-        int m = l + (r - l) / 2;
-
-        if (matrix[row][m] == target) return true;
-        if (matrix[row][m] > target) return binarySearch2(matrix, target, row, l, m - 1);
-        else return binarySearch2(matrix, target, row, m + 1, r);
+            if (matrix[row][mid] == target) {
+                return true;
+            }
+            if (matrix[row][mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
     }
 }
