0da4f3e5616c8bab5f359239ceaa0e8dacf708d4
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-08 17:40:48 +0800

    Time: 1 ms (78%), Space: 46.1 MB (25.75%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,18 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if (prices == null) {
-            throw new IllegalArgumentException("Invalid input"); // for simplicity
-        }
-        if (prices.length <= 1) {
+        if (prices == null || prices.length <= 1) {
             return 0;
         }
 
-        int maxProfit = 0;        
+        int profit = 0;
         for (int i = 1; i < prices.length; i++) {
             if (prices[i] > prices[i - 1]) {
-                maxProfit += prices[i] - prices[i - 1];
+                profit += prices[i] - prices[i - 1];
             }
         }
-        return maxProfit;
+        return profit;
     }
 }
