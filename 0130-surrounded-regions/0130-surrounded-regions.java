b02747b79c13e83fc4423be29d12497485588166
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-08 18:00:31 +0800

    Time: 2 ms (84.3%), Space: 46 MB (16.55%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -2,44 +2,36 @@ class Solution {
     private static final char MARK_O = 'O';
     private static final char MARK_X = 'X';
     private static final char MARK_TEMP = '.';
+    private static final int[][] NEIGHBORS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
     public void solve(char[][] board) {
-        if (board == null) {
-            throw new IllegalArgumentException("Invalid input"); // for simplicity
-        }
-        if (board.length == 0 || board[0].length == 0) {
-            return;
-        }
 
-        markEdgeRegions(board);
-        captureSurroundedAndUnmarkSafeRegions(board);
+        temporarilyMarkEdgeRegions(board);
+        markCapturedRegionsAndUnmarkTemporarilyMarkedRegions(board);
     }
 
-    private void markEdgeRegions(char[][] board) {
+    private void temporarilyMarkEdgeRegions(char[][] board) {
         for (int r = 0; r < board.length; r++) {
-            markEdgeRegion(board, r, 0);
-            markEdgeRegion(board, r, board[0].length - 1);
+            temporarilyMarkRegion(board, r, 0);
+            temporarilyMarkRegion(board, r, board[0].length - 1);
         }
         for (int c = 0; c < board[0].length; c++) {
-            markEdgeRegion(board, 0, c);
-            markEdgeRegion(board, board.length - 1, c);
+            temporarilyMarkRegion(board, 0, c);
+            temporarilyMarkRegion(board, board.length - 1, c);
         }
     }
 
-    private void markEdgeRegion(char[][] board, int r, int c) {
-        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] != MARK_O) {
-            return;
-        }
+    private void temporarilyMarkRegion(char[][] board, int r, int c) {
+        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length) return;
+        if (board[r][c] != MARK_O) return;
 
         board[r][c] = MARK_TEMP;
-
-        markEdgeRegion(board, r + 1, c);
-        markEdgeRegion(board, r, c + 1);
-        markEdgeRegion(board, r - 1, c);
-        markEdgeRegion(board, r, c - 1);
+        for (int[] neighbor : NEIGHBORS) {
+            temporarilyMarkRegion(board, r + neighbor[0], c + neighbor[1]);
+        }
     }
 
-    private void captureSurroundedAndUnmarkSafeRegions(char[][] board) {
+    private void markCapturedRegionsAndUnmarkTemporarilyMarkedRegions(char[][] board) {
         for (int r = 0; r < board.length; r++) {
             for (int c = 0; c < board[0].length; c++) {
                 if (board[r][c] == MARK_O) {
