1a1f95e46d12abd13af61f22e22e4d208bd5cc61
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-08 15:08:37 +0800

    Time: 0 ms (100%), Space: 43.3 MB (68.31%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,7 +15,6 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-
         return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
@@ -26,7 +25,6 @@ class Solution {
         if (node.val <= minLeft || node.val >= maxRight) {
             return false;
         }
-
         return isValidBST(node.left, minLeft, node.val) && isValidBST(node.right, node.val, maxRight);
     }
 }
