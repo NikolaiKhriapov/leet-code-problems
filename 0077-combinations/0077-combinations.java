a26b2406d39f4a8a687f1fdad68f080f608b6dde
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-08 18:52:18 +0800

    Time: 20 ms (37.82%), Space: 95.1 MB (12.21%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -5,11 +5,11 @@ class Solution {
         }
 
         List<List<Integer>> result = new ArrayList<>();
-        helper(n, k, 1, new ArrayList<>(), result);
+        backtrack(n, k, 1, new ArrayList<>(), result);
         return result;
     }
 
-    private void helper(int n, int k, int start, List<Integer> curr, List<List<Integer>> result) {
+    private void backtrack(int n, int k, int start, List<Integer> curr, List<List<Integer>> result) {
         if (curr.size() == k) {
             result.add(new ArrayList<>(curr));
             return;
@@ -17,7 +17,7 @@ class Solution {
 
         for (int i = start; i <= n; i++) {
             curr.add(i);
-            helper(n, k, i + 1, curr, result);
+            backtrack(n, k, i + 1, curr, result);
             curr.remove(curr.size() - 1);
         }
     }
