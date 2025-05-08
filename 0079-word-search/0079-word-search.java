8fd2e3501dddb7ea6c285cda9c0bcad651275067
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-08 15:49:53 +0800

    Time: 197 ms (28.47%), Space: 41.8 MB (50.52%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,34 +1,33 @@
 class Solution {
+    private static final int[][] NEIGHBORS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
     public boolean exist(char[][] board, String word) {
 
-        boolean[][] visited = new boolean[board.length][board[0].length];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    if (exist(board, 0, i, j, word, visited)) {
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                if (board[r][c] == word.charAt(0)) {
+                    if (exists(board, r, c, word, 0, new boolean[board.length][board[0].length])) {
                         return true;
                     }
                 }
             }
         }
-
         return false;
     }
 
-    private boolean exist(char[][] board, int index, int row, int col, String word, boolean[][] visited) {
-        if (index == word.length()) return true;
+    private boolean exists(char[][] board, int row, int col, String word, int index, boolean[][] visited) {
+        if (index >= word.length()) return true;
         if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) return false;
-        if (board[row][col] != word.charAt(index)) return false;
-        
-        if (visited[row][col]) return false;
+        if (board[row][col] != word.charAt(index) || visited[row][col]) return false;
+
         visited[row][col] = true;
-
-        boolean found = exist(board, index + 1, row + 1, col, word, visited) ||
-                        exist(board, index + 1, row, col + 1, word, visited) ||
-                        exist(board, index + 1, row - 1, col, word, visited) ||
-                        exist(board, index + 1, row, col - 1, word, visited);
-
+        for (int[] neighbor : NEIGHBORS) {
+            if (exists(board, row + neighbor[0], col + neighbor[1], word, index + 1, visited)) {
+                return true;
+            }
+        }
         visited[row][col] = false;
-        return found;
+
+        return false;
     }
 }
