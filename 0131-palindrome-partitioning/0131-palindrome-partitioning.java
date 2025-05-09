abdf5c43ed0d7bb6f995dd4b88deb7bee1c7def6
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-09 22:20:20 +0800

    Time: 8 ms (55.06%), Space: 56.7 MB (79.49%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,51 +1,34 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        if (s == null || s.length() == 0) {
-            return new ArrayList<>();
-        }
 
-        Boolean[][] memo = new Boolean[s.length()][s.length()];
         List<List<String>> result = new ArrayList<>();
-        partition(s, 0, new ArrayList<>(), result, memo);
+        partition(s, 0, new ArrayList<>(), result);
         return result;
     }
 
-    private void partition(String s, int index, List<String> curr, List<List<String>> result, Boolean[][] memo) {
-        if (index == s.length()) {
+    private void partition(String s, int start, List<String> curr, List<List<String>> result) {
+        if (start >= s.length()) {
             result.add(new ArrayList<>(curr));
             return;
         }
 
-        int left = index;
-        int right = index;
-        while (right < s.length()) { 
-            if (isPalindrome(s, left, right, memo)) {
-                curr.add(s.substring(left, right + 1));
-                partition(s, right + 1, curr, result, memo);
+        for (int i = start; i < s.length(); i++) {
+            if (isPalindrome(s, start, i)) {
+                curr.add(s.substring(start, i + 1));
+                partition(s, i + 1, curr, result);
                 curr.remove(curr.size() - 1);
             }
-            right++;
         }
     }
 
-    private boolean isPalindrome(String s, int left, int right, Boolean[][] memo) {
-        if (memo[left][right] != null) {
-            return memo[left][right];
-        }
-
-        int l = left;
-        int r = right;
+    private boolean isPalindrome(String s, int l, int r) {
         while (l < r) {
             if (s.charAt(l) != s.charAt(r)) {
-                memo[l][r] = false;
                 return false;
             }
-
             l++;
             r--;
         }
-        
-        memo[left][right] = true;
         return true;
     }
 }
