547c01e315c7740ade3f969838f4338a34af4207
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-09 14:20:54 +0800

    Time: 0 ms (100%), Space: 46.8 MB (29.29%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,19 +1,24 @@
 class Solution {
-    private static final int FREQUENCY = 2;
-    
+    private static final int MAX_FREQUENCY = 2;
+
     public int removeDuplicates(int[] nums) {
-        if (nums.length <= FREQUENCY) {
+        if (nums.length <= MAX_FREQUENCY) {
             return nums.length;
         }
         
-        int left = FREQUENCY;
-        for (int i = FREQUENCY; i < nums.length; i++) {
-            if (nums[i] != nums[left - FREQUENCY]) {
-                nums[left] = nums[i];
+        int left = MAX_FREQUENCY;
+        for (int i = MAX_FREQUENCY; i < nums.length; i++) {
+            if (nums[i] != nums[left - MAX_FREQUENCY]) {
+                swap(nums, i, left);
                 left++;
             }
         }
-
         return left;
+    }
+
+    private void swap(int[] arr, int l, int r) {
+        int temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
     }
 }
