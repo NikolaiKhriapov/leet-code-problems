0925b4661f03e110e34b8f7f8293198e0fe191f3
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-09 15:01:22 +0800

    Time: 1 ms (84.24%), Space: 42 MB (50.3%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,22 +1,23 @@
 class Solution {
     public int numDecodings(String s) {
-        if (s == null || s.isEmpty() || s.startsWith("0")) {
+        if (s.startsWith("0")) {
             return 0;
         }
 
         int[] dp = new int[s.length() + 1];
-        dp[dp.length - 1] = 1;
-        for (int i = dp.length - 2; i >= 0; i--) {
-            int num = s.charAt(i) - '0';
-            if (num == 0) {
+        dp[s.length()] = 1;
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
                 dp[i] = 0;
-            } else {
-                dp[i] = dp[i + 1];
-                if (i < s.length() - 1 && Integer.parseInt(s.substring(i, i + 2)) <= 26) {
-                    dp[i] += dp[i + 2];
-                }
+                continue;
+            }
+            dp[i] = dp[i + 1];
+            if (i < s.length() - 1 && Integer.parseInt(s.substring(i, i + 2)) <= 26) {
+                dp[i] += dp[i + 2];
             }
         }
+
         return dp[0];
     }
 }
