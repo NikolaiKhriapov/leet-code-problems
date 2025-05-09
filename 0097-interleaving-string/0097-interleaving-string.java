7f586c4cbd09846a0fd59d31935a0204409e6e9e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-09 11:49:24 +0800

    Time: 3 ms (68.65%), Space: 41.7 MB (70.81%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,41 +1,37 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1.length() + s2.length() != s3.length()) return false;
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
 
-        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
-        dp[0][0] = true;
+        int rows = s1.length();
+        int cols = s2.length();
 
-        for (int r = 0; r < dp.length; r++) {
-            for (int c = 0; c < dp[0].length; c++) {
-                if (r > 0 && dp[r - 1][c] && s1.charAt(r - 1) == s3.charAt(r + c - 1)) {
-                    dp[r][c] = true;
-                }
-                if (c > 0 && dp[r][c - 1] && s2.charAt(c - 1) == s3.charAt(r + c - 1)) {
-                    dp[r][c] = true;
-                }
+        boolean[] dp = new boolean[cols + 1];
+        dp[0] = true;
+
+        for (int c = 1; c <= cols; c++) {
+            dp[c] = dp[c - 1] && s2.charAt(c - 1) == s3.charAt(c - 1);
+        }
+        
+        for (int r = 1; r <= rows; r++) {
+            dp[0] = dp[0] && s1.charAt(r - 1) == s3.charAt(r - 1);
+            for (int c = 1; c <= cols; c++) {
+                dp[c] = (dp[c] && s1.charAt(r - 1) == s3.charAt(r + c - 1)) ||
+                        (dp[c - 1] && s2.charAt(c - 1) == s3.charAt(r + c - 1));
             }
         }
 
-        return dp[dp.length - 1][dp[0].length - 1];
+        return dp[cols];
     }
 }
 
 //      d b b c a
-//   [+ - - - - -]
-// a [+ - - - - -]
-// a [+ + + + + -]
-// b [- + + - + -]
-// c [- - + + + +]
-// c [- - - + - +]
-
+//   [+,-,-,-,-,-]
+// a [+,-,-,-,-,-]
+// a [+,+,+,+,+,-]
+// b [-,+,+,-,+,-]
+// c [-,-,+,+,+,+]
+// c [-,-,-,+,-,+]
+//
 // a a d b b c b c a c
-
-//      d b b c a
-//   [+ - - - - -]
-// a [+ - - - - -]
-// a [+ + + + - -]
-// b [- + + + - -]
-// c [- - - - - -]
-// c [- - - - - -]
-
-// a a d b b b a c c c
