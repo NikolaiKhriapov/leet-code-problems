6de48e48de771500c9d38b7d3fc7a2eb0064f141
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-09 22:25:23 +0800

    Time: 7 ms (97.87%), Space: 57.8 MB (5.17%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -5,33 +5,41 @@ class Solution {
         }
 
         List<List<String>> result = new ArrayList<>();
-        partition(s, 0, new ArrayList<>(), result);
+        Boolean[][] memo = new Boolean[s.length()][s.length()];
+        partition(s, 0, new ArrayList<>(), result, memo);
         return result;
     }
 
-    private void partition(String s, int start, List<String> curr, List<List<String>> result) {
+    private void partition(String s, int start, List<String> curr, List<List<String>> result, Boolean[][] memo) {
         if (start >= s.length()) {
             result.add(new ArrayList<>(curr));
             return;
         }
 
         for (int i = start; i < s.length(); i++) {
-            if (isPalindrome(s, start, i)) {
+            if (isPalindrome(s, start, i, memo)) {
                 curr.add(s.substring(start, i + 1));
-                partition(s, i + 1, curr, result);
+                partition(s, i + 1, curr, result, memo);
                 curr.remove(curr.size() - 1);
             }
         }
     }
 
-    private boolean isPalindrome(String s, int l, int r) {
+    private boolean isPalindrome(String s, int l, int r, Boolean[][] memo) {
+        if (memo[l][r] != null) {
+            return memo[l][r];
+        }
+
         while (l < r) {
             if (s.charAt(l) != s.charAt(r)) {
+                memo[l][r] = false;
                 return false;
             }
             l++;
             r--;
         }
+
+        memo[l][r] = true;
         return true;
     }
 }
