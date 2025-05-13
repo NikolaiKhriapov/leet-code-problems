3390d2b2aa1090713576852b4bad550b378d181e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-13 12:57:49 +0800

    Time: 4 ms (61.94%), Space: 48.4 MB (5.03%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -6,19 +6,16 @@ class Solution {
 
         int rows = grid.length;
         int cols = grid[0].length;
-
-        int[] dp = new int[cols];
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
                 if (row == 0 && col == 0) {
-                    dp[col] = grid[row][col];
                     continue;
                 }
-                int top = row > 0 ? dp[col] : Integer.MAX_VALUE;
-                int left = col > 0 ? dp[col - 1] : Integer.MAX_VALUE;
-                dp[col] = grid[row][col] + Math.min(top, left);
+                int top = row > 0 ? grid[row - 1][col] : Integer.MAX_VALUE;
+                int left = col > 0 ? grid[row][col - 1] : Integer.MAX_VALUE;
+                grid[row][col] += Math.min(top, left);
             }
         }
-        return dp[cols - 1];
+        return grid[rows - 1][cols - 1];
     }
 }
