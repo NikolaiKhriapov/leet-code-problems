b34ef39500e7aed08a869d0080005624022744e1
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-13 12:43:17 +0800

    Time: 0 ms (100%), Space: 41.7 MB (80.97%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,30 +1,29 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0) {
-            throw new IllegalArgumentException("Invalid input"); // for simplicity
+        if (obstacleGrid == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        if (obstacleGrid.length == 0 || obstacleGrid[0].length == 0) {
+            return 0;
         }
 
         int rows = obstacleGrid.length;
         int cols = obstacleGrid[0].length;
 
         int[] dp = new int[cols];
-
+        dp[0] = 1;
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
                 if (obstacleGrid[row][col] == 1) {
                     dp[col] = 0;
                     continue;
                 }
-                if (row == 0 && col == 0) {
-                    dp[col] = 1;
-                    continue;
-                }
-                int top = row > 0 ? dp[col] : 0;
+                int top = dp[col];
                 int left = col > 0 ? dp[col - 1] : 0;
                 dp[col] = top + left;
             }
         }
-        
+
         return dp[cols - 1];
     }
 }
