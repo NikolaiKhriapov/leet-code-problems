95e511c722ebfa38c40f65312650a7bd832bfa75
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-13 12:55:08 +0800

    Time: 4 ms (61.94%), Space: 48.2 MB (5.03%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,5 +1,8 @@
 class Solution {
     public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return -1;
+        }
 
         int rows = grid.length;
         int cols = grid[0].length;
