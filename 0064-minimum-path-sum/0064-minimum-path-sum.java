3fa057e850d715266dc89b8a09cdac68cd7a458f
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-13 12:54:16 +0800

    Time: 4 ms (61.94%), Space: 48.2 MB (5.03%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,14 +1,10 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        if (grid == null || grid.length == 0 || grid[0].length == 0) {
-            throw new IllegalArgumentException("Invalid input"); // for simplicity
-        }
 
         int rows = grid.length;
         int cols = grid[0].length;
 
         int[] dp = new int[cols];
-
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
                 if (row == 0 && col == 0) {
@@ -20,7 +16,6 @@ class Solution {
                 dp[col] = grid[row][col] + Math.min(top, left);
             }
         }
-        
         return dp[cols - 1];
     }
 }
