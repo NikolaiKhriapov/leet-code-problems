df4bc934459091b45c91d8eae7448a1c40a494e0
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-14 12:52:42 +0800

    Time: 1 ms (95.47%), Space: 44.3 MB (64.26%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,27 +14,29 @@
  * }
  */
 class Solution {
+    int preorderIndex = 0;
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if (preorder.length != inorder.length || inorder.length == 0) {
-            return null;
+        if (preorder == null || inorder == null || preorder.length != inorder.length) {
+            throw new IllegalArgumentException("Invalid input");
         }
-        
+
         Map<Integer, Integer> inorderMap = buildInorderMap(inorder);
-        return helper(preorder, 0, inorder.length - 1, inorderMap, new int[]{0});
+        return helper(preorder, 0, inorder.length - 1, inorderMap);
     }
 
-    private TreeNode helper(int[] preorder, int inorderLeft, int inorderRight, Map<Integer, Integer> inorderMap, int[] preorderIndex) {
-        if (inorderLeft > inorderRight) {
+    private TreeNode helper(int[] preorder, int inorderStart, int inorderEnd, Map<Integer, Integer> inorderMap) {
+        if (inorderStart > inorderEnd) {
             return null;
         }
 
-        int preorderValue = preorder[preorderIndex[0]++];
+        int preorderValue = preorder[preorderIndex++];
         int inorderIndex = inorderMap.get(preorderValue);
 
         TreeNode root = new TreeNode(preorderValue);
-        root.left = helper(preorder, inorderLeft, inorderIndex - 1, inorderMap, preorderIndex);
-        root.right = helper(preorder, inorderIndex + 1, inorderRight, inorderMap, preorderIndex);
-        
+        root.left = helper(preorder, inorderStart, inorderIndex - 1, inorderMap);
+        root.right = helper(preorder, inorderIndex + 1, inorderEnd, inorderMap);
+
         return root;
     }
 
