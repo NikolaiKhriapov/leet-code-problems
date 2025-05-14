3a3cf446f730b409f257f8e9378f892df154065c
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-14 20:16:01 +0800

    Time: 2 ms (97.55%), Space: 55.4 MB (96.58%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -7,18 +7,16 @@ class Solution {
         int currentTank = 0;
         int totalTank = 0;
         int startingStation = 0;
-
         for (int i = 0; i < gas.length; i++) {
             int gasGain = gas[i] - cost[i];
             currentTank += gasGain;
             totalTank += gasGain;
-            
+
             if (currentTank < 0) {
-                startingStation = i + 1;
                 currentTank = 0;
+                startingStation = i + 1;
             }
         }
-
         return totalTank >= 0 ? startingStation : -1;
     }
 }
