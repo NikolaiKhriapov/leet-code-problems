299379fe272e4fa1aca79d8e9aff8d82e8d58112
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-15 22:03:56 +0800

    Time: 0 ms (100%), Space: 42.5 MB (43.57%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,25 +1,25 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         if (nums == null || nums.length == 0) {
-            throw new IllegalArgumentException("Invalid input"); // for simplicity
+            throw new IllegalArgumentException("Invalid input");
+        }
+        if (nums.length == 1) {
+            return 0;
         }
 
-        return helper(nums, 0, nums.length - 1);
-    }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-    private int helper(int[] nums, int left, int right) {
-        if (left > right) {
-            return -1;
+            if (mid == 0 || nums[mid] > nums[mid - 1]) {
+                if (mid == nums.length - 1 || nums[mid] > nums[mid + 1]) return mid;
+                else left = mid + 1;
+            } else {
+                if (mid == 0 || nums[mid] > nums[mid - 1]) return mid;
+                else right = mid - 1;
+            }
         }
-
-        int mid = left + (right - left) / 2;
-
-        if (mid != 0 && nums[mid] < nums[mid - 1]) {
-            return helper(nums, left, mid - 1);
-        }
-        if (mid != nums.length - 1 && nums[mid] < nums[mid + 1]) {
-            return helper(nums, mid + 1, right);
-        }
-        return mid;
+        return -1;
     }
 }
