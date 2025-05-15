42207aedd367dfcc6ee64e467e2e05f8276a3efa
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-15 15:53:10 +0800

    Time: 2 ms (74.37%), Space: 45.3 MB (79.25%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        if (nums == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+
+        int[] bitCount = new int[32];
+
+        for (int num : nums) {
+            for (int i = 0; i < 32; i++) {
+                bitCount[i] += (num & 1);
+                num >>= 1;
+            }
+        }
+        
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            if (bitCount[i] % 3 != 0) {
+                int bit = 1 << i;
+                result += bit;
+            }
+        }
+
+        return result;
+    }
+}
