8fa857751d073216222cac6e1fbe68434c17a834
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-15 11:17:08 +0800

    Time: 3 ms (68.37%), Space: 41.5 MB (89.94%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -4,17 +4,17 @@ class Solution {
             return false;
         }
 
-        int rows = s1.length() + 1;
         int cols = s2.length() + 1;
-
         boolean[] dp = new boolean[cols];
+
         dp[0] = true;
         for (int col = 1; col < cols; col++) {
             if (dp[col - 1] && s2.charAt(col - 1) == s3.charAt(col - 1)) {
                 dp[col] = true;
             }
         }
-        for (int row = 1; row < rows; row++) {
+        
+        for (int row = 1; row <= s1.length(); row++) {
             dp[0] = dp[0] && s1.charAt(row - 1) == s3.charAt(row - 1);
             for (int col = 1; col < cols; col++) {
                 dp[col] = (dp[col] && s1.charAt(row - 1) == s3.charAt(row + col - 1))
