0147cde6afc5aea16b691aa920cb97de52f8140a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-15 11:13:42 +0800

    Time: 4 ms (51.94%), Space: 41.8 MB (56.82%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,26 +1,37 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1 == null || s2 == null || s3 == null || (s1.length() + s2.length() != s3.length())) {
+        if (s1 == null || s2 == null || s3 == null || s1.length() + s2.length() != s3.length()) {
             return false;
         }
 
         int rows = s1.length() + 1;
         int cols = s2.length() + 1;
-        boolean[] dp = new boolean[cols];
-        
-        dp[0] = true;
-        for (int col = 1; col < cols; col++) {
-            dp[col] = dp[col - 1] && s2.charAt(col - 1) == s3.charAt(col - 1);
-        }
+
+        boolean[][] dp = new boolean[rows][cols];
+        dp[0][0] = true;
         for (int row = 1; row < rows; row++) {
-            dp[0] = dp[0] && s1.charAt(row - 1) == s3.charAt(row - 1);
-            for (int col = 1; col < cols; col++) {
-                dp[col] = (dp[col] && s1.charAt(row - 1) == s3.charAt(row + col - 1))
-                       || (dp[col - 1] && s2.charAt(col - 1) == s3.charAt(row + col - 1));
+            if (dp[row - 1][0] && s1.charAt(row - 1) == s3.charAt(row - 1)) {
+                dp[row][0] = true;
             }
         }
-        
-        return dp[cols - 1];
+        for (int col = 1; col < cols; col++) {
+            if (dp[0][col - 1] && s2.charAt(col - 1) == s3.charAt(col - 1)) {
+                dp[0][col] = true;
+            }
+        }
+
+        for (int row = 1; row < rows; row++) {
+            for (int col = 1; col < cols; col++) {
+                if (row == 0 && col == 0) {
+                    dp[row][col] = true;
+                    continue;
+                }
+                dp[row][col] = (dp[row - 1][col] && s1.charAt(row - 1) == s3.charAt(row + col - 1))
+                            || (dp[row][col - 1] && s2.charAt(col - 1) == s3.charAt(row + col - 1));
+            }
+        }
+
+        return dp[rows - 1][cols - 1];
     }
 }
 
@@ -32,4 +43,4 @@ class Solution {
 // c [-,-,+,+,+,+]
 // c [-,-,-,+,-,+]
 
-// aadbbcbcac
+// a a d b b c b c a c
