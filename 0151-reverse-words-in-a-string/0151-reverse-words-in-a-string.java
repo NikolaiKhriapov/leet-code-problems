1cc4dfdc362224ecf16d6bacf12cbd31a0626abf
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-16 20:35:39 +0800

    Time: 3 ms (98.69%), Space: 43 MB (70.32%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -4,18 +4,24 @@ class Solution {
             return s;
         }
         
-        String[] parts = s.split("\\s+");
-        for (int i = 0; i < parts.length / 2; i++) {
-            String temp = parts[i];
-            parts[i] = parts[parts.length - 1 - i];
-            parts[parts.length - 1 - i] = temp;
+        StringBuilder sb = new StringBuilder();
+        int right = s.length() - 1;
+        while (right >= 0) {
+            while (right >= 0 && s.charAt(right) == ' ') {
+                right--;
+            }
+            if (right < 0) {
+                break;
+            }
+            int left = right;
+            
+            while (left >= 0 && s.charAt(left) != ' ') {
+                left--;
+            }
+            sb.append(s.substring(left + 1, right + 1))
+              .append(" ");
+            right = left;
         }
-
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < parts.length; i++) {
-            result.append(parts[i]).append(" ");
-        }
-
-        return result.toString().trim();
+        return sb.toString().trim();
     }
 }
