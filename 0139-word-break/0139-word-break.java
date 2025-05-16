faa81b4a27c0d7715c46848eb0318b0685995e01
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-16 13:53:23 +0800

    Time: 1 ms (99.33%), Space: 41.6 MB (95.02%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -3,22 +3,14 @@ class Solution {
         if (s == null || wordDict == null) {
             throw new IllegalArgumentException("Invalid input");
         }
-        if (s.isEmpty()) {
-            return true;
-        }
-        if (wordDict.size() == 0) {
-            return false;
-        }
 
-        Boolean[] memo = new Boolean[s.length()];
-        return wordBreak(s, wordDict, 0, memo);
+        return wordBreak(s, wordDict, 0, new Boolean[s.length()]);
     }
 
     private boolean wordBreak(String s, List<String> wordDict, int index, Boolean[] memo) {
         if (index == s.length()) {
             return true;
         }
-
         if (memo[index] != null) {
             return memo[index];
         }
