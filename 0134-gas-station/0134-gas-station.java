60a46c8b4dee4cf0e2836733b2b0b953086f5a95
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-16 14:29:58 +0800

    Time: 2 ms (97.48%), Space: 56.6 MB (15.36%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,22 +1,23 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        if (gas == null || cost == null || gas.length != cost.length) {
+        if (gas == null || cost == null || gas.length != cost.length || gas.length == 0) {
             throw new IllegalArgumentException("Invalid input");
         }
 
-        int currentTank = 0;
-        int totalTank = 0;
+        int currentGas = 0;
+        int totalGas = 0;
         int startingStation = 0;
+
         for (int i = 0; i < gas.length; i++) {
             int gasGain = gas[i] - cost[i];
-            currentTank += gasGain;
-            totalTank += gasGain;
-
-            if (currentTank < 0) {
-                currentTank = 0;
+            currentGas += gasGain;
+            totalGas += gasGain;
+            if (currentGas < 0) {
+                currentGas = 0;
                 startingStation = i + 1;
             }
         }
-        return totalTank >= 0 ? startingStation : -1;
+        
+        return totalGas >= 0 ? startingStation : -1;
     }
 }
