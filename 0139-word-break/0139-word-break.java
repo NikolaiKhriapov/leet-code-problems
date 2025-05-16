c3082a213476d9ecdb21314c50e5ac8c05417f01
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-16 13:48:40 +0800

    Time: 1 ms (99.33%), Space: 41.4 MB (98.39%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -3,28 +3,35 @@ class Solution {
         if (s == null || wordDict == null) {
             throw new IllegalArgumentException("Invalid input");
         }
-        return helper(s, wordDict, 0, new Boolean[s.length()]);
-    }
-
-    private boolean helper(String s, List<String> wordDict, int start, Boolean[] memo) {
-        if (start > s.length()) {
-            return false;
-        }
-        if (start == s.length()) {
+        if (s.isEmpty()) {
             return true;
         }
-        if (memo[start] != null) {
+        if (wordDict.size() == 0) {
             return false;
+        }
+
+        Boolean[] memo = new Boolean[s.length()];
+        return wordBreak(s, wordDict, 0, memo);
+    }
+
+    private boolean wordBreak(String s, List<String> wordDict, int index, Boolean[] memo) {
+        if (index == s.length()) {
+            return true;
+        }
+
+        if (memo[index] != null) {
+            return memo[index];
         }
 
         for (String word : wordDict) {
-            if (s.startsWith(word, start)) {
-                if (helper(s, wordDict, start + word.length(), memo)) {
+            if (s.startsWith(word, index)) {
+                if (wordBreak(s, wordDict, index + word.length(), memo)) {
+                    memo[index] = true;
                     return true;
                 }
             }
         }
-        memo[start] = false;
+        memo[index] = false;
         return false;
     }
 }
