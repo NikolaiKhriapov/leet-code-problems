e2770c29d12f7595b4d2c43a9f6539032217b182
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-16 13:15:38 +0800

    Time: 0 ms (100%), Space: 46.2 MB (94.34%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,21 +1,29 @@
 class Solution {
-    private static final int SEQUENCE = 2;
+    private static final int MAX_APPEARANCES = 2;
 
     public int removeDuplicates(int[] nums) {
         if (nums == null) {
             throw new IllegalArgumentException("Invalid input");
         }
-        if (nums.length <= SEQUENCE) {
+        if (nums.length <= MAX_APPEARANCES) {
             return nums.length;
         }
-        
-        int index = SEQUENCE;
-        for (int i = index; i < nums.length; i++) {
-            if (nums[i] != nums[index - SEQUENCE]) {
-                nums[index] = nums[i];
-                index++;
+
+        int left = MAX_APPEARANCES;
+        for (int i = MAX_APPEARANCES; i < nums.length; i++) {
+            if (nums[i] == nums[left - MAX_APPEARANCES]) {
+                continue;
+            } else {
+                swap(nums, i, left);
+                left++;
             }
         }
-        return index;
+        return left;
+    }
+
+    private void swap(int[] arr, int l, int r) {
+        int temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
     }
 }
