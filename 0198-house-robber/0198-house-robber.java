c44ba666183b0aa9f1cea90627c9662aacf497da
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-17 20:44:34 +0800

    Time: 0 ms (100%), Space: 41.1 MB (46.74%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,21 +1,20 @@
 class Solution {
     public int rob(int[] nums) {
-
-        Map<Integer, Integer> memo = new HashMap<>();
-        return Math.max(rob(nums, 0, memo), rob(nums, 1, memo));
-    }
-
-    private int rob(int[] nums, int index, Map<Integer, Integer> memo) {
-        if (index >= nums.length) {
-            return 0;
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        if (nums.length == 1) {
+            return nums[0];
         }
 
-        if (memo.containsKey(index)) {
-            return memo.get(index);
-        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
 
-        int result = nums[index] + Math.max(rob(nums, index + 2, memo), rob(nums, index + 3, memo));
-        memo.put(index, result);
-        return result;
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+        
+        return dp[dp.length - 1];
     }
 }
