d94fc360cbd9a26625d451932ffeab9325c7eae0
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-17 20:18:50 +0800

    Time: 2 ms (27.97%), Space: 57.5 MB (35.68%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,17 +1,19 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         if (nums == null || nums.length == 0 || k < 0) {
-            throw new IllegalArgumentException("Invalid input"); // for simplicity
+            throw new IllegalArgumentException("Invalid input");
         }
-        
-        k %= nums.length;
+        if (nums.length == 1 || k % nums.length == 0) {
+            return;
+        }
 
-        reverse(nums, 0, nums.length - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, nums.length - 1);
+        k %= nums.length;
+        mirror(nums, 0, nums.length - 1);
+        mirror(nums, 0, k - 1);
+        mirror(nums, k, nums.length - 1);
     }
 
-    private void reverse(int[] nums, int left, int right) {
+    private void mirror(int[] nums, int left, int right) {
         while (left < right) {
             int temp = nums[left];
             nums[left] = nums[right];
