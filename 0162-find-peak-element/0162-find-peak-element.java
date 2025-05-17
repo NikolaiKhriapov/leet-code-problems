df20ea28d0a069dcb91318248da8a9915f24b0b6
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-17 17:26:16 +0800

    Time: 0 ms (100%), Space: 42.6 MB (32.39%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -8,14 +8,10 @@ class Solution {
         int right = nums.length - 1;
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (mid == 0 || nums[mid] > nums[mid - 1]) {
-                if (mid == nums.length - 1 || nums[mid] > nums[mid + 1]) {
-                    return mid;
-                } else {
-                    left = mid + 1;
-                }
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
             } else {
-                right = mid - 1;
+                right = mid;
             }
         }
         return left;
