27c2a87ff0c087f0b223ac7ade9266b27797e033
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-17 15:46:00 +0800

    Time: 0 ms (100%), Space: 41.9 MB (86.77%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -4,17 +4,16 @@ class Solution {
             throw new IllegalArgumentException("Invalid input");
         }
         
-        int l = 0;
-        int r = nums.length - 1;
-        while (l < r) {
-            int m = l + (r - l) / 2;
-            
-            if (nums[m] > nums[r]) {
-                l = m + 1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
             } else {
-                r = m;
+                right = mid;
             }
         }
-        return nums[l];
+        return nums[left];
     }
 }
