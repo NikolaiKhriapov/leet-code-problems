f62ec9b673eba919d307c9344518fcbb16166cb7
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-17 15:28:54 +0800

    Time: 5 ms (97.43%), Space: 44.5 MB (81.52%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,9 +1,9 @@
 class Solution {
-    private static final Set<String> OPERATIONS = Set.of("+", "-", "/", "*");
+    private static final Set<String> OPERATIONS = Set.of("+", "-", "*", "/");
 
     public int evalRPN(String[] tokens) {
         if (tokens == null || tokens.length == 0) {
-            return 0;
+            throw new IllegalArgumentException("Invalid input");
         }
 
         Stack<Integer> stack = new Stack<>();
@@ -11,7 +11,7 @@ class Solution {
             if (OPERATIONS.contains(token)) {
                 int b = stack.pop();
                 int a = stack.pop();
-                stack.add(calculate(a, b, token));
+                stack.add(applyOperation(a, b, token));
             } else {
                 stack.add(Integer.parseInt(token));
             }
@@ -19,7 +19,7 @@ class Solution {
         return stack.pop();
     }
 
-    private int calculate(int a, int b, String operation) {
+    private Integer applyOperation(int a, int b, String operation) {
         return switch (operation) {
             case "+" -> a + b;
             case "-" -> a - b;
