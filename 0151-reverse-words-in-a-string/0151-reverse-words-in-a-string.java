b2416e5bad0c8915c2a12fe7fc0f1c47cb054e9a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-17 15:39:02 +0800

    Time: 3 ms (98.7%), Space: 42.9 MB (86.77%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -3,9 +3,11 @@ class Solution {
         if (s == null || s.isEmpty()) {
             return s;
         }
-        
+
         StringBuilder sb = new StringBuilder();
+
         int right = s.length() - 1;
+        int left = right;
         while (right >= 0) {
             while (right >= 0 && s.charAt(right) == ' ') {
                 right--;
@@ -13,18 +15,22 @@ class Solution {
             if (right < 0) {
                 break;
             }
-            int left = right;
             
+            left = right;
+
             while (left >= 0 && s.charAt(left) != ' ') {
                 left--;
             }
+
             sb.append(s.substring(left + 1, right + 1))
               .append(" ");
             right = left;
         }
+        
         if (sb.length() > 0) {
             sb.setLength(sb.length() - 1);
         }
+
         return sb.toString();
     }
 }
