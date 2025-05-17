7e655b8dba525b63141970bba88530a432276959
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-17 14:21:56 +0800

    Time: 2 ms (76.96%), Space: 43.2 MB (98.59%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,19 +1,24 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        if (triangle == null) {
+        if (triangle == null || triangle.isEmpty()) {
             throw new IllegalArgumentException("Invalid input");
         }
 
         int[] dp = new int[triangle.size()];
+
+        // last row
         for (int i = 0; i < dp.length; i++) {
             dp[i] = triangle.get(triangle.size() - 1).get(i);
         }
+        
+        // remaining rows
         for (int i = triangle.size() - 2; i >= 0; i--) {
             List<Integer> row = triangle.get(i);
             for (int j = 0; j < row.size(); j++) {
                 dp[j] = row.get(j) + Math.min(dp[j], dp[j + 1]);
             }
         }
+
         return dp[0];
     }
 }
