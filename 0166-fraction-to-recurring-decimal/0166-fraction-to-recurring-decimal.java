ec93f2554c0004ca36d558071a1a2c7a06694c6c
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-17 18:43:43 +0800

    Time: 1 ms (99.23%), Space: 41 MB (61.07%) - LeetHub

--- 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
@@ -1,32 +1,33 @@
 class Solution {
     public String fractionToDecimal(int numerator, int denominator) {
-        if (numerator == 0) return "0";
+        if (numerator == 0 || denominator == 0) {
+            return "0";
+        }
 
         StringBuilder result = new StringBuilder();
 
-        if ((numerator < 0 || denominator < 0) && !(numerator < 0 && denominator < 0)) {
+        if ((numerator > 0 && denominator < 0) || (numerator < 0 && denominator > 0)) {
             result.append("-");
         }
 
         long numeratorLong = Math.abs((long) numerator);
         long denominatorLong = Math.abs((long) denominator);
-
         result.append(numeratorLong / denominatorLong);
+
         long remainder = numeratorLong % denominatorLong;
         if (remainder == 0) {
             return result.toString();
         }
-        result.append(".");
         
+        result.append(".");
+
         Map<Long, Integer> map = new HashMap<>();
         while (remainder != 0) {
             if (map.containsKey(remainder)) {
-                int repeatIndex = map.get(remainder);
-                result.insert(repeatIndex, "(");
+                result.insert(map.get(remainder), "(");
                 result.append(")");
                 break;
             }
-
             map.put(remainder, result.length());
             remainder *= 10;
             result.append(remainder / denominatorLong);
@@ -36,3 +37,8 @@ class Solution {
         return result.toString();
     }
 }
+
+//  40 / 333 -> 0
+// 400 / 333 -> 1
+// 770 / 333 -> 2
+//  40 / 333 -> 0
