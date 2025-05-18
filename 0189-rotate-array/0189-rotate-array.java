89c1375ea69ee71a2349c5a13fe0f7aa73290a2c
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-18 23:16:48 +0800

    Time: 0 ms (100%), Space: 57.5 MB (35.77%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -6,18 +6,18 @@ class Solution {
 
         k %= nums.length;
 
-        mirror(nums, 0, nums.length - 1);
-        mirror(nums, 0, k - 1);
-        mirror(nums, k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
     }
 
-    private void mirror(int[] nums, int left, int right) {
-        while (left < right) {
-            int temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
-            left++;
-            right--;
+    private void reverse(int[] nums, int l, int r) {
+        while (l < r) {
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
         }
     }
 }
