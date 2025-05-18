d025aba8b4b57d28a83c441a7b313b940d7e43b2
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-18 19:39:43 +0800

    Time: 24 ms (87.13%), Space: 42.8 MB (73.44%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -24,18 +24,18 @@ class Solution {
             return null;
         }
 
-        Map<Node, Node> map = new HashMap<>();
         Queue<Node> queue = new LinkedList<>();
-
-        map.put(node, new Node(node.val));
-        queue.add(node);
+        Map<Node, Node> map = new HashMap<>();
         
+        queue.add(node);
+        map.put(node, new Node(node.val));
+
         while (!queue.isEmpty()) {
             Node curr = queue.poll();
             for (Node neighbor : curr.neighbors) {
                 if (!map.containsKey(neighbor)) {
-                    map.put(neighbor, new Node(neighbor.val));
                     queue.add(neighbor);
+                    map.put(neighbor, new Node(neighbor.val));
                 }
                 map.get(curr).neighbors.add(map.get(neighbor));
             }
