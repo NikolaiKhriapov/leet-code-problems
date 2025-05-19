8e4bdb4affaa9c943700915c00797316f087597b
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-19 11:08:15 +0800

    Time: 45 ms (70.5%), Space: 112.5 MB (84.76%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -5,7 +5,6 @@ class LRUCache {
     public LRUCache(int capacity) {
         this.capacity = capacity;
         this.map = new LinkedHashMap<>(capacity, 0.75f, true);
-        
     }
     
     public int get(int key) {
@@ -15,8 +14,7 @@ class LRUCache {
     public void put(int key, int value) {
         map.put(key, value);
         if (map.size() > capacity) {
-            int keyToRemove = map.keySet().iterator().next();
-            map.remove(keyToRemove);
+            map.remove(map.keySet().iterator().next());
         }
     }
 }
