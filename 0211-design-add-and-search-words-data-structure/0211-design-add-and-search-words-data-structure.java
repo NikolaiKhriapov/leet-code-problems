30b2de4a241733977574cc5bc90058ddf9986323
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-19 21:53:09 +0800

    Time: 257 ms (33.22%), Space: 206.2 MB (5.25%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -31,7 +31,7 @@ class WordDictionary {
                 }
                 return false;
             } else if (node.children[c - 'a'] == null) {
-                return false;
+                    node.children[c - 'a'] = new TrieNode();
             }
             node = node.children[c - 'a'];
         }
