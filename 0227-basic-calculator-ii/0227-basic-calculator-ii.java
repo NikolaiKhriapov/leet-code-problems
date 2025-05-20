2fa481bf19bb3834aaa2bede7460b84ddda79938
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-20 13:29:44 +0800

    Time: 14 ms (86.53%), Space: 44.8 MB (89.5%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,20 +1,20 @@
 class Solution {
     public int calculate(String s) {
-        if (s == null || s.isEmpty()) return 0;
-
-        s = s.replaceAll(" ", "");
+        if (s == null || s.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
 
         int result = 0;
-        int lastNumber = 0;
         int currNumber = 0;
+        int lastNumber = 0;
         char operation = '+';
-
+        
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (Character.isDigit(c)) {
                 currNumber = currNumber * 10 + (c - '0');
             }
-            if (!Character.isDigit(c) || i == s.length() - 1) {
+            if ((!Character.isDigit(c) && c != ' ') || i == s.length() - 1) {
                 switch (operation) {
                     case '+' -> {
                         result += lastNumber;
@@ -30,7 +30,7 @@ class Solution {
                 operation = c;
                 currNumber = 0;
             }
-        }        
+        }
 
         return result + lastNumber;
     }
