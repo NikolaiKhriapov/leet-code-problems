a47a4edb87a9f89582fb6aa78fc0b87ee1fa41fc
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-20 12:54:49 +0800

    Time: 7 ms (90.73%), Space: 56.5 MB (91.64%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,21 +1,21 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return 0;
+            throw new IllegalArgumentException("Invalid input");
         }
 
         int rows = matrix.length;
         int cols = matrix[0].length;
 
         int[] dp = new int[cols];
+        int diag = 0; //temp
         int max = 0;
-        int diag = 0;
-        
+
         for (int col = 0; col < cols; col++) {
             dp[col] = matrix[0][col] - '0';
             max = Math.max(max, dp[col]);
         }
-
+        
         for (int row = 1; row < rows; row++) {
             diag = dp[0];
             dp[0] = matrix[row][0] - '0';
@@ -23,7 +23,7 @@ class Solution {
             for (int col = 1; col < cols; col++) {
                 int temp = dp[col];
                 dp[col] = (matrix[row][col] == '1')
-                    ? (1 + Math.min(diag, Math.min(dp[col], dp[col - 1])))
+                    ? (1 + Math.min(dp[col - 1], Math.min(diag, dp[col])))
                     : 0;
                 diag = temp;
                 max = Math.max(max, dp[col]);
@@ -33,8 +33,3 @@ class Solution {
         return max * max;
     }
 }
-
-// [1,0,1,0,0]
-// [1,0,1,1,1]
-// [1,1,1,2,2]
-// [1,0,0,1,0]
