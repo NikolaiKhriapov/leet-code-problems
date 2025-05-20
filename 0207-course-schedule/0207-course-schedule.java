144d1475bf66543b868a590208449d6204292dac
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-20 10:42:40 +0800

    Time: 7 ms (48.79%), Space: 45.7 MB (35.19%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,22 +1,20 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if (numCourses <= 0 || prerequisites == null) {
+        if (numCourses < 0 || prerequisites == null) {
             throw new IllegalArgumentException("Invalid input");
-        }
-        if (prerequisites.length == 0) {
-            return true;
         }
 
         Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int course = 0; course < numCourses; course++) {
+            graph.put(course, new ArrayList<>());
+        }
         for (int[] prerequisite : prerequisites) {
-            if (!graph.containsKey(prerequisite[1])) {
-                graph.put(prerequisite[1], new ArrayList<>());
-            }
             graph.get(prerequisite[1]).add(prerequisite[0]);
         }
 
         Set<Integer> visited = new HashSet<>();
         Set<Integer> visiting = new HashSet<>();
+
         for (int course = 0; course < numCourses; course++) {
             if (hasCycle(graph, course, visited, visiting)) {
                 return false;
@@ -30,8 +28,8 @@ class Solution {
         if (visiting.contains(course)) return true;
         visiting.add(course);
 
-        for (int el : graph.getOrDefault(course, new ArrayList<>())) {
-            if (hasCycle(graph, el, visited, visiting)) {
+        for (int neighbor : graph.getOrDefault(course, new ArrayList<>())) {
+            if (hasCycle(graph, neighbor, visited, visiting)) {
                 return true;
             }
         }
