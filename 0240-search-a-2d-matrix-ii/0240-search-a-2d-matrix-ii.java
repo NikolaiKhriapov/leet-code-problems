0d40cf89e985e105969a23bbb9fd7c05bcf05e1d
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-20 16:58:59 +0800

    Time: 5 ms (99.52%), Space: 45.6 MB (94.07%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -3,19 +3,16 @@ class Solution {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             throw new IllegalArgumentException("Invalid input");
         }
-        if (matrix[0][0] > target || matrix[matrix.length - 1][matrix[0].length - 1] < target) {
-            return false;
-        }
 
         int row = matrix.length - 1;
         int col = 0;
         while (col < matrix[0].length && row >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            }
             if (col < matrix[0].length && matrix[row][col] > target) {
                 row--;
             } else {
-                if (matrix[row][col] == target) {
-                    return true;
-                }
                 col++;
             }
         }
