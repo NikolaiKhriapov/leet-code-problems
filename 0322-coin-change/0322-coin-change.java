e9b7ff7dd4b0388d7fa9f393599df581e4d2b8af
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-20 21:14:16 +0800

    Time: 14 ms (86.55%), Space: 44.2 MB (89.13%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,21 +1,26 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if (coins == null || coins.length == 0 || amount < 0) return -1;
-        if (amount == 0) return 0;
-
-        int[] dp = new int[amount + 1];
-        for (int i = 1; i <= amount; i++) {
-            dp[i] = amount + 1;
+        if (coins == null || coins.length == 0 || amount < 0) {
+            throw new IllegalArgumentException("Invalid input");
         }
 
-        for (int i = 1; i <= amount; i++) {
+        int[] dp = new int[amount + 1];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = amount + 1;
+        }
+        
+        for (int i = 1; i < dp.length; i++) {
             for (int coin : coins) {
                 if (i >= coin) {
                     dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
-        
-        return dp[amount] == (amount + 1) ? -1 : dp[amount];
+
+        return (dp[amount] == amount + 1) ? -1 : dp[amount];
     }
 }
+
+// coins = [1,2,5], amount = 11
+
+// [ 0,12, 1, 2, 2, 1, 2, 2, 3, 3, 2, 3]
