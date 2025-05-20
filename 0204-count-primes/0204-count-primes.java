f3825ec8fe7aaea94a5bb10c5b0126172f4bad76
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-20 10:36:10 +0800

    Time: 105 ms (39.33%), Space: 49.1 MB (83.26%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,8 +1,5 @@
 class Solution {
     public int countPrimes(int n) {
-        if (n <= 2) {
-            return 0;
-        }
         
         boolean[] primes = new boolean[n];
         for (int i = 2; i < primes.length; i++) {
@@ -13,14 +10,14 @@ class Solution {
             if (!primes[i]) {
                 continue;
             }
-            for (int j = i * i; j < primes.length; j += i) {
+            for (int j = i * 2; j < primes.length; j += i) {
                 primes[j] = false;
             }
         }
 
         int count = 0;
-        for (boolean prime : primes) {
-            if (prime) {
+        for (boolean isPrime : primes) {
+            if (isPrime) {
                 count++;
             }
         }
