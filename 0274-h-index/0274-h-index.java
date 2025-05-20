a4efb0c5344215827963831808949d3524fb7abd
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-20 17:18:52 +0800

    Time: 0 ms (100%), Space: 41.8 MB (41.49%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int hIndex(int[] citations) {
+
+        int[] count = new int[citations.length + 1];
+        
+        for (int citation : citations) {
+            if (citation > citations.length) {
+                count[count.length - 1]++;
+            } else {
+                count[citation]++;
+            }
+        }
+        
+        int total = 0;
+        for (int i = count.length - 1; i > 0; i--) {
+            total += count[i];
+            if (total >= i) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+}
+
+// [3,0,6,1,5]
+
+// [0,1,2,3,4,5]
+// [1,1,0,1,0,2]
+// [5,4,3,3,2,2]
