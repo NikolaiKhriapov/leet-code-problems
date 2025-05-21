8988c784b59a250ce9c2b92f3eb94d28c9e102b7
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-21 10:10:45 +0800

    Time: 3 ms (100%), Space: 43.8 MB (87.36%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
@@ -1,5 +1,9 @@
 class Solution {
     public int rangeBitwiseAnd(int left, int right) {
+        if (left < 0 || right < 0) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        
         int shift = 0;
         while (left != right) {
             left >>= 1;
