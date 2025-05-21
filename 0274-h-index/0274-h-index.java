c7f66af64fe3e9ce8520be37607aff0f129e2833
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-21 11:53:29 +0800

    Time: 0 ms (100%), Space: 41.9 MB (30.44%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,39 +1,33 @@
 class Solution {
     public int hIndex(int[] citations) {
         if (citations == null || citations.length == 0) {
-            return 0;
+            throw new IllegalArgumentException("Invalid input");
         }
 
-        int[] count = countCitations(citations);
-        return calculateHIndex(count);
+        int[] citationsCount = countCitations(citations);
+        return getHIndex(citationsCount);
     }
 
     private int[] countCitations(int[] citations) {
-        int[] count = new int[citations.length + 1];
+        int[] result = new int[citations.length + 1];
         for (int citation : citations) {
-            if (citation > citations.length) {
-                count[count.length - 1]++;
+            if (citation >= result.length) {
+                result[result.length - 1]++;
             } else {
-                count[citation]++;
+                result[citation]++;
+            }
+        }
+        return result;
+    }
+
+    private int getHIndex(int[] citationsCount) {
+        int count = 0;
+        for (int i = citationsCount.length - 1; i >= 1; i--) {
+            count += citationsCount[i];
+            if (count >= i) {
+                return i;
             }
         }
         return count;
     }
-
-    private int calculateHIndex(int[] count) {
-        int total = 0;
-        for (int i = count.length - 1; i > 0; i--) {
-            total += count[i];
-            if (total >= i) {
-                return i;
-            }
-        }
-        return 0;
-    }
 }
-
-// [3,0,6,1,5]
-
-// [0,1,2,3,4,5]
-// [1,1,0,1,0,2]
-// [5,4,3,3,2,2]
