4665bb02691dd4f93965fe1743c9cb73b1c85366
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-21 11:00:52 +0800

    Time: 6 ms (96.14%), Space: 60.4 MB (45.77%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -7,25 +7,25 @@ class Solution {
         int rows = matrix.length;
         int cols = matrix[0].length;
 
-        int[][] dp = new int[rows][cols];
+        int[] dpPrev = new int[cols];        
+        int[] dpCurr = new int[cols];
         int maxSide = 0;
 
-        for (int row = 0; row < rows; row++) {
-            dp[row][0] = matrix[row][0] - '0';
-            maxSide = Math.max(maxSide, dp[row][0]);
-        }
         for (int col = 0; col < cols; col++) {
-            dp[0][col] = matrix[0][col] - '0';
-            maxSide = Math.max(maxSide, dp[0][col]);
+            dpPrev[col] = matrix[0][col] - '0';
+            maxSide = Math.max(maxSide, dpPrev[col]);
         }
 
         for (int row = 1; row < rows; row++) {
+            dpCurr[0] = matrix[row][0] - '0';
+            maxSide = Math.max(maxSide, dpCurr[0]);
             for (int col = 1; col < cols; col++) {
-                dp[row][col] = (matrix[row][col] == '1')
-                    ? (1 + getMin(dp[row][col - 1], dp[row - 1][col - 1], dp[row - 1][col]))
+                dpCurr[col] = (matrix[row][col] == '1')
+                    ? (1 + getMin(dpCurr[col - 1], dpPrev[col - 1], dpPrev[col]))
                     : 0;
-                maxSide = Math.max(maxSide, dp[row][col]);
+                maxSide = Math.max(maxSide, dpCurr[col]);
             }
+            dpPrev = Arrays.copyOf(dpCurr, dpCurr.length);
         }
 
         return maxSide * maxSide;
