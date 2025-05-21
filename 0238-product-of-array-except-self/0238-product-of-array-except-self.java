0db050de9cc4139cc00381761f1a0a896e286c81
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-21 11:37:54 +0800

    Time: 3 ms (22.33%), Space: 55.2 MB (87.19%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        if (nums == null) {
+        if (nums == null || nums.length == 0) {
             throw new IllegalArgumentException("Invalid input");
         }
 
         int[] result = new int[nums.length];
-        result[0] = 1;
+        Arrays.fill(result, 1);
 
         int product = 1;
         for (int i = 1; i < result.length; i++) {
             product *= nums[i - 1];
             result[i] = product;
         }
-        
+
         product = 1;
         for (int i = result.length - 2; i >= 0; i--) {
             product *= nums[i + 1];
@@ -23,6 +23,7 @@ class Solution {
     }
 }
 
-// [1,2,3,4]
-// [1,1,2,6]
-// [24,12,8,6]
+// [ 1, 2, 3, 4]
+
+// [ 1, 1, 1, 1]
+// [24,12, 8, 6]
