b99756e747856e07cf9c573c0ba109a6154af0d5
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-21 20:42:43 +0800

    Time: 1 ms (99.04%), Space: 42.2 MB (72.05%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,54 +1,56 @@
 class Solution {
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-
-        Map<String, Map<String, Double>> graph = populateGraph(equations, values);
-        
-        double[] answers = new double[queries.size()];
-        for (int i = 0; i < queries.size(); i++) {
-            String a = queries.get(i).get(0);
-            String b = queries.get(i).get(1);
-            answers[i] = calculate(a, b, graph);
-        }
-        return answers;
+        Map<String, Map<String, Double>> graph = buildGraph(equations, values);
+        return calculateQueries(queries, graph);
     }
 
-    private Map<String, Map<String, Double>> populateGraph(List<List<String>> equations, double[] values) {
+    private Map<String, Map<String, Double>> buildGraph(List<List<String>> equations, double[] values) {
         Map<String, Map<String, Double>> graph = new HashMap<>();
         for (int i = 0; i < equations.size(); i++) {
-            String a = equations.get(i).get(0);
-            String b = equations.get(i).get(1);
+            String first = equations.get(i).get(0);
+            String second = equations.get(i).get(1);
             double value = values[i];
-            if (!graph.containsKey(a)) {
-                graph.put(a, new HashMap<>());
+            if (!graph.containsKey(first)) {
+                graph.put(first, new HashMap<>());
             }
-            graph.get(a).put(b, value);
-            if (!graph.containsKey(b)) {
-                graph.put(b, new HashMap<>());
+            graph.get(first).put(second, value);
+            if (!graph.containsKey(second)) {
+                graph.put(second, new HashMap<>());
             }
-            graph.get(b).put(a, 1.0 / value);
+            graph.get(second).put(first, 1.0 / value);
         }
         return graph;
     }
 
-    private double calculate(String a, String b, Map<String, Map<String, Double>> graph) {
-        if (!graph.containsKey(a) || !graph.containsKey(b)) return -1.0;
-        if (Objects.equals(a, b)) return 1.0;
-        
-        return helper(a, b, graph, new HashSet<>(), 1.0);
+    private double[] calculateQueries(List<List<String>> queries, Map<String, Map<String, Double>> graph) {
+        double[] result = new double[queries.size()];
+        for (int i = 0; i < queries.size(); i++) {
+            String first = queries.get(i).get(0);
+            String second = queries.get(i).get(1);
+            result[i] = calculateQuery(first, second, graph);
+        }
+        return result;
     }
 
-    private double helper(String a, String b, Map<String, Map<String, Double>> graph,  Set<String> visited, double product) {
-        visited.add(a);
-        Map<String, Double> neighbors = graph.get(a);
-        
-        if (neighbors.containsKey(b)) {
-            return product * neighbors.get(b);
+    private double calculateQuery(String first, String second, Map<String, Map<String, Double>> graph) {
+        if (!graph.containsKey(first) || !graph.containsKey(second)) return -1.0;
+        if (Objects.equals(first, second)) return 1.0;
+
+        return calculateQuery(first, second, graph, new HashSet<>(), 1.0);
+    }
+
+    private double calculateQuery(String first, String second, Map<String, Map<String, Double>> graph, Set<String> visited, double product) {
+        visited.add(first);
+        Map<String, Double> neighbors = graph.get(first);
+
+        if (neighbors.containsKey(second)) {
+            return product * neighbors.get(second);
         }
 
         for (var entry : neighbors.entrySet()) {
             String next = entry.getKey();
             if (visited.contains(next)) continue;
-            double result = helper(next, b, graph, visited, product * entry.getValue());
+            double result = calculateQuery(next, second, graph, visited, product * entry.getValue());
             if (result != -1.0) return result;
         }
         return -1.0;
