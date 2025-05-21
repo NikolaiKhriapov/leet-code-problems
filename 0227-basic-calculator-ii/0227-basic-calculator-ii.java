550dc73de35ef9e4941c37f7a7271fc9418fc81a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-21 11:13:27 +0800

    Time: 14 ms (86.58%), Space: 44.8 MB (93.84%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,35 +1,33 @@
 class Solution {
     public int calculate(String s) {
-        if (s == null || s.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
 
         int result = 0;
+        int prevNumber = 0;
         int currNumber = 0;
-        int lastNumber = 0;
         char operation = '+';
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (Character.isDigit(c)) {
                 currNumber = currNumber * 10 + (c - '0');
             }
-            if ((!Character.isDigit(c) && c != ' ') || i == s.length() - 1) {
+            if ((!Character.isDigit(c) && c != ' ') || (i == s.length() - 1)) {
                 switch (operation) {
                     case '+' -> {
-                        result += lastNumber;
-                        lastNumber = currNumber;
+                        result += prevNumber;
+                        prevNumber = currNumber;
                     }
                     case '-' -> {
-                        result += lastNumber;
-                        lastNumber = -currNumber;
+                        result += prevNumber;
+                        prevNumber = -currNumber;
                     }
-                    case '*' -> lastNumber *= currNumber;
-                    case '/' -> lastNumber /= currNumber;
+                    case '*' -> prevNumber *= currNumber;
+                    case '/' -> prevNumber /= currNumber;
+                    default -> throw new RuntimeException("Invalid operation: " + operation);
                 }
                 operation = c;
                 currNumber = 0;
             }
         }
-        return result + lastNumber;
+        return result + prevNumber;
     }
 }
