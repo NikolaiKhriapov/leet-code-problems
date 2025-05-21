447b97375645b0881461b1e40295a5dc4b31716c
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-21 13:31:14 +0800

    Time: 2 ms (95.4%), Space: 133.9 MB (62.59%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -4,19 +4,25 @@ class Solution {
             return false;
         }
 
-        int first = Integer.MAX_VALUE;
+        int first = Integer.MAX_VALUE; // 0,1
         int second = Integer.MAX_VALUE;
-
-        for (int n : nums) {
-            if (n <= first) {
-                first = n;
-            } else if (n <= second) {
-                second = n;
+        for (int num : nums) {
+            if (num < first && num <= second) {
+                second = num;
+            } else if (num <= first && num > second) {
+                first = num;
             } else {
                 return true;
             }
         }
-
         return false;
     }
 }
+
+// [2,1,5,0,4,6]
+// 2, -
+// 1, -
+// 1, 5
+// 1, 5
+// 0, 4
+// 0, 4, 6
