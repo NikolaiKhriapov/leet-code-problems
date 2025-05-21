85500a594d7feb6be0cedb1cbb1eb04d2e142685
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-21 10:58:38 +0800

    Time: 8 ms (70.84%), Space: 60.7 MB (26.28%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,35 +1,43 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            throw new IllegalArgumentException("Invalid input");
+            return 0;
         }
 
         int rows = matrix.length;
         int cols = matrix[0].length;
 
-        int[] dp = new int[cols];
-        int diag = 0; //temp
-        int max = 0;
+        int[][] dp = new int[rows][cols];
+        int maxSide = 0;
 
-        for (int col = 0; col < cols; col++) {
-            dp[col] = matrix[0][col] - '0';
-            max = Math.max(max, dp[col]);
+        for (int row = 0; row < rows; row++) {
+            dp[row][0] = matrix[row][0] - '0';
+            maxSide = Math.max(maxSide, dp[row][0]);
         }
-        
+        for (int col = 0; col < cols; col++) {
+            dp[0][col] = matrix[0][col] - '0';
+            maxSide = Math.max(maxSide, dp[0][col]);
+        }
+
         for (int row = 1; row < rows; row++) {
-            diag = dp[0];
-            dp[0] = matrix[row][0] - '0';
-            max = Math.max(max, dp[0]);
             for (int col = 1; col < cols; col++) {
-                int temp = dp[col];
-                dp[col] = (matrix[row][col] == '1')
-                    ? (1 + Math.min(dp[col - 1], Math.min(diag, dp[col])))
+                dp[row][col] = (matrix[row][col] == '1')
+                    ? (1 + getMin(dp[row][col - 1], dp[row - 1][col - 1], dp[row - 1][col]))
                     : 0;
-                diag = temp;
-                max = Math.max(max, dp[col]);
+                maxSide = Math.max(maxSide, dp[row][col]);
             }
         }
 
-        return max * max;
+        return maxSide * maxSide;
+    }
+
+    private int getMin(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
     }
 }
+
+// [1,0,1,0,0]
+// [1,0,1,1,1]
+// [1,1,1,2,2]
+// [1,0,0,1,0]
+//  2 * 2 = 4
