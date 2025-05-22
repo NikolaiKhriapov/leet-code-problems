04a912325384f87b022a459df7c1afe97bafce9d
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-22 10:51:38 +0800

    Time: 6 ms (52.54%), Space: 46.2 MB (41.34%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,49 +1,57 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        if (prerequisites == null || numCourses < 0) {
-            throw new IllegalArgumentException("Invalid input");
-        }
 
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int[] prerequisite : prerequisites) {
-            if (!graph.containsKey(prerequisite[1])) {
-                graph.put(prerequisite[1], new ArrayList<>());
-            }
-            graph.get(prerequisite[1]).add(prerequisite[0]);
-        }
 
-        Set<Integer> visited = new HashSet<>();
-        Set<Integer> visiting = new HashSet<>();
-        List<Integer> coursesList = new ArrayList<>();
-
-        for (int course = 0; course < numCourses; course++) {
-            if (hasCycle(graph, course, visited, visiting, coursesList)) {
-                return new int[0];
-            }
-        }
-
-        int[] result = new int[numCourses];
-        for (int i = 0; i < coursesList.size(); i++) {
-            result[i] = coursesList.get(result.length - 1 - i);
-        }
-
-        return result;
+        Map<Integer, List<Integer>> graph = buildGraph(numCourses, prerequisites);
+        List<Integer> list = getOrderedListOfCourses(graph, numCourses);
+        return reverseListAndReturnArray(list);
     }
 
-    private boolean hasCycle(Map<Integer, List<Integer>> graph, int course, Set<Integer> visited, Set<Integer> visiting, List<Integer> coursesList) {
+    private Map<Integer, List<Integer>> buildGraph(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int course = 0; course < numCourses; course++) {
+            graph.put(course, new ArrayList<>());
+        }
+        for (int[] prerequisite : prerequisites) {
+            graph.get(prerequisite[1]).add(prerequisite[0]);
+        }
+        return graph;
+    }
+
+    private List<Integer> getOrderedListOfCourses(Map<Integer, List<Integer>> graph, int numCourses) {
+        List<Integer> list = new ArrayList<>();
+        Set<Integer> visited = new HashSet<>();
+        Set<Integer> visiting = new HashSet<>();
+        for (int course = 0; course < numCourses; course++) {
+            if (hasCycle(graph, course, visited, visiting, list)) {
+                return new ArrayList<>();
+            }
+        }
+        return list;
+    }
+
+    private boolean hasCycle(Map<Integer, List<Integer>> graph, int course, Set<Integer> visited, Set<Integer> visiting, List<Integer> list) {
         if (visited.contains(course)) return false;
         if (visiting.contains(course)) return true;
         visiting.add(course);
 
-        for (int el : graph.getOrDefault(course, new ArrayList<>())) {
-            if (hasCycle(graph, el, visited, visiting, coursesList)) {
+        for (int neighbor : graph.getOrDefault(course, new ArrayList<>())) {
+            if (hasCycle(graph, neighbor, visited, visiting, list)) {
                 return true;
             }
         }
 
+        list.add(course);
         visiting.remove(course);
         visited.add(course);
-        coursesList.add(course);
         return false;
+    }
+
+    private int[] reverseListAndReturnArray(List<Integer> list) {
+        int[] array = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = list.get(list.size() - 1 - i);
+        }
+        return array;
     }
 }
