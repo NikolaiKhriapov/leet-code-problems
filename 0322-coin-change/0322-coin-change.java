bd1ac529211e463a8da3f39df3691b9b4705e1e4
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-22 12:00:27 +0800

    Time: 13 ms (89.18%), Space: 44.2 MB (89.1%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -8,17 +8,20 @@ class Solution {
         for (int i = 1; i < dp.length; i++) {
             dp[i] = amount + 1;
         }
+
         for (int i = 1; i < dp.length; i++) {
             for (int coin : coins) {
-                if (coin <= i) {
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                if (coin > i) {
+                    continue;
                 }
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
             }
         }
+
         return (dp[amount] == amount + 1) ? -1 : dp[amount];
     }
 }
 
-// [1,2,5]
-// [ 0,12,12,12,12,12,12,12,12,12,12,12]
-// [ 0, 1, 1, 2, 2, 1, 2, 2, 3, 3, 2, 3]
+// 1,2,5
+// [0,12,12,12,12,12,12,12,12,12,12,12]
+// [0, 1, 1, 2, 2, 1, 2, 2, 3, 3, 2, 3]
