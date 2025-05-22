d177d71ee79d53de5cb8ee1f1f0bcfbf71b27f2e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-22 20:38:31 +0800

    Time: 9 ms (36.99%), Space: 44.7 MB (67.12%) - LeetHub

---
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int snakesAndLadders(int[][] board) {
+
+        Queue<Integer> q = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+
+        q.add(1);
+        visited.add(1);
+
+        int count = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            while (size-- > 0) {
+                int curr = q.poll();
+                if (curr == board.length * board.length) return count;
+                for (int i = 1; i <= 6 && curr + i <= board.length * board.length; i++) {
+                    int next = curr + i;
+                    int[] nextCoord = getCoordinates(next, board.length);
+                    int value = board[nextCoord[0]][nextCoord[1]];
+                    if (value != -1) next = value;
+                    if (!visited.contains(next)) {
+                        q.add(next);
+                        visited.add(next);
+                    }
+                }
+            }
+            count++;
+        }
+        return -1;
+    }
+
+    private int[] getCoordinates(int num, int size) {
+        int row = size - 1 - (num - 1) / size;
+        int col = (num - 1) % size;
+        if ((size - row) % 2 == 0) col = size - 1 - col;
+        return new int[]{row, col};
+    }
+}
