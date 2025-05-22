87a03c89648db6b6aa7f1ac579ae8081835bec21
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-22 18:42:20 +0800

    Time: 0 ms (100%), Space: 44.4 MB (69.89%) - LeetHub

---
+++ 0772-construct-quad-tree/0772-construct-quad-tree.java
@@ -0,0 +1,77 @@
+/*
+// Definition for a QuadTree node.
+class Node {
+    public boolean val;
+    public boolean isLeaf;
+    public Node topLeft;
+    public Node topRight;
+    public Node bottomLeft;
+    public Node bottomRight;
+
+    
+    public Node() {
+        this.val = false;
+        this.isLeaf = false;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+    }
+}
+*/
+
+class Solution {
+    public Node construct(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid.length != grid[0].length) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+
+        return construct(grid, 0, 0, grid.length);
+    }
+
+    private Node construct(int[][] grid, int row, int col, int size) {
+        if (isLeaf(grid, row, col, size)) {
+            return new Node(grid[row][col] == 1, true);
+        }
+        
+        int halfSize = size / 2;
+        return new Node(
+            false,
+            false,
+            construct(grid, row, col, halfSize),
+            construct(grid, row, col + halfSize, halfSize),
+            construct(grid, row + halfSize, col, halfSize),
+            construct(grid, row + halfSize, col + halfSize, halfSize)
+        );
+    }
+
+    private boolean isLeaf(int[][] grid, int row, int col, int size) {
+        int first = grid[row][col];
+        for (int r = row; r < row + size; r++) {
+            for (int c = col; c < col + size; c++) {
+                if (grid[r][c] != first) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
