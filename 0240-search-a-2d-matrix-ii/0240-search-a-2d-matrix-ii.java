d9f1f804e0c36fa493885ebf3ac1b647a774cf3f
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-23 19:21:43 +0800

    Time: 6 ms (46.48%), Space: 45.5 MB (97.71%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return false;
+            throw new IllegalArgumentException("Invalid input");
         }
 
         int row = matrix.length - 1;
         int col = 0;
-        while (col < matrix[0].length && row >= 0) {
-            int cell = matrix[row][col];
-            if (cell == target) {
-                return true;
-            } else if (cell > target) {
+        while (row >= 0 && col < matrix[0].length) {
+            int val = matrix[row][col];
+            if (val == target) return true;
+            if (val > target) {
                 row--;
             } else {
                 col++;
