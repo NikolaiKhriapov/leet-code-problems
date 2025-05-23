1641af63c3db4f0360210157082b7c9cc131ded4
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-23 18:45:15 +0800

    Time: 1 ms (99.79%), Space: 58.4 MB (30.41%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,9 +1,11 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("Invalid input");
+        }
 
         int minLength = Integer.MAX_VALUE;
         int currSum = 0;
-
         int left = 0;
         for (int right = 0; right < nums.length; right++) {
             currSum += nums[right];
