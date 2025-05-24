be3a6fd8ed33298f4a534f223bb390521d74c4c9
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-24 15:08:59 +0800

    Time: 15 ms (78.26%), Space: 44.8 MB (46.69%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,6 +1,6 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if (coins == null || coins.length == 0 || amount < 0) {
+        if (coins == null || amount < 0) {
             throw new IllegalArgumentException("Invalid input");
         }
 
@@ -11,17 +11,15 @@ class Solution {
 
         for (int i = 1; i < dp.length; i++) {
             for (int coin : coins) {
-                if (coin > i) {
-                    continue;
-                }
+                if (coin > i) continue;
                 dp[i] = Math.min(dp[i], dp[i - coin] + 1);
             }
         }
-
+        
         return (dp[amount] == amount + 1) ? -1 : dp[amount];
     }
 }
 
-// 1,2,5
+// [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11]
 // [0,12,12,12,12,12,12,12,12,12,12,12]
 // [0, 1, 1, 2, 2, 1, 2, 2, 3, 3, 2, 3]
