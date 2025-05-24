d79202fae03e4f063c2c6d404f54162b3d9c80e4
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-24 13:35:11 +0800

    Time: 7 ms (90.59%), Space: 56.1 MB (95.6%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,39 +1,32 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        if (matrix == null) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             throw new IllegalArgumentException("Invalid input");
-        }
-        if (matrix.length == 0 || matrix[0].length == 0) {
-            return 0;
         }
 
         int diag;
-        int[] dp = new int[matrix[0].length];
+        int[] dpCurr = new int[matrix[0].length];
         int maxSide = 0;
 
         for (int col = 0; col < matrix[0].length; col++) {
-            dp[col] = matrix[0][col] - '0';
-            maxSide = Math.max(maxSide, dp[col]);
+            dpCurr[col] = matrix[0][col] - '0';
+            maxSide = Math.max(maxSide, dpCurr[col]);
         }
 
         for (int row = 1; row < matrix.length; row++) {
-            diag = dp[0];
-            dp[0] = matrix[row][0] - '0';
-            maxSide = Math.max(maxSide, dp[0]);
+            diag = dpCurr[0];
+            dpCurr[0] = matrix[row][0] - '0';            
+            maxSide = Math.max(maxSide, dpCurr[0]);
             for (int col = 1; col < matrix[0].length; col++) {
-                int temp = dp[col];
-                dp[col] = (matrix[row][col] == '1')
-                    ? (1 + getMin(diag, dp[col], dp[col - 1]))
+                int temp = dpCurr[col];
+                dpCurr[col] = (matrix[row][col] == '1')
+                    ? (1 + Math.min(diag, Math.min(dpCurr[col], dpCurr[col - 1])))
                     : 0;
                 diag = temp;
-                maxSide = Math.max(maxSide, dp[col]);
+                maxSide = Math.max(maxSide, dpCurr[col]);
             }
         }
 
         return maxSide * maxSide;
-    }
-
-    private int getMin(int a, int b, int c) {
-        return Math.min(a, Math.min(b, c));
     }
 }
