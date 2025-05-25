f42aeec72a121ccbebfe77d9aa83b07280b6b45b
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-25 13:17:29 +0800

    Time: 28 ms (90.18%), Space: 60.1 MB (53.46%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,9 +1,9 @@
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        if (nums1 == null || nums2 == null || k < 0 || nums1.length == 0 || nums2.length == 0) {
+        if (nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0 || k < 0) {
             throw new IllegalArgumentException("Invalid input");
         }
-
+        
         List<List<Integer>> result = new ArrayList<>();
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(nums1[a[0]] + nums2[a[1]], nums1[b[0]] + nums2[b[1]]));
         
@@ -11,11 +11,11 @@ class Solution {
             pq.add(new int[]{i, 0});
         }
 
-        while (k-- > 0) {
-            int[] pair = pq.poll();
-            result.add(List.of(nums1[pair[0]], nums2[pair[1]]));
-            if (pair[1] + 1 < nums2.length) {
-                pq.add(new int[]{pair[0], pair[1] + 1});
+        while (!pq.isEmpty() && k-- > 0) {
+            int[] curr = pq.poll();
+            result.add(List.of(nums1[curr[0]], nums2[curr[1]]));
+            if (curr[1] + 1 < nums2.length) {
+                pq.add(new int[]{curr[0], curr[1] + 1});
             }
         }
 
