f2ad6559b897a9c7915277d32d1a4ea2b96c3073
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-05-26 10:14:11 +0800

    Time: 1 ms (99.05%), Space: 42.3 MB (71.31%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -23,23 +23,23 @@ class Solution {
         return graph;
     }
 
-    private double[] calcQueries(Map<String, Map<String, Double>> graph, List<List<String>> equations) {
-        double[] result = new double[equations.size()];
-        for (int i = 0; i < equations.size(); i++) {
-            String first = equations.get(i).get(0);
-            String second = equations.get(i).get(1);
+    private double[] calcQueries(Map<String, Map<String, Double>> graph, List<List<String>> queries) {
+        double[] result = new double[queries.size()];
+        for (int i = 0; i < queries.size(); i++) {
+            String first = queries.get(i).get(0);
+            String second = queries.get(i).get(1);
             result[i] = calcQuery(graph, first, second, new HashSet<>(), 1.0);
         }
         return result;
     }
 
     private double calcQuery(Map<String, Map<String, Double>> graph, String first, String second, Set<String> visited, double product) {
-        if (!graph.containsKey(first) || !graph.containsKey(second)) return -1.0;
+        if (!graph.containsKey(first)) return -1.0;
         if (Objects.equals(first, second)) return 1.0;
         visited.add(first);
 
         Map<String, Double> neighbors = graph.get(first);
-        
+
         if (neighbors.containsKey(second)) {
             return product * neighbors.get(second);
         }
@@ -47,14 +47,10 @@ class Solution {
         for (var neighbor : neighbors.entrySet()) {
             String next = neighbor.getKey();
             if (visited.contains(next)) continue;
-            double result = calcQuery(graph, next, second, visited, product * neighbor.getValue());
-            if (result != -1.0) return result;
+            double value = calcQuery(graph, next, second, visited, product * neighbor.getValue());
+            if (value != -1.0) return value;
         }
 
         return -1.0;
     }
-
 }
-
-// equations = [["a","b"],["b","c"]], values = [2.0,3.0]
-// queries =   [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
