cf3ed9e3df1988c5a48a65b2f508d3edc0baa94a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-06-27 19:08:57 +0800

    Time: 1 ms (100%), Space: 42.5 MB (28.61%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -9,15 +9,15 @@ class Solution {
             return memo[ps][pp];
         }
 
-        if (pp == p.length()) {
-            memo[ps][pp] = ps == s.length();
+        if (pp >= p.length()) {
+            memo[ps][pp] = ps >= s.length();
             return memo[ps][pp];
         }
 
-        boolean isFirstMatch = (ps < s.length()) && (s.charAt(ps) == p.charAt(pp) || p.charAt(pp) == '.');
+        boolean isFirstMatch = ps < s.length() && (s.charAt(ps) == p.charAt(pp) || p.charAt(pp) == '.');
 
-        if ((pp + 1) < p.length() && p.charAt(pp + 1) == '*') {
-            memo[ps][pp] = (isMatch(s, p, ps, pp + 2, memo)) || (isFirstMatch && isMatch(s, p, ps + 1, pp, memo));
+        if (pp + 1 < p.length() && p.charAt(pp + 1) == '*') {
+            memo[ps][pp] = isMatch(s, p, ps, pp + 2, memo) || (isFirstMatch && isMatch(s, p, ps + 1, pp, memo));
         } else {
             memo[ps][pp] = isFirstMatch && isMatch(s, p, ps + 1, pp + 1, memo);
         }
