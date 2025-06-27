27174ee3f320cbdcebc9158e26be249b413ee2d7
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-06-27 16:37:53 +0800

    Time: 1 ms (100%), Space: 46.5 MB (12.08%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,30 +1,41 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
 
-        int[] nums = new int[nums1.length + nums2.length];
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
 
-        int p1 = 0;
-        int p2 = 0;
-        int p = 0;
-        while (p1 < nums1.length || p2 < nums2.length) {
-            if (p1 == nums1.length) {
-                nums[p++] = nums2[p2++];
-            } else if (p2 == nums2.length) {
-                nums[p++] = nums1[p1++];
-            } else if (nums1[p1] < nums2[p2]) {
-                nums[p++] = nums1[p1++];
+        int left = 0;
+        int right = nums1.length;
+        while (left <= right) {
+            int p1 = (left + right) / 2;
+            int p2 = (nums1.length + nums2.length + 1) / 2 - p1;
+
+            int maxLeft1 = (p1 == 0) ? Integer.MIN_VALUE : nums1[p1 - 1];
+            int minRight1 = (p1 == nums1.length) ? Integer.MAX_VALUE : nums1[p1];
+
+            int maxLeft2 = (p2 == 0) ? Integer.MIN_VALUE : nums2[p2 - 1];
+            int minRight2 = (p2 == nums2.length) ? Integer.MAX_VALUE : nums2[p2];
+
+            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+                if ((nums1.length + nums2.length) % 2 == 0) {
+                    return (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) / 2.0;
+                } else {
+                    return Math.max(maxLeft1, maxLeft2);
+                }
+            } else if (maxLeft1 < minRight2) {
+                left = p1 + 1;
             } else {
-                nums[p++] = nums2[p2++];
+                right = p1 - 1;
             }
         }
-
-        if (nums.length % 2 != 0) {
-            return nums[nums.length / 2];
-        } else {
-            return ((double) nums[nums.length / 2 - 1] + (double) nums[nums.length / 2]) / (double) 2;
-        }
+        
+        throw new IllegalArgumentException("Input arrays are not sorted");
     }
 }
 
-// 1,2,3,4
-// [2 2 2 4 5] [2 4]
+// [1,2,3,4,5,6,7,8,9], [5,6,7,8,9]
+//          -                  -
