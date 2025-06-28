2ae09a5b66a128eae8f8bdd28385e8ab50b7752e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-06-28 18:59:43 +0800

    Time: 2 ms (61.98%), Space: 40.4 MB (73.92%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,43 +1,37 @@
 class Solution {
     public int totalNQueens(int n) {
-        if (n <= 0) {
+        if (n < 1) {
             return 0;
         }
-        
-        boolean[][] board = new boolean[n][n];
-        int[] count = new int[]{0};
-        helper(board, n, 0, count);
-        return count[0];
+        return helper(new boolean[n][n], n, 0);
     }
 
-    private void helper(boolean[][] board, int n, int row, int[] count) {
+    private int helper(boolean[][] board, int n, int row) {
         if (row == n) {
-            count[0]++;
-            return;
+            return 1;
         }
 
+        int count = 0;
         for (int col = 0; col < n; col++) {
             if (isSafe(board, n, row, col)) {
                 board[row][col] = true;
-                helper(board, n, row + 1, count);
+                count += helper(board, n, row + 1);
                 board[row][col] = false;
             }
         }
+        return count;
     }
 
     private boolean isSafe(boolean[][] board, int n, int row, int col) {
         for (int r = 0; r < row; r++) {
             if (board[r][col]) return false;
         }
-
         for (int r = row - 1, c = col - 1; r >= 0 && c >= 0; r--, c--) {
             if (board[r][c]) return false;
         }
-
         for (int r = row - 1, c = col + 1; r >= 0 && c < n; r--, c++) {
             if (board[r][c]) return false;
         }
-
         return true;
     }
 }
