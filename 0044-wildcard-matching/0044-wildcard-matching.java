bf17e10ec081a9de7b18aa8eb26f4ea927d069fc
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-06-28 17:21:20 +0800

    Time: 17 ms (73.92%), Space: 48.5 MB (33.61%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        
+        Boolean[][] memo = new Boolean[s.length() + 1][p.length() + 1];
+        return isMatch(s, p, 0, 0, memo);
+    }
+
+    private boolean isMatch(String s, String p, int ps, int pp, Boolean[][] memo) {
+        if (memo[ps][pp] != null) {
+            return memo[ps][pp];
+        }
+
+        if (pp >= p.length()) {
+            memo[ps][pp] = ps >= s.length();
+            return memo[ps][pp];
+        }
+
+        boolean isFirstCharMatch = ps < s.length() && (s.charAt(ps) == p.charAt(pp) || p.charAt(pp) == '?');
+
+        if (p.charAt(pp) == '*') {
+            memo[ps][pp] = isMatch(s, p, ps, pp + 1, memo) || (ps < s.length() && isMatch(s, p, ps + 1, pp, memo));
+        } else {
+            memo[ps][pp] = isFirstCharMatch && isMatch(s, p, ps + 1, pp + 1, memo);
+        }
+        return memo[ps][pp];
+    }
+}
