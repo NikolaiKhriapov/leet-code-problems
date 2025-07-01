0e2dfd844e36aea966f3dec087aa878c01273ae0
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-01 12:19:41 +0800

    Time: 1 ms (100%), Space: 46.2 MB (44.58%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -3,8 +3,8 @@ class Solution {
         if (nums1 == null || nums2 == null || (nums1.length == 0 && nums2.length == 0)) {
             throw new IllegalArgumentException("Invalid input");
         }
-        
-        if (nums2.length < nums1.length) {
+
+        if (nums1.length > nums2.length) {
             return findMedianSortedArrays(nums2, nums1);
         }
 
@@ -14,25 +14,25 @@ class Solution {
             int p1 = (left + right) / 2;
             int p2 = (nums1.length + nums2.length + 1) / 2 - p1;
 
-            int maxLeft1 = (p1 == 0) ? Integer.MIN_VALUE : nums1[p1 - 1];
-            int minRight1 = (p1 == nums1.length) ? Integer.MAX_VALUE : nums1[p1];
+            int leftMax1 = (p1 == 0) ? Integer.MIN_VALUE : nums1[p1 - 1];
+            int rightMin1 = (p1 == nums1.length) ? Integer.MAX_VALUE : nums1[p1];
+            
+            int leftMax2 = (p2 == 0) ? Integer.MIN_VALUE : nums2[p2 - 1];
+            int rightMin2 = (p2 == nums2.length) ? Integer.MAX_VALUE : nums2[p2];
 
-            int maxLeft2 = (p2 == 0) ? Integer.MIN_VALUE : nums2[p2 - 1];
-            int minRight2 = (p2 == nums2.length) ? Integer.MAX_VALUE : nums2[p2];
-
-            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+            if (leftMax1 <= rightMin2 && leftMax2 <= rightMin1) {
                 if ((nums1.length + nums2.length) % 2 == 0) {
-                    return (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) / 2.0;
+                    return (Math.max(leftMax1, leftMax2) + Math.min(rightMin1, rightMin2)) / 2.0;
                 }
-                return Math.max(maxLeft1, maxLeft2);
-            } else if (maxLeft1 > minRight2) {
+                return Math.max(leftMax1, leftMax2);
+            } else if (leftMax1 > rightMin2) {
                 right = p1 - 1;
             } else {
                 left = p1 + 1;
             }
         }
 
-        throw new IllegalArgumentException("Invalid input: input array is not sorted");
+        throw new IllegalArgumentException("Invalid input: one of input arrays is not sorted");
     }
 }
 
