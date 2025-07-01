5572283c26dde541d53e291c54d014367f68d0e0
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-01 13:31:09 +0800

    Time: 1 ms (100%), Space: 57.2 MB (67.05%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -5,7 +5,7 @@ class Solution {
         }
 
         for (int i = 0; i < nums.length; i++) {
-            while (nums[i] > 0 && nums[i] < nums.length && nums[nums[i] - 1] != nums[i]) {
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
                 int temp = nums[nums[i] - 1];
                 nums[nums[i] - 1] = nums[i];
                 nums[i] = temp;
