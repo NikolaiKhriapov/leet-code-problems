b170827e726f40599a21fa0a9e9d0fb81a940aad
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-02 20:41:41 +0800

    Time: 1 ms (100%), Space: 45.9 MB (84.4%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -13,26 +13,23 @@ class Solution {
         while (left <= right) {
             int p1 = (left + right) / 2;
             int p2 = (nums1.length + nums2.length + 1) / 2 - p1;
-
-            int leftMax1 = (p1 == 0) ? Integer.MIN_VALUE : nums1[p1 - 1];
-            int rightMin1 = (p1 == nums1.length) ? Integer.MAX_VALUE : nums1[p1];
-            
-            int leftMax2 = (p2 == 0) ? Integer.MIN_VALUE : nums2[p2 - 1];
-            int rightMin2 = (p2 == nums2.length) ? Integer.MAX_VALUE : nums2[p2];
-
-            if (leftMax1 <= rightMin2 && leftMax2 <= rightMin1) {
+            int maxLeft1 = (p1 == 0) ? Integer.MIN_VALUE : nums1[p1 - 1];
+            int minRight1 = (p1 == nums1.length) ? Integer.MAX_VALUE : nums1[p1];
+            int maxLeft2 = (p2 == 0) ? Integer.MIN_VALUE : nums2[p2 - 1];
+            int minRight2 = (p2 == nums2.length) ? Integer.MAX_VALUE : nums2[p2];
+            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
                 if ((nums1.length + nums2.length) % 2 == 0) {
-                    return (Math.max(leftMax1, leftMax2) + Math.min(rightMin1, rightMin2)) / 2.0;
+                    return (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) / 2.0;
                 }
-                return Math.max(leftMax1, leftMax2);
-            } else if (leftMax1 > rightMin2) {
+                return Math.max(maxLeft1, maxLeft2);
+            } else if (maxLeft1 > minRight2) {
                 right = p1 - 1;
             } else {
                 left = p1 + 1;
             }
         }
-
-        throw new IllegalArgumentException("Invalid input: one of input arrays is not sorted");
+        
+        throw new IllegalArgumentException("Invalid input: one of arrays is not sorted");
     }
 }
 
