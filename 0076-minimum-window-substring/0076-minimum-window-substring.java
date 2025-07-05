af17b921fdf3a2b7c9bc9d90165f324834a67508
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-05 17:51:10 +0800

    Time: 22 ms (29.95%), Space: 45.3 MB (49.49%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,29 +1,31 @@
 class Solution {
     public String minWindow(String s, String t) {
-
+        if (s == null || t == null || s.length() == 0 || t.length() == 0) {
+            return "";
+        }
+        
         Map<Character, Integer> mapChars = new HashMap<>();
         for (char c : t.toCharArray()) {
             mapChars.put(c, mapChars.getOrDefault(c, 0) + 1);
         }
 
+        Map<Character, Integer> mapSeen = new HashMap<>();
         int left = 0;
         int right = 0;
-        int required = mapChars.size();
-        int formed = 0;
         int start = 0;
         int minLength = Integer.MAX_VALUE;
-
-        Map<Character, Integer> mapSeen = new HashMap<>();
+        int required = mapChars.size();
+        int found = 0;
 
         while (right < s.length()) {
             char c = s.charAt(right);
             if (mapChars.containsKey(c)) {
                 mapSeen.put(c, mapSeen.getOrDefault(c, 0) + 1);
                 if (Objects.equals(mapSeen.get(c), mapChars.get(c))) {
-                    formed++;
+                    found++;
                 }
             }
-            while (formed == required) {
+            while (required == found) {
                 if (right - left + 1 < minLength) {
                     minLength = right - left + 1;
                     start = left;
@@ -31,10 +33,10 @@ class Solution {
                 char leftChar = s.charAt(left);
                 left++;
                 if (mapSeen.containsKey(leftChar)) {
-                    mapSeen.put(leftChar, mapSeen.get(leftChar) - 1);
-                    if (mapSeen.get(leftChar) < mapChars.get(leftChar)) {
-                        formed--;
+                    if (Objects.equals(mapSeen.get(leftChar), mapChars.get(leftChar))) {
+                        found--;
                     }
+                    mapSeen.put(leftChar, mapSeen.get(leftChar) - 1);
                 }
             }
             right++;
