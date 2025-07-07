40f4abfdba9b0d65285f59a28576fbf4c8edea67
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-07 12:24:01 +0800

    Time: 22 ms (29.79%), Space: 45.1 MB (60.18%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,43 +1,43 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s == null || t == null || s.length() == 0 || t.length() == 0) {
+        if (s == null || t == null || s.isEmpty() || t.isEmpty()) {
             return "";
         }
-        
+
         Map<Character, Integer> mapChars = new HashMap<>();
         for (char c : t.toCharArray()) {
             mapChars.put(c, mapChars.getOrDefault(c, 0) + 1);
         }
 
-        Map<Character, Integer> mapSeen = new HashMap<>();
         int left = 0;
         int right = 0;
-        int start = 0;
         int minLength = Integer.MAX_VALUE;
-        int required = mapChars.size();
-        int found = 0;
+        int start = 0;
+        int requiredCharsCount = mapChars.size();
+        int foundCharsCount = 0;
+        Map<Character, Integer> mapSeen = new HashMap<>();
 
         while (right < s.length()) {
-            char c = s.charAt(right);
-            if (mapChars.containsKey(c)) {
-                mapSeen.put(c, mapSeen.getOrDefault(c, 0) + 1);
-                if (Objects.equals(mapSeen.get(c), mapChars.get(c))) {
-                    found++;
+            char nextChar = s.charAt(right);
+            if (mapChars.containsKey(nextChar)) {
+                mapSeen.put(nextChar, mapSeen.getOrDefault(nextChar, 0) + 1);
+                if (Objects.equals(mapSeen.get(nextChar), mapChars.get(nextChar))) {
+                    foundCharsCount++;
                 }
             }
-            while (required == found) {
+            while (foundCharsCount == requiredCharsCount) {
                 if (right - left + 1 < minLength) {
                     minLength = right - left + 1;
                     start = left;
                 }
                 char leftChar = s.charAt(left);
-                left++;
                 if (mapSeen.containsKey(leftChar)) {
                     if (Objects.equals(mapSeen.get(leftChar), mapChars.get(leftChar))) {
-                        found--;
+                        foundCharsCount--;
                     }
                     mapSeen.put(leftChar, mapSeen.get(leftChar) - 1);
                 }
+                left++;
             }
             right++;
         }
