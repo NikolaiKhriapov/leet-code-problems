bedf27761627b64bfae998b6df5885a74ae2fb35
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-07 16:24:09 +0800

    Time: 0 ms (100%), Space: 41.9 MB (54.62%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -4,33 +4,28 @@ class Solution {
             return new ArrayList<>();
         }
 
-        Set<String> wordSet = new HashSet<>();
-        for (String word : wordDict) {
-            wordSet.add(word);
-        }
-
         List<String> result = new ArrayList<>();
-        
         StringBuilder curr = new StringBuilder();
-        helper(s, wordSet, 0, curr, result);
+        
+        helper(s, wordDict, 0, curr, result);
         
         return result;
     }
 
-    private void helper(String s, Set<String> wordSet, int idx, StringBuilder curr, List<String> result) {
+    private void helper(String s, List<String> wordDict, int idx, StringBuilder curr, List<String> result) {
         if (idx >= s.length()) {
             result.add(curr.toString());
             return;
         }
 
         int currLength = curr.length();
-        for (String word : wordSet) {
+        for (String word : wordDict) {
             if (s.startsWith(word, idx)) {
                 if (!curr.isEmpty()) {
                     curr.append(" ");
                 }
                 curr.append(word);
-                helper(s, wordSet, idx + word.length(), curr, result);
+                helper(s, wordDict, idx + word.length(), curr, result);
                 curr.delete(currLength, curr.length());
             }
         }
