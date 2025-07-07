f7b0115670a9524ce6fedbe531cd55b9cf2b936c
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-07 16:22:05 +0800

    Time: 1 ms (93.77%), Space: 41.9 MB (54.62%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+
+        Set<String> wordSet = new HashSet<>();
+        for (String word : wordDict) {
+            wordSet.add(word);
+        }
+
+        List<String> result = new ArrayList<>();
+        
+        StringBuilder curr = new StringBuilder();
+        helper(s, wordSet, 0, curr, result);
+        
+        return result;
+    }
+
+    private void helper(String s, Set<String> wordSet, int idx, StringBuilder curr, List<String> result) {
+        if (idx >= s.length()) {
+            result.add(curr.toString());
+            return;
+        }
+
+        int currLength = curr.length();
+        for (String word : wordSet) {
+            if (s.startsWith(word, idx)) {
+                if (!curr.isEmpty()) {
+                    curr.append(" ");
+                }
+                curr.append(word);
+                helper(s, wordSet, idx + word.length(), curr, result);
+                curr.delete(currLength, curr.length());
+            }
+        }
+    }
+}
