af0ae891ec88c938072fc250e6c24bc37b9a0541
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-07 16:27:56 +0800

    Time: 0 ms (100%), Space: 41.6 MB (94.66%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -5,10 +5,7 @@ class Solution {
         }
 
         List<String> result = new ArrayList<>();
-        StringBuilder curr = new StringBuilder();
-        
-        helper(s, wordDict, 0, curr, result);
-        
+        helper(s, wordDict, 0, new StringBuilder(), result);
         return result;
     }
 
