aec846608b5c2ac4922da61089fa97da75d658b0
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-07 20:19:11 +0800

    Time: 186 ms (66.18%), Space: 44.9 MB (74.86%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -0,0 +1,56 @@
+class Solution {
+    private static final int[][] NEIGHBORS = new int[][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    private static final char MARK_VISITED = '#';
+
+    class TrieNode {
+        private TrieNode[] children = new TrieNode[26];
+        private String word;
+    }
+
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> result = new ArrayList<>();
+        TrieNode root = buildTrie(words);
+
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                helper(board, r, c, root, result);
+            }
+        }
+        
+        return result;
+    }
+
+    private void helper(char[][] board, int row, int col, TrieNode node, List<String> result) {
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) return;
+        char ch = board[row][col];
+        if (ch == MARK_VISITED || node.children[ch - 'a'] == null) return;
+
+        node = node.children[ch - 'a'];
+        if (node.word != null) {
+            result.add(node.word);
+            node.word = null;
+        }
+
+        board[row][col] = MARK_VISITED;
+        for (int[] neighbor : NEIGHBORS) {
+            helper(board, row + neighbor[0], col + neighbor[1], node, result);
+        }
+        board[row][col] = ch;
+    }
+
+    private TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for (String word : words) {
+            TrieNode node = root;
+            for (char ch : word.toCharArray()) {
+                int idx = ch - 'a';
+                if (node.children[idx] == null) {
+                    node.children[idx] = new TrieNode();
+                }
+                node = node.children[idx];
+            }
+            node.word = word;
+        }
+        return root;
+    }
+}
