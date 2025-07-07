5a53877a1eded4e6b3f399e0ad017944d5c74c68
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-07 16:36:15 +0800

    Time: 5 ms (32.06%), Space: 42.3 MB (9.66%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -3,11 +3,14 @@ class Solution {
         if (s == null || wordDict == null || s.isEmpty() || wordDict.isEmpty()) {
             return new ArrayList<>();
         }
-
-        return helper(s, wordDict, 0);
+        return helper(s, wordDict, 0, new HashMap<>());
     }
 
-    private List<String> helper(String s, List<String> wordDict, int idx) {
+    private List<String> helper(String s, List<String> wordDict, int idx, Map<Integer, List<String>> memo) {
+        if (memo.containsKey(idx)) {
+            return memo.get(idx);
+        }
+
         List<String> result = new ArrayList<>();
 
         if (idx >= s.length()) {
@@ -17,7 +20,7 @@ class Solution {
 
         for (String word : wordDict) {
             if (s.startsWith(word, idx)) {
-                List<String> sublist = helper(s, wordDict, idx + word.length());
+                List<String> sublist = helper(s, wordDict, idx + word.length(), memo);
                 for (String sub : sublist) {
                     String spaceOptional = sub.isEmpty() ? "" : " ";
                     result.add(word + spaceOptional + sub);
@@ -25,6 +28,7 @@ class Solution {
             }
         }
 
+        memo.put(idx, result);
         return result;
     }
 }
