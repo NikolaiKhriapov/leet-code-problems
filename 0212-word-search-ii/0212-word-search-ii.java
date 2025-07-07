bc8b081d8aa987424344b319f1dbbe99b4c49abc
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-07 20:47:10 +0800

    Time: 233 ms (55.27%), Space: 45.2 MB (57.17%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -2,29 +2,34 @@ class Solution {
     private static final int[][] NEIGHBORS = new int[][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     private static final char MARK_VISITED = '#';
 
-    class TrieNode {
+    private class TrieNode {
         private TrieNode[] children = new TrieNode[26];
         private String word;
     }
 
     public List<String> findWords(char[][] board, String[] words) {
-        List<String> result = new ArrayList<>();
-        TrieNode root = buildTrie(words);
+        if (board == null || words == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        if (board.length == 0 || board[0].length == 0 || words.length == 0) {
+            return new ArrayList<>();
+        }
 
+        List<String> result = new ArrayList<>();;
+        TrieNode root = buildTrie(words);
         for (int r = 0; r < board.length; r++) {
             for (int c = 0; c < board[0].length; c++) {
                 helper(board, r, c, root, result);
             }
         }
-        
         return result;
     }
 
     private void helper(char[][] board, int row, int col, TrieNode node, List<String> result) {
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) return;
+        if (row < 0 || col < 0 || row >= board.length || col >= board[0].length) return;
         char ch = board[row][col];
         if (ch == MARK_VISITED || node.children[ch - 'a'] == null) return;
-
+        
         node = node.children[ch - 'a'];
         if (node.word != null) {
             result.add(node.word);
@@ -43,11 +48,10 @@ class Solution {
         for (String word : words) {
             TrieNode node = root;
             for (char ch : word.toCharArray()) {
-                int idx = ch - 'a';
-                if (node.children[idx] == null) {
-                    node.children[idx] = new TrieNode();
+                if (node.children[ch - 'a'] == null) {
+                    node.children[ch - 'a'] = new TrieNode();
                 }
-                node = node.children[idx];
+                node = node.children[ch - 'a'];
             }
             node.word = word;
         }
