4aeee0efda92aac52799bdd958a0b89824a27aa7
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-08 12:28:54 +0800

    Time: 2 ms (98.76%), Space: 45.9 MB (55.47%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -12,18 +12,18 @@ class Solution {
                 candies[i] = candies[i - 1] + 1;
             }
         }
-
+        
         for (int i = candies.length - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
                 candies[i] = candies[i + 1] + 1;
             }
         }
 
-        int candiesCount = 0;
+        int totalCandies = 0;
         for (int i = 0; i < candies.length; i++) {
-            candiesCount += candies[i];
+            totalCandies += candies[i];
         }
 
-        return candiesCount;
+        return totalCandies;
     }
 }
