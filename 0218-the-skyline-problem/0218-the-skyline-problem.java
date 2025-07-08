5436c71835137315243012c91ba0d1c6407df744
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-08 14:29:27 +0800

    Time: 239 ms (19.64%), Space: 51 MB (73.61%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.java
+++ 0218-the-skyline-problem/0218-the-skyline-problem.java
@@ -1,6 +1,9 @@
 class Solution {
     public List<List<Integer>> getSkyline(int[][] buildings) {
-        if (buildings == null || buildings.length == 0) {
+        if (buildings == null || buildings[0].length != 3) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        if (buildings.length == 0) {
             return new ArrayList<>();
         }
 
@@ -10,10 +13,10 @@ class Solution {
             events.add(new int[] {building[1], building[2]});
         }
         events.sort((a, b) -> {
-            if (a[0] != b[0]) return Integer.compare(a[0], b[0]);
-            return Integer.compare(a[1], b[1]);
+            if (a[0] == b[0]) return Integer.compare(a[1], b[1]);
+            return Integer.compare(a[0], b[0]);
         });
-
+        
         List<List<Integer>> result = new ArrayList<>();
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
         pq.add(0);
@@ -22,20 +25,19 @@ class Solution {
         for (int[] event : events) {
             int x = event[0];
             int h = event[1];
-
             if (h < 0) {
                 pq.add(-h);
             } else {
                 pq.remove(h);
             }
 
-            int currHeight = pq.peek();
-            if (currHeight != prevHeight) {
-                result.add(List.of(x, currHeight));
-                prevHeight = currHeight;
+            int height = pq.peek();
+            if (height != prevHeight) {
+                result.add(List.of(x, height));
+                prevHeight = height;
             }
         }
-        
+
         return result;
     }
 }
