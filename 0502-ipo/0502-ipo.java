9cc8b6d2c327e06492caac51d48d5590e2183e0a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-09 14:58:27 +0800

    Time: 167 ms (11.37%), Space: 63.3 MB (49.11%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -3,24 +3,24 @@ class Solution {
         if (k < 0 || w < 0 || profits == null || capital == null || profits.length != capital.length) {
             throw new IllegalArgumentException("Invalid input");
         }
-        
-        PriorityQueue<int[]> minCapital = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
-        PriorityQueue<Integer> maxProfit = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
 
-        for (int i = 0; i < profits.length; i++) {
+        PriorityQueue<int[]> minCapital = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
+        PriorityQueue<Integer> maxProfits = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+
+        for (int i = 0; i < capital.length; i++) {
             minCapital.add(new int[] {capital[i], profits[i]});
         }
 
         while (k-- > 0) {
             while (!minCapital.isEmpty() && minCapital.peek()[0] <= w) {
-                maxProfit.add(minCapital.poll()[1]);
+                maxProfits.add(minCapital.poll()[1]);
             }
-            if (maxProfit.isEmpty()) {
+            if (maxProfits.isEmpty()) {
                 break;
             }
-            w += maxProfit.poll();
+            w += maxProfits.poll();
         }
-
+        
         return w;
     }
 }
