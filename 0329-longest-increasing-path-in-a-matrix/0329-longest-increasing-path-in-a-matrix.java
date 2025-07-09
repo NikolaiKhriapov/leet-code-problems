ed156c1c6b81471e4021aca38be21a484a4ed9df
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-09 14:28:52 +0800

    Time: 9 ms (52.83%), Space: 45.2 MB (53.89%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -1,15 +1,17 @@
 class Solution {
-    private static final int[][] NEIGHBORS = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private static final int[][] NEIGHBORS = new int[][] {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
     public int longestIncreasingPath(int[][] matrix) {
 
         int longest = 0;
         int[][] memo = new int[matrix.length][matrix[0].length];
+
         for (int r = 0; r < matrix.length; r++) {
             for (int c = 0; c < matrix[0].length; c++) {
                 longest = Math.max(longest, helper(matrix, r, c, -1, memo));
             }
         }
+
         return longest;
     }
 
@@ -17,17 +19,16 @@ class Solution {
         if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length || matrix[row][col] <= prev) {
             return 0;
         }
-
+        
         if (memo[row][col] != 0) {
             return memo[row][col];
         }
 
         int longest = 1;
-
         for (int[] neighbor : NEIGHBORS) {
             longest = Math.max(longest, 1 + helper(matrix, row + neighbor[0], col + neighbor[1], matrix[row][col], memo));
         }
-    
+
         memo[row][col] = longest;
         return longest;
     }
