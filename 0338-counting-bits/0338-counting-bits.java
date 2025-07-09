db54798af1ff12cee78c8f1ecbca729e9f4fc2d8
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-09 19:27:09 +0800

    Time: 2 ms (94.41%), Space: 46.9 MB (19.94%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,7 +1,11 @@
 class Solution {
     public int[] countBits(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+
         int[] result = new int[n + 1];
-        for (int i = 0; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             result[i] = result[i / 2] + (i % 2);
         }
         return result;
