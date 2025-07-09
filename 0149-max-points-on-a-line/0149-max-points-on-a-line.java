8d5f50fc2cfa54b69f6a946409309386ff4a0b8e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-09 12:13:49 +0800

    Time: 36 ms (35.04%), Space: 44.6 MB (69.75%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -1,15 +1,18 @@
 class Solution {
     public int maxPoints(int[][] points) {
-        if (points == null || points.length == 0 || points[0].length == 0) {
+        if (points == null || points.length == 0 || points[0].length != 2) {
             return 0;
+        }
+        if (points.length <= 2) {
+            return points.length;
         }
 
         int result = 0;
         for (int i = 0; i < points.length; i++) {
             Map<String, Integer> slopeCount = new HashMap<>();
+            int sum = 0;;
             int duplicates = 0;
-            int max = 0;
-            for (int j = i + 1; j < points.length; j++) {
+            for (int j  = i + 1; j < points.length; j++) {
                 int dx = points[j][0] - points[i][0];
                 int dy = points[j][1] - points[i][1];
                 if (dx == 0 && dy == 0) {
@@ -24,10 +27,10 @@ class Solution {
                     }
                     String slopeKey = dy + "/" + dx;
                     slopeCount.put(slopeKey, slopeCount.getOrDefault(slopeKey, 0) + 1);
-                    max = Math.max(max, slopeCount.get(slopeKey));
+                    sum = Math.max(sum, slopeCount.get(slopeKey));
                 }
             }
-            result = Math.max(result, max + 1 + duplicates);
+            result = Math.max(result, sum + 1 + duplicates);
         }
         return result;
     }
