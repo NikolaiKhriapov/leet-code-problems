8c5466c0a60c83e9effc44ca3da725103d733e9a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-09 20:07:06 +0800

    Time: 0 ms (100%), Space: 45.3 MB (71.55%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
@@ -18,20 +18,12 @@ class Solution {
         if (root == null) {
             return null;
         }
+        
         if (root.val == val) {
             return root;
+        } else if (root.val > val) {
+            return searchBST(root.left, val);
         }
-        
-        TreeNode left = searchBST(root.left, val);
-        if (left != null) {
-            return left;
-        }
-        
-        TreeNode right = searchBST(root.right, val);
-        if (right != null) {
-            return right;
-        }
-
-        return null;
+        return searchBST(root.right, val);
     }
 }
