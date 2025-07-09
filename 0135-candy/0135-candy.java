4ffcdd2614589f122a92d0feb62fb46f6d2d5521
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-09 16:19:57 +0800

    Time: 3 ms (74.59%), Space: 45.7 MB (79.66%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,29 +1,29 @@
 class Solution {
     public int candy(int[] ratings) {
         if (ratings == null || ratings.length == 0) {
-            return 0;
+            throw new IllegalArgumentException("Invalid input");
         }
 
         int[] candies = new int[ratings.length];
         Arrays.fill(candies, 1);
 
-        for (int i = 1; i < candies.length; i++) {
+        for (int i = 1; i < ratings.length; i++) {
             if (ratings[i] > ratings[i - 1]) {
                 candies[i] = candies[i - 1] + 1;
             }
         }
-        
-        for (int i = candies.length - 2; i >= 0; i--) {
+
+        for (int i = ratings.length - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
                 candies[i] = candies[i + 1] + 1;
             }
         }
-
-        int totalCandies = 0;
+        
+        int count = 0;
         for (int i = 0; i < candies.length; i++) {
-            totalCandies += candies[i];
+            count += candies[i];
         }
 
-        return totalCandies;
+        return count;
     }
 }
