2cf415f2c7b6765a59a17d802e07aefb6bfe083a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-09 16:48:05 +0800

    Time: 1 ms (99.86%), Space: 42.3 MB (75.32%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -17,15 +17,17 @@ class Solution {
             return maxProfit;
         }
 
-        int[][] dp = new int[k + 1][prices.length];
+        int[] prev = new int[prices.length];
+        int[] curr = new int[prices.length];
         for (int i = 1; i <= k; i++) {
             int maxDiff = -prices[0];
             for (int j = 1; j < prices.length; j++) {
-                dp[i][j] = Math.max(dp[i][j - 1], prices[j] + maxDiff);
-                maxDiff = Math.max(maxDiff, dp[i - 1][j] - prices[j]);
+                curr[j] = Math.max(curr[j - 1], prices[j] + maxDiff);
+                maxDiff = Math.max(maxDiff, prev[j] - prices[j]);
             }
+            prev = Arrays.copyOf(curr, curr.length);
         }
-        return dp[k][prices.length - 1];
+        return prev[prices.length - 1];
     }
 }
 
