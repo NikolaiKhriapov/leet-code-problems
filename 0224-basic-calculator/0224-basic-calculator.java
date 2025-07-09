db0f48d550e843643b532e854fac8d19582f15dc
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-09 12:23:46 +0800

    Time: 7 ms (86.44%), Space: 44.5 MB (34.78%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,30 +1,30 @@
 class Solution {
     public int calculate(String s) {
-        if (s == null || s.isEmpty()) {
+        if (s == null) {
             throw new IllegalArgumentException("Invalid input");
         }
 
+        Stack<Integer> stack = new Stack<>();
         int result = 0;
         int number = 0;
         int sign = 1;
-        Stack<Integer> stack = new Stack<>();
-
+        
         for (char ch : s.toCharArray()) {
             if (Character.isDigit(ch)) {
                 number = number * 10 + (ch - '0');
             } else if (ch == '+') {
-                result += number * sign;
-                number = 0;
+                result += sign * number;
                 sign = 1;
-            } else if (ch == '-') {
-                result += number * sign;
                 number = 0;
+            } else if (ch == '-') {
+                result += sign * number;
                 sign = -1;
+                number = 0;
             } else if (ch == '(') {
                 stack.add(result);
                 stack.add(sign);
-                result = 0;
                 sign = 1;
+                result = 0;
             } else if (ch == ')') {
                 result += sign * number;
                 result *= stack.pop();
@@ -32,7 +32,7 @@ class Solution {
                 number = 0;
             }
         }
-        
+
         result += sign * number;
         return result;
     }
