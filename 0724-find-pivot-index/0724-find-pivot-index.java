cc37ac7830b8e4b076e85356e9b34b2e3879d638
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-10 17:58:43 +0800

    Time: 1 ms (92.85%), Space: 45.3 MB (77.99%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -11,7 +11,7 @@ class Solution {
 
         int leftSum = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (leftSum == totalSum - leftSum - nums[i]) {
+            if (totalSum - leftSum - nums[i] == leftSum) {
                 return i;
             }
             leftSum += nums[i];
