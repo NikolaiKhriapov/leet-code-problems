cc15aacc7869c1e7c5a51a3837daa6dc65751890
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-10 16:17:16 +0800

    Time: 32 ms (51.29%), Space: 58.6 MB (63.82%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,12 +1,11 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if (nums == null || nums.length < k) {
+        if (nums == null || k <= 0) {
             throw new IllegalArgumentException("Invalid input");
         }
 
         int[] result = new int[nums.length - k + 1];
         Deque<Integer> deque = new LinkedList<>();
-
         for (int i = 0; i < nums.length; i++) {
             if (!deque.isEmpty() && deque.peekFirst() <= i - k) {
                 deque.pollFirst();
@@ -15,11 +14,10 @@ class Solution {
                 deque.pollLast();
             }
             deque.offerLast(i);
-            if (i >= k - 1) {
+            if (i - k + 1 >= 0) {
                 result[i - k + 1] = nums[deque.peekFirst()];
             }
         }
-        
         return result;
     }
 }
