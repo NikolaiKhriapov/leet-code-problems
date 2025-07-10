61f9e2aeece2ef32d404751c46380d4fa24a763e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-10 20:10:43 +0800

    Time: 1 ms (70.73%), Space: 42.3 MB (63.67%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -3,15 +3,17 @@ class Solution {
         if (str1 == null || str2 == null) {
             throw new IllegalArgumentException("Invalid input");
         }
-        
-        if (!Objects.equals(str1 + str2, str2 + str1)) {
+
+        if (!(str1 + str2).equals(str2 + str1)) {
             return "";
         }
         return str1.substring(0, findGcd(str1.length(), str2.length()));
     }
 
     private int findGcd(int a, int b) {
-        if (b == 0) return a;
+        if (b == 0) {
+            return a;
+        }
         return findGcd(b, a % b);
     }
 }
