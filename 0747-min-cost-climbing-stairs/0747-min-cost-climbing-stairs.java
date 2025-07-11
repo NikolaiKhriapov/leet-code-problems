48a860e381c873861c635e9217f8df181f4654b0
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-11 14:57:47 +0800

    Time: 0 ms (100%), Space: 43.1 MB (63.27%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -7,11 +7,14 @@ class Solution {
             return 0;
         }
 
-        int[] dp = new int[cost.length + 1];
-        for (int i = 2; i < dp.length; i++) {
-            dp[i] = Math.min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
+        int prevPrev = 0;
+        int prev = 0;
+        for (int i = 2; i <= cost.length; i++) {
+            int curr = Math.min(prevPrev + cost[i - 2], prev + cost[i - 1]);
+            prevPrev = prev;
+            prev = curr;
         }
-        return dp[dp.length - 1];
+        return prev;
     }
 }
 
