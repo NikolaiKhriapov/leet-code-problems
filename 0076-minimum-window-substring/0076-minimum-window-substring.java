d9b04a417ab4ff754049d7c06ae946b776398c82
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-11 11:39:57 +0800

    Time: 20 ms (45.48%), Space: 45.4 MB (35.99%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,36 +1,41 @@
 class Solution {
     public String minWindow(String s, String t) {
-
+        if (s == null || t == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        if (s.length() < t.length() || s.isEmpty()) {
+            return "";
+        }
+        
         Map<Character, Integer> mapChars = new HashMap<>();
         for (char ch : t.toCharArray()) {
             mapChars.put(ch, mapChars.getOrDefault(ch, 0) + 1);
         }
-        
-        Map<Character, Integer> mapSeen = new HashMap<>();
-        int left = 0;
-        int right = left;
+        Map<Character, Integer> mapSeen = new HashMap<>();        
+        int requiredChars = mapChars.size();
+        int foundChars = 0;
         int start = 0;
         int minLength = Integer.MAX_VALUE;
-        int requiredSize = mapChars.size();
-        int currentSize = 0;
+        int left = 0;
+        int right = 0;
 
         while (right < s.length()) {
-            char ch = s.charAt(right);
-            if (mapChars.containsKey(ch)) {
-                mapSeen.put(ch, mapSeen.getOrDefault(ch, 0) + 1);
-                if (Objects.equals(mapSeen.get(ch), mapChars.get(ch))) {
-                    currentSize++;
+            char rightChar = s.charAt(right);
+            if (mapChars.containsKey(rightChar)) {
+                mapSeen.put(rightChar, mapSeen.getOrDefault(rightChar, 0) + 1);
+                if (Objects.equals(mapChars.get(rightChar), mapSeen.get(rightChar))) {
+                    foundChars++;
                 }
             }
-            while (currentSize == requiredSize) {
-                if (minLength > right - left + 1) {
+            while (foundChars == requiredChars) {
+                if (right - left + 1 < minLength) {
                     minLength = right - left + 1;
                     start = left;
                 }
                 char leftChar = s.charAt(left);
                 if (mapChars.containsKey(leftChar)) {
-                    if (Objects.equals(mapSeen.get(leftChar), mapChars.get(leftChar))) {
-                        currentSize--;
+                    if (Objects.equals(mapChars.get(leftChar), mapSeen.get(leftChar))) {
+                        foundChars--;
                     }
                     mapSeen.put(leftChar, mapSeen.get(leftChar) - 1);
                 }
@@ -38,7 +43,7 @@ class Solution {
             }
             right++;
         }
-        
+
         return (minLength == Integer.MAX_VALUE) ? "" : s.substring(start, start + minLength);
     }
 }
