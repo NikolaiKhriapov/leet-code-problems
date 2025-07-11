a8af58a40969bca2ee9fd6475270182df255cc46
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-11 14:56:12 +0800

    Time: 1 ms (38.05%), Space: 43.5 MB (18.93%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -3,18 +3,15 @@ class Solution {
         if (cost == null) {
             throw new IllegalArgumentException("Invalid input");
         }
-
-        int prev2 = 0;
-        int prev1 = 0;
-
-        int curr = 0;
-        for (int i = 2; i <= cost.length; i++) {
-            curr = Math.min(prev2 + cost[i - 2], prev1 + cost[i - 1]);
-            prev2 = prev1;
-            prev1 = curr;
+        if (cost.length <= 1) {
+            return 0;
         }
 
-        return curr;
+        int[] dp = new int[cost.length + 1];
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
+        }
+        return dp[dp.length - 1];
     }
 }
 
