2a6db5165f44227e91759ccbd3d0f110067a61d2
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-11 14:31:21 +0800

    Time: 239 ms (19.16%), Space: 50.9 MB (76.48%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.java
+++ 0218-the-skyline-problem/0218-the-skyline-problem.java
@@ -1,20 +1,18 @@
 class Solution {
     public List<List<Integer>> getSkyline(int[][] buildings) {
-        if (buildings == null || buildings.length == 0) {
-            return new ArrayList<>();
-        }
+
+        List<List<Integer>> result = new ArrayList<>();
 
         List<int[]> events = new ArrayList<>();
         for (int[] building : buildings) {
             events.add(new int[] {building[0], -building[2]});
             events.add(new int[] {building[1], building[2]});
         }
-        Collections.sort(events, (a, b) ->{
+        Collections.sort(events, (a, b) -> {
             if (a[0] == b[0]) return Integer.compare(a[1], b[1]);
             return Integer.compare(a[0], b[0]);
         });
 
-        List<List<Integer>> result = new ArrayList<>();
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
         pq.add(0);
 
@@ -27,7 +25,6 @@ class Solution {
             } else {
                 pq.remove(h);
             }
-
             int currHeight = pq.peek();
             if (currHeight != prevHeight) {
                 result.add(List.of(x, currHeight));
