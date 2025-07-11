a6f512c92ab91f1d194074216f1032ca28ca1b15
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-11 13:16:05 +0800

    Time: 3 ms (74.61%), Space: 45.6 MB (86.82%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,27 +1,29 @@
 class Solution {
     public int candy(int[] ratings) {
-        if (ratings == null || ratings.length == 0) {
+        if (ratings == null) {
             throw new IllegalArgumentException("Invalid input");
+        }
+        if (ratings.length <= 1) {
+            return ratings.length;
         }
 
         int[] candies = new int[ratings.length];
         Arrays.fill(candies, 1);
 
-        for (int i = 1; i < ratings.length; i++) {
+        for (int i = 1; i < candies.length; i++) {
             if (ratings[i] > ratings[i - 1]) {
-                candies[i] = candies[i - 1] + 1;
+                candies[i] = Math.max(candies[i], candies[i - 1] + 1);
+            }
+        }
+        for (int i = candies.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
             }
         }
 
-        for (int i = ratings.length - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
-                candies[i] = candies[i + 1] + 1;
-            }
-        }
-        
         int count = 0;
-        for (int i = 0; i < candies.length; i++) {
-            count += candies[i];
+        for (int n : candies) {
+            count += n;
         }
 
         return count;
