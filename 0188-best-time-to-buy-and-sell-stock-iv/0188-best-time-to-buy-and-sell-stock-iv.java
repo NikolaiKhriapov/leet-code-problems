230e79df62d5fb21fe9b62911a37b7db7e66d8f0
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-11 13:53:37 +0800

    Time: 3 ms (62.04%), Space: 41.8 MB (85.3%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,24 +1,15 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
-        if (prices == null || k < 0 || prices.length == 0) {
+        if (prices == null || k < 0) {
             throw new IllegalArgumentException("Invalid input");
         }
-        if (k == 0 || prices.length == 1) {
+        if (k == 0 || prices.length <= 1) {
             return 0;
-        }
-
-        if (k >= prices.length / 2) {
-            int maxProfit = 0;
-            for (int i = 1; i < prices.length; i++) {
-                if (prices[i] > prices[i - 1]) {
-                    maxProfit += prices[i] - prices[i - 1];
-                }
-            }
-            return maxProfit;
         }
 
         int[] prev = new int[prices.length];
         int[] curr = new int[prices.length];
+
         for (int i = 1; i <= k; i++) {
             int maxDiff = -prices[0];
             for (int j = 1; j < prices.length; j++) {
@@ -29,6 +20,7 @@ class Solution {
             prev = curr;
             curr = temp;
         }
+        
         return prev[prices.length - 1];
     }
 }
