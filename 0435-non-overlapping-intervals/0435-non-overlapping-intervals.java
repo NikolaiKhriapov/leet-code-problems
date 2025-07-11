7a6674092817ae4a210b301b9bad82e339078695
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-11 16:53:15 +0800

    Time: 46 ms (81.76%), Space: 74 MB (17.52%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -3,18 +3,16 @@ class Solution {
 
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
 
-        int maxCount = 1;
         int count = 1;
         int prevEnd = intervals[0][1];
         for (int i = 1; i < intervals.length; i++) {
             if (intervals[i][0] >= prevEnd) {
                 prevEnd = intervals[i][1];
                 count++;
-                maxCount = Math.max(maxCount, count);
             }
         }
         
-        return intervals.length - maxCount;
+        return intervals.length - count;
     }
 }
 
