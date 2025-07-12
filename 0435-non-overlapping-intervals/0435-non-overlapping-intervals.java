b8befebbba2f1dd546da4b02b6783d137ecb8885
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-12 14:38:32 +0800

    Time: 48 ms (56.23%), Space: 73.3 MB (90.62%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -3,21 +3,18 @@ class Solution {
         if (intervals == null || intervals.length == 0 || intervals[0].length != 2) {
             throw new IllegalArgumentException("Invalid input");
         }
-        
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
 
-        int count = 1;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+        
         int prevEnd = intervals[0][1];
+        int count = 1;
         for (int i = 1; i < intervals.length; i++) {
             if (intervals[i][0] >= prevEnd) {
                 prevEnd = intervals[i][1];
                 count++;
             }
         }
-        
+
         return intervals.length - count;
     }
 }
-
-// [[1,2],[2,3],[3,4],[1,3]]
-// [[1,2],[2,3],[1,3],[3,4]]
