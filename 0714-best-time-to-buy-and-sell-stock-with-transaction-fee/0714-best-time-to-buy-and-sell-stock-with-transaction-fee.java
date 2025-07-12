cf4dd2ee19dea734eac40d25edba51f5911befeb
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-12 17:06:06 +0800

    Time: 4 ms (99.42%), Space: 54.9 MB (89.75%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        if (prices == null || prices.length == 0) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        if (prices.length == 1) {
+            return 0;
+        }
+
+        int cash = 0;
+        int hold = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            cash = Math.max(cash, hold + prices[i] - fee);
+            hold = Math.max(hold, cash - prices[i]);
+        }
+        return cash;
+    }
+}
