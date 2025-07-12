5c9f61185bba894d38c32a892e7ae91718372fe5
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-12 12:01:23 +0800

    Time: 0 ms (100%), Space: 42.2 MB (17.57%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -1,38 +1,25 @@
 class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
-        if (s == null || wordDict == null) {
-            throw new IllegalArgumentException("Invalid input");
-        }
-        if (s.isEmpty() || wordDict.isEmpty()) {
-            return new ArrayList<>();
-        }
-
-        return helper(s, wordDict, 0, new HashMap<>());
-    }
-
-    private List<String> helper(String s, List<String> wordDict, int idx, Map<Integer, List<String>> memo) {
-        if (memo.containsKey(idx)) {
-            return memo.get(idx);
-        }
 
         List<String> result = new ArrayList<>();
+        helper(s, wordDict, 0, new StringBuilder(), result);
+        return result;
+    }
 
-        if (idx >= s.length()) {
-            result.add("");
-            return result;
+    private void helper(String s, List<String> wordDict, int idx, StringBuilder curr, List<String> result) {
+        if (idx == s.length()) {
+            result.add(curr.toString());
+            return;
         }
 
+        int currLength = curr.length();
         for (String word : wordDict) {
             if (s.startsWith(word, idx)) {
-                List<String> sublist = helper(s, wordDict, idx + word.length(), memo);
-                for (String sub : sublist) {
-                    String spaceOptional = sub.isEmpty() ? "" : " ";
-                    result.add(word + spaceOptional + sub);
-                }
+                if (!curr.isEmpty()) curr.append(" ");
+                curr.append(word);
+                helper(s, wordDict, idx + word.length(), curr, result);
+                curr.delete(currLength, curr.length());
             }
         }
-
-        memo.put(idx, result);
-        return result;
     }
 }
