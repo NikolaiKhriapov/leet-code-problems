ef23196e0cdbe0a676333157b32eb429d040cc2b
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-12 15:48:19 +0800

    Time: 4 ms (17.81%), Space: 47.5 MB (69.15%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,38 +1,21 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
 
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int i = 0; i < isConnected.length; i++) {
-            graph.put(i, new ArrayList<>());
-        }
-        for (int i = 0; i < isConnected.length; i++) {
-            for (int j = 0; j < isConnected[0].length; j++) {
-                if (isConnected[i][j] == 1) {
-                    graph.get(i).add(j);
-                    graph.get(j).add(i);
-                }
-            }
-        }
-
         Queue<Integer> queue = new LinkedList<>();
         Set<Integer> visited = new HashSet<>();
-
         int provinces = 0;
-        for (var entry : graph.entrySet()) {
-            if (!entry.getValue().isEmpty()) {
-                queue.add(entry.getKey());
-                visited.add(entry.getKey());
+
+        for (int i = 0; i < isConnected.length; i++) {
+            if (!visited.contains(i)) {
+                queue.add(i);
+                visited.add(i);
                 while (!queue.isEmpty()) {
-                    int size = queue.size();
-                    while (size-- > 0) {
-                        int currCity = queue.poll();
-                        for (int neighbor : graph.get(currCity)) {
-                            if (!visited.contains(neighbor)) {
-                                queue.add(neighbor);
-                                visited.add(neighbor);
-                            }
+                    int currCity = queue.poll();
+                    for (int j = 0; j < isConnected.length; j++) {
+                        if (isConnected[currCity][j] == 1 && !visited.contains(j)) {
+                            queue.add(j);
+                            visited.add(j);
                         }
-                        graph.put(currCity, new ArrayList<>());
                     }
                 }
                 provinces++;
