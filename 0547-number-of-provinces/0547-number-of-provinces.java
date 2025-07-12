fcb6441dbec433cdf0791f1deaf2bcb4d2560b48
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-12 15:49:59 +0800

    Time: 2 ms (46.27%), Space: 48.1 MB (37.07%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -2,19 +2,19 @@ class Solution {
     public int findCircleNum(int[][] isConnected) {
 
         Queue<Integer> queue = new LinkedList<>();
-        Set<Integer> visited = new HashSet<>();
+        boolean[] visited = new boolean[isConnected.length];
         int provinces = 0;
 
         for (int i = 0; i < isConnected.length; i++) {
-            if (!visited.contains(i)) {
+            if (!visited[i]) {
                 queue.add(i);
-                visited.add(i);
+                visited[i] = true;
                 while (!queue.isEmpty()) {
                     int currCity = queue.poll();
                     for (int j = 0; j < isConnected.length; j++) {
-                        if (isConnected[currCity][j] == 1 && !visited.contains(j)) {
+                        if (isConnected[currCity][j] == 1 && !visited[j]) {
                             queue.add(j);
-                            visited.add(j);
+                            visited[j] = true;
                         }
                     }
                 }
