d5bec76bd1dec752181a3310a935d88e0fd0a591
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-12 13:53:29 +0800

    Time: 8 ms (84.56%), Space: 44.7 MB (98.02%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -2,8 +2,11 @@ class Solution {
     private static final int[][] NEIGHBORS = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
     public int longestIncreasingPath(int[][] matrix) {
-        if (matrix == null || matrix.length == 0) {
+        if (matrix == null) {
             throw new IllegalArgumentException("Invalid input");
+        }
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
         }
 
         int longest = 0;
@@ -17,14 +20,13 @@ class Solution {
     }
 
     private int helper(int[][] matrix, int row, int col, int prev, int[][] memo) {
-        if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length || matrix[row][col] <= prev) {
-            return 0;
-        }
+        if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length || matrix[row][col] <= prev) return 0;
+
         if (memo[row][col] != 0) {
             return memo[row][col];
         }
 
-        int longest = 0;
+        int longest = 1;
         for (int[] neighbor : NEIGHBORS) {
             longest = Math.max(longest, 1 + helper(matrix, row + neighbor[0], col + neighbor[1], matrix[row][col], memo));
         }
