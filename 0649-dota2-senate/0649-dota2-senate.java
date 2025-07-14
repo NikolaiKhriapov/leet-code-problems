2f160521afa3b15ae9b1aac0c122bb68dee4cdb5
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-14 14:03:56 +0800

    Time: 12 ms (44.37%), Space: 45 MB (70.7%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.java
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -9,24 +9,24 @@ class Solution {
             throw new IllegalArgumentException("Invalid input");
         }
 
-        Queue<Integer> queue1 = new LinkedList<>();
-        Queue<Integer> queue2 = new LinkedList<>();
+        Deque<Integer> queue1 = new ArrayDeque<>();
+        Deque<Integer> queue2 = new ArrayDeque<>();
         for (int i = 0; i < senate.length(); i++) {
             char ch = senate.charAt(i);
             switch (ch) {
-                case CHAR_1 -> queue1.add(i);
-                case CHAR_2 -> queue2.add(i);
+                case CHAR_1 -> queue1.offerLast(i);
+                case CHAR_2 -> queue2.offerLast(i);
                 default -> throw new IllegalArgumentException("Invalid input");
             }
         }
 
         while (!queue1.isEmpty() && !queue2.isEmpty()) {
-            int a = queue1.poll();
-            int b = queue2.poll();
+            int a = queue1.pollFirst();
+            int b = queue2.pollFirst();
             if (a < b) {
-                queue1.add(a + senate.length());
+                queue1.offerLast(a + senate.length());
             } else {
-                queue2.add(b + senate.length());
+                queue2.offerLast(b + senate.length());
             }
         }
 
