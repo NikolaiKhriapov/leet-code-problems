30b116a03b9be06b940d0d3999a869cace366306
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-14 14:02:57 +0800

    Time: 11 ms (73.66%), Space: 45.8 MB (40.29%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.java
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -1,35 +1,35 @@
 class Solution {
-    private static final char CHAR_1 = 'R';
-    private static final char CHAR_2 = 'D';
-    private static final String NAME_1 = "Radiant";
-    private static final String NAME_2 = "Dire";
+    private final static char CHAR_1 = 'R';
+    private final static char CHAR_2 = 'D';
+    private final static String NAME_1 = "Radiant";
+    private final static String NAME_2 = "Dire";
 
     public String predictPartyVictory(String senate) {
         if (senate == null || senate.isEmpty()) {
             throw new IllegalArgumentException("Invalid input");
         }
-        
+
         Queue<Integer> queue1 = new LinkedList<>();
         Queue<Integer> queue2 = new LinkedList<>();
-
         for (int i = 0; i < senate.length(); i++) {
-            switch (senate.charAt(i)) {
+            char ch = senate.charAt(i);
+            switch (ch) {
                 case CHAR_1 -> queue1.add(i);
                 case CHAR_2 -> queue2.add(i);
                 default -> throw new IllegalArgumentException("Invalid input");
             }
         }
-        
+
         while (!queue1.isEmpty() && !queue2.isEmpty()) {
-            int idx1 = queue1.poll();
-            int idx2 = queue2.poll();
-            if (idx1 < idx2) {
-                queue1.add(idx1 + senate.length());
+            int a = queue1.poll();
+            int b = queue2.poll();
+            if (a < b) {
+                queue1.add(a + senate.length());
             } else {
-                queue2.add(idx2 + senate.length());
+                queue2.add(b + senate.length());
             }
         }
 
-        return queue1.isEmpty() ? NAME_2 : NAME_1;
+        return (!queue1.isEmpty()) ? NAME_1 : NAME_2;
     }
 }
