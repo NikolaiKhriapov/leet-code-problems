5b93dd0864627be377a37a938b7cd527c9ead2ce
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-14 16:36:39 +0800

    Time: 12 ms (66.49%), Space: 44.9 MB (95.65%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,32 +1,35 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-
-        int maxPile = Integer.MIN_VALUE;
-        for (int pile : piles) {
-            maxPile = Math.max(maxPile, pile);
+        if (piles == null || h < 0) {
+            throw new IllegalArgumentException("Invalid input");
         }
 
         int left = 1;
-        int right = maxPile;
-
+        int right = getMaxPileSize(piles);
         while (left < right) {
             int mid = left + (right - left) / 2;
             int hours = countHours(piles, mid);
-            
             if (hours > h) {
                 left = mid + 1;
             } else {
                 right = mid;
             }
         }
-        
         return left;
     }
 
-    private int countHours(int[] piles, int k) {
+    private int getMaxPileSize(int[] piles) {
+        int maxPile = Integer.MIN_VALUE;
+        for (int pile : piles) {
+            maxPile = Math.max(maxPile, pile);
+        }
+        return maxPile;
+    }
+
+    private int countHours(int[] piles, int mid) {
         int hours = 0;
         for (int pile : piles) {
-            hours += pile / k + (pile % k == 0 ? 0 : 1);
+            hours += (pile / mid) + (pile % mid == 0 ? 0 : 1);
         }
         return hours;
     }
