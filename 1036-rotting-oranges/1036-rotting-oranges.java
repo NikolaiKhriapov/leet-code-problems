d2b57272921d538a5c48ac9924521e2b4caa20e5
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-14 18:07:10 +0800

    Time: 1 ms (100%), Space: 43 MB (15.36%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,28 +1,33 @@
 class Solution {
     private static final int FRESH = 1;
     private static final int ROTTEN = 2;
-    private static final int[][] NEIGHBORS = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private static final int[][] NEIGHBORS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
     public int orangesRotting(int[][] grid) {
+        if (grid == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        if (grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
 
         Queue<int[]> queue = new LinkedList<>();
 
         int countFresh = 0;
         for (int r = 0; r < grid.length; r++) {
             for (int c = 0; c < grid[0].length; c++) {
-                if (grid[r][c] == ROTTEN) {
-                    queue.add(new int[] {r, c});
-                }
                 if (grid[r][c] == FRESH) {
                     countFresh++;
+                } else if (grid[r][c] == ROTTEN) {
+                    queue.offer(new int[] {r, c});
                 }
             }
         }
 
-        int minutes = 0;
+        int countMinutes = 0;
         while (!queue.isEmpty()) {
             int size = queue.size();
-            boolean isSuccessful = false;
+            boolean isRottenThisMinute = false;
             while (size-- > 0) {
                 int[] curr = queue.poll();
                 for (int[] neighbor : NEIGHBORS) {
@@ -31,16 +36,16 @@ class Solution {
                     if (r >= 0 && r < grid.length && c >= 0 && c < grid[0].length && grid[r][c] == FRESH) {
                         grid[r][c] = ROTTEN;
                         queue.add(new int[] {r, c});
-                        isSuccessful = true;
+                        isRottenThisMinute = true;
                         countFresh--;
                     }
                 }
             }
-            if (isSuccessful) {
-                minutes++;
+            if (isRottenThisMinute) {
+                countMinutes++;
             }
         }
-
-        return countFresh == 0 ? minutes : -1;
+        
+        return countFresh == 0 ? countMinutes : -1;
     }
 }
