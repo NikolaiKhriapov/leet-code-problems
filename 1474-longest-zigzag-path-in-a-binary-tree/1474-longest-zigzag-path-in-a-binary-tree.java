d35e95cf591f7b23fa6618b1b478a797b0cea54f
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-14 20:39:26 +0800

    Time: 5 ms (58.55%), Space: 55.3 MB (23.78%) - LeetHub

---
+++ 1474-longest-zigzag-path-in-a-binary-tree/1474-longest-zigzag-path-in-a-binary-tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int longestZigZag(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int[] maxLength = new int[] {0};
+        helper(root.left, true, 1, maxLength);
+        helper(root.right, false, 1, maxLength);
+        return maxLength[0];
+    }
+
+    private void helper(TreeNode node, boolean isLeft, int length, int[] maxLength) {
+        if (node == null) {
+            return;
+        }
+
+        maxLength[0] = Math.max(maxLength[0], length);
+        if (isLeft) {
+            helper(node.right, false, length + 1, maxLength);
+            helper(node.left, true, 1, maxLength);
+        } else {
+            helper(node.left, true, length + 1, maxLength);
+            helper(node.right, false, 1, maxLength);
+        }
+    }
+}
