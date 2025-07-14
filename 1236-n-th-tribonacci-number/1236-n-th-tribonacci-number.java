12ace47772b32bc60cbb8c94fff80f60546e770b
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-14 12:15:24 +0800

    Time: 0 ms (100%), Space: 40.7 MB (17.59%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -3,6 +3,7 @@ class Solution {
         if (n < 0) {
             throw new IllegalArgumentException("Invalid input");
         }
+
         if (n == 0) return 0;
         if (n == 1) return 1;
         if (n == 2) return 1;
@@ -10,12 +11,14 @@ class Solution {
         int a = 0;
         int b = 1;
         int c = 1;
+
         for (int i = 3; i <= n; i++) {
             int curr = a + b + c;
             a = b;
             b = c;
             c = curr;
         }
+
         return c;
     }
 }
