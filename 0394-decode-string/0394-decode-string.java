3ee214dd596fa89008cb13bb8817492eeaba6bfe
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-14 12:29:44 +0800

    Time: 1 ms (68.77%), Space: 41.5 MB (81.73%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,34 +1,32 @@
 class Solution {
     public String decodeString(String s) {
-        if (s == null) {
-            throw new IllegalArgumentException("Invalid input");
-        }
 
-        Stack<Integer> stackNum = new Stack<>();
-        Stack<StringBuilder> stackStr = new Stack<>();
+        Deque<Integer> stackInt = new ArrayDeque<>();
+        Deque<String> stackStr = new ArrayDeque<>();
+
         int currNumber = 0;
         StringBuilder currString = new StringBuilder();
-
+        
         for (char currChar : s.toCharArray()) {
             if (Character.isDigit(currChar)) {
                 currNumber = currNumber * 10 + (currChar - '0');
             } else if (Character.isLetter(currChar)) {
                 currString.append(currChar);
             } else if (currChar == '[') {
-                stackStr.add(currString);
-                stackNum.add(currNumber);
-                currString = new StringBuilder();
+                stackInt.push(currNumber);
+                stackStr.push(currString.toString());
                 currNumber = 0;
+                currString = new StringBuilder();
             } else if (currChar == ']') {
-                int prevNumber = stackNum.pop();
-                StringBuilder prevString = stackStr.pop();
+                StringBuilder prevString = new StringBuilder(stackStr.pop());
+                int prevNumber = stackInt.pop();
                 for (int i = 0; i < prevNumber; i++) {
                     prevString.append(currString);
                 }
-                currString = prevString;
                 currNumber = 0;
+                currString = prevString;
             }
-        }        
+        }
 
         return currString.toString();
     }
