c7fe0db0644fe77b8702f9236409dd2dca98327f
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-14 20:20:11 +0800

    Time: 0 ms (100%), Space: 40.8 MB (23.68%) - LeetHub

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -1,22 +1,17 @@
 class Solution {
     public int minFlips(int a, int b, int c) {
-
         int flipsCount = 0;
-
         while (a != 0 || b != 0 || c != 0) {
             if ((c & 1) == 0) {
                 if ((a & 1) == 1) flipsCount++;
                 if ((b & 1) == 1) flipsCount++;
             } else {
-                if ((a & 1) == 0 && (b & 1) == 0) {
-                    flipsCount++;
-                }
+                if ((a & 1) == 0 && (b & 1) == 0) flipsCount++;
             }
             a >>= 1;
             b >>= 1;
             c >>= 1;
         }
-        
         return flipsCount;
     }
 }
