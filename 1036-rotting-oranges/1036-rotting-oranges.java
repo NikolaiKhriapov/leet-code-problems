29754ca12b09eae8006697531f0597bb97914f00
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-14 17:58:37 +0800

    Time: 2 ms (67.82%), Space: 42.4 MB (75.76%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,66 +1,46 @@
 class Solution {
     private static final int FRESH = 1;
     private static final int ROTTEN = 2;
-    private static final int FRESH_TO_ROTTEN = 12;
     private static final int[][] NEIGHBORS = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
     public int orangesRotting(int[][] grid) {
-        
-        int minutes = helper(grid);
 
-        if (isAllRotten(grid)) {
-            return minutes;
-        }
-        return -1;
-    }
+        Queue<int[]> queue = new LinkedList<>();
 
-    private int helper(int[][] grid) {
-        boolean isSuccessful = false;
+        int countFresh = 0;
         for (int r = 0; r < grid.length; r++) {
             for (int c = 0; c < grid[0].length; c++) {
                 if (grid[r][c] == ROTTEN) {
-                    for (int[] neighbor : NEIGHBORS) {
-                        if (rotNeighborsTemp(grid, r + neighbor[0], c + neighbor[1])) {
-                            isSuccessful = true;
-                        }
+                    queue.add(new int[] {r, c});
+                }
+                if (grid[r][c] == FRESH) {
+                    countFresh++;
+                }
+            }
+        }
+
+        int minutes = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            boolean isSuccessful = false;
+            while (size-- > 0) {
+                int[] curr = queue.poll();
+                for (int[] neighbor : NEIGHBORS) {
+                    int r = curr[0] + neighbor[0];
+                    int c = curr[1] + neighbor[1];
+                    if (r >= 0 && r < grid.length && c >= 0 && c < grid[0].length && grid[r][c] == FRESH) {
+                        grid[r][c] = ROTTEN;
+                        queue.add(new int[] {r, c});
+                        isSuccessful = true;
+                        countFresh--;
                     }
                 }
             }
-        }
-
-        if (isSuccessful) {
-            rotNeighborsPerm(grid);
-            return 1 + helper(grid);
-        }
-        return 0;
-    }
-
-    private boolean rotNeighborsTemp(int[][] grid, int row, int col) {
-        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] != FRESH) {
-            return false;
-        }
-        grid[row][col] = FRESH_TO_ROTTEN;
-        return true;
-    }
-
-    private void rotNeighborsPerm(int[][] grid) {
-        for (int r = 0; r < grid.length; r++) {
-            for (int c = 0; c < grid[0].length; c++) {
-                if (grid[r][c] == FRESH_TO_ROTTEN) {
-                    grid[r][c] = ROTTEN;
-                }
+            if (isSuccessful) {
+                minutes++;
             }
         }
-    }
 
-    private boolean isAllRotten(int[][] grid) {
-        for (int r = 0; r < grid.length; r++) {
-            for (int c = 0; c < grid[0].length; c++) {
-                if (grid[r][c] == FRESH) {
-                    return false;
-                }
-            }
-        }
-        return true;
+        return countFresh == 0 ? minutes : -1;
     }
 }
