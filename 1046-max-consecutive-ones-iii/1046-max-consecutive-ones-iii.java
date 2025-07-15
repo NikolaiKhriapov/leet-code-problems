47be4f189814c9c5a0e0c10d76668fc29f24e8cc
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-15 11:59:13 +0800

    Time: 3 ms (94.03%), Space: 46.5 MB (68.97%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -1,25 +1,24 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
+        if (nums == null || k < 0) {
+            throw new IllegalArgumentException("Invalid input");
+        }
 
         int longest = 0;
         int left = 0;
         int right = 0;
         int countZero = 0;
-
         while (right < nums.length) {
-            if (nums[right] == 0) {
+            if (nums[right++] == 0) {
                 countZero++;
             }
-            right++;
             while (countZero > k) {
-                if (nums[left] == 0) {
+                if (nums[left++] == 0) {
                     countZero--;
                 }
-                left++;
             }
             longest = Math.max(longest, right - left);
         }
-        
         return longest;
     }
 }
