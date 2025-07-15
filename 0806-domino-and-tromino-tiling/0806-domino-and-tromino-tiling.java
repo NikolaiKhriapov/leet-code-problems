8d4b599bccc96335964a842dfb3cb0aed49a97b0
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-15 11:12:59 +0800

    Time: 0 ms (100%), Space: 40.6 MB (56.72%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.java
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.java
@@ -2,18 +2,18 @@ class Solution {
     private static final long MODULO = 1_000_000_007L;
 
     public int numTilings(int n) {
-        if (n < 1) {
+        if (n < 0) {
             throw new IllegalArgumentException("Invalid input");
         }
         if (n == 0) return 1;
         if (n == 1) return 1;
         if (n == 2) return 2;
 
-        long a = 1;
-        long b = 1;
-        long c = 2;
-
+        long a = 1; // i - 3
+        long b = 1; // i - 2
+        long c = 2; // i - 1
         long presum = 0;
+
         for (int i = 3; i <= n; i++) {
             presum += 2 * a;
             long curr = (c + b + presum) % MODULO;
