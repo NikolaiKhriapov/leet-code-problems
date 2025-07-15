1db40cab98a40765c9962cac344d45c19bb9d4d7
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-15 15:54:33 +0800

    Time: 72 ms (45.56%), Space: 73 MB (63.83%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,33 +1,39 @@
 class Solution {
     public int minReorder(int n, int[][] connections) {
+        if (n <= 0 || connections == null || connections.length == 0 || connections[0].length != 2) {
+            throw new IllegalArgumentException("Invalid input");
+        }
 
         Map<Integer, List<Integer>> graph = new HashMap<>();
         for (int i = 0; i < n; i++) {
             graph.put(i, new ArrayList<>());
         }
         for (int[] connection : connections) {
-            graph.get(connection[1]).add(-connection[0]);
-            graph.get(connection[0]).add(connection[1]);
+            graph.get(connection[0]).add(-connection[1]);
+            graph.get(connection[1]).add(connection[0]);
         }
 
         Queue<Integer> queue = new ArrayDeque<>();
         boolean[] visited = new boolean[n];
         queue.add(0);
         visited[0] = true;
-        
+
         int countChanges = 0;
         while (!queue.isEmpty()) {
             int curr = queue.poll();
-            for (int next : graph.get(curr)) {
-                if (visited[Math.abs(next)]) continue;
-                if (next > 0) {
-                    countChanges++;
+            for (int neighbor : graph.get(curr)) {
+                if (visited[Math.abs(neighbor)]) {
+                    continue;
                 }
-                queue.add(Math.abs(next));
-                visited[Math.abs(next)] = true;
+                if (neighbor < 0) {
+                    countChanges++;
+                    neighbor = -neighbor;
+                }
+                queue.add(neighbor);
+                visited[neighbor] = true;
             }
         }
-
+        
         return countChanges;
     }
 }
