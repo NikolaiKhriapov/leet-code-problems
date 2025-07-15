30accbe660a1bfa6f84f867b227459920d42495e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-15 11:49:35 +0800

    Time: 1 ms (100%), Space: 43 MB (15.31%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,51 +1,49 @@
 class Solution {
     private static final int FRESH = 1;
     private static final int ROTTEN = 2;
-    private static final int[][] NEIGHBORS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private static final int[][] NEIGHBORS = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
     public int orangesRotting(int[][] grid) {
         if (grid == null) {
             throw new IllegalArgumentException("Invalid input");
         }
-        if (grid.length == 0 || grid[0].length == 0) {
-            return 0;
-        }
 
-        Queue<int[]> queue = new LinkedList<>();
+        Deque<int[]> queue = new ArrayDeque<>();
 
         int countFresh = 0;
         for (int r = 0; r < grid.length; r++) {
             for (int c = 0; c < grid[0].length; c++) {
+                if (grid[r][c] == ROTTEN) {
+                    queue.offerLast(new int[] {r, c});
+                }
                 if (grid[r][c] == FRESH) {
                     countFresh++;
-                } else if (grid[r][c] == ROTTEN) {
-                    queue.offer(new int[] {r, c});
                 }
             }
         }
 
-        int countMinutes = 0;
+        int minutes = 0;
         while (!queue.isEmpty()) {
             int size = queue.size();
             boolean isRottenThisMinute = false;
             while (size-- > 0) {
-                int[] curr = queue.poll();
+                int[] curr = queue.pollFirst();
                 for (int[] neighbor : NEIGHBORS) {
                     int r = curr[0] + neighbor[0];
                     int c = curr[1] + neighbor[1];
                     if (r >= 0 && r < grid.length && c >= 0 && c < grid[0].length && grid[r][c] == FRESH) {
+                        queue.offerLast(new int[] {r, c});
                         grid[r][c] = ROTTEN;
-                        queue.add(new int[] {r, c});
-                        isRottenThisMinute = true;
                         countFresh--;
+                        isRottenThisMinute = true;
                     }
                 }
             }
             if (isRottenThisMinute) {
-                countMinutes++;
+                minutes++;
             }
         }
         
-        return countFresh == 0 ? countMinutes : -1;
+        return countFresh == 0 ? minutes : -1;
     }
 }
