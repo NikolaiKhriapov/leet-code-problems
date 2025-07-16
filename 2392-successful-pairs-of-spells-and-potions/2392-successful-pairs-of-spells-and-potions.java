c81ddda03e1570c47ccf756bdb89af337d69184a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-16 22:41:00 +0800

    Time: 72 ms (5.16%), Space: 65.1 MB (53.86%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
@@ -1,29 +1,34 @@
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int n = spells.length, m = potions.length;
+        int[] answer = new int[n];
 
-        int[] result = new int[spells.length];
-
+        // 1. sort potions
         Arrays.sort(potions);
 
-        for (int i = 0; i < spells.length; i++) {
-            int left = 0;
-            int right = potions.length - 1;
-            while (left < right) {
-                int mid = left + (right - left) / 2;
-                if ((long) spells[i] * (long) potions[mid] >= success) {
-                    right = mid;
-                } else {
-                    left = mid + 1;
-                }
-            }
-            if ((long) spells[i] * (long) potions[left] >= success) {
-                result[i] = potions.length - left;
-            } else {
-                result[i] = 0;
-            }
-            
+        // 2. decorate spells with original indices and sort descending by spell value
+        int[][] decorated = new int[n][2];          // [spell, originalIdx]
+        for (int i = 0; i < n; i++) {
+            decorated[i][0] = spells[i];
+            decorated[i][1] = i;
         }
-        
-        return result;
+        Arrays.sort(decorated, (a, b) -> Integer.compare(b[0], a[0])); // descending
+
+        // 3. two-pointer sweep
+        int j = 0;
+        for (int[] pair : decorated) {
+            int spell = pair[0];
+            int idx   = pair[1];
+
+            // smallest potion that works for this spell
+            long minPotion = (success + spell - 1L) / spell; // ceil division
+
+            // advance j until potions[j] >= minPotion
+            while (j < m && potions[j] < minPotion) {
+                j++;
+            }
+            answer[idx] = m - j; // could be zero
+        }
+        return answer;
     }
 }
