274469ba67edb0715f0fffa30c34d643ec271430
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-16 22:43:13 +0800

    Time: 46 ms (67.22%), Space: 67.4 MB (8.76%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
@@ -1,34 +1,29 @@
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
-        int n = spells.length, m = potions.length;
-        int[] answer = new int[n];
 
-        // 1. sort potions
+        int[] result = new int[spells.length];
+
         Arrays.sort(potions);
 
-        // 2. decorate spells with original indices and sort descending by spell value
-        int[][] decorated = new int[n][2];          // [spell, originalIdx]
-        for (int i = 0; i < n; i++) {
-            decorated[i][0] = spells[i];
-            decorated[i][1] = i;
-        }
-        Arrays.sort(decorated, (a, b) -> Integer.compare(b[0], a[0])); // descending
-
-        // 3. two-pointer sweep
-        int j = 0;
-        for (int[] pair : decorated) {
-            int spell = pair[0];
-            int idx   = pair[1];
-
-            // smallest potion that works for this spell
-            long minPotion = (success + spell - 1L) / spell; // ceil division
-
-            // advance j until potions[j] >= minPotion
-            while (j < m && potions[j] < minPotion) {
-                j++;
+        for (int i = 0; i < spells.length; i++) {
+            int left = 0;
+            int right = potions.length - 1;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if ((long) spells[i] * (long) potions[mid] >= success) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
             }
-            answer[idx] = m - j; // could be zero
+            if ((long) spells[i] * (long) potions[left] >= success) {
+                result[i] = potions.length - left;
+            } else {
+                result[i] = 0;
+            }
+            
         }
-        return answer;
+        
+        return result;
     }
 }
