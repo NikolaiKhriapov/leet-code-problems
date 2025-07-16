27296b5126c5de7d7bea801d5b5495f16c3614ef
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-16 17:56:16 +0800

    Time: 17 ms (11.45%), Space: 46.4 MB (12.73%) - LeetHub

---
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.java
@@ -0,0 +1,48 @@
+class Solution {
+    private static final char EMPTY = '.';
+    private static final char WALL = '+';
+    private static final int[][] NEIGHBORS = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+    public int nearestExit(char[][] maze, int[] entrance) {
+
+        Queue<int[]> queue = new ArrayDeque<>();
+        Set<Integer> visited = new HashSet<>();
+        queue.add(entrance);
+        visited.add(generateKey(entrance[0], entrance[1], maze[0].length));
+
+        int counter = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                int[] curr = queue.poll();
+                if (isExit(maze, entrance, curr)) {
+                    return counter;
+                }
+                for (int[] neighbor : NEIGHBORS) {
+                    int r = curr[0] + neighbor[0];
+                    int c = curr[1] + neighbor[1];
+                    if (
+                        r >= 0 && r < maze.length && c >= 0 && c < maze[0].length
+                        && maze[r][c] == EMPTY
+                        && !visited.contains(generateKey(r, c, maze[0].length))
+                    ) {
+                        queue.add(new int[] {r, c});
+                        visited.add(generateKey(r, c, maze[0].length));
+                    }
+                }
+            }
+            counter++;
+        }
+        
+        return -1;
+    }
+
+    private int generateKey(int row, int col, int cols) {
+        return row * cols + col;
+    }
+
+    private boolean isExit(char[][] maze, int[] entrance, int[] cell) {
+        return !Arrays.equals(entrance, cell) && 
+               (cell[0] == 0 || cell[0] == maze.length - 1 || cell[1] == 0 || cell[1] == maze[0].length - 1);
+    }
+}
