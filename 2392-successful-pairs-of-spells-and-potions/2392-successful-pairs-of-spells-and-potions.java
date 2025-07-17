854d0f8d51c6a35d84cb28d7e688eaaa95fe04d0
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-17 12:32:48 +0800

    Time: 48 ms (30.94%), Space: 57.6 MB (99.53%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
@@ -1,27 +1,24 @@
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        if (spells == null || potions == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
 
         int[] result = new int[spells.length];
-
         Arrays.sort(potions);
 
         for (int i = 0; i < spells.length; i++) {
             int left = 0;
             int right = potions.length - 1;
-            while (left < right) {
+            while (left <= right) {
                 int mid = left + (right - left) / 2;
                 if ((long) spells[i] * (long) potions[mid] >= success) {
-                    right = mid;
+                    right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
-            }
-            if ((long) spells[i] * (long) potions[left] >= success) {
                 result[i] = potions.length - left;
-            } else {
-                result[i] = 0;
             }
-            
         }
         
         return result;
