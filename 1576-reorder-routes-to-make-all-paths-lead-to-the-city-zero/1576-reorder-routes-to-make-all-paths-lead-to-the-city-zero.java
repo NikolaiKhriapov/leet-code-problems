250f8063e5a06d74e003efa332d059130c35d808
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-17 12:53:38 +0800

    Time: 56 ms (57.15%), Space: 73.3 MB (60.27%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -3,36 +3,37 @@ class Solution {
         if (n <= 0 || connections == null || connections.length == 0 || connections[0].length != 2) {
             throw new IllegalArgumentException("Invalid input");
         }
-        
-        Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            graph.put(i, new ArrayList<>());
+            map.put(i, new ArrayList<>());
         }
         for (int[] connection : connections) {
-            graph.get(connection[0]).add(-connection[1]);
-            graph.get(connection[1]).add(connection[0]);
+            map.get(connection[0]).add(-connection[1]);
+            map.get(connection[1]).add(connection[0]);
         }
 
         Queue<Integer> queue = new ArrayDeque<>();
         boolean[] visited = new boolean[n];
         queue.add(0);
         visited[0] = true;
-
-        int countChanges = 0;
+        
+        int countChanged = 0;
         while (!queue.isEmpty()) {
             int curr = queue.poll();
-            for (int neighbor : graph.get(curr)) {
-                if (!visited[Math.abs(neighbor)]) {
-                    if (neighbor < 0) {
-                        countChanges++;
-                        neighbor = -neighbor;
-                    }
-                    queue.add(neighbor);
-                    visited[neighbor] = true;
+            for (int next : map.get(curr)) {
+                if (visited[Math.abs(next)]) {
+                    continue;
                 }
+                if (next < 0) {
+                    next = -next;
+                    countChanged++;
+                }
+                queue.add(next);
+                visited[next] = true;
             }
         }
 
-        return countChanges;
+        return countChanged;
     }
 }
