ffc232608e8a8935dc8a45e11268330966cefb5c
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-17 15:04:12 +0800

    Time: 37 ms (32.32%), Space: 54.4 MB (16.01%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
@@ -6,9 +6,9 @@ class Solution {
         Map<String, int[]> map = new HashMap<>();
         int countPairs = 0;
 
-        StringBuilder rowSb = new StringBuilder();
-        StringBuilder colSb = new StringBuilder();
         for (int r = 0; r < size; r++) {
+            StringBuilder rowSb = new StringBuilder();
+            StringBuilder colSb = new StringBuilder();
             for (int c = 0; c < size; c++) {
                 rowSb.append(grid[r][c]).append(",");
                 colSb.append(grid[c][r]).append(",");
@@ -17,13 +17,11 @@ class Solution {
             int[] rowValue = map.getOrDefault(rowKey, new int[2]);
             rowValue[0]++;
             map.put(rowKey, rowValue);
-            rowSb.delete(0, rowSb.length());
 
             String colKey = colSb.toString();
             int[] colValue = map.getOrDefault(colKey, new int[2]);
             colValue[1]++;
             map.put(colKey, colValue);
-            colSb.delete(0, colSb.length());
         }
 
         for (int[] value : map.values()) {
