f8f6bfeed96916868f3461c831726150a56f0df4
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-17 13:06:12 +0800

    Time: 10 ms (82.61%), Space: 45.1 MB (99.79%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
@@ -6,27 +6,27 @@ class Solution {
         if (word1.length() != word2.length()) {
             return false;
         }
-        
+
         int[] freq1 = getFrequencyArray(word1);
         int[] freq2 = getFrequencyArray(word2);
-
         boolean isSameSetOfCharacters = isSameSetOfCharacters(freq1, freq2);
 
         Arrays.sort(freq1);
         Arrays.sort(freq2);
-
-        return isSameSetOfCharacters && Arrays.equals(freq1, freq2);
+        boolean isSameCharacterFrequencies = Arrays.equals(freq1, freq2);
+        
+        return isSameSetOfCharacters && isSameCharacterFrequencies;
     }
 
-    private static int[] getFrequencyArray(String word) {
+    private int[] getFrequencyArray(String word) {
         int[] result = new int[26];
         for (char c : word.toCharArray()) {
             result[c - 'a']++;
         }
         return result;
     }
-    
-    private static boolean isSameSetOfCharacters(int[] arr1, int[] arr2) {
+
+    private boolean isSameSetOfCharacters(int[] arr1, int[] arr2) {
         for (int i = 0; i < 26; i++) {
             if ((arr1[i] == 0 && arr2[i] != 0) || (arr1[i] != 0 && arr2[i] == 0)) {
                 return false;
