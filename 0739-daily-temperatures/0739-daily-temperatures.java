c1319c84d9cc78b3c182e3346f6da32eabe7e05a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-17 14:01:30 +0800

    Time: 61 ms (78.73%), Space: 62.1 MB (43.56%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -5,14 +5,16 @@ class Solution {
         }
 
         int[] result = new int[temperatures.length];
-        Deque<Integer> stack = new ArrayDeque<>();
+        Stack<Integer> stack = new Stack<>();
+
         for (int i = 0; i < temperatures.length; i++) {
             while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
-                int prevIndex = stack.pop();
-                result[prevIndex] = i - prevIndex;
+                int prevIdx = stack.pop();
+                result[prevIdx] = i - prevIdx;
             }
             stack.push(i);
         }
+        
         return result;
     }
 }
