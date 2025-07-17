d50bbb6ac47713b034fdeff557e48cef7d3c202c
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-17 16:32:23 +0800

    Time: 46 ms (90.28%), Space: 57 MB (22.63%) - LeetHub

---
+++ 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.java
@@ -0,0 +1,39 @@
+class Solution {
+    public long totalCost(int[] costs, int k, int candidates) {
+
+        int left = 0;
+        int right = costs.length - 1;
+
+        PriorityQueue<Integer> minHeapLeft = new PriorityQueue<>();
+        PriorityQueue<Integer> minHeapRight = new PriorityQueue<>();
+        
+        while (left <= right && minHeapLeft.size() < candidates) {
+            minHeapLeft.add(costs[left++]);
+        }
+        while (left <= right && minHeapRight.size() < candidates) {
+            minHeapRight.add(costs[right--]);
+        }
+
+        long totalCost = 0;
+
+        while (k-- > 0 && (!minHeapLeft.isEmpty() || !minHeapRight.isEmpty())) {
+            if (minHeapLeft.isEmpty()) {
+                totalCost += minHeapRight.poll();
+            } else if (minHeapRight.isEmpty()) {
+                totalCost += minHeapLeft.poll();
+            } else if (minHeapLeft.peek() <= minHeapRight.peek()) {
+                totalCost += minHeapLeft.poll();
+                if (left <= right) {
+                    minHeapLeft.add(costs[left++]);
+                }
+            } else {
+                totalCost += minHeapRight.poll();
+                if (left <= right) {
+                    minHeapRight.add(costs[right--]);
+                }
+            }
+        }
+        
+        return totalCost;
+    }
+}
