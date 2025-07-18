9224224922259c99bc31cec181f96d6cb33331d5
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-18 17:17:33 +0800

    Time: 2 ms (98.97%), Space: 45.6 MB (5.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,20 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        if (nums == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        
         Map<Integer, Integer> map = new HashMap<>();
-
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if (map.containsKey(complement)) {
-                return new int[] {i, map.get(complement)};
+                return new int[] {map.get(complement), i};
             } else {
                 map.put(nums[i], i);
             }
         }
         
-        return new int[] {-1, -1};
+        throw new IllegalArgumentException("Invalid input");
     }
 }
+
