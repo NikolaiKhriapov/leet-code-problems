1a7a0e0224de7a519833d3ed5945c45cbcc43d65
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-18 19:27:02 +0800

    Time: 3 ms (38.99%), Space: 41.9 MB (47.45%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,20 +1,29 @@
 class Solution {
-    public boolean isValid(String s) {
-        String opening = "({[";
-        String closing = ")}]";
+    private static final String OPENING_BRACKETS = "({[";
+    private static final String CLOSING_BRACKETS = ")}]";
 
-        Stack<Character> stack = new Stack<>();
-        
-        for (char c : s.toCharArray()) {
-            if (opening.indexOf(c) != -1) {
-                stack.add(c);
-            } else {
-                if (opening.indexOf(stack.pop()) != closing.indexOf(c)) {
-                    return false;
-                }
-            }
+    public boolean isValid(String s) {
+        if (s == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        if (s.isBlank()) {
+            return true;
         }
 
-        return stack.size() == 0;
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (OPENING_BRACKETS.indexOf(c) != -1) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty() || OPENING_BRACKETS.indexOf(stack.peek()) != CLOSING_BRACKETS.indexOf(c)) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
     }
 }
+
+// time  - O(n)
+// space - O(n)
