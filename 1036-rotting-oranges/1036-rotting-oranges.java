5b64982f20cdab04dcf89a2333bcf6c42332bea8
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-18 12:32:15 +0800

    Time: 2 ms (68.03%), Space: 42.1 MB (93.51%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -7,25 +7,21 @@ class Solution {
         if (grid == null) {
             throw new IllegalArgumentException("Invalid input");
         }
-        if (grid.length == 0 || grid[0].length == 0) {
-            return 0;
-        }
 
         Queue<int[]> queue = new ArrayDeque<>();
-        int countFresh = 0;
-        int countMinutes = 0;
+        int freshCount = 0;
+        int minuteCount = 0;
 
         for (int r = 0; r < grid.length; r++) {
             for (int c = 0; c < grid[0].length; c++) {
                 if (grid[r][c] == ROTTEN) {
                     queue.add(new int[] {r, c});
-                }
-                if (grid[r][c] == FRESH) {
-                    countFresh++;
+                } else if (grid[r][c] == FRESH) {
+                    freshCount++;
                 }
             }
         }
-        
+
         while (!queue.isEmpty()) {
             int size = queue.size();
             boolean isRottenThisMinute = false;
@@ -35,18 +31,18 @@ class Solution {
                     int r = curr[0] + neighbor[0];
                     int c = curr[1] + neighbor[1];
                     if (r >= 0 && r < grid.length && c >= 0 && c < grid[0].length && grid[r][c] == FRESH) {
-                        grid[r][c] = ROTTEN;
-                        countFresh--;
-                        isRottenThisMinute = true;
                         queue.add(new int[] {r, c});
+                        grid[r][c] = ROTTEN;
+                        freshCount--;
+                        isRottenThisMinute = true;
                     }
                 }
             }
             if (isRottenThisMinute) {
-                countMinutes++;
+                minuteCount++;
             }
         }
 
-        return countFresh == 0 ? countMinutes : -1;
+        return freshCount == 0 ? minuteCount : -1;
     }
 }
