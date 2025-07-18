3e4ea0c297812d1e296eea00b438b92bc0d96297
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-18 23:37:24 +0800

    Time: 0 ms (100%), Space: 42.9 MB (82.4%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,15 +1,27 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        return helper(nums, target, 0, nums.length - 1);
-    }
+        if (nums == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        if (target < nums[0]) {
+            return 0;
+        }
+        if (target > nums[nums.length - 1]) {
+            return nums.length;
+        }
 
-    private int helper(int[] nums, int target, int l, int r) {
-        if (l > r) return l;
-
-        int m = l + (r - l) / 2;
-
-        if (nums[m] == target) return m;
-        else if (nums[m] > target) return helper(nums, target, l, m - 1);
-        else return helper(nums, target, m + 1, r);
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 }
