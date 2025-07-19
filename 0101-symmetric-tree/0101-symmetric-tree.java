e81ad591cf9d35c1c4a1dc3841a48ecd34746fae
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-19 16:20:28 +0800

    Time: 0 ms (100%), Space: 41.9 MB (40.91%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,17 +15,22 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        return helper(root.left, root.right);
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
     }
 
-    private boolean helper(TreeNode left, TreeNode right) {
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
         if (left == null && right == null) {
             return true;
         }
         if (left == null || right == null) {
             return false;
         }
-
-        return left.val == right.val && helper(left.left, right.right) && helper(left.right, right.left);
+        return left.val == right.val && isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
     }
 }
+
+// time  - O(n)
+// space - O(n)
