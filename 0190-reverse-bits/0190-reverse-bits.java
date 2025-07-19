f542c4a3edfe8f0df139112eeef17a2396e855c7
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-19 21:29:59 +0800

    Time: 0 ms (100%), Space: 40.9 MB (99.99%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,13 +1,17 @@
 public class Solution {
     public int reverseBits(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Invalid input");
+        }
         int result = 0;
-
         for (int i = 0; i < 32; i++) {
             result <<= 1;
-            result += n & 1;
+            result += (n & 1);
             n >>= 1;
         }
-
         return result;
     }
 }
+
+// time  - O(1)
+// space - O(1)
