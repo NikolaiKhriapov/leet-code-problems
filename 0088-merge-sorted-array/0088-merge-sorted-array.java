d241272af1e203b79f358f776845126fa066f576
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-19 15:51:09 +0800

    Time: 0 ms (100%), Space: 42.3 MB (60.37%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,23 +1,22 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if (n == 0) return;
-
-        int pm = m - 1;
-        int pn = n - 1;
-        int p = nums1.length - 1;
-        
-        while (m > 0 && pm >= 0 && pn >= 0) {
-            if (nums2[pn] >= nums1[pm]) {
-                nums1[p--] = nums2[pn--];
-            } else {
-                nums1[p--] = nums1[pm--];
-            }
+        if (nums1 == null || nums2 == null || nums2.length != n || nums1.length != m + n) {
+            throw new IllegalArgumentException("Invalid input");
         }
 
-        if (pn >= 0) {
-            for (int i = p; i >= 0; i--) {
-                nums1[p--] = nums2[pn--];
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;
+
+        while (p >= 0 && p1 < p) {
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[p--] = nums1[p1--];
+            } else {
+                nums1[p--] = nums2[p2--];
             }
         }
     }
 }
+
+// time  - O(n)
+// space - O(1)
