85259e6cf7c6da48bca61de706774c15675fc8d4
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-19 14:35:30 +0800

    Time: 47 ms (69.2%), Space: 56.4 MB (80.5%) - LeetHub

--- 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.java
+++ 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.java
@@ -1,36 +1,39 @@
 class Solution {
     public long totalCost(int[] costs, int k, int candidates) {
-        if (costs == null || k < 0 || candidates < 0) {
+        if (costs == null || k <= 0 || candidates <= 0) {
             throw new IllegalArgumentException("Invalid input");
         }
-        if (k == 0 || candidates == 0 || costs.length == 0) {
-            return 0L;
-        }
 
-        PriorityQueue<Integer> minHeapLeft = new PriorityQueue<>();
-        PriorityQueue<Integer> minHeapRight = new PriorityQueue<>();
-        
+        PriorityQueue<Integer> leftMinHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> rightMinHeap = new PriorityQueue<>();
+
         int left = 0;
         int right = costs.length - 1;
-        while (left <= right && minHeapLeft.size() < candidates) {
-            minHeapLeft.add(costs[left++]);
+        while (left <= right && leftMinHeap.size() < candidates) {
+            leftMinHeap.offer(costs[left++]);
         }
-        while (left <= right && minHeapRight.size() < candidates) {
-            minHeapRight.add(costs[right--]);
+        while (left <= right && rightMinHeap.size() < candidates) {
+            rightMinHeap.offer(costs[right--]);
         }
 
         long totalCost = 0;
-        while (k-- > 0 && (!minHeapLeft.isEmpty() || !minHeapRight.isEmpty())) {
-            if (minHeapLeft.isEmpty()) {
-                totalCost += minHeapRight.poll();
-            } else if (minHeapRight.isEmpty()) {
-                totalCost += minHeapLeft.poll();
-            } else if (minHeapLeft.peek() <= minHeapRight.peek()) {
-                totalCost += minHeapLeft.poll();
-                if (left <= right) minHeapLeft.add(costs[left++]);
+        while (k-- > 0) {
+            if (leftMinHeap.isEmpty() && rightMinHeap.isEmpty()) {
+                break;
+            } else if (leftMinHeap.isEmpty()) {
+                totalCost += rightMinHeap.poll();
+            } else if (rightMinHeap.isEmpty()) {
+                totalCost += leftMinHeap.poll();
+            } else if (leftMinHeap.peek() <= rightMinHeap.peek()) {
+                totalCost += leftMinHeap.poll();
+                if (left <= right) {
+                    leftMinHeap.offer(costs[left++]);
+                }
             } else {
-                totalCost += minHeapRight.poll();
-                if (left <= right) minHeapRight.add(costs[right--]);
+                totalCost += rightMinHeap.poll();
+                if (left <= right) {
+                    rightMinHeap.offer(costs[right--]);
+                }
             }
         }
         
