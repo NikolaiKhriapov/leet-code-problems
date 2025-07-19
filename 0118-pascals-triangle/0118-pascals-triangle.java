25565722b5a41e461c2fd85562ba294c1b74fed1
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-19 17:09:42 +0800

    Time: 1 ms (83.01%), Space: 41.9 MB (78.87%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,21 +1,29 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> list = new ArrayList<>();
-        list.add(List.of(1));
-        
-        for (int i = 2; i <= numRows; i++) {
-            List<Integer> prev = list.get(list.size() - 1);
-            
-            List<Integer> curr = new ArrayList<>();
-            curr.add(1);
-            for (int j = 1; j < i - 1; j++) {
-                curr.add(prev.get(j - 1) + prev.get(j));
-            }
-            curr.add(1);
-            
-            list.add(curr);
+        if (numRows < 0) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        if (numRows == 0) {
+            return new ArrayList<>();
         }
 
-        return list;
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(List.of(1));
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> list = new ArrayList<>();
+            list.add(1);
+            for (int j = 1; j <= i - 1; j++) {
+                List<Integer> prevRow = result.get(result.size() - 1);
+                list.add(prevRow.get(j - 1) + prevRow.get(j));
+            }
+            list.add(1);
+            result.add(list);
+        }
+        
+        return result;
     }
 }
+
+// time  - O(n)
+// space - O(n)
