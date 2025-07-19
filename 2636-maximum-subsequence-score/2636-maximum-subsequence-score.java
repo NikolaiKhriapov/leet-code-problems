80f9a8aec72a1cffa3e9c8d163a90c5d7758c56a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-19 13:38:34 +0800

    Time: 79 ms (36.7%), Space: 67.8 MB (5.85%) - LeetHub

--- 2636-maximum-subsequence-score/2636-maximum-subsequence-score.java
+++ 2636-maximum-subsequence-score/2636-maximum-subsequence-score.java
@@ -12,27 +12,19 @@ class Solution {
         Arrays.sort(pairs, (a, b) -> Integer.compare(b[0], a[0]));
 
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        long result = 0;
         long sum = 0;
-        for (int i = 0; i < pairs.length; i++) {
-            sum += pairs[i][1];
-            minHeap.offer(pairs[i][1]);
+        long result = 0;
+        for (int[] pair : pairs) {
+            sum += pair[1];
+            minHeap.offer(pair[1]);
             if (minHeap.size() > k) {
                 sum -= minHeap.poll();
             }
             if (minHeap.size() == k) {
-                result = Math.max(result, sum * (long) pairs[i][0]);
+                result = Math.max(result, sum * (long) pair[0]);
             }
         }
 
         return result;
     }
 }
-
-// [2,1], [1,3], [3,3], [4,2]
-// [4,2], [3,3], [2,1], [1,3]
-
-// sum = +2, [2]
-// sum = +3, [2,3]
-// sum = +1, [1,2,3]   result = 6*2=12
-// sum = +3-1, [2,3,3] result = 8*1=12
