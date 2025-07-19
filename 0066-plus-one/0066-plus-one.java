6f7a7059fd5529f6d93942419e7512a33be54e64
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-19 15:00:07 +0800

    Time: 0 ms (100%), Space: 42.1 MB (30.58%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,5 +1,9 @@
 class Solution {
     public int[] plusOne(int[] digits) {
+        if (digits == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+
         for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] != 9) {
                 digits[i]++;
@@ -8,11 +12,15 @@ class Solution {
             digits[i] = 0;
         }
 
-        int[] newDigits = new int[digits.length + 1];
-        newDigits[0] = 1;
-        for (int i = 0; i < digits.length; i++) {
-            newDigits[i + 1] = digits[i];
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        for (int i = 1; i < result.length; i++) {
+            result[i] = digits[i - 1];
         }
-        return newDigits;
+
+        return result;
     }
 }
+
+// time  - O(n)
+// space - O(n)
