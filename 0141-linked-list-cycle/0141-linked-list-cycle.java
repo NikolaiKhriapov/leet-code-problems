b3fd3efb71e301e37329d61fe022359231aa8c6a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-19 20:44:05 +0800

    Time: 0 ms (100%), Space: 44.4 MB (61.85%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,18 +11,19 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if (head == null) {
+        if (head == null || head.next == null) {
             return false;
         }
 
         ListNode slow = head;
         ListNode fast = head.next;
-        
         while (slow != fast && fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
-
         return slow == fast;
     }
 }
+
+// time  - O(n)
+// space - O(1)
