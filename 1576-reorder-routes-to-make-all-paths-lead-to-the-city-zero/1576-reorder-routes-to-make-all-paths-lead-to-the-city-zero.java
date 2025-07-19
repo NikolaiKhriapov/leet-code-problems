1a911da12a647c3de4b1bb031ab022e1058c46be
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-19 13:49:45 +0800

    Time: 77 ms (42.94%), Space: 73 MB (63.36%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,6 +1,6 @@
 class Solution {
     public int minReorder(int n, int[][] connections) {
-        if (n <= 1 || connections == null || connections.length == 0 || connections[0].length != 2) {
+        if (connections == null || n <= 1 || connections.length != n - 1 || connections[0].length != 2) {
             throw new IllegalArgumentException("Invalid input");
         }
 
@@ -12,13 +12,13 @@ class Solution {
             map.get(connection[0]).add(-connection[1]);
             map.get(connection[1]).add(connection[0]);
         }
-
+        
         Queue<Integer> queue = new ArrayDeque<>();
         boolean[] visited = new boolean[n];
         queue.add(0);
         visited[0] = true;
-        
-        int countChanged = 0;
+
+        int changeCount = 0;
         while (!queue.isEmpty()) {
             int curr = queue.poll();
             for (int next : map.get(curr)) {
@@ -27,13 +27,13 @@ class Solution {
                 }
                 if (next < 0) {
                     next = -next;
-                    countChanged++;
+                    changeCount++;
                 }
                 queue.add(next);
                 visited[next] = true;
             }
         }
 
-        return countChanged;
+        return changeCount;
     }
 }
