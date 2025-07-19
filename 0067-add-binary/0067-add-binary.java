e500562bbdb2fd0b824139c37f6cbd88b83fab6c
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-19 15:16:57 +0800

    Time: 1 ms (99.8%), Space: 42.3 MB (51.4%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,24 +1,30 @@
 class Solution {
     public String addBinary(String a, String b) {
-        int la = a.length();
-        int lb = b.length();
-        int l = la >= lb ? la : lb;
+        if (a == null || b == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
 
         StringBuilder sb = new StringBuilder();
+        int length = Math.max(a.length(), b.length());
+        int pa = a.length() - 1;
+        int pb = b.length() - 1;
         int carry = 0;
-        for (int i = 0; i < l; i++) {
-            int pa = la - 1 - i;
-            int pb = lb - 1 - i;
-
-            int na = (pa >= 0) ? (a.charAt(pa) - '0') : 0;
-            int nb = (pb >= 0) ? (b.charAt(pb) - '0') : 0;
+        
+        while (pa >= 0 || pb >= 0) {
+            int na = (pa >= 0 ? a.charAt(pa--) : '0') - '0';
+            int nb = (pb >= 0 ? b.charAt(pb--) : '0') - '0';
+            
             int sum = na + nb + carry;
-
             sb.append(sum % 2);
             carry = sum / 2;
         }
-        if (carry > 0) sb.append(carry);
+        if (carry != 0) {
+            sb.append(carry);
+        }
 
         return sb.reverse().toString();
     }
 }
+
+// time  - O(n)
+// space - O(n)
