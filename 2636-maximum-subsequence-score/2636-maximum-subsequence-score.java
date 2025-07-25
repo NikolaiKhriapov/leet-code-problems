8c75ce881d4c88da10ef95a1284134b86adaa2b4
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-25 21:37:05 +0800

    Time: 81 ms (19.8%), Space: 67.3 MB (17.06%) - LeetHub

--- 2636-maximum-subsequence-score/2636-maximum-subsequence-score.java
+++ 2636-maximum-subsequence-score/2636-maximum-subsequence-score.java
@@ -1,6 +1,6 @@
 class Solution {
     public long maxScore(int[] nums1, int[] nums2, int k) {
-        if (nums1 == null || nums2 == null || nums1.length != nums2.length || k <= 0) {
+        if (nums1 == null || nums2 == null || nums1.length == 0 || nums1.length != nums2.length || k <= 0) {
             throw new IllegalArgumentException("Invalid input");
         }
 
@@ -12,19 +12,29 @@ class Solution {
         Arrays.sort(pairs, (a, b) -> Integer.compare(b[0], a[0]));
 
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        long sum = 0;
+        long currSum = 0;
         long result = 0;
         for (int[] pair : pairs) {
-            sum += pair[1];
+            currSum += pair[1];
             minHeap.offer(pair[1]);
             if (minHeap.size() > k) {
-                sum -= minHeap.poll();
+                currSum -= minHeap.poll();
             }
             if (minHeap.size() == k) {
-                result = Math.max(result, sum * (long) pair[0]);
+                result = Math.max(result, currSum * (long) pair[0]);
             }
         }
 
         return result;
     }
 }
+
+// [1,3,3,2], [2,1,3,4]
+
+// [2,1],[1,3],[3,3],[4,2]
+// [4,2],[3,3],[2,1],[1,3]
+
+// sum = +2, [2]
+// sum = +3, [2,3]
+// sum = +1, [1,2,3], result = 6*2=12
+// sum = +3-1, [2,3,3], result = 8*1=12
