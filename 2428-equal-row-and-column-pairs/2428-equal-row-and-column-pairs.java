f28cdf8a63e48aebfb4a12f642ca91fe5066cd96
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-25 21:14:31 +0800

    Time: 34 ms (50.46%), Space: 55.3 MB (7.49%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
@@ -1,29 +1,32 @@
 class Solution {
     public int equalPairs(int[][] grid) {
-        if (grid == null || grid.length == 0 || grid.length != grid[0].length) {
+        if (grid == null) {
             throw new IllegalArgumentException("Invalid input");
         }
+        if (grid.length == 0 || grid.length != grid[0].length) {
+            return 0;
+        }
 
-        Map<String, Integer> rowsMap = new HashMap<>();
+        Map<String, Integer> mapRows = new HashMap<>();
         for (int r = 0; r < grid.length; r++) {
             StringBuilder sb = new StringBuilder();
-            for (int c = 0; c < grid.length; c++) {
+            for (int c = 0; c < grid[0].length; c++) {
                 sb.append(grid[r][c]).append(",");
             }
             String key = sb.toString();
-            rowsMap.put(key, rowsMap.getOrDefault(key, 0) + 1);
+            mapRows.put(key, mapRows.getOrDefault(key, 0) + 1);
         }
 
         int pairCount = 0;
-        for (int r = 0; r < grid.length; r++) {
+        for (int c = 0; c < grid[0].length; c++) {
             StringBuilder sb = new StringBuilder();
-            for (int c = 0; c < grid.length; c++) {
-                sb.append(grid[c][r]).append(",");
+            for (int r = 0; r < grid.length; r++) {
+                sb.append(grid[r][c]).append(",");
             }
             String key = sb.toString();
-            pairCount += rowsMap.getOrDefault(key, 0);
+            pairCount += mapRows.getOrDefault(key, 0);
         }
-        
+
         return pairCount;
     }
 }
