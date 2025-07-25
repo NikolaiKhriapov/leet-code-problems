c92a98314574a9b5668819a25564b4573c779fab
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-25 22:03:55 +0800

    Time: 0 ms (100%), Space: 41.1 MB (13.24%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,13 +1,13 @@
 class Solution {
     public int hammingWeight(int n) {
-        int count = 0;
+        int countSetBits = 0;
         for (int i = 0; i < 32; i++) {
-            count += (n & 1);
+            countSetBits += (n & 1);
             n >>= 1;
         }
-        return count;
+        return countSetBits;
     }
 }
 
-// time  - O(log n)
+// time. - O(1)
 // space - O(1)
