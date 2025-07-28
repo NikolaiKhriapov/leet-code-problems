3f22fd9ce2f3c7b5bec25fc0cf53f7ca3be6ad31
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-28 13:14:58 +0800

    Time: 1 ms (99.85%), Space: 46 MB (79.34%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,5 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        if (nums == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+
         int result = 0;
         for (int num : nums) {
             result ^= num;
