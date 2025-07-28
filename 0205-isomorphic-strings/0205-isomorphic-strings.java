da83078cf8cdfc5a30fef5970eea433f452d39e5
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-28 13:39:02 +0800

    Time: 11 ms (67.55%), Space: 42.6 MB (52.43%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,25 +1,29 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
+        if (s == null || t == null || s.length() != t.length()) {
+            throw new IllegalArgumentException("Invalid input");
         }
 
-        int[] sMap = new int[256];
-        int[] tMap = new int[256];
-        Arrays.fill(sMap, -1);
-        Arrays.fill(tMap, -1);
-
+        Map<Character, Character> map = new HashMap<>();
+        Set<Character> set = new HashSet<>();
         for (int i = 0; i < s.length(); i++) {
-            char cs = s.charAt(i);
-            char ct = t.charAt(i);
-            if (sMap[cs] == -1 && tMap[ct] == -1) {
-                sMap[cs] = ct;
-                tMap[ct] = cs;
-            } else if (sMap[cs] != ct || tMap[ct] != cs) {
-                return false;
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+            if (map.containsKey(sChar)) {
+                if (map.get(sChar) != tChar) {
+                    return false;
+                }
+            } else {
+                if (set.contains(tChar)) {
+                    return false;
+                }
+                map.put(sChar, tChar);
+                set.add(tChar);
             }
         }
-        
         return true;
     }
 }
+
+// time  - O(n)
+// space - O(n)
