f6e9a4dfaf722379c5ada65b4ad192cd88abe8d2
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-07-28 13:18:57 +0800

    Time: 1 ms (16.64%), Space: 40.4 MB (99.46%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -3,7 +3,7 @@ class Solution {
         int result = 0;
         for (int i = 0; i < 32; i++) {
             result <<= 1;
-            result += (n & 1);
+            result += n & 1;
             n >>= 1;
         }
         return result;
