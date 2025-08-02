e7e5ab0526889f3a89113059d82d5eaf2af56385
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-02 21:37:27 +0800

    Time: 6 ms (47.64%), Space: 68.5 MB (48%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,30 +10,46 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode first = head;
+        ListNode second = findBeginningOfSecondHalf(head);
+        second = reverse(second);
+
+        while (first != null && second != null) {
+            if (first.val != second.val) {
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+        
+        return true;
+    }
+
+    private ListNode findBeginningOfSecondHalf(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
+        return slow;
+    }
 
+    private ListNode reverse(ListNode head) {
         ListNode prev = null;
-        while (slow != null) {
-            ListNode temp = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = temp;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
         }
-        slow = prev;
-        
-        while (head != null && slow != null) {
-            if (head.val != slow.val) {
-                return false;
-            }
-            head = head.next;
-            slow = slow.next;
-        }
-
-        return true;
+        return prev;
     }
 }
+
+// time. - O(n)
+// space - O(1)
