203a29c5e9f42e1a69dd5597945cc651d9e2885c
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-02 21:53:59 +0800

    Time: 13 ms (29.62%), Space: 45.1 MB (10.89%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,26 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s == null || t == null) {
-            throw new IllegalArgumentException("Invalid input");
-        }
-        if (s.length() != t.length()) {
-            return false;
-        }
-
         Map<Character, Integer> map = new HashMap<>();
+
         for (char c : s.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
+        
         for (char c : t.toCharArray()) {
-            int val = map.getOrDefault(c, 0);
-            if (val <= 0) {
+            int v = map.getOrDefault(c, 0) - 1;
+            if (v < 0) return false;
+            map.put(c, v);
+        }
+
+        for (var entry : map.entrySet()) {
+            if (entry.getValue() != 0) {
                 return false;
             }
-            map.put(c, val - 1);
         }
+
         return true;
     }
 }
-
-// time. - O(n)
-// space - O(1)
