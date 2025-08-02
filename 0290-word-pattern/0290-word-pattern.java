e93457f7fb0ff3269100cbeeef422c7e4752fe8e
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-02 22:38:09 +0800

    Time: 0 ms (100%), Space: 41.8 MB (10.06%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,31 +1,38 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        String[] parts = s.split(" ");
-        if (pattern.length() != parts.length) {
+        if (pattern == null || s == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+
+        String[] words = s.split(" ");
+        
+        if (pattern.length() != words.length) {
             return false;
         }
 
-        Map<Character, String> map = new HashMap<>();
+        String[] map = new String[26];
         Set<String> set = new HashSet<>();
 
-        for (int i = 0; i < parts.length; i++) {
+        for (int i = 0; i < pattern.length(); i++) {
             char c = pattern.charAt(i);
-            String w = parts[i];
-
-            if (!map.containsKey(c)) {
-                if (!set.contains(w)) {
-                    map.put(c, w);
-                    set.add(w);
-                } else {
+            String word = words[i];
+            if (map[c - 'a'] == null) {
+                if (set.contains(word)) {
                     return false;
+                } else {
+                    map[c - 'a'] = word;
+                    set.add(word);
                 }
             } else {
-                if (!Objects.equals(map.get(c), w)) {
+                if (!Objects.equals(map[c - 'a'], word)) {
                     return false;
                 }
             }
         }
 
-        return true;        
+        return true;
     }
 }
+
+// time. - O(n)
+// space - O(n)
