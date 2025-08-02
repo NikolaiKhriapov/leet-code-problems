9beaf0c5547115c31dbf78b4b0286f953f1f3e7c
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-03 00:47:24 +0800

    Time: 4 ms (28.89%), Space: 43.7 MB (26.77%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,24 +1,42 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        int[] array = new int[1001];
-
-        for (int n : nums1) {
-            array[n]++;
+        if (nums1 == null || nums2 == null) {
+            throw new IllegalArgumentException("Invalid input");
         }
-        
+
+        Map<Integer, Integer> map = getFrequencyMap(nums1);
+        List<Integer> list = getIntersectingElements(map, nums2);
+        return listToArray(list);
+    }
+
+    private Map<Integer, Integer> getFrequencyMap(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        return map;
+    }
+
+    private List<Integer> getIntersectingElements(Map<Integer, Integer> map, int[] arr) {
         List<Integer> list = new ArrayList<>();
-        for (int n : nums2) {
-            if (array[n] > 0) {
-                array[n]--;
-                list.add(n);
+        for (int num : arr) {
+            int val = map.getOrDefault(num, 0);
+            if (val > 0) {
+                list.add(num);
             }
+            map.put(num, val - 1);
         }
+        return list;
+    }
 
+    private int[] listToArray(List<Integer> list) {
         int[] result = new int[list.size()];
-        for (int i = 0; i < result.length; i++) {
+        for (int i = 0; i < list.size(); i++) {
             result[i] = list.get(i);
         }
-
         return result;
     }
 }
+
+// time. - O(n)
+// space - O(n)
