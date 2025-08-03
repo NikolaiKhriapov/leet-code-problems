5240335147f0b31b93d05fdaaa430b9745ab9bcc
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-03 13:58:59 +0800

    Time: 2 ms (68.86%), Space: 42 MB (9.49%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,18 +1,20 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int ls = s.length();
-        int lt = t.length();
-
+        if (s == null || t == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        
         int ps = 0;
         int pt = 0;
-        
-        while (ps < ls && pt < lt) {
+        while (ps < s.length() && pt < t.length()) {
             if (s.charAt(ps) == t.charAt(pt)) {
                 ps++;
             }
             pt++;
         }
-        
-        return ps == ls;
+        return ps == s.length();
     }
 }
+
+// time  - O(n)
+// space - O(1)
