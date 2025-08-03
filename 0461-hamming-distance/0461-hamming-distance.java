31d499aaa7e6afa5d6cedd52b483be29fa646ffc
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-03 14:39:19 +0800

    Time: 0 ms (100%), Space: 40.7 MB (26.99%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.java
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -1,13 +1,16 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        int count = 0;
-        
+        int hammingDistance = 0;
         for (int i = 0; i < 32; i++) {
-            if ((x & 1) != (y & 1)) count++;
+            if ((x & 1) != (y & 1)) {
+                hammingDistance++;
+            }
             x >>= 1;
             y >>= 1;
         }
-
-        return count;
+        return hammingDistance;
     }
 }
+
+// time. - O(1)
+// space - O(1)
