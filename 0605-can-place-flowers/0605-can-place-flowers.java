ea2aefd131697322cb135d39cd52ae427cee8265
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-03 18:33:04 +0800

    Time: 1 ms (97.5%), Space: 45.5 MB (89.07%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,17 +1,26 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        if (flowerbed == null || flowerbed.length == 0 || n < 0) {
+        if (flowerbed == null || n < 0) {
             throw new IllegalArgumentException("Invalid input");
         }
+        if (n == 0) {
+            return true;
+        }
 
-        for (int i = 0; i < flowerbed.length && n > 0; i++) {
+        for (int i = 0; i < flowerbed.length; i++) {
             boolean prevIsZero = i == 0 || flowerbed[i - 1] == 0;
             boolean nextIsZero = i == flowerbed.length - 1 || flowerbed[i + 1] == 0;
             if (flowerbed[i] == 0 && prevIsZero && nextIsZero) {
+                flowerbed[i] = 1;
                 n--;
-                i++;
+                if (n == 0) {
+                    return true;
+                }
             }
         }
         return n == 0;
     }
 }
+
+// time. - O(n)
+// space - O(1)
