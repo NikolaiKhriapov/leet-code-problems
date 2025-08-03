1c5acc258dcf6fc6f633857ed632c82ee6f19476
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-04 00:44:15 +0800

    Time: 0 ms (100%), Space: 42.2 MB (72.01%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.java
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -1,30 +1,38 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
-        int[] result = new int[code.length];
-
-        if (k == 0) return result;
-
-        int start;
-        int end;
-        if (k > 0) {
-            start = 1;
-            end = k;
-        } else {
-            start = code.length + k;
-            end = code.length - 1;
+        if (code == null || k >= code.length) {
+            throw new IllegalArgumentException("Invalid input");
         }
         
-        int sum = 0;
-        for (int i = start; i <= end; i++) {
-            sum += code[i];
-        }
+        int[] result = new int[code.length];
 
-        for (int i = 0; i < code.length; i++) {
-            result[i] = sum;
-            sum -= code[start++ % code.length];
-            sum += code[++end % code.length];
+        if (k == 0) {
+            return result;
+        } else if (k > 0) {
+            int sum = 0;
+            for (int i = 1; i <= k; i++) {
+                sum += code[i];
+            }
+            for (int i = 0; i < result.length; i++) {
+                result[i] = sum;
+                sum -= code[(i + 1) % code.length];
+                sum += code[(i + 1 + k) % code.length];
+            }
+        } else {
+            int sum = 0;
+            for (int i = code.length - 1; i >= code.length + k; i--) {
+                sum += code[i];
+            }
+            for (int i = 0; i < result.length; i++) {
+                result[i] = sum;
+                sum += code[i];
+                sum -= code[(i + code.length + k) % code.length];
+            }
         }
 
         return result;
     }
 }
+
+// time  - O(n)
+// space - O(n)
