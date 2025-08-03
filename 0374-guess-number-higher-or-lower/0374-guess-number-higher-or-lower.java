3dca691c700aa4c2fc2a92d19b83640c6d7e5f4a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-03 13:29:28 +0800

    Time: 0 ms (100%), Space: 40.6 MB (38.36%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
@@ -9,23 +9,22 @@
 
 public class Solution extends GuessGame {
     public int guessNumber(int n) {
-        if (n < 1) {
-            throw new IllegalArgumentException("Invalid input");
-        }
-
         int left = 1;
         int right = n;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            int guess = guess(mid);
-            if (guess == 0) {
+            int answer = guess(mid);
+            if (answer == 0) {
                 return mid;
-            } else if (guess == -1) {
-                right = mid - 1;
-            } else {
+            } else if (answer == 1) {
                 left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         return -1;
     }
 }
+
+// time. - O(log n)
+// space - O(1)
