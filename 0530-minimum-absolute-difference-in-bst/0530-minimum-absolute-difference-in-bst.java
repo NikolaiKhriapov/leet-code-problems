a191e51c49f5c6428f467b3348df312de9d0e594
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-03 18:04:32 +0800

    Time: 0 ms (100%), Space: 44.7 MB (31.82%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -14,31 +14,24 @@
  * }
  */
 class Solution {
+    private int minDiff = Integer.MAX_VALUE;
+    private TreeNode prev = null;
+
     public int getMinimumDifference(TreeNode root) {
         if (root == null) {
-            throw new IllegalArgumentException("Invalid input");
+            return -1;
         }
-        
-        List<Integer> list = new ArrayList<>();
-        dfs(root, list);
 
-        int minDiff = Integer.MAX_VALUE;
-        for (int i = 1; i < list.size(); i++) {
-            minDiff = Math.min(minDiff, list.get(i) - list.get(i - 1));
+        getMinimumDifference(root.left);
+        if (prev != null) {
+            minDiff = Math.min(minDiff, root.val - prev.val);
         }
+        prev = root;
+        getMinimumDifference(root.right);
 
         return minDiff;
-    }
-
-    private void dfs(TreeNode node, List<Integer> list) {
-        if (node == null) {
-            return;
-        }
-        dfs(node.left, list);
-        list.add(node.val);
-        dfs(node.right, list);
     }
 }
 
 // time. - O(n)
-// space - O(n)
+// space - O(1)
