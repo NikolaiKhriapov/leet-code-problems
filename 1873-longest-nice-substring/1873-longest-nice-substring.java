0066517f19c1a207e4ab33a6f97569a563ef9f29
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-04 10:55:55 +0800

    Time: 2 ms (82.04%), Space: 43.1 MB (40.16%) - LeetHub

--- 1873-longest-nice-substring/1873-longest-nice-substring.java
+++ 1873-longest-nice-substring/1873-longest-nice-substring.java
@@ -1,22 +1,27 @@
 class Solution {
     public String longestNiceSubstring(String s) {
-        return longestNiceSubstring(s, 0, s.length());
+        if (s == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        return longestNiceSubstring(s, 0, s.length() - 1);
     }
 
-    private String longestNiceSubstring(String s, int l, int r) {
+    public String longestNiceSubstring(String s, int left, int right) {
         Set<Character> set = new HashSet<>();
-        for (int i = l; i < r; i++) {
+        for (int i = left; i <= right; i++) {
             set.add(s.charAt(i));
         }
 
-        for (int i = l; i < r; i++) {
-            char c = s.charAt(i);
-            if (!set.contains(Character.toLowerCase(c)) || !set.contains(Character.toUpperCase(c))) {
-                String left = longestNiceSubstring(s, l, i);
-                String right = longestNiceSubstring(s, i + 1, r);
-                return left.length() >= right.length() ? left : right;
+        for (int i = left; i <= right; i++) {
+            if (!set.contains(Character.toLowerCase(s.charAt(i))) || !set.contains(Character.toUpperCase(s.charAt(i)))) {
+                String longestA = longestNiceSubstring(s, left, i - 1);
+                String longestB = longestNiceSubstring(s, i + 1, right);
+                return longestA.length() >= longestB.length() ? longestA : longestB;
             }
         }
-        return s.substring(l, r);
+        return s.substring(left, right + 1);
     }
 }
+
+// time  - O(n)
+// space - O(n)
