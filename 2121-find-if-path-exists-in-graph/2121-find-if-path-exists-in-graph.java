1d9fe772fc46e570b631f1913cdfeb6846f62e85
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-04 12:28:24 +0800

    Time: 147 ms (43.01%), Space: 153.7 MB (65.35%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
@@ -1,34 +1,43 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        if (source == destination) return true;
+        if (edges == null || n < 1) {
+            throw new IllegalArgumentException("Invalid input");
+        }
 
-        List<Integer>[] neighbors = new ArrayList[n];
-        for (int i = 0; i < neighbors.length; i++) {
-            neighbors[i] = new ArrayList<>();
+        if (source == destination) {
+            return true;
+        }
+
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            graph.put(i, new ArrayList<>());
         }
         for (int[] edge : edges) {
-            neighbors[edge[0]].add(edge[1]);
-            neighbors[edge[1]].add(edge[0]);
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
         }
 
+        Queue<Integer> queue = new ArrayDeque<>();
         boolean[] visited = new boolean[n];
-        Queue<Integer> unvisited = new LinkedList<>();
-        unvisited.add(source);
+        queue.offer(source);
         visited[source] = true;
 
-        while (!unvisited.isEmpty()) {
-            int curr = unvisited.poll();
-            for (int neighbor : neighbors[curr]) {
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            for (int neighbor : graph.get(curr)) {
                 if (neighbor == destination) {
                     return true;
-                } else if (!visited[neighbor]) {
-                    unvisited.add(neighbor);
+                }
+                if (!visited[neighbor]) {
+                    queue.offer(neighbor);
                     visited[neighbor] = true;
                 }
             }
-
         }
-        
+
         return false;
     }
 }
+
+// time  - O(n)
+// space - O(n)
