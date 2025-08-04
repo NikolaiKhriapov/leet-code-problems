ca14a240544f4da0473700fa433443bd947b48af
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-04 12:17:18 +0800

    Time: 1 ms (88.27%), Space: 41.1 MB (99.28%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.java
@@ -1,17 +1,27 @@
 class Solution {
-    public int countGoodSubstrings(String s) {
-        int count = 0;
+    private static final int GOOD_SIZE = 3;
 
-        for (int i = 0, sl = s.length(); i < sl - 2; i++) {
-            if (isGood(s.charAt(i), s.charAt(i + 1), s.charAt(i + 2))) {
-                count++;
+    public int countGoodSubstrings(String s) {
+        if (s == null) {
+            throw new IllegalArgumentException("INnvalid input");
+        }
+        if (s.length() < GOOD_SIZE) {
+            return 0;
+        }
+
+        int goodSubstringsCount = 0;
+        for (int i = 2; i < s.length(); i++) {
+            if (isDifferent(s.charAt(i - 2), s.charAt(i - 1), s.charAt(i))) {
+                goodSubstringsCount++;
             }
         }
-        
-        return count;
+        return goodSubstringsCount;
     }
 
-    private boolean isGood(char a, char b, char c) {
+    private static boolean isDifferent(char a, char b, char c) {
         return a != b && b != c && a != c;
     }
 }
+
+// time  - O(n)
+// space - O(1)
