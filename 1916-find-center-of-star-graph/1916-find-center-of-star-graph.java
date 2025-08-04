48c62f5ee320bed0ee948eea8c92e7c510bd4b52
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-04 12:06:14 +0800

    Time: 0 ms (100%), Space: 72.4 MB (53.02%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.java
@@ -1,5 +1,11 @@
 class Solution {
     public int findCenter(int[][] edges) {
+        if (edges == null || edges.length == 0 || edges[0].length != 2) {
+            throw new IllegalArgumentException("Invalid input");
+        }
         return (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) ? edges[0][0] : edges[0][1];
     }
 }
+
+// time  - O(1)
+// space - O(1)
