8d45230d15e1176bd4836367771bfca9b2932a8c
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-05 10:17:48 +0800

    Time: 5 ms (89.13%), Space: 44.9 MB (43.77%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -10,11 +10,10 @@ class Solution {
         int left = 0;
         int right = 0;
         while (right < s.length()) {
-            while (set.contains(s.charAt(right))) {
+            while (!set.add(s.charAt(right))) {
                 set.remove(s.charAt(left));
                 left++;
             }
-            set.add(s.charAt(right));
             right++;
             maxLength = Math.max(maxLength, set.size());
         }
