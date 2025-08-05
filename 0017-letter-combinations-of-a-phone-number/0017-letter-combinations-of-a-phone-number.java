fabc907fca6c31d0b80e6df4d5e29ae9f8cbd462
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-05 13:52:44 +0800

    Time: 0 ms (100%), Space: 42 MB (78.57%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,27 +1,44 @@
 class Solution {
-    private static final String[] LETTERS = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-
     public List<String> letterCombinations(String digits) {
-        if (digits == null || digits.length() == 0) {
+        if (digits == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        if (digits.isEmpty()) {
             return new ArrayList<>();
         }
-
         List<String> result = new ArrayList<>();
-        addDigitToCombination(digits, 0, new StringBuilder(), result);
+        addLetter(digits, 0, new StringBuilder(), result);
         return result;
     }
 
-    private void addDigitToCombination(String digits, int index, StringBuilder curr, List<String> result) {
+    private void addLetter(String digits, int index, StringBuilder curr, List<String> result) {
         if (index == digits.length()) {
             result.add(curr.toString());
             return;
         }
 
-        int digit = digits.charAt(index) - '0';
-        for (char c : LETTERS[digit].toCharArray()) {
-            curr.append(c);
-            addDigitToCombination(digits, index + 1, curr, result);
+        char digit = digits.charAt(index);
+        for (char letter : getLetters(digit).toCharArray()) {
+            curr.append(letter);
+            addLetter(digits, index + 1, curr, result);
             curr.deleteCharAt(curr.length() - 1);
         }
     }
+
+    private static String getLetters(char digit) {
+        return switch (digit) {
+            case '2' -> "abc";
+            case '3' -> "def";
+            case '4' -> "ghi";
+            case '5' -> "jkl";
+            case '6' -> "mno";
+            case '7' -> "pqrs";
+            case '8' -> "tuv";
+            case '9' -> "wxyz";
+            default -> throw new IllegalArgumentException("Invalid input");
+        };
+    }
 }
+
+// time  - O(3^n)
+// space - O(3^n)
