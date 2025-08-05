ea5c1d87fd480fd4454e8fd1468db1f2039aef7f
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-05 10:29:18 +0800

    Time: 19 ms (52%), Space: 45.4 MB (26.52%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,31 +1,30 @@
 class Solution {
     public String longestPalindrome(String s) {
         if (s == null) {
-            throw new IllegalArgumentException("Invalid input"); // for simplicity
+            throw new IllegalArgumentException("Invalid input");
         }
         if (s.length() <= 1) {
             return s;
         }
 
-        int start = 0;
-        int end = 0;
+        String longest = "";
         for (int i = 0; i < s.length(); i++) {
-            int lengthOdd = getLongestPalindrome(s, i, i);
-            int lengthEven = getLongestPalindrome(s, i, i + 1);
-            int length = Math.max(lengthOdd, lengthEven);
-            if (length > end - start) {
-                start = i - (length - 1) / 2;
-                end = i + length / 2;
-            }
+            String even = getCurrentLongest(s, i, i + 1);
+            String odd = getCurrentLongest(s, i, i);
+            String currentLongest = even.length() >= odd.length() ? even : odd;
+            longest = currentLongest.length() > longest.length() ? currentLongest : longest;
         }
-        return s.substring(start, end + 1);
+        return longest;
     }
 
-    private int getLongestPalindrome(String s, int left, int right) {
+    private String getCurrentLongest(String s, int left, int right) {
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
         }
-        return right - left - 1;
+        return s.substring(left + 1, right);
     }
 }
+
+// time  - O(n)
+// space - O(n)
