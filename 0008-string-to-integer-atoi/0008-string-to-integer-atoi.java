244d1b21195a5a055d74de171c1d585517f8469b
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-05 11:45:29 +0800

    Time: 1 ms (100%), Space: 42.3 MB (80.36%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,33 +1,43 @@
 class Solution {
     public int myAtoi(String s) {
-        if (s == null || s.isEmpty()) {
-            return 0;
+        if (s == null) {
+            throw new IllegalArgumentException("Invalid input");
         }
-
+        
         int index = 0;
         
+        // remove leading whitespaces
         while (index < s.length() && s.charAt(index) == ' ') {
             index++;
         }
 
+        // get sign
         int sign = 1;
-        if (index < s.length() && (s.charAt(index) == '-' || s.charAt(index) == '+')) {
+        if (index < s.length() && (s.charAt(index) == '+' || s.charAt(index) == '-')) {
             if (s.charAt(index) == '-') {
                 sign = -1;
             }
             index++;
         }
 
-        int result = 0;
+        // get number
+        int number = 0;
         while (index < s.length() && Character.isDigit(s.charAt(index))) {
             int digit = s.charAt(index) - '0';
-            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && digit > 7)) {
-                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            if (!isWithinBounds(number, digit, sign)) {
+                return sign > 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
-            result = result * 10 + digit;
+            number = number * 10 + digit;
             index++;
         }
-        
-        return sign * result;
+
+        return sign * number;
+    }
+
+    private static boolean isWithinBounds(int number, int digit, int sign) {
+        return (number < Integer.MAX_VALUE / 10) || (number == Integer.MAX_VALUE / 10 && digit <= 7);
     }
 }
+
+// time  - O(n)
+// space - O(1)
