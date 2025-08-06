3165cc3f810b01fe7aa82aabe7d6068f4d5ccbd3
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-06 14:06:48 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,34 +1,38 @@
 class Solution {
     public int divide(int dividend, int divisor) {
         if (divisor == 0) {
-            throw new IllegalArgumentException("Invalid input"); // for simplicity
-        }
-        if (dividend == 0) {
-            return 0;
+            throw new IllegalArgumentException("Invalid input");
         }
         if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE;
+        }
+        if (dividend == 0) {
+            return 0;
         }
 
         int sign = 1;
         if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)) {
             sign = -1;
         }
+
         int dividendNeg = dividend < 0 ? dividend : -dividend;
         int divisorNeg = divisor < 0 ? divisor : -divisor;
 
         int result = 0;
         while (dividendNeg <= divisorNeg) {
-            int temp = divisorNeg;
-            int multiple = 1;
-            while (temp >= Integer.MIN_VALUE / 2 && dividendNeg <= temp + temp) {
-                temp += temp;
-                multiple += multiple;
+            int divisorNegTemp = divisorNeg;
+            int multiply = 1;
+            while (divisorNegTemp <= Integer.MIN_VALUE / 2 && dividendNeg <= divisorNegTemp + divisorNegTemp) {
+                divisorNegTemp <<= 1;
+                multiply <<= 1;
             }
-            dividendNeg -= temp;
-            result += multiple;
+            dividendNeg -= divisorNegTemp;
+            result += multiply;
         }
         
         return sign * result;
     }
 }
+
+// time  - O(log n)
+// space - O(1)
