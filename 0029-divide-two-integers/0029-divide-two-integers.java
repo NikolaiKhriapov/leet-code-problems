27bae270951baa5744d261bfdb5f463c34b1774a
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-06 14:08:15 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -23,8 +23,8 @@ class Solution {
             int divisorNegTemp = divisorNeg;
             int multiply = 1;
             while (divisorNegTemp <= Integer.MIN_VALUE / 2 && dividendNeg <= divisorNegTemp + divisorNegTemp) {
-                divisorNegTemp <<= 1;
-                multiply <<= 1;
+                divisorNegTemp += divisorNegTemp;
+                multiply += multiply;
             }
             dividendNeg -= divisorNegTemp;
             result += multiply;
