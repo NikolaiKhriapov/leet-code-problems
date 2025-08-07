4210708fa13d629ee233096a7c37a627ae17e02b
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-07 15:09:53 +0800

    Time: 2 ms (98.99%), Space: 52.9 MB (89.13%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.java
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -1,25 +1,26 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        if (n < 1 || trust == null) {
+        if (n <= 0 || trust == null) {
             throw new IllegalArgumentException("Invalid input");
         }
 
-        int[] trusters = new int[n + 1];
         int[] trustees = new int[n + 1];
-        for (int[] t : trust) {
-            trusters[t[0]]++;
-            trustees[t[1]]++;
-        }
+        int[] trusters = new int[n + 1];
 
-        int candidate = 0;
+        for (int[] t : trust) {
+            trustees[t[0]]++;
+            trusters[t[1]]++;
+        }
+        
         for (int i = 1; i <= n; i++) {
-            if (trusters[i] == 0 && trustees[i] == n - 1) {
+            if (trustees[i] == 0 && trusters[i] == n - 1) {
                 return i;
             }
         }
+
         return -1;
     }
 }
 
-// [[1,2]]
-// [0,1,0], [0,0,1]
+// time  - O(2^n)
+// space - O(n)
