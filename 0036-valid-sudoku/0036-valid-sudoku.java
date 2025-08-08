357279b7cc3418c6aa5301e3a79892640fdbb960
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-08 13:50:37 +0800

    Time: 3 ms (53.76%), Space: 44.3 MB (89.37%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,55 +1,47 @@
 class Solution {
+    private static final int BOARD_SIZE = 9;
+    private static final char EMPTY_CELL = '.';
+
     public boolean isValidSudoku(char[][] board) {
-        if (board == null || board.length != 9 || board[0].length != 9) {
-            throw new IllegalArgumentException("Invalid input"); // for simplicity
+        if (board == null || board.length != BOARD_SIZE || board.length != board[0].length) {
+            throw new IllegalArgumentException("Invalid input");
         }
-        return isValidRows(board) && isValidCols(board) && isValidSubboxes(board);
+
+        return isRowsValid(board) && isColumnsValid(board) && isSubboxesValid(board);
     }
 
-    private boolean isValidRows(char[][] board) {
-        for (int r = 0; r < board.length; r++) {
-            boolean[] set = new boolean[board.length];
-            for (int c = 0; c < board[0].length; c++) {
-                if (board[r][c] != '.') {
-                    int idx = board[r][c] - '1';
-                    if (set[idx]) {
-                        return false;
-                    }
-                    set[idx] = true;
+    private boolean isRowsValid(char[][] board) {
+        for (int r = 0; r < BOARD_SIZE; r++) {
+            Set<Character> set = new HashSet<>();
+            for (int c = 0; c < BOARD_SIZE; c++) {
+                if (board[r][c] != EMPTY_CELL && !set.add(board[r][c])) {
+                    return false;
                 }
             }
         }
         return true;
     }
 
-    private boolean isValidCols(char[][] board) {
-        for (int c = 0; c < board[0].length; c++) {
-            boolean[] set = new boolean[board[0].length];
-            for (int r = 0; r < board.length; r++) {
-                if (board[r][c] != '.') {
-                    int idx = board[r][c] - '1';                    
-                    if (set[idx]) {
-                        return false;
-                    }
-                    set[idx] = true;
+    private boolean isColumnsValid(char[][] board) {
+        for (int c = 0; c < BOARD_SIZE; c++) {
+            Set<Character> set = new HashSet<>();
+            for (int r = 0; r < BOARD_SIZE; r++) {
+                if (board[r][c] != EMPTY_CELL && !set.add(board[r][c])) {
+                    return false;
                 }
             }
         }
         return true;
     }
 
-    private boolean isValidSubboxes(char[][] board) {
-        for (int r = 0; r < board.length; r += 3) {    
-            for (int c = 0; c < board[0].length; c += 3) {
-                boolean[] set = new boolean[board.length];
-                for (int i = r; i < r + 3; i++) {
-                    for (int j = c; j < c + 3; j++) {
-                        if (board[i][j] != '.') {
-                            int idx = board[i][j] - '1';                    
-                            if (set[idx]) {
-                                return false;
-                            }
-                            set[idx] = true;
+    private boolean isSubboxesValid(char[][] board) {
+        for (int r = 0; r < BOARD_SIZE; r += 3) {
+            for (int c = 0; c < BOARD_SIZE; c += 3) {
+                Set<Character> set = new HashSet<>();
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        if (board[r + i][c + j] != EMPTY_CELL && !set.add(board[r + i][c + j])) {
+                            return false;
                         }
                     }
                 }
@@ -58,3 +50,6 @@ class Solution {
         return true;
     }
 }
+
+// time  - O(n^2)
+// space - O(n)
