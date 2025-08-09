474a776a353906d51e8b6198bd55641ca934b267
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-09 15:37:14 +0800

    Time: 5 ms (60.49%), Space: 44.8 MB (93.14%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         if (word1 == null || word2 == null) {
-            throw new IllegalArgumentException("Invalid state");
+            throw new IllegalArgumentException("Invalid input");
         }
         if (word1.isEmpty()) {
             return word2.length();
@@ -12,32 +12,38 @@ class Solution {
 
         int length1 = word1.length();
         int length2 = word2.length();
-        int[][] dp = new int[length1 + 1][length2 + 1];
 
-        for (int row = 0; row <= length1; row++) {
-            dp[row][0] = row;
+        int[] prev = new int[length2 + 1];
+        int[] curr = new int[length2 + 1];
+
+        for (int c = 1; c <= length2; c++) {
+            prev[c] = c;
         }
-        for (int col = 0; col <= length2; col++) {
-            dp[0][col] = col;
-        }
-        for (int row = 1; row <= length1; row++) {
-            for (int col = 1; col <= length2; col++) {
-                if (word1.charAt(row - 1) == word2.charAt(col - 1)) {
-                    dp[row][col] = dp[row - 1][col - 1];
+
+        for (int r = 1; r <= length1; r++) {
+            curr[0] = r;
+            for (int c = 1; c <= length2; c++) {
+                if (word1.charAt(r - 1) == word2.charAt(c - 1)) {
+                    curr[c] = prev[c - 1];
                 } else {
-                    dp[row][col] = 1 + Math.min(dp[row - 1][col - 1], Math.min(dp[row - 1][col], dp[row][col - 1]));
+                    curr[c] = 1 + Math.min(prev[c - 1], Math.min(prev[c], curr[c - 1]));
                 }
             }
+            prev = Arrays.copyOf(curr, curr.length);
         }
         
-        return dp[length1][length2];
+        return curr[length2];
     }
 }
 
-//      r o s
-//   [0,1,2,3]
-// h [1,1,2,3]
-// o [2,2,1,2]
-// r [3,2,2,2]
-// s [4,3,3,2]
-// e [5,4,4,3]
+//     i n t e n t i o n
+//   0 1 2 3 4 5 6 7 8 9
+// e 1 1 2 3 3 4 5 6 7 8
+// x 2 2 2 3 4 4 5 6 7 8
+// e 3 3 3 3 3 4 5 6 7 8
+// c 4 4 4 4 4 4 5 6 7 8
+// u 5 5 5 5 5 5 5 6 7 8
+// t 6 6 6 5 6 6 5 6 7 8
+// i 7 6 7 6 6 7 6 5 0 0
+// o 8 0 0 0 0 0 0 0 5 0
+// n 9 0 0 0 0 0 0 0 0 5
