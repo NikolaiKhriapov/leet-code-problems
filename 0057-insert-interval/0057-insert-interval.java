4729469cb107f6eadaca0fd0866fff85a4d12f93
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-09 13:29:33 +0800

    Time: 1 ms (98.92%), Space: 44.8 MB (90.33%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,28 +1,38 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         if (intervals == null || newInterval == null) {
-            throw new IllegalArgumentException("Invalid input"); // for simplicity
+            throw new IllegalArgumentException("Invalid input");
         }
 
         List<int[]> list = new ArrayList<>();
 
-        int index = 0;
-        while (index < intervals.length && intervals[index][1] < newInterval[0]) {
-            list.add(intervals[index]);
-            index++;
+        if (intervals.length == 0) {
+            return new int[][] {newInterval};
+        }
+
+        int p = 0;
+
+        while (p < intervals.length && intervals[p][0] <= newInterval[0]) {
+            list.add(intervals[p]);
+            p++;
         }
         
-        while (index < intervals.length && intervals[index][0] <= newInterval[1]) {
-            newInterval[0] = Math.min(newInterval[0], intervals[index][0]);
-            newInterval[1] = Math.max(newInterval[1], intervals[index][1]);
-            index++;
+        if (!list.isEmpty() && list.get(list.size() - 1)[1] >= newInterval[0]) {
+            int[] prev = list.get(list.size() - 1);
+            prev[1] = Math.max(prev[1], newInterval[1]);
+        } else {
+            list.add(newInterval);
         }
 
-        list.add(newInterval);
-
-        while (index < intervals.length) {
-            list.add(intervals[index]);
-            index++;
+        int[] prev = list.get(list.size() - 1);
+        while (p < intervals.length && prev[1] >= intervals[p][0]) {
+            prev[1] = Math.max(prev[1], intervals[p][1]);
+            p++;
+        }
+        
+        for (int i = p; i < intervals.length; i++) {
+            list.add(intervals[i]);
+            p++;
         }
 
         int[][] result = new int[list.size()][2];
@@ -33,3 +43,6 @@ class Solution {
         return result;
     }
 }
+
+// time  - O(n)
+// space - O(n)
