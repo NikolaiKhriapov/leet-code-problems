fb9a29759bf44985229600c47cd05fd1b8526832
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-09 14:03:16 +0800

    Time: 0 ms (100%), Space: 40.4 MB (77.19%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,18 +1,23 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         if (m <= 0 || n <= 0) {
-            return 0;
+            throw new IllegalArgumentException("Invalid input");
         }
-
+        
         int[] dp = new int[n];
         Arrays.fill(dp, 1);
-        for (int row = 1; row < m; row++) {
-            for (int col = 0; col < n; col++) {
-                int top = dp[col];
-                int left = col > 0 ? dp[col - 1] : 0;
-                dp[col] = top + left;
+
+        for (int r = 1; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                int top = dp[c];
+                int left = c == 0 ? 0 : dp[c - 1];
+                dp[c] = top + left;
             }
         }
+
         return dp[n - 1];
     }
 }
+
+// time  - O(m * n)
+// space - O(n)
