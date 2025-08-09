5c055de4498663cdbc8e7f34f37aed28518854c9
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-09 21:22:54 +0800

    Time: 3 ms (63.69%), Space: 41.4 MB (96.36%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,36 +1,29 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1 == null || s2 == null || s3 == null || s1.length() + s2.length() != s3.length()) {
+        if (s1 == null || s2 == null || s3 == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        if (s1.length() + s2.length() != s3.length()) {
             return false;
         }
 
         boolean[] dp = new boolean[s2.length() + 1];
-
-        // first row
         dp[0] = true;
-        for (int col = 1; col <= s2.length(); col++) {
-            dp[col] = dp[col - 1] && s2.charAt(col - 1) == s3.charAt(col - 1);
+        for (int c = 1; c <= s2.length(); c++) {
+            dp[c] = dp[c - 1] && s2.charAt(c - 1) == s3.charAt(c - 1);
         }
 
-        // remaining rows
-        for (int row = 1; row <= s1.length(); row++) {
-            dp[0] = dp[0] && s1.charAt(row - 1) == s3.charAt(row - 1);
-            for (int col = 1; col <= s2.length(); col++) {
-                dp[col] = (dp[col] && s1.charAt(row - 1) == s3.charAt(row + col - 1))
-                       || (dp[col - 1] && s2.charAt(col - 1) == s3.charAt(row + col - 1));
+        for (int r = 1; r <= s1.length(); r++) {
+            dp[0] = dp[0] && s1.charAt(r - 1) == s3.charAt(r - 1);
+            for (int c = 1; c <= s2.length(); c++) {
+                dp[c] = (dp[c - 1] && s2.charAt(c - 1) == s3.charAt(r + c - 1)) ||
+                        (dp[c] && s1.charAt(r - 1) == s3.charAt(r + c - 1));
             }
         }
 
-        return dp[s2.length()];        
+        return dp[s2.length()];
     }
 }
 
-//      d b b c a
-//   [+,-,-,-,-,-]
-// a [+,-,-,-,-,-]
-// a [+,+,+,+,+,-]
-// b [-,+,+,-,+,-]
-// c [-,-,+,+,+,+]
-// c [-,-,-,+,-,+]
-
-// a a d b b c b c a c
+// time  - O(n + m)
+// space - O(m)
