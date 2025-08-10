11d41231f884b6e558ce47fd817898c94f6c1537
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-10 18:37:06 +0800

    Time: 2 ms (35.16%), Space: 44.2 MB (66.25%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -1,49 +1,31 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
-
 class Solution {
     public Node connect(Node root) {
         if (root == null) {
-            return null;
+            return root;
         }
 
-        Node curr = root;
-        while (curr != null) {
-            Node dummyHead = new Node(0);
-            Node dummy = dummyHead;
-            while (curr != null) {
-                if (curr.left != null) {
-                    dummy.next = curr.left;
-                    dummy = dummy.next;
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                Node node = queue.poll();
+                if (i < levelSize - 1 && !queue.isEmpty()) {
+                    node.next = queue.peek();
                 }
-                if (curr.right != null) {
-                    dummy.next = curr.right;
-                    dummy = dummy.next;
+                if (node.left != null) {
+                    queue.offer(node.left);
                 }
-                curr = curr.next;
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
             }
-            curr = dummyHead.next;
         }
+
         return root;
     }
 }
+
+// time  - O(n)
+// space - O(w)
