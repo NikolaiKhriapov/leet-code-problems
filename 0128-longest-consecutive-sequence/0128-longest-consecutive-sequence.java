8c171dc295afcaadc77667092728c30d5a98e6fd
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-10 19:34:08 +0800

    Time: 42 ms (12.48%), Space: 74.5 MB (5.86%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -13,7 +13,7 @@ class Solution {
 
         Set<Integer> visited = new HashSet<>();
         for (int n : set) {
-            if (visited.contains(n)) {
+            if (visited.contains(n) || set.contains(n - 1)) {
                 continue;
             }
             int consecutiveCount = 1;
