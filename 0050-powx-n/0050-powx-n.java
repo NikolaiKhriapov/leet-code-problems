c66d429cef9c0d3f8fb09a8b9b53c14e3a663c63
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-12 23:55:35 +0800

    Time: 0 ms (100%), Space: 42.2 MB (61.58%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,6 +1,7 @@
 class Solution {
     public double myPow(double x, int n) {
         long nLong = n;
+
         if (nLong < 0) {
             x = 1 / x;
             nLong = -nLong;
@@ -8,22 +9,16 @@ class Solution {
 
         double result = 1;
         while (nLong > 0) {
-            if (nLong % 2 == 1) {
+            if (nLong % 2 != 0) {
                 result *= x;
             }
             x *= x;
             nLong /= 2;
         }
-
+        
         return result;
     }
 }
 
 // time  - O(log n)
 // space - O(1)
-
-// x = 2.0   n = 10      result = 1.0;
-// x = 4.0   n = 5       result = 1.0;
-// x = 16.0  n = 2       result = 4.0;
-// x = 256.0 n = 1       result = 4.0;
-// x = 256.0 n = 0       result = 1024.0;
