765d3656f1873a8201d1a4d29628e786e7786b1d
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-14 14:25:49 +0800

    Time: 0 ms (100%), Space: 44.1 MB (81.25%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -1,27 +1,50 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
 class Solution {
     public Node connect(Node root) {
-        if (root == null) {
-            return root;
-        }
 
         Node curr = root;
         while (curr != null) {
-            Node dummyHead = new Node(0);
-            Node dummy = dummyHead;
+            Node dummy = new Node();
+            Node dummyTail = dummy;
             while (curr != null) {
                 if (curr.left != null) {
-                    dummy.next = curr.left;
-                    dummy = dummy.next;
+                    dummyTail.next = curr.left;
+                    dummyTail = dummyTail.next;
                 }
                 if (curr.right != null) {
-                    dummy.next = curr.right;
-                    dummy = dummy.next;
+                    dummyTail.next = curr.right;
+                    dummyTail = dummyTail.next;
                 }
                 curr = curr.next;
             }
-            curr = dummyHead.next;
+            curr = dummy.next;
         }
         
         return root;
     }
 }
+
+// time  - O(n)
+// space - O(1)
