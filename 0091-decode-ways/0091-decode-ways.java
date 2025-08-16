047d76ec3c7c1f0e70753d837f5faf49fa173af6
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-16 12:15:13 +0800

    Time: 1 ms (82.91%), Space: 41.6 MB (87.43%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -3,28 +3,27 @@ class Solution {
         if (s == null) {
             throw new IllegalArgumentException("Invalid input");
         }
+        if (s.isEmpty() || s.startsWith("0")) {
+            return 0;
+        }
 
         int nextPrev = 0;
         int prev = 1;
-
         for (int i = s.length() - 1; i >= 0; i--) {
+            int digit = s.charAt(i) - '0';
             int curr = 0;
-            if (s.charAt(i) != '0') {
-                curr = prev;
-                if (i < s.length() - 1 && Integer.parseInt(s.substring(i, i + 2)) <= 26) {
+            if (digit > 0) {
+                curr += prev;
+                if (i < s.length() - 1 && (digit == 1 || digit == 2) && (digit * 10 + (s.charAt(i + 1) - '0')) <= 26) {
                     curr += nextPrev;
                 }
             }
             nextPrev = prev;
             prev = curr;
         }
-        
         return prev;
     }
 }
 
 // time  - O(n)
 // space - O(1)
-
-// "0 6"
-// [0,1,1,0]
