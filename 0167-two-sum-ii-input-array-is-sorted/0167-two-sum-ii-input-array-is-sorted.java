97c5f470e11bb8fbac3b1d300bef564c947b2d9d
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-16 22:24:27 +0800

    Time: 5 ms (17.36%), Space: 48.2 MB (6.35%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -4,20 +4,32 @@ class Solution {
             throw new IllegalArgumentException("Invalid input");
         }
 
-        Map<Integer, Integer> map = new HashMap<>();
-
         for (int i = 0; i < numbers.length; i++) {
             int complement = target - numbers[i];
-            if (map.containsKey(complement)) {
-                return new int[] {map.get(complement) + 1, i + 1};
-            } else {
-                map.put(numbers[i], i);
+            int right = binarySearch(numbers, complement, i + 1, numbers.length - 1);
+            if (right != -1) {
+                return new int[] {i + 1, right + 1};
             }
         }
         
         throw new IllegalArgumentException("Invalid input");
     }
+
+    private int binarySearch(int[] numbers, int target, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+        
+        int mid = left + (right - left) / 2;
+        if (numbers[mid] == target) {
+            return mid;
+        } else if (numbers[mid] > target) {
+            return binarySearch(numbers, target, left, mid - 1);
+        }
+        return binarySearch(numbers, target, mid + 1, right);
+    }
+
 }
 
-// time  - O(n)
-// space - O(n)
+// time  - O(n log n)
+// space - O(1)
