d0766349b351c1301ff0938ca943e2b087f28372
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-16 22:27:13 +0800

    Time: 2 ms (92.57%), Space: 47.4 MB (38.19%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -4,32 +4,20 @@ class Solution {
             throw new IllegalArgumentException("Invalid input");
         }
 
-        for (int i = 0; i < numbers.length; i++) {
-            int complement = target - numbers[i];
-            int right = binarySearch(numbers, complement, i + 1, numbers.length - 1);
-            if (right != -1) {
-                return new int[] {i + 1, right + 1};
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[] {left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
             }
         }
         
         throw new IllegalArgumentException("Invalid input");
     }
-
-    private int binarySearch(int[] numbers, int target, int left, int right) {
-        if (left > right) {
-            return -1;
-        }
-        
-        int mid = left + (right - left) / 2;
-        if (numbers[mid] == target) {
-            return mid;
-        } else if (numbers[mid] > target) {
-            return binarySearch(numbers, target, left, mid - 1);
-        }
-        return binarySearch(numbers, target, mid + 1, right);
-    }
-
 }
-
-// time  - O(n log n)
-// space - O(1)
