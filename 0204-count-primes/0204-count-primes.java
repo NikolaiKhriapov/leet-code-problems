6de6ccdcb5b5385a2ee6d15ed3d53f96dbb5f0d7
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-17 17:07:32 +0800

    Time: 114 ms (21.75%), Space: 49.2 MB (70.14%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,33 +1,28 @@
 class Solution {
     public int countPrimes(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException("Invalid input");
+        if (n <= 1) {
+            return 0;
         }
-        
+
         boolean[] primes = new boolean[n];
         for (int i = 2; i < primes.length; i++) {
             primes[i] = true;
         }
 
-        for (int i = 2; i * i < primes.length; i++) {
-            if (!primes[i]) continue;
-            for (int j = i * i; j < primes.length; j += i) {
+        int primesCount = 0;
+        for (int i = 2; i < primes.length; i++) {
+            if (!primes[i]) {
+                continue;
+            }
+            for (int j = i * 2; j < primes.length && j < Integer.MAX_VALUE; j += i) {
                 primes[j] = false;
             }
+            primesCount++;
         }
-
-        int count = 0;
-        for (boolean isPrime : primes) {
-            if (isPrime) {
-                count++;
-            }
-        }
-
-        return count;
+        
+        return primesCount;
     }
 }
 
-// [0,1,2,3,4,5,6,7,8,9,10]
-// [-,-,-,-,-,-,-,-,-,-, -]
-// [-,-,+,+,+,+,+,+,+,+, +]
-// [-,-,+,+,-,+,-,+,-,-, -] = 4
+// [0,1,2,3,4,5,6,7,8,9]
+// [f,f,t,t,f,t,f,t,f,f]
