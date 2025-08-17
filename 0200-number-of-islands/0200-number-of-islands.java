cd2ce08bd5322425aceeae71acdb54a33694eda1
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-17 16:26:25 +0800

    Time: 3 ms (82.96%), Space: 49.6 MB (62.43%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,38 +1,42 @@
 class Solution {
-    private static final char MARK_ISLAND = '1';
-    private static final char MARK_WATER = '0';
-    private static final char MARK_TEMP = '.';
+    private static final char LAND = '1';
+    private static final char WATER = '0';
+    private static final char TEMP = '.';
+    private static final int[][] NEIGHBORS = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
     public int numIslands(char[][] grid) {
         if (grid == null) {
-            throw new IllegalArgumentException("Invalid input"); // for simplicity
+            throw new IllegalArgumentException("Invalid input");
         }
         if (grid.length == 0 || grid[0].length == 0) {
             return 0;
         }
+        return markIslandsAndReturnIslandsCount(grid);
+    }
 
-        int count = 0;
+    private int markIslandsAndReturnIslandsCount(char[][] grid) {
+        int islandsCount = 0;
         for (int r = 0; r < grid.length; r++) {
             for (int c = 0; c < grid[0].length; c++) {
-                if (grid[r][c] == MARK_ISLAND) {
-                    count++;
+                if (grid[r][c] == LAND) {
                     markIsland(grid, r, c);
+                    islandsCount++;
                 }
             }
-        }  
-        return count;
+        }
+        return islandsCount;
     }
 
-    private void markIsland(char[][] grid, int r, int c) {
-        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] != MARK_ISLAND) {
+    private void markIsland(char[][] grid, int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] != LAND) {
             return;
         }
-        
-        grid[r][c] = MARK_TEMP;
-
-        markIsland(grid, r + 1, c);
-        markIsland(grid, r, c + 1);
-        markIsland(grid, r - 1, c);
-        markIsland(grid, r, c - 1);
+        grid[row][col] = TEMP;
+        for (int[] neighbor : NEIGHBORS) {
+            markIsland(grid, row + neighbor[0], col + neighbor[1]);
+        }
     }
 }
+
+// time  - O(n)
+// space - O(1)
