e310a989413c477df1e08659b6142a7309ff1998
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-17 17:12:19 +0800

    Time: 156 ms (11.09%), Space: 49.2 MB (57.71%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -14,7 +14,7 @@ class Solution {
             if (!primes[i]) {
                 continue;
             }
-            for (int j = i * 2; j < primes.length && j < Integer.MAX_VALUE; j += i) {
+            for (int j = i * i; j < primes.length && i <= Integer.MAX_VALUE / i; j += i) {
                 primes[j] = false;
             }
             primesCount++;
@@ -24,5 +24,5 @@ class Solution {
     }
 }
 
-// [0,1,2,3,4,5,6,7,8,9]
-// [f,f,t,t,f,t,f,t,f,f]
+// time  - O(n)
+// space - O(n)
