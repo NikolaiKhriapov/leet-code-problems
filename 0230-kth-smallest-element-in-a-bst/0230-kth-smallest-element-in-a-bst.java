24933ce360726df7125c4892559acedbf5cf5ae9
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-27 12:17:48 +0800

    Time: 1 ms (31.06%), Space: 45 MB (5.6%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -15,29 +15,29 @@
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
-        if (k <= 0) {
+        if (root == null || k <= 0) {
             throw new IllegalArgumentException("Invalid input");
         }
 
-        int kthSmallest = helper(root, new int[]{k});
-        if (kthSmallest == -1) {
+        List<Integer> list = new ArrayList<>();
+        traverse(root, list);
+
+        if (list.size() < k) {
             throw new IllegalArgumentException("Invalid input");
         }
 
-        return kthSmallest;
+        return list.get(k - 1);
     }
 
-    private int helper(TreeNode node, int[] k) {
-        if (node == null) return -1;
-
-        int left = helper(node.left, k);
-        if (left != -1) return left;
-
-        if (--k[0] == 0) return node.val;
-
-        int right = helper(node.right, k);
-        if (right != -1) return right;
-
-        return -1;
+    private void traverse(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+        traverse(node.left, list);
+        list.add(node.val);
+        traverse(node.right, list);
     }
 }
+
+// time  - O(n)
+// space - O(n)
