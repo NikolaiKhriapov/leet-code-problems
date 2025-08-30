7babf8c67250a9143e576a249a8e7fe7812c4668
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-30 19:47:51 +0800

    Time: 15 ms (77.29%), Space: 44.1 MB (95.92%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,25 +1,28 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if (coins == null || amount < 0) {
+        if (coins == null || coins.length == 0 || amount < 0) {
             throw new IllegalArgumentException("Invalid input");
         }
+
+        Arrays.sort(coins);
 
         int[] dp = new int[amount + 1];
         for (int i = 1; i < dp.length; i++) {
             dp[i] = amount + 1;
         }
-
+        
         for (int i = 1; i < dp.length; i++) {
             for (int coin : coins) {
-                if (coin > i) continue;
+                if (coin > i) {
+                    break;
+                }
                 dp[i] = Math.min(dp[i], dp[i - coin] + 1);
             }
         }
-        
-        return (dp[amount] == amount + 1) ? -1 : dp[amount];
+
+        return dp[amount] != (amount + 1) ? dp[amount] : -1;
     }
 }
 
-// [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11]
-// [0,12,12,12,12,12,12,12,12,12,12,12]
-// [0, 1, 1, 2, 2, 1, 2, 2, 3, 3, 2, 3]
+// time  - O(n + m log(m))
+// space - O(n)
