4d29db3f0891d5f641ce2a8335e79983acb09ad3
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-08-30 17:51:09 +0800

    Time: 36 ms (53.41%), Space: 43.1 MB (40.89%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,24 +1,23 @@
 class Solution {
     public int numSquares(int n) {
         if (n <= 0) {
-            return 0;
+            throw new IllegalArgumentException("Invalid input");
         }
 
         int[] dp = new int[n + 1];
         for (int i = 1; i < dp.length; i++) {
-            dp[i] = Integer.MAX_VALUE;
+            dp[i] = n;
         }
 
         for (int i = 1; i < dp.length; i++) {
             for (int j = 1; j * j <= i; j++) {
-                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+                dp[i] = Math.min(dp[i], dp[(i - j * j)] + 1);
             }
         }
-
+        
         return dp[n];
     }
 }
 
-// [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-// [0,12,12,12,12,12,12,12,12,12,12,12,12]
-// [0, 1, 2, 3, 1, 2, 3, 4, 2, 1, 2, 3, 3]
+// time  - O(n * logn) (not sure)
+// space - O(n)
