83b66c30c86c4675c87de937ab9a5249b05c5aae
Nikolai Khriapov <kolyakhryapov@gmail.com>
2025-09-15 10:31:29 +0800

    Time: 2 ms (96.21%), Space: 133.4 MB (91.29%) - LeetHub

---
+++ LeetCode/Medium/0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        if (nums == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        if (nums.length < 3) {
+            return false;
+        }
+
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int n : nums) {
+            if (n <= first) {
+                first = n;
+            } else if (n <= second) {
+                second = n;
+            } else {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
+
+// time  - O(n)
+// space - O(1)
